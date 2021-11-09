21b24a20a07408c16f3611243122b413f55fa56d
0x6861746366574 <hatchet@symbol.dev>
2021-11-09 13:08:09 -0800

    Add auto-refresh
    - Adds required functionality to refresh data from pollers on target interval.
    
    - App will now only reload when it sees updates to files on disk.
    
    - README.md: Added quick information on certs and cert placement.

--- nodewatch/README.md
+++ nodewatch/README.md
@@ -2,6 +2,9 @@
 
 Monitors network version upgrades.
 
+For pull to work, certificates must be placed in puller/certs/
+To produce certificates follow the instructions for generating keys and certs in the [symbol node guide](https://docs.symbolplatform.com/guides/network/running-a-symbol-node-manually.html).
+
 ```sh
 git clone https://github.com/symbol/miscellaneous.git
 
--- nodewatch/webapp/app.py
+++ nodewatch/webapp/app.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 from collections import namedtuple
 from pathlib import Path
 
@@ -6,6 +7,8 @@ import dash
 import pandas as pd
 import plotly.express as px
 from dash import dash_table, dcc, html
+from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
 
 from .NemLoader import NemLoader
 from .SymbolLoader import SymbolLoader
@@ -15,13 +18,18 @@ COLORS = namedtuple('Colors', ['success', 'neutral', 'failure'])('lightgreen', '
 
 
 class WebApp:
-    def __init__(self, resources_path):
+    def __init__(self, resources_path, host, port, proxy, base_path, serve):  # pylint: disable=too-many-arguments
         self.resources_path = Path(resources_path)
 
         self.nem_loader = None
         self.symbol_loader = None
+        self.mtime = os.path.getmtime(self.resources_path / 'nem_harvesters.csv')  # last file to be pulled
 
-        self.app = dash.Dash(__name__)
+        self.host = host
+        self.port = port
+        self.proxy = proxy
+
+        self.app = dash.Dash(__name__, serve_locally=serve, url_base_pathname=base_path)
         self.app.title = 'Pirate Fork Progress'
 
     def reload(self):
@@ -86,17 +94,20 @@ class WebApp:
         return self._make_table(
             [descriptor for descriptor in self.symbol_loader.descriptors if descriptor.is_voting and 'NGL' not in descriptor.categories],
             ['address', 'name', 'host', 'balance', 'version', 'voting_end_epoch'],
-            SymbolLoader.version_to_tag)
+            SymbolLoader.version_to_tag,
+            'symbol-table')
 
     def make_nem_table(self):
         return self._make_table(
             [descriptor for descriptor in self.nem_loader.descriptors if descriptor.version],
             ['main_address', 'name', 'host', 'balance', 'version'],
-            NemLoader.version_to_tag)
+            NemLoader.version_to_tag,
+            'nem-table')
 
     @staticmethod
-    def _make_table(descriptors, column_names, version_to_tag):
+    def _make_table(descriptors, column_names, version_to_tag, identifier):
         return dash_table.DataTable(
+            id=identifier,
             data=[
                 {
                     'tag': version_to_tag(descriptor.version),
@@ -128,19 +139,52 @@ class WebApp:
             self.make_symbol_table(),
 
             html.H1(children='Harlock Hardfork Table'),
-            self.make_nem_table()
+            self.make_nem_table(),
+
+            dcc.Interval(
+                id='load-interval',
+                interval=60000,  # in milliseconds
+                n_intervals=0
+            )
         ])
 
+        def update_graphs(n_intervals):
+            mtime = os.path.getmtime(self.resources_path / 'nem_harvesters.csv')
+            if mtime > self.mtime:
+                self.mtime = mtime
+                print(f'Updating at check {n_intervals}, modified time: {mtime}')
+                self.reload()
+                symbol_figure = self.make_symbol_figure()
+                symbol_allnodes_figure = self.make_symbol_allnodes_figure()
+                nem_figure = self.make_nem_figure()
+                symbol_table = self.make_symbol_table()
+                nem_table = self.make_nem_table()
+                return symbol_figure, symbol_allnodes_figure, nem_figure, symbol_table.data, nem_table.data  # pylint: disable=no-member
+            raise PreventUpdate()
+
+        self.app.callback([
+            Output('cyprus-graph', 'figure'),
+            Output('cyprus-allnodes-graph', 'figure'),
+            Output('harlock-graph', 'figure'),
+            Output('symbol-table', 'data'),
+            Output('nem-table', 'data'),
+            Input('load-interval', 'n_intervals')])(update_graphs)
+
     def run(self):
-        self.app.run_server(debug=True)
+        self.app.run_server(host=self.host, port=self.port, threaded=True, proxy=self.proxy, debug=True)
 
 
 def main():
     parser = argparse.ArgumentParser(description='webapp that processes data files and renders fork information')
     parser.add_argument('--resources', help='directory containing resources', required=True)
+    parser.add_argument('--host', help='host ip, defaults to localhost', default='127.0.0.1')
+    parser.add_argument('--port', type=int, help='port for webserver', default=8080)
+    parser.add_argument('--proxy', help='proxy spec of the form ip:port::gateway to render urls', default=None)
+    parser.add_argument('--base_path', help='extension if server is not at root of url', default=None)
+    parser.add_argument('--serve', action='store_true', help='flag to indicate whether server will recieve external requests')
     args = parser.parse_args()
 
-    app = WebApp(args.resources)
+    app = WebApp(args.resources, args.host, args.port, args.proxy, args.base_path, args.serve)
     app.reload()
     app.layout()
     app.run()
