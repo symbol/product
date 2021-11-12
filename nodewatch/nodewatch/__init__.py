1c2b82d014fcf83cf76c380e6f5795768bca544a
Jaguar0625 <jaguar0625@127.0.0.1>
2021-11-12 15:14:54 -0400

    replace Dash with Flask for better future maintainability
     - a.k.a put Dash in the trash

---
+++ nodewatch/nodewatch/__init__.py
@@ -0,0 +1,164 @@
+from pathlib import Path
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from flask import Flask, redirect, render_template, url_for
+from zenlog import log
+
+from .BarChartBuilder import BarChartBuilder
+from .NetworkConnector import NetworkConnector
+from .NetworkRepository import NetworkRepository
+
+
+def nem_version_to_css_class(version):
+    tag = 'danger'
+    if not version:
+        tag = 'warning'
+    if '0.6.98' in version or '0.6.99' in version:
+        tag = 'success'
+
+    return tag
+
+
+def symbol_version_to_css_class(version):
+    tag = 'danger'
+    if not version:
+        tag = 'warning'
+    if '1.0.3.0' == version:
+        tag = 'success'
+
+    return tag
+
+
+def create_app():
+    # pylint: disable=too-many-locals
+
+    app = Flask(__name__)
+    app.config.from_envvar('NODEWATCH_SETTINGS')
+
+    resources_path = Path(app.config.get('RESOURCES_PATH'))
+    log.info('loading resources from {}'.format(resources_path))
+
+    nem_repository = NetworkRepository('nem')
+    symbol_repository = NetworkRepository('symbol')
+
+    @app.route('/')
+    def index():  # pylint: disable=unused-variable
+        return redirect(url_for('nem_summary'))
+
+    # region nem routes
+
+    @app.route('/nem/harvesters')
+    def nem_harvesters():  # pylint: disable=unused-variable
+        return render_template(
+            'nem_nodes.html',
+            title='NEM Recent Harvesters',
+            descriptors=nem_repository.harvester_descriptors,
+            version_to_css_class=nem_version_to_css_class,
+            show_balance=True)
+
+    @app.route('/nem/nodes')
+    def nem_nodes():  # pylint: disable=unused-variable
+        return render_template(
+            'nem_nodes.html',
+            title='NEM Nodes',
+            descriptors=nem_repository.node_descriptors,
+            version_to_css_class=nem_version_to_css_class)
+
+    @app.route('/nem/summary')
+    def nem_summary():  # pylint: disable=unused-variable
+        builder = BarChartBuilder({
+            '0.6.99': ('#006400', 1),
+            '0.6.98': ('#008000', 2),
+            '0.6.98-BETA': ('#2E8B57', 3),
+            'delegating / updating': ('#FCFFA4', 4),
+            '0.6.97-BETA': ('#DC143C', 5),
+            '0.6.96-BETA': ('#FF4500', 6),
+            '0.6.95-BETA': ('#FF0000', 7)
+        })
+        builder.add(nem_repository.harvester_descriptors, 'harvesting_power', 'harvesting_count')
+        builder.add(nem_repository.node_descriptors, None, 'node_count')
+
+        return render_template(
+            'nem_summary.html',
+            harvesting_power_chart_json=builder.create_chart('harvesting_power', 0.5),
+            harvesting_count_chart_json=builder.create_chart('harvesting_count'),
+            node_count_chart_json=builder.create_chart('node_count'))
+
+    @app.route('/nem/height')
+    def nem_height():  # pylint: disable=unused-variable
+        return NetworkConnector('nem').get_height(nem_repository.node_descriptors)
+
+    # endregion
+
+    # region symbol routes
+
+    @app.route('/symbol/voters')
+    def symbol_voters():  # pylint: disable=unused-variable
+        return render_template(
+            'symbol_nodes.html',
+            title='Symbol Voters',
+            descriptors=[descriptor for descriptor in symbol_repository.voter_descriptors if descriptor.is_voting],
+            version_to_css_class=symbol_version_to_css_class,
+            show_balance=True,
+            show_voting=True)
+
+    @app.route('/symbol/harvesters')
+    def symbol_harvesters():  # pylint: disable=unused-variable
+        return render_template(
+            'symbol_nodes.html',
+            title='Symbol Recent Harvesters',
+            descriptors=symbol_repository.harvester_descriptors,
+            version_to_css_class=symbol_version_to_css_class,
+            show_balance=True)
+
+    @app.route('/symbol/nodes')
+    def symbol_nodes():  # pylint: disable=unused-variable
+        return render_template(
+            'symbol_nodes.html',
+            title='Symbol Nodes',
+            descriptors=symbol_repository.node_descriptors,
+            version_to_css_class=symbol_version_to_css_class)
+
+    @app.route('/symbol/summary')
+    def symbol_summary():  # pylint: disable=unused-variable
+        builder = BarChartBuilder({
+            '1.0.3.0': ('#006400', 1),
+            'delegating / updating': ('#FCFFA4', 2),
+            '1.0.2.0': ('#DC143C', 3),
+            '1.0.1.0': ('#FF4500', 4),
+            '0.0.0.0': ('#FF0000', 5)
+        })
+        builder.add([descriptor for descriptor in symbol_repository.voter_descriptors if descriptor.is_voting], 'voting_power')
+        builder.add(symbol_repository.harvester_descriptors, 'harvesting_power', 'harvesting_count')
+        builder.add(symbol_repository.node_descriptors, None, 'node_count')
+
+        return render_template(
+            'symbol_summary.html',
+            voting_power_chart_json=builder.create_chart('voting_power', 0.67),
+            harvesting_power_chart_json=builder.create_chart('harvesting_power'),
+            harvesting_count_chart_json=builder.create_chart('harvesting_count'),
+            node_count_chart_json=builder.create_chart('node_count'))
+
+    @app.route('/symbol/height')
+    def symbol_height():  # pylint: disable=unused-variable
+        return NetworkConnector('symbol').get_height(symbol_repository.node_descriptors)
+
+    # endregion
+
+    def reload_all():
+        log.debug('reloading all data')
+
+        nem_repository.load_node_descriptors(resources_path / 'nem_nodes.json')
+        nem_repository.load_harvester_descriptors(resources_path / 'nem_harvesters.csv')
+
+        symbol_repository.load_node_descriptors(resources_path / 'symbol_nodes.json')
+        symbol_repository.load_harvester_descriptors(resources_path / 'symbol_harvesters.csv')
+        symbol_repository.load_voter_descriptors(resources_path / 'symbol_richlist.csv')
+
+    reload_all()
+
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=reload_all, trigger='interval', seconds=300)
+    scheduler.start()
+
+    return app
