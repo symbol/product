330f45c2787950fbb870f272386c164c4f530165
Jaguar0625 <jaguar0625@127.0.0.1>
2021-11-19 12:05:19 -0400

    feature: breakdown height charts by client version

--- explorer/nodewatch/nodewatch/HeightChartBuilder.py
+++ explorer/nodewatch/nodewatch/HeightChartBuilder.py
@@ -1,8 +1,16 @@
 import json
+from collections import namedtuple
 
-import pandas as pd
 import plotly
 import plotly.express as px
+
+from .chart_utils import VersionAggregator, pods_to_dataframe
+
+ScatterPoint = namedtuple('ScatterPoint', ['version', 'height', 'count', 'power', 'label', 'point_label'])
+HeightDescriptor = namedtuple('HeightDescriptor', ['field_name', 'label', 'power_field', 'count_field'])
+
+HEIGHT_DESCRIPTOR = HeightDescriptor('height', 'height', 'height_power', 'height_count')
+FINALIZED_HEIGHT_DESCRIPTOR = HeightDescriptor('finalized_height', 'finalized height', 'finalized_height_power', 'finalized_height_count')
 
 
 class DataPoint:
@@ -14,57 +22,55 @@ class DataPoint:
 
 
 class HeightChartBuilder:
-    def __init__(self):
-        self.all = {}
-        self.height_fields = []
+    def __init__(self, version_customizations, min_cluster_size=1):
+        self.version_customizations = version_customizations
+        self.min_cluster_size = min_cluster_size
 
-    def add(self, descriptors, height_field, label):
-        self._process(self.all, descriptors, height_field)
-        self.height_fields.append((height_field, label))
+        self.heights = VersionAggregator(DataPoint, HEIGHT_DESCRIPTOR.field_name)
+        self.finalized_heights = VersionAggregator(DataPoint, FINALIZED_HEIGHT_DESCRIPTOR.field_name)
 
-    @staticmethod
-    def _process(height_data_point_map, descriptors, height_field):
-        count_field = '{}_count'.format(height_field)
-        power_field = '{}_power'.format(height_field)
+    def add_heights(self, descriptors):
+        self.heights.add(descriptors, HEIGHT_DESCRIPTOR.power_field, HEIGHT_DESCRIPTOR.count_field)
 
-        for descriptor in descriptors:
-            height = getattr(descriptor, height_field)
-            power = descriptor.balance
-            if height not in height_data_point_map:
-                height_data_point_map[height] = DataPoint()
-
-            data_point = height_data_point_map[height]
-            setattr(data_point, count_field, getattr(data_point, count_field) + 1)
-            setattr(data_point, power_field, getattr(data_point, power_field) + power)
+    def add_finalized_heights(self, descriptors):
+        self.finalized_heights.add(descriptors, FINALIZED_HEIGHT_DESCRIPTOR.power_field, FINALIZED_HEIGHT_DESCRIPTOR.count_field)
 
     def create_chart(self):
-        data_vectors = {'height': [], 'count': [], 'power': [], 'label': []}
-        for (height_field, label) in self.height_fields:
-            count_field = '{}_count'.format(height_field)
-            power_field = '{}_power'.format(height_field)
+        scatter_points = []
+        self._append_scatter_points(scatter_points, self.heights.map, HEIGHT_DESCRIPTOR)
+        self._append_scatter_points(scatter_points, self.finalized_heights.map, FINALIZED_HEIGHT_DESCRIPTOR)
 
-            for height, data_point in self.all.items():
-                if not height:
-                    continue
+        scatter_points.sort(key=lambda scatter_point: self.version_customizations.get_weight(scatter_point.version), reverse=True)
+        data_frame = pods_to_dataframe(scatter_points)
 
-                count = getattr(data_point, count_field)
-                power = getattr(data_point, power_field)
-                if not count:
-                    continue
-
-                data_vectors['height'].append(height)
-                data_vectors['count'].append(count)
-                data_vectors['power'].append(power)
-                data_vectors['label'].append(label)
-
-        data_frame = pd.DataFrame(data_vectors)
         figure = px.scatter(
             data_frame,
             y='count',
             x='height',
-            color='label',
+            color='version',
+            color_discrete_map=self.version_customizations.to_color_map(),
+            symbol='label',
+            symbol_map={HEIGHT_DESCRIPTOR.label: 'circle', FINALIZED_HEIGHT_DESCRIPTOR.label: 'diamond'},
+            text='point_label',
             size='power',
             size_max=75)
         figure.update_xaxes(tickformat='d')
 
         return json.dumps(figure, cls=plotly.utils.PlotlyJSONEncoder)
+
+    def _append_scatter_points(self, scatter_points, version_data_point_map, height_descriptor):
+        for version_height_key, data_point in version_data_point_map.items():
+            parts = version_height_key.split('@')
+            version = parts[0]
+            height = None if not parts[1] else int(parts[1])
+
+            if not height:
+                continue
+
+            count = getattr(data_point, height_descriptor.count_field)
+            power = getattr(data_point, height_descriptor.power_field)
+            if count < self.min_cluster_size:
+                continue
+
+            point_label = '{}M'.format(round(power / 1000000))
+            scatter_points.append(ScatterPoint(version, height, count, power, height_descriptor.label, point_label))
---
+++ explorer/nodewatch/nodewatch/chart_utils.py
@@ -0,0 +1,49 @@
+from collections import namedtuple
+
+import pandas as pd
+
+VersionCustomization = namedtuple('VersionCustomization', ['color', 'weight'])
+
+
+def pods_to_dataframe(pods):
+    field_names = pods[0]._fields
+    data_vectors = {field_name: [getattr(pod, field_name) for pod in pods] for field_name in field_names}
+    return pd.DataFrame(data_vectors)
+
+
+class VersionCustomizations:
+    def __init__(self, customizations):
+        self.customizations = {version: VersionCustomization(*tuple) for version, tuple in customizations.items()}
+
+    def to_color_map(self):
+        return {version: customization.color for version, customization in self.customizations.items()}
+
+    def get_weight(self, version):
+        return self.customizations.get(version, VersionCustomization(None, 0)).weight
+
+
+class VersionAggregator:
+    def __init__(self, data_point_class, secondary_key=None):
+        self.data_point_class = data_point_class
+        self.secondary_key = secondary_key
+
+        self.map = {}
+
+    def add(self, descriptors, balance_field, count_field):
+        for descriptor in descriptors:
+            key = self._get_key(descriptor)
+            if key not in self.map:
+                self.map[key] = self.data_point_class()
+
+            data_point = self.map[key]
+            if balance_field:
+                setattr(data_point, balance_field, getattr(data_point, balance_field) + descriptor.balance)
+
+            if count_field:
+                setattr(data_point, count_field, getattr(data_point, count_field) + 1)
+
+    def _get_key(self, descriptor):
+        if not self.secondary_key:
+            return descriptor.version
+
+        return '{}@{}'.format(descriptor.version, getattr(descriptor, self.secondary_key))
