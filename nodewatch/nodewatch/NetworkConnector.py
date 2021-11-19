7cb46ba8fbdcb45c1717164bda1e0f8593ba2fed
Jaguar0625 <jaguar0625@127.0.0.1>
2021-11-19 19:30:57 -0400

    task: batch node info requests to reduce concurrent file handle use

--- nodewatch/nodewatch/NetworkConnector.py
+++ nodewatch/nodewatch/NetworkConnector.py
@@ -3,6 +3,8 @@ import asyncio
 import aiohttp
 from zenlog import log
 
+MAX_BATCH_SIZE = 100
+
 
 class NetworkConnector:
     def __init__(self, network_name):
@@ -17,7 +19,14 @@ class NetworkConnector:
         if not self.is_nem:
             descriptors = [descriptor for descriptor in descriptors if descriptor.endpoint.endswith(':3000')]
 
-        asyncio.run(self._update_heights_async(descriptors))
+        log.info(f'updating heights from {len(descriptors)} nodes for {self.network_name} network')
+
+        start_index = 0
+        while start_index < len(descriptors):
+            asyncio.run(self._update_heights_async(descriptors[start_index:start_index+MAX_BATCH_SIZE]))
+            start_index += MAX_BATCH_SIZE
+
+        log.info(f'done updating heights from {len(descriptors)} nodes for {self.network_name} network')
 
     async def _update_heights_async(self, descriptors):
         endpoints = [descriptor.endpoint for descriptor in descriptors]
@@ -28,13 +37,17 @@ class NetworkConnector:
             tasks = [
                 asyncio.ensure_future(self._update_height(session, descriptor)) for descriptor in descriptors
             ]
-            await asyncio.gather(*tasks)
+            results = await asyncio.gather(*tasks)
+
+            num_failures = sum(1 for result in results if not result)
+            if num_failures:
+                log.warning(f'{num_failures}/{len(endpoints)} height queries failed for {self.network_name} network')
 
     async def _update_height(self, session, descriptor):
         url = self.url_pattern.format(descriptor.endpoint)
 
         try:
-            async with session.get(url, timeout=10) as response:
+            async with session.get(url, timeout=5) as response:
                 response_json = await response.json()
                 if 'height' in response_json:
                     descriptor.height = int(response_json['height'])
@@ -43,3 +56,6 @@ class NetworkConnector:
                     descriptor.finalized_height = int(response_json['finalized_height'])
         except (aiohttp.client_exceptions.ClientConnectorError, asyncio.TimeoutError) as ex:
             log.warning(f'failed retrieving height from endpoint "{descriptor.endpoint}"\n{ex}')
+            return False
+
+        return True
