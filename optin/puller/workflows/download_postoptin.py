d04936e9837cabb4d8926fbdf2ee2b7f26b2600a
Jaguar0625 <jaguar0625@127.0.0.1>
2022-03-22 17:03:30 -0400

    [optin/puller]: update 'download_postoptin' to check multiple optin signers

--- optin/puller/workflows/download_postoptin.py
+++ optin/puller/workflows/download_postoptin.py
@@ -17,7 +17,7 @@ from puller.processors.NemOptinProcessor import process_nem_optin_request
 
 
 OPTIN_ADDRESS = 'NAQ7RCYM4PRUAKA7AMBLN4NPBJEJMRCHHJYAVA72'
-OPTIN_SIGNER_PUBLIC_KEY = '7AEC08AA66CB50B0C3D180DE7508D5D82ECEDCDC9E73F61FA7069868BEABA856'
+OPTIN_SIGNER_PUBLIC_KEYS = '7AEC08AA66CB50B0C3D180DE7508D5D82ECEDCDC9E73F61FA7069868BEABA856'
 
 
 def parse_args():
@@ -26,11 +26,12 @@ def parse_args():
 	parser.add_argument('--symbol-node', help='Symbol node url', default='http://wolf.importance.jp:3000')
 	parser.add_argument('--database-directory', help='output database directory', default='_temp')
 	parser.add_argument('--optin-address', help='optin account address', default=OPTIN_ADDRESS)
+	parser.add_argument('--optin-signer-public-keys', help='optin signer public keys (comma separated)', default=OPTIN_SIGNER_PUBLIC_KEYS)
 	parser.add_argument('--snapshot-height', help='snapshot height', default=3105500)
 	return parser.parse_args()
 
 
-async def process_transaction(transaction, nem_client, symbol_client, database):
+async def process_transaction(transaction, nem_client, symbol_client, database, optin_signer_public_keys):
 	transaction_height = transaction['meta']['height']
 
 	nem_network = await nem_client.node_network()
@@ -45,20 +46,24 @@ async def process_transaction(transaction, nem_client, symbol_client, database):
 		database.add_error(process_result)
 	else:
 		destination_address = symbol_network.public_key_to_address(process_result.destination_public_key)
-		optin_transaction_infos = await symbol_client.find_optin_transactions(PublicKey(OPTIN_SIGNER_PUBLIC_KEY), destination_address)
-		optin_transaction_info = next((info for info in optin_transaction_infos if process_result.address == info.address), None)
-		if optin_transaction_info:
-			print(f'{transaction_height} SUCCESS (DUPLICATE)')
-			database.add_request(process_result)
-			database.set_request_status(process_result, OptinRequestStatus.DUPLICATE, optin_transaction_info.transaction_hash)
-		else:
-			print(f'{transaction_height} SUCCESS (NEW)')
-			database.add_request(process_result)
+		for optin_signer_public_key in optin_signer_public_keys:
+			optin_transaction_infos = await symbol_client.find_optin_transactions(PublicKey(optin_signer_public_key), destination_address)
+			optin_transaction_info = next((info for info in optin_transaction_infos if process_result.address == info.address), None)
+			if optin_transaction_info:
+				print(f'{transaction_height} SUCCESS (DUPLICATE) (from {optin_signer_public_key})')
+				database.add_request(process_result)
+				database.set_request_status(process_result, OptinRequestStatus.DUPLICATE, optin_transaction_info.transaction_hash)
+				return process_result
+
+		print(f'{transaction_height} SUCCESS (NEW)')
+		database.add_request(process_result)
 
 	return process_result
 
 
 async def main():
+	# pylint: disable=too-many-locals
+
 	args = parse_args()
 
 	nem_client = NemClient(args.nem_node)
@@ -66,6 +71,12 @@ async def main():
 
 	finalized_height = await nem_client.finalized_height()
 	snapshot_height = int(args.snapshot_height)
+
+	optin_signer_public_keys = [PublicKey(public_key) for public_key in args.optin_signer_public_keys.split(',')]
+
+	print('optin signer public keys')
+	for public_key in optin_signer_public_keys:
+		print(f' * {public_key}')
 
 	with sqlite3.connect(Path(args.database_directory) / 'in_progress.db') as connection:
 		database = InProgressOptinDatabase(connection)
@@ -86,7 +97,7 @@ async def main():
 			if transaction_height <= database_height:
 				break
 
-			process_result = await process_transaction(transaction, nem_client, symbol_client, database)
+			process_result = await process_transaction(transaction, nem_client, symbol_client, database, optin_signer_public_keys)
 			if process_result.is_error:
 				error_count += 1
 
