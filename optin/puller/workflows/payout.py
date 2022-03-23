e4a6372931527803d3c3389a6a134ab3286665d2
Jaguar0625 <jaguar0625@127.0.0.1>
2022-03-23 15:30:09 -0400

    [optin/puller] task: make processing interactive

--- optin/puller/workflows/payout.py
+++ optin/puller/workflows/payout.py
@@ -6,9 +6,8 @@ from pathlib import Path
 from symbolchain.nem.Network import Network as NemNetwork
 from symbolchain.PrivateKeyStorage import PrivateKeyStorage
 from symbolchain.symbol.KeyPair import KeyPair
-from symbolchain.symbol.Network import Network as SymbolNetwork
 
-from puller.client.SymbolClient import SymbolClient, filter_finalized_transactions
+from puller.client.SymbolClient import SymbolClient
 from puller.db.Databases import Databases
 from puller.db.InProgressOptinDatabase import OptinRequestStatus
 from puller.processors.RequestGrouper import group_requests
@@ -24,47 +23,14 @@ def get_key_pair(filepath):
 
 
 class Processor:
-	def __init__(self, databases, client, symbol_network, transaction_preparer, is_dry_run):
+	def __init__(self, databases, client, symbol_network, transaction_preparer):
 		# pylint: disable=too-many-arguments
 
 		self.databases = databases
 		self.client = client
 		self.symbol_network = symbol_network
 		self.transaction_preparer = transaction_preparer
-		self.is_dry_run = is_dry_run
-
-	async def process_sent_requests(self, sent_requests):
-		if not sent_requests:
-			return
-
-		hashes = []
-		for request_group in sent_requests.values():
-			hashes.extend([request.payout_transaction_hash for request in request_group.requests])
-
-		finalized_transaction_hashes = set(await filter_finalized_transactions(self.client, set(hashes)))
-
-		completed = 0
-		for address, request_group in sent_requests.items():
-			is_request_group_finalized = True
-			for request in request_group.requests:
-				if request.payout_transaction_hash in finalized_transaction_hashes:
-					print(f'marking request with transaction hash {request.payout_transaction_hash} COMPLETED')
-					if not self.is_dry_run:
-						self.databases.inprogress.set_request_status(request, OptinRequestStatus.COMPLETED, request.payout_transaction_hash)
-
-					completed += 1
-				else:
-					is_request_group_finalized = False
-
-			if is_request_group_finalized:
-				balance = self.databases.balances.lookup_balance(address)
-				symbol_address = self.symbol_network.public_key_to_address(request_group.requests[0].destination_public_key)
-
-				print(f'updating completed database with mapping from {address} to {symbol_address} with amount {balance}')
-				if not self.is_dry_run:
-					self.databases.completed.insert_mapping({str(address): balance}, {str(symbol_address): balance})
-
-		print(f'transactions completed since last run: {completed}')
+		self.is_dry_run = True
 
 	def _set_request_status_all(self, requests, new_status, payout_transaction_hash):
 		for request in requests:
@@ -75,28 +41,25 @@ class Processor:
 			if not self.is_dry_run:
 				self.databases.inprogress.set_request_status(request, new_status, payout_transaction_hash)
 
-	async def process_unprocessed_requests(self, unprocessed_requests, sent_requests):
-		node_time = await self.client.node_time()
-		deadline = node_time.add_hours(1).timestamp
+	async def process_unprocessed_request(self, address, request_group, sent_requests, deadline):
+		if request_group.is_error:
+			# inconsistent error detection - what to do?
+			print('request error detected, skipping')
+			return
 
-		for address, request_group in unprocessed_requests.items():
-			if request_group.is_error:
-				# inconsistent error detection - what to do?
-				continue
+		if address in sent_requests or self.databases.completed.is_opted_in(address):
+			self._set_request_status_all(request_group.requests, OptinRequestStatus.DUPLICATE, None)
+		else:
+			if self.databases.multisig.check_cosigners(address, [request.address for request in request_group.requests]):
+				balance = self.databases.balances.lookup_balance(address)
+				if not balance:
+					self._set_request_status_all(request_group.requests, OptinRequestStatus.ERROR_ZERO, None)
+					return
 
-			if address in sent_requests or self.databases.completed.is_opted_in(address):
-				self._set_request_status_all(request_group.requests, OptinRequestStatus.DUPLICATE, None)
-			else:
-				if self.databases.multisig.check_cosigners(address, [request.address for request in request_group.requests]):
-					balance = self.databases.balances.lookup_balance(address)
-					if not balance:
-						self._set_request_status_all(request_group.requests, OptinRequestStatus.ERROR_ZERO, None)
-						continue
+				symbol_address = self.symbol_network.public_key_to_address(request_group.requests[0].destination_public_key)
+				sent_transaction_hash = await self.send_funds(symbol_address, balance, deadline, address)
 
-					symbol_address = self.symbol_network.public_key_to_address(request_group.requests[0].destination_public_key)
-					sent_transaction_hash = await self.send_funds(symbol_address, balance, deadline, address)
-
-					self._set_request_status_all(request_group.requests, OptinRequestStatus.SENT, sent_transaction_hash)
+				self._set_request_status_all(request_group.requests, OptinRequestStatus.SENT, sent_transaction_hash)
 
 	async def send_funds(self, destination_address, amount, deadline, nem_address):
 		transaction, transaction_hash = self.transaction_preparer.prepare_transaction(destination_address, amount, deadline, nem_address)
@@ -110,8 +73,31 @@ class Processor:
 		return transaction_hash
 
 
+async def process_all(processor, unprocessed_requests, sent_requests, deadline, is_dry_run):
+	print(f'processing {len(sent_requests)} SENT requests and {len(unprocessed_requests)} UNPROCESSED requests')
+
+	for address, request_group in unprocessed_requests.items():
+		print()
+
+		processor.is_dry_run = True
+		await processor.process_unprocessed_request(address, request_group, sent_requests, deadline)
+
+		if is_dry_run:
+			continue
+
+		user_input = input('Press Y to send transaction and update databases: ')
+		if 'y' != user_input.lower():
+			print('goodbye')
+			return
+
+		print()
+
+		processor.is_dry_run = False
+		await processor.process_unprocessed_request(address, request_group, sent_requests, deadline)
+
+
 async def main():
-	parser = argparse.ArgumentParser(description='process post opt in requests')
+	parser = argparse.ArgumentParser(description='inspects unprocessed optin requests and initiates new payouts')
 	parser.add_argument('--symbol-node', help='Symbol node url', default='http://wolf.importance.jp:3000')
 	parser.add_argument('--database-directory', help='output database directory', default='_temp')
 	parser.add_argument('--network', help='NEM and Symbol network', choices=['testnet', 'mainnet'], default='mainnet')
@@ -119,31 +105,28 @@ async def main():
 	parser.add_argument('--dry-run', help='print transactions without sending', action='store_true')
 	args = parser.parse_args()
 
-	nem_network = NemNetwork.MAINNET
-	symbol_network = SymbolNetwork.MAINNET
-	if 'testnet' == args.network:
-		nem_network = NemNetwork.TESTNET
-		symbol_network = SymbolNetwork.TESTNET
-
 	client = SymbolClient(args.symbol_node)
-
 	currency_mosaic_id = await client.network_currency()
-	key_pair = get_key_pair(Path(args.hot))
+	symbol_network = await client.node_network()
+	nem_network = NemNetwork.TESTNET if 'testnet' == args.network else NemNetwork.MAINNET
 
+	key_pair = get_key_pair(Path(args.hot))
 	print(f'building preparer around {symbol_network.public_key_to_address(key_pair.public_key)}')
+
 	transaction_preparer = TransactionPreparer(args.network, currency_mosaic_id, key_pair)
 
+	node_time = await client.node_time()
+	deadline = node_time.add_hours(1).timestamp
+	print(f'using deadline {deadline}')
+
 	with Databases(args.database_directory) as databases:
-		processor = Processor(databases, client, symbol_network, transaction_preparer, args.dry_run)
-
+		processor = Processor(databases, client, symbol_network, transaction_preparer)
 		sent_requests = group_requests(nem_network, databases.inprogress.get_requests_by_status(OptinRequestStatus.SENT))
-		print(f'sent requests count: {len(sent_requests)}')
-		await processor.process_sent_requests(sent_requests)
-
 		unprocessed_requests = group_requests(
 			nem_network,
 			databases.inprogress.get_requests_by_status(OptinRequestStatus.UNPROCESSED))
-		await processor.process_unprocessed_requests(unprocessed_requests, sent_requests)
+
+		await process_all(processor, unprocessed_requests, sent_requests, deadline, args.dry_run)
 
 
 if '__main__' == __name__:
