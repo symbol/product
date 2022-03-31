19a4ba3aeb26741eba5d493df27d4b5562dc7ba1
AnthonyLaw <yc-law1015@hotmail.com>
2022-03-31 15:21:23 +0800

    [optin/reporting/client] task: add global config for formatting value

---
+++ optin/reporting/client/src/config.js
@@ -0,0 +1,14 @@
+const config = {
+  language : {
+    nemAddress: 'NEM Address',
+    nemBalance: 'NEM Balance',
+    symbolAddress: 'Symbol Address',
+    symbolBalance: 'Symbol Balance',
+  },
+  keyRedirects: {
+    nemAddress: 'https://explorer.nemtool.com/#/s_account?account=',
+    symbolAddress: 'https://symbol.fyi/accounts/'
+  }
+}
+
+export default config;
