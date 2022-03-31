8024c0e5769e70d8f88b8d891aefc2de2392fa98
AnthonyLaw <yc-law1015@hotmail.com>
2022-03-31 15:26:06 +0800

    [optin/reporting/client] task: update home page

--- optin/reporting/client/src/pages/Home/index.jsx
+++ optin/reporting/client/src/pages/Home/index.jsx
@@ -1,30 +1,58 @@
-import "./Home.scss";
-import Table from "../../components/Table";
-import React, { useState, useEffect } from "react";
+import './Home.scss';
+import Completed from '../Completed';
+import Balances from '../Balances';
+import React from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
+const tabConfig = {
+  balance: {
+    label: 'Balances',
+    table: <Balances />
+  },
+  completed: {
+    label: 'Completed',
+    table: <Completed />
+  },
+  inProgress : {
+    label: 'In Progress',
+    table: 'In Progress'
+  }
+}
 
 const Home = function () {
-  const [balances, setBalances] = useState({
-    data: [],
-    success: false,
-  });
+  const renderTabListItem = () => {
+    return Object.keys(tabConfig).map((key) => {
+      return (
+          <Tab>{tabConfig[key].label}</Tab>
+      )
+    })
+  }
 
-  useEffect(() => {
-    const getBalances = async () => {
-      const balances = await fetch("/api/balances").then((res) => res.json());
-      setBalances(balances);
-    };
-
-    getBalances();
-  }, []);
+  const renderTabContent = () => {
+    return Object.keys(tabConfig).map((key) => {
+      return (
+        <TabPanel key={key}>
+          {tabConfig[key].table}
+        </TabPanel>
+      )
+    })
+  }
 
   return (
-    <div className="App">
-      <div className="bgContainer" />
-      <div className="mainContainerWrapper">
-        <div className="mainContainer">
-          <h2>NEM Address Balance</h2>
+    <div className='App'>
+      <div className='bgContainer' />
+      <div className='mainContainerWrapper'>
+        <div className='mainContainer'>
+          <h2>Opt-in Summary</h2>
         </div>
-        <Table dataList={balances} dataHeader={["Address", "Balance"]} />
+        <Tabs>
+            <TabList>
+              { renderTabListItem() }
+            </TabList>
+
+            { renderTabContent() }
+          </Tabs>
       </div>
     </div>
   );
