a3dc75979a99647b175726b7c8389650bec32cb5
AnthonyLaw <yc-law1015@hotmail.com>
2022-03-31 15:23:08 +0800

    [optin/reporting/client] task: to handle dynamic table

--- optin/reporting/client/src/components/Table/index.jsx
+++ optin/reporting/client/src/components/Table/index.jsx
@@ -2,42 +2,64 @@ import "./Table.scss";
 import PropTypes from "prop-types";
 import React from "react";
 
-const Table = function ({ dataList, dataHeader }) {
-  const renderTableHeader = () =>
-    dataHeader.map((header, key) => <th key={key}>{header}</th>);
+const Table = function ({ dataList, formatting }) {
+  const renderTableHeader = () => {
+    return Object.keys(dataList.data[0]).map((key, index) => {
+      return (<th key={"header_" + index}>{formatting.language[key]}</th>)
+    })
+  }
 
-  const renderItem = () =>
-    dataList.data.map((balance, key) => (
-      <tr key={key}>
-        <td>
-          <a
-            href={`https://explorer.nemtool.com/#/s_account?account=${balance.address}`}
-          >
-            {balance.address}
-          </a>
-        </td>
-        <td>{balance.balance}</td>
-      </tr>
-    ));
+  const renderItem = () => {
+    return dataList.data.map((item, index) => {
+      return (
+        <tr key={"row_" + index}>
+          {Object.keys(item).map((key, index) => {
+            if (Array.isArray(item[key])) {
+              // Todo: handle Array data
+
+            } else {
+              return (<td key={"item_" + index}>
+                {
+                  (formatting.keyRedirects[key]) ?
+                  <a href={formatting.keyRedirects[key] + item[key]} target="_blank">{item[key]}</a> :
+                  item[key]
+                }
+              </td>)
+            }
+          })}
+        </tr>
+      )
+    })
+  }
 
   return (
+    <>
     <table className="tableContainer">
-      <thead>
-        <tr>{renderTableHeader()}</tr>
-      </thead>
-      <tbody>
-        {0 < dataList.data.length ? renderItem() : "No data available"}
-      </tbody>
+      {0 < dataList.data.length ?
+      <div>
+        <thead>
+          <tr>{ renderTableHeader() }</tr>
+        </thead>
+        <tbody>
+          { renderItem() }
+        </tbody>
+      </div>
+      : "No data available"}
     </table>
+      </>
   );
 };
 
 Table.propTypes = {
   dataList: PropTypes.exact({
     data: PropTypes.array.isRequired,
-    success: PropTypes.bool.isRequired,
+    pagination: PropTypes.shape({
+      pageNumber: PropTypes.number.isRequired,
+      pageSize: PropTypes.number.isRequired,
+      totalRecord: PropTypes.number.isRequired,
+    })
   }).isRequired,
-  dataHeader: PropTypes.array.isRequired,
+  formatting: PropTypes.object
 };
 
 export default Table;
