78282227798eddc208c62884ad2ade5a320b862e
AnthonyLaw <yc-law1015@hotmail.com>
2022-04-01 15:14:17 +0800

    [optin/reporting/client] task: update table components handle array in col

--- optin/reporting/client/src/components/Table/index.jsx
+++ optin/reporting/client/src/components/Table/index.jsx
@@ -4,6 +4,12 @@ import React from "react";
 import Helper from '../../utils/helper'
 
 const Table = function ({ dataList, formatting }) {
+  /**
+   * Format value in table.
+   * @param {string} key Object key.
+   * @param {string} value Value need to be format.
+   * @returns {string} Formatted value.
+   */
   const formatValue = (key, value) => {
 	if (formatting.keyFormat[key] === 'relative') {
 		return Helper.toRelativeAmount(value).toLocaleString('en-US', { minimumFractionDigits: 6 });
@@ -14,29 +20,98 @@ const Table = function ({ dataList, formatting }) {
 	return value
   }
 
+  /**
+   * Dynamic render table header by object.
+   * @returns {HTMLTableHeaderCellElement} HTML table header.
+   */
   const renderTableHeader = () => {
     return Object.keys(dataList.data[0]).map((key, index) => {
-      return (<th key={"header_" + index}>{formatting.language[key]}</th>)
+      return (<th key={"header_" + index}>{formatting.language[key] || key}</th>)
     })
   }
 
-  const renderItem = () => {
+  /**
+   * Render array item in table column.
+   * @param {array} items List of items.
+   * @param {string} key Object key.
+   * @returns {HTMLDivElement} HTML element.
+   */
+  const renderArrayItem = (items, key) => {
+    return items.map(item => {
+      return (<div>
+        {
+          (formatting.keyRedirects[key]) ?
+          <a href={formatting.keyRedirects[key] + item} target="_blank">
+            {formatValue(key, item)}
+          </a> :
+
+          formatValue(key, item)
+      }
+    </div>)
+    })
+  }
+
+  /**
+   * Render single item in table column.
+   * @param {string} item value.
+   * @param {string} key Object key.
+   * @returns {HTMLDivElement} HTML element.
+   */
+  const renderItem = (item, key) => {
+    return (<div>
+      {
+        (formatting.keyRedirects[key]) ?
+        <a href={formatting.keyRedirects[key] + item} target="_blank">
+          {formatValue(key, item)}
+        </a> :
+
+        formatValue(key, item)
+    }
+  </div>)
+  }
+
+  /**
+   * Render total balance.
+   * @param {string} key Object key.
+   * @param {array} balances Balances of address.
+   * @returns {HTMLElement} HTML element.
+   */
+  const renderTotal = (key, balances) => {
+    if (2 > balances.length) {
+      return null;
+    }
+
+    if (key === 'nemAddress' || key === 'symbolAddress') {
+      return (<b>Total</b>)
+    }
+
+    if (key === 'nemBalance' || key === 'symbolBalance') {
+      const total = balances.reduce((balance, currentBalance) => balance + currentBalance, 0);
+      const formatBalance = Helper.toRelativeAmount(total).toLocaleString('en-US', { minimumFractionDigits: 6 });
+      return (<b>{formatBalance}</b>)
+    }
+  }
+
+  /**
+   * Render table row.
+   * @returns {HTMLTableRowElement} HTML element.
+   */
+  const renderContent = () => {
     return dataList.data.map((item, index) => {
       return (
         <tr key={"row_" + index}>
           {Object.keys(item).map((key, index) => {
-            if (Array.isArray(item[key])) {
-              // Todo: handle Array data
+            return (<td key={"item_" + index}>
+              {
+                (Array.isArray(item[key])) ? (
+                  <>
+                    { renderArrayItem(item[key], key) }
 
-            } else {
-              return (<td key={"item_" + index}>
-                {
-                  (formatting.keyRedirects[key]) ?
-                  <a href={formatting.keyRedirects[key] + item[key]} target="_blank" rel="noreferrer">{formatValue(key, item[key])}</a> :
-                  formatValue(key, item[key])
-                }
-              </td>)
-            }
+                    { renderTotal(key, item[key]) }
+                  </>
+                ) : renderItem(item[key], key)
+              }
+            </td>)
           })}
         </tr>
       )
@@ -51,7 +126,7 @@ const Table = function ({ dataList, formatting }) {
           <tr>{ renderTableHeader() }</tr>
         </thead>
         <tbody>
-          { renderItem() }
+          { renderContent() }
         </tbody>
       </div>
       : "No data available"}
