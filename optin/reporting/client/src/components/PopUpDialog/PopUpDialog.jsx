32044560c7b0b383b62a967d533a579852533238
AnthonyLaw <yc-law1015@hotmail.com>
2022-04-08 14:45:06 +0800

    [optin/reporting/client] feat: add popup dialog component

---
+++ optin/reporting/client/src/components/PopUpDialog/PopUpDialog.jsx
@@ -0,0 +1,26 @@
+import { Button } from 'primereact/button';
+import { Dialog } from 'primereact/dialog';
+import React, { useState } from 'react';
+
+const PopUpDialog = ({title, content}) => {
+	const [visible, setVisible] = useState(false);
+
+	const onHide = () => {
+		setVisible(false);
+	};
+
+	const onShow = () => {
+		setVisible(true);
+	};
+
+	return  (
+		<>
+			<Button id="button" onClick={onShow} label={title}  className="p-button-secondary p-button-text" />
+			<Dialog header={title} visible={visible} onHide={onHide} dismissableMask={true} >
+				{ content }
+			</Dialog>
+		</>
+	);
+};
+
+export default PopUpDialog;
