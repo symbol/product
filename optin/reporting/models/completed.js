d0632c9271a486f223d2ce1ade39baef80a6360a
AnthonyLaw <yc-law1015@hotmail.com>
2022-04-08 15:24:43 +0800

    [optin/reporting] task: update latest completed db query

--- optin/reporting/models/completed.js
+++ optin/reporting/models/completed.js
@@ -4,34 +4,38 @@ const { QueryTypes } = require('sequelize');
 const completedDB = {
 	getCompletedPagination: async ({ pageNumber, pageSize }) => {
 		const result = await completed.query(
-			`SELECT opt.id as optin_id,
-			(SELECT json_group_array(json_object('address', hex(address)))
-			 FROM nem_source
-			 WHERE optin_id = opt.id
-			) AS nemAddress,
-			(SELECT json_group_array(json_object('balance', balance))
-			 FROM nem_source
-			 WHERE optin_id = opt.id
-			) AS nemBalance,
-			(SELECT json_group_array(json_object('address', hex(address)))
-			 FROM symbol_destination
-			 WHERE optin_id = opt.id
-			) AS symbolAddress,
-			(SELECT json_group_array(json_object('balance', balance))
-			 FROM symbol_destination
-			 WHERE optin_id = opt.id
-			) AS symbolBalance
-		  	FROM optin_id opt
+			`SELECT
+				opt.id,
+				opt.is_postoptin,
+				(SELECT json_group_array(nem_address_object)
+					FROM (
+						SELECT json_object(
+						'address', hex(nem_source.address),
+						'balance', nem_source.balance,
+						'hashes', GROUP_CONCAT(CASE WHEN nem_transaction.hash IS NULL THEN NULL ELSE hex(nem_transaction.hash) END, ';'),
+						'height', GROUP_CONCAT(CASE WHEN nem_transaction.height IS NULL THEN NULL ELSE nem_transaction.height END, ';'),
+						'label', nem_label.label
+					) as nem_address_object
+					FROM nem_source
+					LEFT JOIN nem_transaction ON nem_transaction.address = nem_source.address
+					LEFT JOIN nem_label ON nem_label.address = nem_source.address
+					where optin_id = opt.id
+					GROUP BY nem_source.address, nem_source.balance
+					) temp
+				) AS nem_source,
+				(SELECT json_group_array(json_object('address', hex(address), 'balance', balance, 'hashes', hash, 'height', height))
+				FROM symbol_destination
+				WHERE optin_id = opt.id
+				) AS symbol_destination
+			FROM optin_id opt
             LIMIT ${pageSize} OFFSET ${(pageNumber - 1) * pageSize}`,
 			{ type: QueryTypes.SELECT }
 		);
 
 		return result.map(item => ({
 			...item,
-			nemAddress: JSON.parse(item.nemAddress),
-			nemBalance: JSON.parse(item.nemBalance),
-			symbolAddress: JSON.parse(item.symbolAddress),
-			symbolBalance: JSON.parse(item.symbolBalance)
+			nem_source: JSON.parse(item.nem_source),
+			symbol_destination: JSON.parse(item.symbol_destination)
 		}));
 	},
 	getTotalRecord: async () => {
