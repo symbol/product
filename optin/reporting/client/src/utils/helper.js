b111d4e8b3495220bd3e28d86706fce6d8e4ab65
AnthonyLaw <yc-law1015@hotmail.com>
2022-04-08 14:50:39 +0800

    [optin/reporting/client] feat: add trunc string in helper

--- optin/reporting/client/src/utils/helper.js
+++ optin/reporting/client/src/utils/helper.js
@@ -2,7 +2,6 @@ const Helper = {
 	toRelativeAmount: amount => amount / (10 ** 6),
 	downloadFile: ({ data, fileName, fileType }) => {
 		const blob = new Blob([data], { type: fileType });
-  
 		const a = document.createElement('a');
 		a.download = fileName;
 		a.href = window.URL.createObjectURL(blob);
@@ -23,6 +22,14 @@ const Helper = {
 			}
 		}).then(async res => Helper.downloadFile({ data: await res.text(), fileName, fileType: 'text/csv' }));
 		setDownloading(false);
+	},
+	truncString: (str, strLen = 6) => {
+		if ('string' === typeof str) {
+			if (str.length > (strLen * 2) + 1)
+				return `${str.substring(0, strLen)}...${str.substring(str.length - strLen, str.length)}`;
+			return str;
+		}
+		return str;
 	}
 };
 
