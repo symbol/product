d1a9ff45ed650b5b3dc3816d24f9dd3dfc2f0aad
AnthonyLaw <yc-law1015@hotmail.com>
2022-04-08 15:27:51 +0800

    [optin/reporting/client] task: add transaction hash, optin, label column

--- optin/reporting/client/src/pages/Completed/Completed.jsx
+++ optin/reporting/client/src/pages/Completed/Completed.jsx
@@ -2,7 +2,7 @@ import Table from '../../components/Table';
 import TableColumn from '../../components/Table/TableColumn';
 import config from '../../config';
 import Helper from '../../utils/helper';
-import { addressTemplate, balanceTemplate } from '../../utils/pageUtils';
+import { addressTemplate, balanceTemplate, transactionHashTemplate, optinTypeTemplate, infoTemplate } from '../../utils/pageUtils';
 import { Button } from 'primereact/button';
 import { InputSwitch } from 'primereact/inputswitch';
 import { InputText } from 'primereact/inputtext';
@@ -32,10 +32,10 @@ const Completed = ({defaultPaginationType}) => {
 
 	// TODO use this in the API call
 	const parseFilterSearch = filterSearch => {
-		if (40 === filterSearch.length) // NEM Address 
+		if (40 === filterSearch.length) // NEM Address
 			return [filterSearch, '', ''];
 		else if (39 === filterSearch.length) // Symbol Address
-			return ['', filterSearch, ''];	
+			return ['', filterSearch, ''];
 		else if (64 === filterSearch.length) // Transaction Hash
 			return ['', '', filterSearch];
 		return ['', ''];
@@ -50,8 +50,8 @@ const Completed = ({defaultPaginationType}) => {
 			pageSize: rows
 		});
 
-		if ('scroll' === paginationType && 1 !== nextPage) 
-			setCompleted({data: [...completed.data, ...result.data], pagination: result.pagination});  
+		if ('scroll' === paginationType && 1 !== nextPage)
+			setCompleted({data: [...completed.data, ...result.data], pagination: result.pagination});
 		else
 		  setCompleted(result);
 		setLoading(false);
@@ -86,11 +86,11 @@ const Completed = ({defaultPaginationType}) => {
 		setPaginationType(e.value ? 'scroll' : 'paginator');
 		await onFilterSubmit();
 	};
-	
+
 	const nemAddressTemplate = rowData => {
 		return addressTemplate(rowData, 'nemAddress', config);
 	};
-  
+
 	const symbolAddressTemplate = rowData => {
 		return addressTemplate(rowData, 'symbolAddress', config);
 	};
@@ -103,6 +103,22 @@ const Completed = ({defaultPaginationType}) => {
 		return balanceTemplate(rowData, 'symbolBalance');
 	};
 
+	const nemHashesTemplate = rowData => {
+		return transactionHashTemplate(rowData, 'nemHashes', config);
+	};
+
+	const symbolHashesTemplate = rowData => {
+		return transactionHashTemplate(rowData, 'symbolHashes', config);
+	};
+
+	const isPostoptinTemplate = rowData => {
+		return optinTypeTemplate(rowData, 'isPostoptin');
+	};
+
+	const labelTemplate = rowData => {
+		return infoTemplate(rowData, 'label');
+	};
+
 	const optinTypes = [{label: 'Pre-launch', value: 'pre-launch'}, {label: 'Post-launch', value: 'post-launch'}];
 
 	const header = (
@@ -112,13 +128,14 @@ const Completed = ({defaultPaginationType}) => {
 					<i className="pi pi-search" />
 					<span className="p-input-icon-right">
 						<i className="pi pi-times" onClick={() => setFilterSearch('')}/>
-						<InputText value={filterSearch} onChange={onFilterChange} 
+						<InputText value={filterSearch} onChange={onFilterChange}
 							placeholder="NEM Address / Symbol Address / Transaction Hash" className='w-28rem' />
 					</span>
 				</span>
 				<div className='field'>
-					<SelectButton optionLabel="label" optionValue="value" value={filterOptinType} options={optinTypes} 
-						onChange={e => setFilterOptinType(e.value)}></SelectButton>
+					<SelectButton optionLabel="label" optionValue="value" value={filterOptinType} options={optinTypes}
+						onChange={e => setFilterOptinType(e.value)}>
+					</SelectButton>
 				</div>
 				<Button type="button" icon="pi pi-search" className="p-button-outlined" onClick={onFilterSubmit} />
 
@@ -126,24 +143,28 @@ const Completed = ({defaultPaginationType}) => {
 			<div className="formgroup-inline">
 				{/* <div className="card" style={{marginRight: '20px'}}>
 					<h5 style={{margin: 0, marginLeft: '-15px', color: '#b4b2b2'}}>Infinite Scroll</h5>
-					<InputSwitch checked={'scroll' === paginationType} 
+					<InputSwitch checked={'scroll' === paginationType}
 						onChange={onPaginationTypeChange}/>
 				</div> */}
-				<Button type="button" icon="pi pi-download" className="p-button-outlined" onClick={downloadAllAsCSV} 
+				<Button type="button" icon="pi pi-download" className="p-button-outlined" onClick={downloadAllAsCSV}
 					loading={downloading} tooltip="Download All Data as CSV File" tooltipOptions={{position: 'top'}}/>
 			</div>
 		</div>
 	);
 
 	return (
-		<Table value={completed.data} rows={completed.pagination.pageSize} 
+		<Table value={completed.data} rows={completed.pagination.pageSize}
 			onPage={handlePageChange} loading={loading} totalRecords={completed.pagination.totalRecord}
 			first={first} header={header} paginator={'paginator' === paginationType}>
-			<TableColumn field="optin_id" header="Opt-in ID" align="left"/>
+			<TableColumn field="optin_id" header="#" align="left"/>
 			<TableColumn field="nemAddress" header="NEM Address" body={nemAddressTemplate} align="left"/>
+			<TableColumn field="label" header="Info" body={labelTemplate} align="left"/>
+			<TableColumn field="nemHashes" header="Hash" body={nemHashesTemplate} align="left"/>
 			<TableColumn field="nemBalance" header="NEM Balance" body={nemBalanceTemplate} align="right"/>
 			<TableColumn field="symbolAddress" header="Symbol Address" body={symbolAddressTemplate} align="left"/>
+			<TableColumn field="symbolHashes" header="Hash" body={symbolHashesTemplate} align="left"/>
 			<TableColumn field="symbolBalance" header="Symbol Balance" body={symbolBalanceTemplate} align="right"/>
+			<TableColumn field="isPostoptin" header="Opt-in Type" body={isPostoptinTemplate} align="right"/>
 		</Table>
 	);
 };
