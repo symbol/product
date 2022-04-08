9097bf25fb048d039e93459262506a984ce623b6
Baha <baha@symbol.dev>
2022-04-08 03:42:03 +0300

    [optin/reporting/client] feat: infinite-scroll is added to the table and page components, made toggleable with default value

--- optin/reporting/client/src/components/Table/Table.jsx
+++ optin/reporting/client/src/components/Table/Table.jsx
@@ -2,8 +2,26 @@ import './Table.scss';
 import { DataTable } from 'primereact/datatable';
 import PropTypes from 'prop-types';
 import React from 'react';
+import VisibilitySensor from 'react-visibility-sensor';
 
 const Table = props => {
+
+	const checkVisible = async visible => {
+		if (visible) 
+			await props.onPage({});
+	};
+
+	const footer = (
+		<React.Fragment>
+			{props.footer}
+			{
+				!props.paginator && <VisibilitySensor onChange={checkVisible}>
+					<div>&nbsp;</div>
+				</VisibilitySensor>
+			}
+		</React.Fragment>
+	);
+
 	return  (
 		<DataTable lazy={props.lazy} value={props.value} stripedRows showGridlines responsiveLayout="stack" 
 			breakpoint={props.breakpoint} paginator={props.paginator}
@@ -11,14 +29,14 @@ const Table = props => {
 			currentPageReportTemplate="Showing {first}-{last} of {totalRecords}" rows={props.rows} 
 			rowsPerPageOptions={props.rowsPerPageOptions}	onPage={props.onPage} 
 			loading={props.loading} totalRecords={props.totalRecords} first={props.first} header={props.header} 
-			emptyMessage={props.emptyMessage}>
+			emptyMessage={props.emptyMessage} footer={footer}>
 			{props.children}
 		</DataTable>
 	);
 };
 
 Table.propTypes = {
-  
+	// TODO fill the propTypes?
 };
 
 Table.defaultProps = {
