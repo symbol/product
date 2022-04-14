265d66cffa635b5af364f486ee426f87bc5ee3cb
AnthonyLaw <yc-law1015@hotmail.com>
2022-04-14 13:59:14 +0800

    [optin/reporting] task: add timestamp in completed query

--- optin/reporting/models/completed.js
+++ optin/reporting/models/completed.js
@@ -27,18 +27,29 @@ const completedDB = {
 						'balance', nem_source.balance,
 						'hashes', GROUP_CONCAT(CASE WHEN nem_transaction.hash IS NULL THEN NULL ELSE hex(nem_transaction.hash) END, ';'),
 						'height', GROUP_CONCAT(CASE WHEN nem_transaction.height IS NULL THEN NULL ELSE nem_transaction.height END, ';'),
-						'label', nem_label.label
+						'label', nem_label.label,
+						'timestamps', GROUP_CONCAT(
+							CASE WHEN nem_block_timestamps.timestamp IS NULL THEN NULL ELSE nem_block_timestamps.timestamp END, ';')
 					) as nem_address_object
 					FROM nem_source
 					LEFT JOIN nem_transaction ON nem_transaction.address = nem_source.address
 					LEFT JOIN nem_label ON nem_label.address = nem_source.address
+					LEFT JOIN nem_block_timestamps ON nem_block_timestamps.height = nem_transaction.height
 					where optin_id = opt.id
 					GROUP BY nem_source.address, nem_source.balance
 					) temp
 				) AS nem_source,
-				(SELECT json_group_array(json_object('address', hex(address), 'balance', balance, 'hashes', hash, 'height', height))
-				FROM symbol_destination
-				WHERE optin_id = opt.id
+				(
+					SELECT json_group_array(
+						json_object(
+							'address', hex(address),
+							'balance', balance,
+							'hashes', hex(hash),
+							'height', height,
+							'timestamps',timestamp)
+					)
+					FROM symbol_destination
+					WHERE optin_id = opt.id
 				) AS symbol_destination
 			FROM optin_id opt
 			WHERE (is_postoptin = $1 OR $1 is null) ${condition}
