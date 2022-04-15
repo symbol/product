8370edd2e48dfedc54f9467840fdb4137e0f35a4
AnthonyLaw <yc-law1015@hotmail.com>
2022-04-15 23:59:38 +0800

    [optin/reporting/client] task: fix css more button aligment

--- optin/reporting/client/src/components/PopUpDialog/PopUpDialog.jsx
+++ optin/reporting/client/src/components/PopUpDialog/PopUpDialog.jsx
@@ -15,7 +15,8 @@ const PopUpDialog = ({title, content, buttonText}) => {
 
 	return  (
 		<>
-			<Button id="button" onClick={onShow} label={buttonText ?? title}  className="p-button-secondary p-button-text" />
+			<Button id="button" onClick={onShow} label={buttonText ?? title}
+				className="p-button-secondary p-button-text" />
 			<Dialog header={title} visible={visible} onHide={onHide} dismissableMask={true} >
 				{ content }
 			</Dialog>
