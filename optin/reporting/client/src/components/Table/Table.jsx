134df2b540428f2513ef7c32bbd3189aa3ab90e6
OlegMakarenko <33131259+OlegMakarenko@users.noreply.github.com>
2022-04-18 22:50:05 +0300

    [optin/reporting/client] fix: table body overflow (#70)
    * [optin/reporting/client] fix: table body overflow
    
    * [optin/reporting/client] fix: loose of reactivity of the table component

--- optin/reporting/client/src/components/Table/Table.jsx
+++ optin/reporting/client/src/components/Table/Table.jsx
@@ -1,40 +1,70 @@
 import './Table.scss';
 import { DataTable } from 'primereact/datatable';
 import {ProgressSpinner} from 'primereact/progressspinner';
-import PropTypes from 'prop-types';
 import React from 'react';
-import VisibilitySensor from 'react-visibility-sensor';
 
 const Table = props => {
+	const ref = React.useRef();
 
-	const checkVisible = async visible => {
-		if (visible && !props.allPagesLoaded && props.rows <= props.value.length) 
-			await props.onPage({});
-	};
+	const allowLoadNextPage = !props.allPagesLoaded && props.rows <= props.value.length && !props.loading;
+
+	const infiniteLoaderHandler = React.useCallback(event => {
+		const bottom = Math.trunc(event.target.scrollHeight - event.target.scrollTop) <= event.target.clientHeight;
+				
+		if (bottom && allowLoadNextPage) 
+			props.onPage({});
+	}, [allowLoadNextPage, props.onPage]);
+
+	React.useEffect(() => {
+		if (ref) {
+			const scrollableElement = ref.current.el.lastChild;
+			scrollableElement.addEventListener('scroll', infiniteLoaderHandler);
+
+			return () => {
+				scrollableElement.removeEventListener('scroll', infiniteLoaderHandler);
+			};
+		}
+	}, [ref, infiniteLoaderHandler]);
 
 	const footer = (
 		<React.Fragment>
 			{props.footer}
 			{
-				!props.paginator && !props.allPagesLoaded && props.rows <= props.value.length && 
-				<VisibilitySensor onChange={checkVisible}>
-					<div class="grid flex align-items-center">
-						<div class="col-2"><ProgressSpinner className="w-6"/></div>
-						<div class="col-10">{props.loadingMessage}</div>
-					</div>
-				</VisibilitySensor>
+				props.loading && !props.paginator && 
+					<td colSpan={12}>
+						<div className="grid flex align-items-center w-100">
+							<div className="col-2"><ProgressSpinner className="w-6"/></div>
+							<div className="col-10">{props.loadingMessage}</div>
+						</div>
+					</td>
 			}
 		</React.Fragment>
 	);
 
 	return  (
-		<DataTable lazy={props.lazy} value={props.value} stripedRows showGridlines responsiveLayout="stack" 
-			breakpoint={props.breakpoint} paginator={props.paginator}
+		<DataTable 
+			lazy={props.lazy} 
+			value={props.value} 
+			stripedRows 
+			showGridlines 
+			responsiveLayout="stack" 
+			rowGroupMode="subheader" 
+			groupRowsBy="___all"
+			breakpoint={props.breakpoint} 
+			paginator={props.paginator}
 			paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
-			currentPageReportTemplate="Showing {first}-{last} of {totalRecords}" rows={props.rows} 
-			rowsPerPageOptions={props.rowsPerPageOptions}	onPage={props.onPage} 
-			loading={props.loading} totalRecords={props.totalRecords} first={props.first} header={props.header} 
-			emptyMessage={props.emptyMessage} footer={footer}>
+			currentPageReportTemplate="Showing {first}-{last} of {totalRecords}" 
+			rows={props.rows} 
+			rowsPerPageOptions={props.rowsPerPageOptions}
+			onPage={props.onPage} 
+			loading={props.loading} 
+			totalRecords={props.totalRecords} 
+			first={props.first} 
+			header={props.header} 
+			emptyMessage={props.emptyMessage} 
+			rowGroupFooterTemplate={footer} 
+			ref={ref}
+		>
 			{props.children}
 		</DataTable>
 	);
