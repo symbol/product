a404115de69ad2c6f7a7dbd1b15c6498ba2f7082
Baha <baha@symbol.dev>
2022-04-18 23:55:49 +0300

    [optin/reporting/client] fix: line max-length error fixed

--- optin/reporting/client/src/components/PopUpDialog/PopUpDialog.jsx
+++ optin/reporting/client/src/components/PopUpDialog/PopUpDialog.jsx
@@ -18,7 +18,8 @@ const PopUpDialog = ({title, content, buttonText}) => {
 		<>
 			<Button id="button" onClick={onShow} label={buttonText ?? title}
 				className="p-button-secondary p-button-text" />
-			<Dialog header={title} className={'dialog-'+ title.toLowerCase().replace(/\s/g, '-')} visible={visible} onHide={onHide} dismissableMask={true} >
+			<Dialog header={title} className={'dialog-'+ title.toLowerCase().replace(/\s/g, '-')} visible={visible} 
+				onHide={onHide} dismissableMask={true} >
 				{ content }
 			</Dialog>
 		</>
