113883fcfe86691950d5751df0dae5df3a151bd1
AnthonyLaw <yc-law1015@hotmail.com>
2022-04-19 02:21:44 +0800

    [optin/reporting/client] task: rework on Responsive text, removed Truncate text component

--- optin/reporting/client/src/components/ResponsiveText/ResponsiveText.jsx
+++ optin/reporting/client/src/components/ResponsiveText/ResponsiveText.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './ResponsiveText.scss';
 
-const ResponsiveText = ({value, lastPartLength = 8}) => {
+const ResponsiveText = ({value, lastPartLength = 8, isFixLength = false}) => {
 
 	return (
 		<React.Fragment>
 			<div class="container-responsive-text">
-				<span class="firstPart">{value.substring(0, value.length - lastPartLength)}</span>
-				<span class="lastPart">{value.substring(value.length - lastPartLength)}</span>
+				{
+					isFixLength ?
+					<span class="firstPart">{`${value === '' ? '' : value.substring(0, lastPartLength) + '...'}`}</span>
+					:
+					(
+						<>
+							<span class="firstPart">{value.substring(0, value.length - lastPartLength)}</span>
+							<span class="lastPart">{value.substring(value.length - lastPartLength)}</span>
+						</>
+					)
+				}
 			</div>
 			<div class="container-full-text">
 				{value}
