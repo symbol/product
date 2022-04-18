d143937d0a4598b5885e39aacb2aefa89e2bf074
Baha <baha@symbol.dev>
2022-04-19 02:06:33 +0300

    [optin/reporting/client] fix: linter max-length issue fixed

--- optin/reporting/client/src/pages/Home/DownloadCSVButton.jsx
+++ optin/reporting/client/src/pages/Home/DownloadCSVButton.jsx
@@ -8,9 +8,11 @@ const DownloadCSVButton = ({activePage}) => {
 
 	const downloadAllAsCSV = async () => {
 		if ('completed' === activePage)
-			await Helper.downloadAllAsCSV({apiUrl: `/api/completed/download?tz=${moment.tz.guess().toString()}`, fileName: 'optin-completed.csv', setDownloading});
+		{await Helper.downloadAllAsCSV({apiUrl: `/api/completed/download?tz=${moment.tz.guess().toString()}`, 
+			fileName: 'optin-completed.csv', setDownloading});}
 		else if ('inProgress' === activePage)
-			await Helper.downloadAllAsCSV({apiUrl: `/api/requests/download?tz=${moment.tz.guess().toString()}`, fileName: 'optin-requests.csv', setDownloading});
+		{await Helper.downloadAllAsCSV({apiUrl: `/api/requests/download?tz=${moment.tz.guess().toString()}`, 
+			fileName: 'optin-requests.csv', setDownloading});}
 	};
 
 	return (
