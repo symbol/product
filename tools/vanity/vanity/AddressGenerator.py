a32e8ec4faffda6c8dd71b89d0508ae29eac0567
Jaguar0625 <jaguar0625@127.0.0.1>
2022-04-19 12:11:48 -0400

    [tools/vanity]: make AddressGenerator multithreaded

--- tools/vanity/vanity/AddressGenerator.py
+++ tools/vanity/vanity/AddressGenerator.py
@@ -1,4 +1,6 @@
+import os
 import secrets
+from threading import Lock, Thread
 
 from mnemonic import Mnemonic
 from symbolchain.Bip32 import Bip32
@@ -14,9 +16,20 @@ class AddressGenerator:
 		self.max_wallet_accounts = max_wallet_accounts
 		self.key_pair_acceptor = key_pair_acceptor
 
+		self.lock = Lock()
+
 	def match_all(self, matcher):
 		"""Matches all patterns specified in the provided matcher."""
 
+		threads = [Thread(target=self._match_all_thread, args=(matcher,)) for i in range(0, os.cpu_count())]
+
+		for thread in threads:
+			thread.start()
+
+		for thread in threads:
+			thread.join()
+
+	def _match_all_thread(self, matcher):
 		coin_id = 1 if 'testnet' == self.facade.network.name else self.facade.BIP32_COIN_ID
 		bip32 = Bip32(self.facade.BIP32_CURVE_NAME)
 
@@ -29,10 +42,12 @@ class AddressGenerator:
 			for account_index in range(0, self.max_wallet_accounts):
 				private_key = bip32_root_node.derive_path({44, coin_id, account_index, 0, 0}).private_key
 				key_pair = self.facade.KeyPair(private_key)
-				match_result = matcher.accept(key_pair)
 
-				if match_result[1]:
-					self.key_pair_acceptor(mnemonic, key_pair)
+				with self.lock:
+					match_result = matcher.accept(key_pair)
 
-				if matcher.is_complete:
-					return
+					if match_result[1]:
+						self.key_pair_acceptor(mnemonic, key_pair)
+
+					if matcher.is_complete:
+						return
