a1c78547d6de105332264864532ce7267811b1c9
Baha <baha@symbol.dev>
2022-04-20 01:00:37 +0300

    [optin/reporting] feat: duplicate status added to optin requests API, frontend statuses updated

--- optin/reporting/models/optinRequests.js
+++ optin/reporting/models/optinRequests.js
@@ -9,20 +9,20 @@ const optinRequestDB = {
 			`select optin_transaction_height as optinTransactionHeight, address as nemAddressBytes,
 					hex(optin_transaction_hash) as optinTransactionHashHex, 
 					payout_transaction.height as payoutTransactionHeight, 
-					hex(payout_transaction_hash) as payoutTransactionHash, payout_status as payoutStatus, '' as message,
+					hex(payout_transaction_hash) as payoutTransactionHash, payout_status as payoutStatus, message,
 					nem_block_timestamps.timestamp as optinTimestamp, payout_transaction.timestamp as payoutTimestamp
 				from optin_request
 				LEFT JOIN nem_block_timestamps ON nem_block_timestamps.height = optin_request.optin_transaction_height
 				LEFT JOIN payout_transaction ON payout_transaction.transaction_hash = optin_request.payout_transaction_hash
-				where payout_status in (0, 1, 5) and (address = $1 or $1 is null) and (optin_transaction_hash = $2 or $2 is null)
+				where payout_status in (0, 1, 3, 4) and (address = $1 or $1 is null) and (optin_transaction_hash = $2 or $2 is null)
 					and (payout_status = $3 or $3 is null)
 			union all
 			select	optin_transaction_height as optinTransactionHeight, address as nemAddressBytes,
-					hex(optin_transaction_hash) as optinTransactionHashHex,	'' as payoutTransactionHeight, '' as payoutTransactionHash, 5, 
+					hex(optin_transaction_hash) as optinTransactionHashHex,	'' as payoutTransactionHeight, '' as payoutTransactionHash, 4, 
 					message, nem_block_timestamps.timestamp as optinTimestamp, null as payoutTimestamp
 				from optin_error
 				LEFT JOIN nem_block_timestamps ON nem_block_timestamps.height = optin_error.optin_transaction_height
-				where (address = $1 or $1 is null) and (optin_transaction_hash = $2 or $2 is null) and ($3 = 5 or $3 is null)
+				where (address = $1 or $1 is null) and (optin_transaction_hash = $2 or $2 is null) and ($3 = 4 or $3 is null)
 			order by optin_transaction_height DESC
             limit ${pageSize} offset ${(pageNumber - 1) * pageSize}`,
 			{ bind: [nemAddressBytes, transactionHashBytes, status], type: QueryTypes.SELECT }
