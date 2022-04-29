510d82a948845ebd2d7ebdefc2f498a5eb4e59ce
Baha <baha@symbol.dev>
2022-04-29 18:46:38 +0300

    [optin/reporting/client] task: showing last updated time(Generated at) of the DB got by consuming version API in the home page

--- optin/reporting/client/src/pages/Home/Home.jsx
+++ optin/reporting/client/src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import DownloadCSVButton from './../../components/DownloadCSVButton';
 import Completed from '../Completed';
 import Requests from '../Requests';
 import { TabPanel, TabView } from 'primereact/tabview';
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'react-tabs/style/react-tabs.css';
 import './Home.scss';
 
@@ -20,6 +20,7 @@ const tabConfig = {
 const Home = function () {
 	const [activeIndex, setActiveIndex] = React.useState(0);
 	const [activePage, setActivePage] = React.useState('completed');
+	const [generatedAt, setGeneratedAt] = React.useState('');
 	const tabChangeHandler = e => {
 		setActiveIndex(e.index);
 		if (1 === e.index) 
@@ -42,12 +43,21 @@ const Home = function () {
 		return tabList;
 	};
 
+	useEffect(() => {
+		const fetchGeneratedAt = async () => {
+			const data = await fetch('/api/version')
+				.then(res => res.json());
+			setGeneratedAt(data.lastUpdated);
+		};
+		fetchGeneratedAt();
+	}, []);
 	return (
 		<div className='App'>
 			<div className='mainContainerWrapper'>
 				<div className='mainContainer'>
 					<h2>Opt-in Summary</h2>
 				</div>
+				<div className="generatedAt">Generated at: {generatedAt}</div>
 				<div className='tableContainer'>
 					<TabView activeIndex={activeIndex} onTabChange={tabChangeHandler}>
 						{ renderTabList() }
