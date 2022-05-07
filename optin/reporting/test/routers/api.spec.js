64c08dfc7d9feb26efb57e0548aec83b272de67b
AnthonyLaw <yc-law1015@hotmail.com>
2022-05-07 15:30:06 +0800

    [optin/reporting] task: refactor unit test for api.spec

--- optin/reporting/test/routers/api.spec.js
+++ optin/reporting/test/routers/api.spec.js
@@ -6,29 +6,55 @@ const { expect } = require('chai');
 const { stub, restore } = require('sinon');
 const request = require('supertest');
 
-const runBasicRouteDownloadCsvTests = response => {
-	it('should return content-type in text/csv', () => {
+const runBasicRouteDataResponseTests = ({ path, queryParams }) => {
+	it('should return records with pagination', async () => {
+		// Act:
+		const response = await request(app).get(path).query(queryParams);
+
 		// Assert:
-		expect(response.header['content-type']).to.equal('text/csv; charset=utf-8');
 		expect(response.status).to.equal(200);
+		expect(response.body.data.length).to.be.equal(10);
+		expect(response.body.pagination).to.be.eql({ pageSize: 10, pageNumber: 1, totalRecord: 10 });
+	});
+
+	it('should return error required params not given', async () => {
+		// Act:
+		const response = await request(app).get(path);
+
+		// Assert:
+		expect(response.status).to.equal(422);
 	});
 };
 
-const runBasicRouteDataResponseTests = response => {
-	it('should return content-type in text/csv', () => {
+const runBasicRouteDownloadCsvTests = ({ queryParams, path }) => {
+	it('should return content-type in text/csv', async () => {
+		// Act:
+		const response = await request(app).get(path).query(queryParams);
+
 		// Assert:
-		expect(response.body).to.have.property('data');
-		expect(response.body).to.have.property('pagination');
 		expect(response.status).to.equal(200);
+		expect(response.header['content-type']).to.equal('text/csv; charset=utf-8');
+	});
+
+	it('should return error timezone not given', async () => {
+		// Act:
+		const response = await request(app).get(path);
+
+		// Assert:
+		expect(response.status).to.equal(422);
 	});
 };
 
 describe('API Route', () => {
+	let getPaginationStub = {};
+	let getTotalRecordStub = {};
+	const numberOfRecords = 10;
+
 	describe('completed', () => {
 		beforeEach(() => {
 			// Arrange:
-			const getCompletedPaginationStub = stub(CompletedDB, 'getCompletedPagination');
-			const getTotalRecordStub = stub(CompletedDB, 'getTotalRecord');
+			getPaginationStub = stub(CompletedDB, 'getCompletedPagination');
+			getTotalRecordStub = stub(CompletedDB, 'getTotalRecord');
 
 			const mockNemSources = [{
 				address: '682FAFBA20454869F0278748FD9790CFFCE35E8722647B4039',
@@ -47,33 +73,52 @@ describe('API Route', () => {
 				timestamps: 1615853185
 			}];
 
-			const mockDb = TestUtils.mockCompletedDBRecord(1, mockNemSources, mockSymbolSources);
+			const mockDb = TestUtils.mockCompletedDBRecord(numberOfRecords, mockNemSources, mockSymbolSources);
 
-			getCompletedPaginationStub.returns(Promise.resolve(mockDb));
-			getTotalRecordStub.returns(Promise.resolve(1));
+			getPaginationStub.returns(Promise.resolve(mockDb));
+			getTotalRecordStub.returns(Promise.resolve(numberOfRecords));
 		});
 
 		afterEach(() => {
+			getPaginationStub = {};
+			getTotalRecordStub = {};
 			restore();
 		});
 
 		describe('GET api/completed', () => {
-			it('should return a row data', async () => {
-				// Act:
-				const response = await request(app).get('/api/completed').query({
-					pageSize: 1, pageNumber: 1, optinType: 0, nemAddress: '', symbolAddress: '', transactionHash: ''
-				});
+			// Arrange:
+			const queryParams = {
+				pageSize: 10, pageNumber: 1, optinType: '', nemAddress: '', symbolAddress: '', transactionHash: ''
+			};
 
-				runBasicRouteDataResponseTests(response);
+			it('should return empty records with pagination', async () => {
+				// Arrange:
+				const mockDbEmpty = [];
+
+				getPaginationStub.returns(Promise.resolve(mockDbEmpty));
+				getTotalRecordStub.returns(Promise.resolve(mockDbEmpty.length));
+
+				// Act:
+				const response = await request(app).get('/api/completed').query(queryParams);
+
+				// Assert:
+				expect(response.status).to.equal(200);
+				expect(response.body.data.length).to.be.equal(0);
+				expect(response.body.pagination).to.be.eql({ pageSize: 10, pageNumber: 1, totalRecord: 0 });
+			});
+
+			runBasicRouteDataResponseTests({
+				path: '/api/completed',
+				queryParams
 			});
 		});
 
 		describe('GET api/completed/download', () => {
-			it('should return content-type in text/csv', async () => {
-				// Act:
-				const response = await request(app).get('/api/completed/download');
-
-				runBasicRouteDownloadCsvTests(response);
+			runBasicRouteDownloadCsvTests({
+				path: '/api/completed/download',
+				queryParams: {
+					timezone: 'America/Los_Angeles'
+				}
 			});
 		});
 	});
@@ -81,36 +126,55 @@ describe('API Route', () => {
 	describe('requests', () => {
 		beforeEach(() => {
 			// Arrange:
-			const getOptinRequestPaginationStub = stub(OptinRequestsDB, 'getOptinRequestPagination');
-			const getTotalRecordStub = stub(OptinRequestsDB, 'getTotalRecord');
+			getPaginationStub = stub(OptinRequestsDB, 'getOptinRequestPagination');
+			getTotalRecordStub = stub(OptinRequestsDB, 'getTotalRecord');
 
-			const mockDb = TestUtils.mockInProgressDBRecord(1);
+			const mockDb = TestUtils.mockInProgressDBRecord(10);
 
-			getOptinRequestPaginationStub.returns(Promise.resolve(mockDb));
-			getTotalRecordStub.returns(Promise.resolve(1));
+			getPaginationStub.returns(Promise.resolve(mockDb));
+			getTotalRecordStub.returns(Promise.resolve(mockDb.length));
 		});
 
 		afterEach(() => {
+			getPaginationStub = {};
+			getTotalRecordStub = {};
 			restore();
 		});
 
 		describe('GET api/requests', () => {
-			it('should return a row data', async () => {
-				// Act:
-				const response = await request(app).get('/api/requests').query({
-					pageSize: 1, pageNumber: 1, nemAddressBytes: '', transactionHashBytes: '', status: ''
-				});
+			// Arrange:
+			const queryParams = {
+				pageSize: 10, pageNumber: 1, nemAddressBytes: '', transactionHashBytes: '', status: ''
+			};
 
-				runBasicRouteDataResponseTests(response);
+			it('should return empty records with pagination', async () => {
+				// Arrange:
+				const mockDbEmpty = [];
+
+				getPaginationStub.returns(Promise.resolve(mockDbEmpty));
+				getTotalRecordStub.returns(Promise.resolve(mockDbEmpty.length));
+
+				// Act:
+				const response = await request(app).get('/api/requests').query(queryParams);
+
+				// Assert:
+				expect(response.status).to.equal(200);
+				expect(response.body.data.length).to.be.equal(0);
+				expect(response.body.pagination).to.be.eql({ pageSize: 10, pageNumber: 1, totalRecord: 0 });
+			});
+
+			runBasicRouteDataResponseTests({
+				path: '/api/requests',
+				queryParams
 			});
 		});
 
 		describe('GET api/requests/download', () => {
-			it('should return content-type in text/csv', async () => {
-				// Act:
-				const response = await request(app).get('/api/requests/download');
-
-				runBasicRouteDownloadCsvTests(response);
+			runBasicRouteDownloadCsvTests({
+				path: '/api/requests/download',
+				queryParams: {
+					timezone: 'America/Los_Angeles'
+				}
 			});
 		});
 	});
