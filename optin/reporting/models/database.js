09524758ca253940a9eefef344b100ec44627afa
AnthonyLaw <anthony@symbol.dev>
2022-05-12 14:11:54 +0800

    [optin/reporting] task: patch for loop to foreach

--- optin/reporting/models/database.js
+++ optin/reporting/models/database.js
@@ -5,13 +5,13 @@ const dbList = ['completed', 'in_progress'];
 const database = {};
 
 const refreshDBs = () => {
-	for (let i = 0; i <= dbList.length - 1; i++) {
-		const db = dbList[i];
+	dbList.forEach(db => {
 		database[db] = new Sequelize(db, null, null, {
 			dialect: 'sqlite',
 			storage: `./data/${db}.db`
 		});
-	}
+	});
+
 	return database;
 };
 refreshDBs();
