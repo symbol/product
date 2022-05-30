5e71c85cb4e52bcea8af99938111f650b3e2e200
AnthonyLaw <anthony@symbol.dev>
2022-05-31 04:38:27 +0800

    [optin/reporting] task: replace mock data to object property instead of indexes

--- optin/reporting/test/controllers/completedController.spec.js
+++ optin/reporting/test/controllers/completedController.spec.js
@@ -6,6 +6,111 @@ const { expect } = require('chai');
 const { stub, restore } = require('sinon');
 const { NemFacade, SymbolFacade } = require('symbol-sdk').facade;
 
+const mockCompletedData = {
+	basic: TestUtils.mockCompletedDBRecord(
+		1,
+		[{
+			address: '682FAFBA20454869F0278748FD9790CFFCE35E8722647B4039',
+			balance: 79268194338335,
+			hashes: 'CDB0AF349823F1638E032DCAB15FD33049C383FEEE642CBC67E11020DC30F190',
+			height: 3004351,
+			label: 'Bithumb',
+			timestamps: '1609403596'
+		}],
+		[{
+			address: '688ADC5D31F49F918AC71DCE18E7085944AEAB0AF60F8AD7',
+			balance: 79268194338335,
+			hashes: 'E49B240D76DAE7277089C2BDA66B297A05AE700361ED253ED48435E9AF9B0FE1',
+			height: 1,
+			timestamps: 1615853185
+		}], 1
+	)[0],
+	multi1: TestUtils.mockCompletedDBRecord(
+		1,
+		[{
+			address: '681AE19253937E6A7FD07ACCC4FC2230800D3CF50E734A23DA',
+			balance: 18983656691000,
+			hashes: null,
+			height: null,
+			label: 'NEM Group Trust',
+			timestamps: null
+		},
+		{
+			address: '685575B61A32A6B80677695C7A2DA9183F3D43BF164D0D4CAF',
+			balance: 234150985700000,
+			hashes: null,
+			height: null,
+			label: 'NEM Group Trust',
+			timestamps: null
+		}],
+		[{
+			address: '68EFC33FCE5EF44748D7907A5999571A72FE0FBBEE738195',
+			balance: 51000000000000,
+			hashes: 'F24F32738B32B7D6F798CDDB065AB2974D387C83A8A4B03385C8F2C8DC8B1BF7',
+			height: 1,
+			timestamps: 1615853185
+		},
+		{
+			address: '68FDEF7D690A53EA3132CA9BA0ACF014B766E776E50C22A9',
+			balance: 51000000000000,
+			hashes: '598EF664F3B7F48ECD13486E29269F4D55949505A901D25C8EA619344BC17671',
+			height: 1,
+			timestamps: 1615853185
+		},
+		{
+			address: '6816E83034E76CE5E78110719BEDBAC6AB2C40AEB980EDF0',
+			balance: 51000000000000,
+			hashes: 'B825082702295438CC331A0BCC8AD698D22CCE848661BE55C4389140C36C67EE',
+			height: 1,
+			timestamps: 1615853185
+		}], 0
+	)[0],
+	multi2: TestUtils.mockCompletedDBRecord(
+		1,
+		[{
+			address: '681AE19253937E6A7FD07ACCC4FC2230800D3CF50E734A23DA',
+			balance: 18983656691000,
+			hashes: null,
+			height: null,
+			label: 'NEM Group Trust',
+			timestamps: null
+		},
+		{
+			address: '685575B61A32A6B80677695C7A2DA9183F3D43BF164D0D4CAF',
+			balance: 234150985700000,
+			hashes: null,
+			height: null,
+			label: 'NEM Group Trust',
+			timestamps: null
+		}],
+		[{
+			address: '68EFC33FCE5EF44748D7907A5999571A72FE0FBBEE738195',
+			balance: 51000000000000,
+			hashes: 'F24F32738B32B7D6F798CDDB065AB2974D387C83A8A4B03385C8F2C8DC8B1BF7',
+			height: 1,
+			timestamps: 1615853185
+		}], 0
+	)[0],
+	merge: TestUtils.mockCompletedDBRecord(
+		1,
+		[{
+			address: '6832AC59C5F6B1856B20A31DE1C365833CA594053F492BD940',
+			balance: 749450900000,
+            hashes: 'EB4E08242F8A2CC0A163465F2DD7D9DA48ACB5DB10BAEA324FA8A5233372428E;6E67F6688B34AE387E08D72C9C4D3A96806AAB0C9468B0B39F25823848903567;C27F9084A4441622F307085A191C30AF8708293E8A57342B26CAE20239D54265', //eslint-disable-line
+			height: '3207402;3207400;3207398',
+			label: null,
+			timestamps: '1621687271;1621687173;1621687107'
+		}],
+		[{
+			address: '68EE7FAEEA0EACF510D5294D8BDB3CF6A48AE0971164392E',
+			balance: 749450900000,
+			hashes: '78702FF2EED9121B618B6595AF0FCE455A9D18BD61455FD733BC57C069DD825A',
+			height: 202884,
+			timestamps: 1622028233.132
+		}]
+	)[0]
+};
+
 describe('completed controller', () => {
 	let getCompletedPaginationStub = {};
 	let getTotalRecordStub = {};
@@ -19,7 +124,6 @@ describe('completed controller', () => {
 		symbolAddress: '',
 		transactionHash: ''
 	};
-	const mockCompletedData = TestUtils.buildCompletedDBData();
 
 	beforeEach(() => {
 		getCompletedPaginationStub = stub(CompletedDB, 'getCompletedPagination');
@@ -70,11 +174,11 @@ describe('completed controller', () => {
 			expect(result.symbolBalance).to.be.eql(mockSymbolSources.map(symbolInfo => symbolInfo.balance));
 		};
 
-		const runBasicQueryTests = async overWriteParams => {
+		const runBasicQueryTests = async overwriteParams => {
 			// Arrange:
 			req = TestUtils.mockRequest({
 				...parameter,
-				...overWriteParams
+				...overwriteParams
 			});
 
 			getCompletedPaginationStub.returns(Promise.resolve([]));
@@ -89,22 +193,22 @@ describe('completed controller', () => {
 
 		it('returns basic data', async () => {
 			await runBasicCompletedDataTests({
-				mockNemSources: mockCompletedData[0].nemSource,
-				mockSymbolSources: mockCompletedData[0].symbolDestination
+				mockNemSources: mockCompletedData.basic.nemSource,
+				mockSymbolSources: mockCompletedData.basic.symbolDestination
 			});
 		});
 
 		it('returns data with optin type merge', async () => {
 			await runBasicCompletedDataTests({
-				mockNemSources: mockCompletedData[1].nemSource,
-				mockSymbolSources: mockCompletedData[1].symbolDestination
+				mockNemSources: mockCompletedData.merge.nemSource,
+				mockSymbolSources: mockCompletedData.merge.symbolDestination
 			});
 		});
 
 		it('returns data with optin type multi', async () => {
 			await runBasicCompletedDataTests({
-				mockNemSources: mockCompletedData[2].nemSource,
-				mockSymbolSources: mockCompletedData[2].symbolDestination
+				mockNemSources: mockCompletedData.multi2.nemSource,
+				mockSymbolSources: mockCompletedData.multi2.symbolDestination
 			});
 		});
 
@@ -138,7 +242,7 @@ describe('completed controller', () => {
 			});
 		});
 
-		it('throw error', async () => {
+		it('renders error when database query fails', async () => {
 			// Arrange:
 			getCompletedPaginationStub.throws(new Error('database error'));
 
@@ -156,7 +260,12 @@ describe('completed controller', () => {
 	describe('exportCsv', async () => {
 		beforeEach(async () => {
 			// Arrange:
-			getCompletedPaginationStub.returns(Promise.resolve(mockCompletedData));
+			getCompletedPaginationStub.returns(Promise.resolve([
+				mockCompletedData.basic,
+				mockCompletedData.merge,
+				mockCompletedData.multi1,
+				mockCompletedData.multi2
+			]));
 
 			// Act:
 			await CompletedController.exportCsv(req, res);
