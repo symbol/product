d6048844d534ecd8ab4d9b3cb2d6466084142310
Baha <baha@symbol.dev>
2022-06-10 02:05:31 +0300

    [optin/reporting] fix: merge client coverage report with backends for ci check

--- optin/reporting/scripts/ci/test.sh
+++ optin/reporting/scripts/ci/test.sh
@@ -6,3 +6,7 @@ TEST_MODE=$([ "$1" = "code-coverage" ] && echo "test:jenkins" || echo "test")
 npm run "${TEST_MODE}"
 
 cd ./client && npm run "${TEST_MODE}"
+
+# copy client jest coverage json report to ../.nyc_output to merge client and server coverage
+cd $(git rev-parse --show-toplevel)/optin/reporting
+mkdir -p .nyc_output && cp ./client/coverage/coverage-final.json ./.nyc_output 2>/dev/null || :
