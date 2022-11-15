8b766f368920da92e30f6689e747e666c267c5b6
Jaguar0625 <jaguar0625@127.0.0.1>
2022-11-15 18:46:16 -0400

    [explorer/nodewatch]: add initial NEM and Symbol clients
    clients have min functionality needed to (re)implement search in nodes.py

---
+++ explorer/nodewatch/puller/client/NemClient.py
@@ -0,0 +1,93 @@
+from symbolchain.CryptoTypes import PublicKey
+from symbolchain.nem.Network import Address
+
+from ..model.Endpoint import Endpoint
+from ..model.NodeInfo import NodeInfo
+from .BasicClient import BasicClient
+
+MICROXEM_PER_XEM = 1000000
+
+
+class NemAccountInfo:
+	"""Represents a NEM account."""
+
+	def __init__(self, address):
+		"""Creates a NEM account."""
+
+		self.address = address
+
+		self.vested_balance = 0
+		self.balance = 0
+		self.public_key = None
+		self.importance = 0
+		self.harvested_blocks = 0
+
+		self.remote_status = None
+
+
+class NemClient(BasicClient):
+	"""Async client for connecting to a NEM node."""
+
+	def __init__(self, endpoint, network=None):
+		"""Creates a NEM async client."""
+
+		super().__init__(endpoint)
+		self.network = network
+
+	async def chain_height(self):
+		"""Gets chain height."""
+
+		chain_height = await self.get('chain/height', 'height')
+		return int(chain_height)
+
+	async def node_info(self):
+		"""Gets node information."""
+
+		node_dict = await self.get('node/info')
+		node_info = self._map_to_node_info(node_dict)
+
+		# lookup main account public key
+		node_address = self.network.public_key_to_address(node_info.node_public_key)
+
+		main_account_info = await self.account_info(node_address, forwarded=True)
+		if 'ACTIVE' == main_account_info.remote_status:
+			node_info.main_public_key = main_account_info.public_key
+		else:
+			node_info.main_public_key = node_info.node_public_key
+
+		return node_info
+
+	async def peers(self):
+		"""Gets peer nodes information."""
+
+		nodes_dict = await self.get('node/peer-list/reachable', 'data')
+		return [self._map_to_node_info(node_dict) for node_dict in nodes_dict]
+
+	async def account_info(self, address, forwarded=False):
+		subpath = '/forwarded' if forwarded else ''
+		response_dict = await self.get(f'account/get{subpath}?address={address}')
+
+		account_dict = response_dict['account']
+		account_info = NemAccountInfo(Address(account_dict['address']))
+		account_info.vested_balance = account_dict['vestedBalance'] / MICROXEM_PER_XEM
+		account_info.balance = account_dict['balance'] / MICROXEM_PER_XEM
+		account_info.public_key = PublicKey(account_dict['publicKey']) if account_dict['publicKey'] else None
+		account_info.importance = account_dict['importance']
+		account_info.harvested_blocks = account_dict['harvestedBlocks']
+
+		meta_dict = response_dict['meta']
+		account_info.remote_status = meta_dict['remoteStatus']
+		return account_info
+
+	@staticmethod
+	def _map_to_node_info(node_dict):
+		endpoint_dict = node_dict['endpoint']
+		return NodeInfo(
+			node_dict['metaData']['networkId'],
+			None,  # NEM does not have network generation hash seed
+			None,
+			PublicKey(node_dict['identity']['public-key']),
+			Endpoint(endpoint_dict['protocol'], endpoint_dict['host'], endpoint_dict['port']),
+			node_dict['identity']['name'],
+			node_dict['metaData']['version'],
+			NodeInfo.API_ROLE_FLAG)
---
+++ explorer/nodewatch/tests/client/test_SymbolClient.py
@@ -0,0 +1,223 @@
+import json
+
+import pytest
+from aiohttp import web
+from symbolchain.CryptoTypes import Hash256, PublicKey
+
+from puller.client.SymbolClient import SymbolClient
+from puller.model.Endpoint import Endpoint
+from puller.model.NodeInfo import NodeInfo
+
+# region test data
+
+
+NODE_INFO_1 = {
+	'friendlyName': 'The Wolf Farm owned by Tresto(@TrendStream)',
+	'host': 'wolf.importance.jp',
+	'networkGenerationHashSeed': '57F7DA205008026C776CB6AED843393F04CD458E0AA2D9F1D5F31A402072B2D6',
+	'networkIdentifier': 104,
+	'nodePublicKey': 'A4714451910AF026AF7A3960FF87EDCA0BA4E80CABCB350FA2B69439E1CB0B97',
+	'port': 7900,
+	'publicKey': 'D8F4FE47F1F5B1046748067E52725AEBAA1ED9F3CE45D02054011A39671DD9AA',
+	'roles': 3,
+	'version': 16777988
+}
+
+NODE_INFO_2 = {
+	'friendlyName': 'Allnodes619',
+	'host': 'xym619.allnodes.me',
+	'networkGenerationHashSeed': '57F7DA205008026C776CB6AED843393F04CD458E0AA2D9F1D5F31A402072B2D6',
+	'networkIdentifier': 104,
+	'nodePublicKey': 'A19C14033EB508EF6AD48A3E5D1E7DEDF0B1DCBAD1978CD61797017749A1B999',
+	'port': 7900,
+	'publicKey': 'FB744B408D392E0F99701432E0BC8A0D38BEFDEA8019826CCE91458C6E734ADB',
+	'roles': 3,
+	'version': 16777989
+}
+
+NODE_INFO_3 = {
+	'friendlyName': 'tiger',
+	'host': 'tiger.catapult.ninja',
+	'networkGenerationHashSeed': '57F7DA205008026C776CB6AED843393F04CD458E0AA2D9F1D5F31A402072B2D6',
+	'networkIdentifier': 104,
+	'port': 7900,
+	'publicKey': 'C807BE28855D0C87A8A2C032E51790CCB9158C15CBACB8B222E27DFFFEB3697D',
+	'roles': 5,
+	'version': 16777989
+}
+
+# endregion
+
+
+# region server fixture
+
+@pytest.fixture
+def server(event_loop, aiohttp_client):
+	class MockSymbolServer:
+		def __init__(self):
+			self.urls = []
+
+		async def network_properties(self, request):
+			return await self._process(request, {
+				'chain': {'currencyMosaicId': '0x72C0\'212E\'67A0\'8BCE'}
+			})
+
+		async def chain_info(self, request):
+			return await self._process(request, {
+				'height': 1234,
+				'latestFinalizedBlock': {
+					'finalizationEpoch': 222,
+					'finalizationPoint': 10,
+					'height': 1198
+				}
+			})
+
+		async def node_info(self, request):
+			return await self._process(request, NODE_INFO_1)
+
+		async def node_peers(self, request):
+			return await self._process(request, [NODE_INFO_2, NODE_INFO_3])
+
+		async def _process(self, request, response_body):
+			self.urls.append(str(request.url))
+			return web.Response(body=json.dumps(response_body), headers={'Content-Type': 'application/json'})
+
+	# create a mock server
+	mock_server = MockSymbolServer()
+
+	# create an app using the server
+	app = web.Application()
+	app.router.add_get('/network/properties', mock_server.network_properties)
+	app.router.add_get('/chain/info', mock_server.chain_info)
+	app.router.add_get('/node/info', mock_server.node_info)
+	app.router.add_get('/node/peers', mock_server.node_peers)
+	server = event_loop.run_until_complete(aiohttp_client(app))  # pylint: disable=redefined-outer-name
+
+	server.mock = mock_server
+	return server
+
+# endregion
+
+# pylint: disable=invalid-name
+
+
+# region GET (currency_mosaic_id)
+
+async def test_can_query_currency_mosaic_id(server):  # pylint: disable=redefined-outer-name
+	# Arrange:
+	client = SymbolClient(server.make_url(''))
+
+	# Act:
+	currency_mosaic_id = await client.currency_mosaic_id()
+
+	# Assert:
+	assert [f'{server.make_url("")}/network/properties'] == server.mock.urls
+	assert 0x72C0212E67A08BCE == currency_mosaic_id
+
+
+async def test_can_cache_currency_mosaic_id(server):  # pylint: disable=redefined-outer-name
+	# Arrange:
+	client = SymbolClient(server.make_url(''))
+
+	# Act:
+	currency_mosaic_id = None
+	for _ in range(4):
+		currency_mosaic_id = await client.currency_mosaic_id()
+
+	# Assert: only one network call
+	assert [f'{server.make_url("")}/network/properties'] == server.mock.urls
+	assert 0x72C0212E67A08BCE == currency_mosaic_id
+
+# endregion
+
+
+# region GET (chain_height)
+
+async def test_can_query_chain_height(server):  # pylint: disable=redefined-outer-name
+	# Arrange:
+	client = SymbolClient(server.make_url(''))
+
+	# Act:
+	height = await client.chain_height()
+
+	# Assert:
+	assert [f'{server.make_url("")}/chain/info'] == server.mock.urls
+	assert 1234 == height
+
+# endregion
+
+
+# region GET (finalization_info)
+
+async def test_can_query_finalization_info(server):  # pylint: disable=redefined-outer-name
+	# Arrange:
+	client = SymbolClient(server.make_url(''))
+
+	# Act:
+	finalization_info = await client.finalization_info()
+
+	# Assert:
+	assert [f'{server.make_url("")}/chain/info'] == server.mock.urls
+	assert 222 == finalization_info.epoch
+	assert 10 == finalization_info.point
+	assert 1198 == finalization_info.height
+
+# endregion
+
+
+# region GET (node_info)
+
+async def test_can_query_node_info(server):  # pylint: disable=redefined-outer-name
+	# Arrange:
+	client = SymbolClient(server.make_url(''))
+
+	# Act:
+	node_info = await client.node_info()
+
+	# Assert:
+	assert [f'{server.make_url("")}/node/info'] == server.mock.urls
+	assert NodeInfo(
+		104,
+		Hash256('57F7DA205008026C776CB6AED843393F04CD458E0AA2D9F1D5F31A402072B2D6'),
+		PublicKey('D8F4FE47F1F5B1046748067E52725AEBAA1ED9F3CE45D02054011A39671DD9AA'),
+		PublicKey('A4714451910AF026AF7A3960FF87EDCA0BA4E80CABCB350FA2B69439E1CB0B97'),
+		Endpoint('http', 'wolf.importance.jp', 3000),
+		'The Wolf Farm owned by Tresto(@TrendStream)',
+		'1.0.3.4',
+		3) == node_info
+
+# endregion
+
+
+# region GET (peers)
+
+async def test_can_query_peers(server):  # pylint: disable=redefined-outer-name
+	# Arrange:
+	client = SymbolClient(server.make_url(''))
+
+	# Act:
+	peers = await client.peers()
+
+	# Assert:
+	assert [f'{server.make_url("")}/node/peers'] == server.mock.urls
+	assert 2 == len(peers)
+	assert NodeInfo(
+		104,
+		Hash256('57F7DA205008026C776CB6AED843393F04CD458E0AA2D9F1D5F31A402072B2D6'),
+		PublicKey('FB744B408D392E0F99701432E0BC8A0D38BEFDEA8019826CCE91458C6E734ADB'),
+		PublicKey('A19C14033EB508EF6AD48A3E5D1E7DEDF0B1DCBAD1978CD61797017749A1B999'),
+		Endpoint('http', 'xym619.allnodes.me', 3000),
+		'Allnodes619',
+		'1.0.3.5',
+		3) == peers[0]
+	assert NodeInfo(
+		104,
+		Hash256('57F7DA205008026C776CB6AED843393F04CD458E0AA2D9F1D5F31A402072B2D6'),
+		PublicKey('C807BE28855D0C87A8A2C032E51790CCB9158C15CBACB8B222E27DFFFEB3697D'),
+		None,
+		Endpoint('http', 'tiger.catapult.ninja', 7900),
+		'tiger',
+		'1.0.3.5',
+		5) == peers[1]
+
+# endregion
