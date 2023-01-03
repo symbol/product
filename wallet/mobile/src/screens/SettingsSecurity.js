99322d720b896f2b6120c7259c2cadcc2bc89b2c
Oleg Makarenko <olegomm@gmail.com>
2023-01-03 14:40:30 +0200

    [wallet/mobile] feat: add SettingsSecurity screen

---
+++ wallet/mobile/src/screens/SettingsSecurity.js
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { hasUserSetPinCode, deleteUserPinCode } from '@haskkor/react-native-pincode';
+import { Screen, FormItem, StyledText, LoadingIndicator, Checkbox, MnemonicView } from 'src/components';
+import { borders, colors, spacings } from 'src/styles';
+import {  handleError, useDataManager, usePasscode } from 'src/utils';
+import { SecureStorage } from 'src/storage';
+import { $t } from 'src/localization';
+import { Router } from 'src/Router';
+
+export function SettingsSecurity() {
+    const [isPasscodeEnabled, setIsPasscodeEnabled] = useState(false);
+    const [mnemonic, setMnemonic] = useState('');
+    const [isMnemonicShown, setIsMnemonicShown] = useState(false);
+    const [loadData, isDataLoading] = useDataManager(async () => {
+        const isPasscodeEnabled = await hasUserSetPinCode();
+        const mnemonic = await SecureStorage.getMnemonic()
+        
+        setIsPasscodeEnabled(isPasscodeEnabled);
+        setMnemonic(mnemonic);
+    }, null, handleError);
+    const [togglePasscodeEnabled, isSetPasscodeLoading] = useDataManager(async () => {
+        if (isPasscodeEnabled) {
+            await deleteUserPinCode();
+        }
+
+        loadData();
+        Router.goBack();
+    }, null, handleError);
+    
+
+    const passcodeAction = isPasscodeEnabled ? 'enter' : 'choose';
+    const confirmEnablePasscode = usePasscode(passcodeAction, togglePasscodeEnabled);
+
+    const showMnemonic = () => {
+        setIsMnemonicShown(true);
+        Router.goBack();
+    };
+    const confirmShowMnemonic = usePasscode('enter', showMnemonic);
+
+    useEffect(loadData, []);
+
+    const isLoading = isDataLoading || isSetPasscodeLoading;
+
+    return (
+        <Screen>
+            {isLoading && <LoadingIndicator />}
+            <FormItem>
+                <StyledText type="title">
+                    {$t('settings_security_pin_title')}
+                </StyledText>
+                <StyledText type="body">
+                    {$t('settings_security_pin_body')}
+                </StyledText>
+            </FormItem>
+            <FormItem>
+                <Checkbox
+                    title={$t('settings_security_pin_toggle')} 
+                    value={isPasscodeEnabled} 
+                    onChange={confirmEnablePasscode} 
+                />
+            </FormItem>
+            <FormItem>
+                <StyledText type="title">
+                    {$t('settings_security_mnemonic_title')}
+                </StyledText>
+                <StyledText type="body">
+                    {$t('settings_security_mnemonic_body')}
+                </StyledText>
+            </FormItem>
+            <FormItem>
+                <MnemonicView mnemonic={mnemonic} isShown={isMnemonicShown} onShowPress={confirmShowMnemonic} />
+            </FormItem>
+        </Screen>
+    );
+};
+
+const styles = StyleSheet.create({
+    fill: {
+        flex: 1
+    },
+    item: {
+        borderRadius: borders.borderRadius,
+        backgroundColor: colors.bgCard,
+        padding: spacings.padding,
+    },
+    itemSelected: {
+        backgroundColor: colors.accentLightForm
+    }
+});
