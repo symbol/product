c6da6ceff06871a72afe2b0f99e26795e621306d
Oleg Makarenko <olegomm@gmail.com>
2023-01-09 10:48:27 +0200

    [wallet/mobile] feat: add fetch date and status methods to TransactionService, format date, add address name helper

--- wallet/mobile/src/services/TransactionService.js
+++ wallet/mobile/src/services/TransactionService.js
@@ -1,6 +1,7 @@
-import { isAggregateTransaction, networkIdentifierToNetworkType, transferTransactionToDTO } from 'src/utils';
+import { isAggregateTransaction, makeRequest, networkIdentifierToNetworkType, transferTransactionToDTO } from 'src/utils';
 import _ from 'lodash';
 import { Account, Address, Order, TransactionHttp } from 'symbol-sdk';
+import { Duration, Instant, LocalDateTime, ZoneId } from '@js-joda/core';
 export class TransactionService {
     static async fetchAccountTransactions(account, networkProperties, { 
         pageNumber = 1, 
@@ -50,5 +51,25 @@ export class TransactionService {
         const transactionHttp = new TransactionHttp(networkProperties.nodeUrl);
 
         return transactionHttp.announce(signedTransaction).toPromise();
+    }
+
+    static async fetchDate(height, networkProperties) {
+        const endpoint = `${networkProperties.nodeUrl}/blocks/${height}`;
+        const { block } = await makeRequest(endpoint);
+        const timestamp = parseInt(block.timestamp);
+
+        return LocalDateTime.ofInstant(
+            Instant.ofEpochMilli(timestamp).plusMillis(Duration.ofSeconds(networkProperties.epochAdjustment).toMillis()),
+            ZoneId.SYSTEM,
+        );
+    }
+
+    static async fetchStatus(hash, networkProperties) {
+        const endpoint = `${networkProperties.nodeUrl}/transactionStatus/${hash}`;
+        const { group } = await makeRequest(endpoint);
+
+        return {
+            group
+        };
     }
 };
