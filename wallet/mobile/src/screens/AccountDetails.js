0781005a76752aff3e297cb048abaaf1ce84af29
Oleg Makarenko <olegomm@gmail.com>
2023-01-10 11:11:06 +0200

    [wallet/mobile] feat: add avatar and explorer url to AccountDetails

--- wallet/mobile/src/screens/AccountDetails.js
+++ wallet/mobile/src/screens/AccountDetails.js
@@ -1,20 +1,36 @@
 import React from 'react';
-import { TableView, Screen, FormItem } from 'src/components';
+import { Linking } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { TableView, Screen, FormItem, AccountAvatar, ButtonPlain } from 'src/components';
+import { config } from 'src/config';
+import { $t } from 'src/localization';
 import { connect } from 'src/store';
+import { layout } from 'src/styles';
 
 export const AccountDetails = connect(state => ({
     currentAccount: state.account.current,
+    networkIdentifier: state.network.networkIdentifier
 }))(function AccountDetails(props) {
-    const { currentAccount } = props;
+    const { currentAccount, networkIdentifier } = props;
     const tableData = {};
     
     delete Object.assign(tableData, currentAccount, {['seedIndex']: currentAccount['index'] })['index'];
 
+    const openBlockExplorer = () => Linking.openURL(config.explorerURL[networkIdentifier] + '/accounts/' + currentAccount.address)
+
     return (
         <Screen>
-            <FormItem>
-                <TableView data={tableData} />
-            </FormItem>
+            <ScrollView>
+                <FormItem style={layout.alignCenter}>
+                    <AccountAvatar address={currentAccount.address} size="lg" />
+                </FormItem>
+                <FormItem>
+                    <TableView data={tableData} />
+                </FormItem>
+                <FormItem>
+                    <ButtonPlain title={$t('button_openTransactionInExplorer')} onPress={openBlockExplorer} />
+                </FormItem>
+            </ScrollView>
         </Screen>
     );
 });
