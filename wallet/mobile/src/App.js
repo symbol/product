9c2c31f6a409bb6c793d357a069b490390102052
Oleg Makarenko <olegomm@gmail.com>
2023-02-01 12:14:11 +0200

    [wallet/mobile] task: enable passcode on app start

--- wallet/mobile/src/App.js
+++ wallet/mobile/src/App.js
@@ -19,7 +19,7 @@ const appBackgroundColor = { backgroundColor: colors.bgGray };
 
 const App = () => {
     const [isPasscodeEnabled, setIsPasscodeEnabled] = useState(false);
-    const [isUnlocked, setIsUnlocked] = useState(true);
+    const [isUnlocked, setIsUnlocked] = useState(false);
     const [hasBeenUnlocked, setHasBeenUnlocked] = useState(false);
     const [isWalletExist, setIsWalletExist] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
