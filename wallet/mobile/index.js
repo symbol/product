1cc80d534692ae12a3fd5daf9d256feec319b9dd
Oleg Makarenko <olegomm@gmail.com>
2023-02-14 20:28:57 +0200

    [wallet/mobile] fix: test script

--- wallet/mobile/index.js
+++ wallet/mobile/index.js
@@ -1,10 +1,8 @@
-/**
- * @format
- */
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
 
+// Override Node modules
 if (!process.nextTick) {
     process = require('process');
 }
@@ -13,4 +11,5 @@ if (!global.Buffer) {
     global.Buffer = require('buffer/').Buffer;
 }
 
+// Register main component
 AppRegistry.registerComponent(appName, () => App);
