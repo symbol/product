0c0c19a0545edda02e74e5127c84299c47f11f94
Oleg Makarenko <olegomm@gmail.com>
2023-03-09 21:01:00 +0200

    [wallet/mobile] task: add cancel button to Passcode and QRScanner components

--- wallet/mobile/src/screens/Passcode.js
+++ wallet/mobile/src/screens/Passcode.js
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { DeviceEventEmitter, Image, StyleSheet, View } from 'react-native';
 import PINCode, { hasUserSetPinCode } from '@haskkor/react-native-pincode';
-import { colors, fonts } from 'src/styles';
-import { ButtonPlain, LoadingIndicator } from 'src/components';
+import { colors, fonts, spacings } from 'src/styles';
+import { ButtonClose, LoadingIndicator } from 'src/components';
 import { $t } from 'src/localization';
-import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Router } from 'src/Router';
 
 const translate = (_) => _;
@@ -53,43 +52,41 @@ export const Passcode = (props) => {
     return (
         <View style={styles.root}>
             {!isLoading && (
-                <PINCode
-                    status={type}
-                    titleEnter={translate('Unlock the Wallet')}
-                    titleChoose={translate('Create a PIN')}
-                    subtitleChoose={translate('Enter new PIN-code')}
-                    titleConfirm={translate('Confirm')}
-                    subtitleConfirm={translate('Enter PIN-code again')}
-                    titleAttemptFailed={translate('Attempt Failed')}
-                    subtitleError={translate('Incorrect')}
-                    maxAttempts={maxAttempts}
-                    stylePinCodeTextTitle={fonts.title}
-                    stylePinCodeTextSubtitle={fonts.body}
-                    stylePinCodeColorTitle={colors.textBody}
-                    stylePinCodeColorTitleError={colors.danger}
-                    stylePinCodeColorSubtitle={colors.textForm}
-                    stylePinCodeColorSubtitleError={colors.danger}
-                    stylePinCodeButtonNumber={colors.bgMain}
-                    styleMainContainer={styles.styleMainContainer} //
-                    stylePinCodeTextButtonCircle={fonts.button}
-                    numbersButtonOverlayColor={colors.secondary}
-                    colorCircleButtons={colors.primary}
-                    colorPassword={colors.primary}
-                    buttonDeleteText="Delete"
-                    stylePinCodeDeleteButtonText={fonts.button}
-                    stylePinCodeColumnDeleteButton={styles.buttonDelete}
-                    styleLockScreenColorIcon={styles.buttonDelete}
-                    customBackSpaceIcon={() => (
-                        <Image source={require('src/assets/images/icon-backspace.png')} style={styles.button} />
-                    )}
-                    bottomLeftComponent={() => !hideCancelButton && (
-                        <TouchableOpacity onPress={onCancel} style={styles.buttonCloseContainer} >
-                            <Image source={require('src/assets/images/icon-primary-close.png')} style={styles.button} />
-                        </TouchableOpacity>
-                    )}
-                    finishProcess={onFinish}
-                    touchIDDisabled={type === 'choose'}
-                />
+                <>
+                    <PINCode
+                        status={type}
+                        titleEnter={translate('Unlock the Wallet')}
+                        titleChoose={translate('Create a PIN')}
+                        subtitleChoose={translate('Enter new PIN-code')}
+                        titleConfirm={translate('Confirm')}
+                        subtitleConfirm={translate('Enter PIN-code again')}
+                        titleAttemptFailed={translate('Attempt Failed')}
+                        subtitleError={translate('Incorrect')}
+                        maxAttempts={maxAttempts}
+                        stylePinCodeTextTitle={fonts.title}
+                        stylePinCodeTextSubtitle={fonts.body}
+                        stylePinCodeColorTitle={colors.textBody}
+                        stylePinCodeColorTitleError={colors.danger}
+                        stylePinCodeColorSubtitle={colors.textForm}
+                        stylePinCodeColorSubtitleError={colors.danger}
+                        stylePinCodeButtonNumber={colors.bgMain}
+                        styleMainContainer={styles.styleMainContainer} //
+                        stylePinCodeTextButtonCircle={fonts.button}
+                        numbersButtonOverlayColor={colors.secondary}
+                        colorCircleButtons={colors.primary}
+                        colorPassword={colors.primary}
+                        buttonDeleteText="Delete"
+                        stylePinCodeDeleteButtonText={fonts.button}
+                        stylePinCodeColumnDeleteButton={styles.buttonDelete}
+                        styleLockScreenColorIcon={styles.buttonDelete}
+                        customBackSpaceIcon={() => (
+                            <Image source={require('src/assets/images/icon-backspace.png')} style={styles.buttonBackspace} />
+                        )}
+                        finishProcess={onFinish}
+                        touchIDDisabled={type === 'choose'}
+                    />
+                    {!hideCancelButton && <ButtonClose type="cancel" style={styles.buttonCancel} onPress={onCancel} />}
+                </>
             )}
             {isLoading && <LoadingIndicator fill />}
         </View>
@@ -98,17 +95,19 @@ export const Passcode = (props) => {
 
 const styles = StyleSheet.create({
     root: {
+        position: 'relative',
         width: '100%',
         height: '100%',
     },
     styleMainContainer: {
         backgroundColor: colors.bgMain,
     },
-    buttonCloseContainer: {
-        justifyContent:'center',
-        alignItems: 'center'
+    buttonCancel: {
+        position: 'absolute',
+        right: spacings.margin,
+        top: spacings.margin
     },
-    button: {
+    buttonBackspace: {
         height: '100%',
         width: 30,
         margin: 'auto',
