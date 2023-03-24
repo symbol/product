1d384e531605a306db29a08a4cc5861813c9d8f9
wayonb <wayon@symbol.dev>
2023-03-24 11:28:11 -0400

    [explorer/nodewatch] fix: disable invalid name for the rest of the tests

--- explorer/nodewatch/tests/test_NetworkRepository.py
+++ explorer/nodewatch/tests/test_NetworkRepository.py
@@ -1,3 +1,6 @@
+# pylint: disable=invalid-name
+
+
 import unittest
 
 from symbolchain.CryptoTypes import PublicKey
--- explorer/nodewatch/tests/test_chart_utils.py
+++ explorer/nodewatch/tests/test_chart_utils.py
@@ -1,3 +1,6 @@
+# pylint: disable=invalid-name
+
+
 import unittest
 from collections import namedtuple
 
