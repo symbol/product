91baff5d3d12058589bd420f6ccdc9daea9215d5
Jaguar0625 <jaguar0625@127.0.0.1>
2023-05-16 14:17:31 -0400

    [explorer/nodewatch]: remove clients after promotion to connectors/python

--- explorer/nodewatch/puller/bindings/openssl.py
+++
@@ -1,7 +0,0 @@
-from cryptography.hazmat.bindings.openssl.binding import Binding as OpensslBinding
-
-from ._openssl_symbol import ffi, lib  # pylint: disable=no-name-in-module, unused-import
-
-# initialize openssl
-if not OpensslBinding.lib:
-	OpensslBinding()
--- explorer/nodewatch/puller/model/Exceptions.py
+++
@@ -1,6 +0,0 @@
-class NodeException(Exception):
-	"""Exception raised when there is an error communicating with a node."""
-
-
-class CorruptDataException(NodeException):
-	"""Exception raised when corrupt data is received from a node."""
