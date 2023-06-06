e7bae1ee49a6b9ba9b0fde59c09f7a912d69b389
AnthonyLaw <anthony@symbol.dev>
2023-06-07 07:32:22 +0800

    [explorer/puller] task: refactor moving to facade

--- explorer/puller/facade/NemPullerFacade.py
+++ explorer/puller/facade/NemPullerFacade.py
@@ -1,7 +1,11 @@
 import configparser
 
+from symbolchain.facade.NemFacade import NemFacade
+from zenlog import log
+
 from client.NemClient import NemClient
 from db.NemDatabase import NemDatabase
+from model.Block import Block
 
 
 class NemPullerFacade:
@@ -17,6 +21,7 @@ class NemPullerFacade:
 
 		self.nem_db = NemDatabase(db_config)
 		self._client = NemClient(node_url)
+		self.nem_facade = None
 
 	def database(self):
 		"""Gets database object."""
@@ -27,3 +32,49 @@ class NemPullerFacade:
 		"""Gets client object."""
 
 		return self._client
+
+	async def setup_facade(self):
+		"""Setup the facade."""
+
+		network = await self.client().node_network()
+		self.nem_facade = NemFacade(network.name)
+
+		log.info(f'Network: {network}')
+
+	async def sync_nemesis_block(self):
+		"""Sync the Nemesis block."""
+
+		block = await self.client().get_block(1)
+
+		nemesis_block = {
+			'difficulty': 0,
+			'block': block,
+			'txes': [{
+				'tx': tx,
+				'hash': f'#NemesisBlock# {index}'
+			} for index, tx in enumerate(block['transactions'], start=1)
+			],
+			'hash': '#'
+		}
+
+		save_block = Block.from_nem_block_data(nemesis_block, self.nem_facade)
+		self.database().insert_block(save_block)
+
+		log.info(f'added block from height 1')
+
+	async def sync_blocks(self, db_height, chain_height):
+		"""Sync network blocks."""
+
+		# sync network blocks in database
+		while chain_height > db_height:
+
+			blocks = await self.client().get_blocks_after(db_height)
+
+			for block in blocks['data']:
+				save_block = Block.from_nem_block_data(block, self.nem_facade)
+				self.database().insert_block(save_block)
+
+			db_height = blocks['data'][-1]['block']['height']
+			first_block_height = blocks['data'][0]['block']['height']
+
+			log.info(f'added block from height {first_block_height} - {db_height}')
