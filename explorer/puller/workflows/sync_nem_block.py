e7fb6496d7080dbc191f2e9ca8cc811d6467ce66
AnthonyLaw <anthony@symbol.dev>
2023-06-07 07:32:22 +0800

    [explorer/puller] task: refactor moving to facade

--- explorer/puller/workflows/sync_nem_block.py
+++ explorer/puller/workflows/sync_nem_block.py
@@ -1,12 +1,9 @@
 import argparse
 from asyncio import run
 
-from symbolchain.CryptoTypes import PublicKey
-from symbolchain.facade.NemFacade import NemFacade
 from zenlog import log
 
 from facade.NemPullerFacade import NemPullerFacade
-from model.Block import Block
 
 
 def parse_args():
@@ -18,69 +15,29 @@ def parse_args():
 	return parser.parse_args()
 
 
-async def sync_nemesis_block(nem_client, databases, nem_facade):
-	"""Sync the Nemesis block."""
-
-	block = await nem_client.get_block(1)
-
-	save_block = Block(
-		block['height'],
-		Block.convert_timestamp_to_datetime(nem_facade, block['timeStamp']),
-		0,
-		len(block['transactions']),
-		0,
-		'#',
-		str(nem_facade.network.public_key_to_address(PublicKey(block['signer'])))
-	)
-
-	databases.insert_block(save_block)
-
-
-async def sync_blocks(nem_client, databases, db_height, chain_height, nem_facade):
-	"""Sync network blocks in the database."""
-
-	# sync network blocks in database
-	while chain_height > db_height:
-
-		blocks = await nem_client.get_blocks_after(db_height)
-
-		for block in blocks['data']:
-			save_block = Block.from_nem_block_data(block, nem_facade)
-			databases.insert_block(save_block)
-
-		db_height = blocks['data'][-1]['block']['height']
-
-		log.info(f'added block from height {blocks["data"][0]["block"]["height"]} - {blocks["data"][-1]["block"]["height"]}')
-
-
 async def main():
 	args = parse_args()
 
 	facade = NemPullerFacade(args.nem_node, args.db_config)
 
-	nem_client = facade.client()
-	nem_databases = facade.database
-
-	nem_network = await nem_client.node_network()
-	nem_facade = NemFacade(nem_network.name)
+	await facade.setup_facade()
 
 	log.info(f'Node URL: {args.nem_node}')
-	log.info(f'Syncing with network {nem_network}')
 
-	with nem_databases() as databases:
+	with facade.database() as databases:
 		databases.create_tables()
 
 		db_height = databases.get_current_height()
 		log.info(f'current database height: {db_height}')
-		chain_height = await nem_client.height()
+		chain_height = await facade.client().height()
 
-		# save Nemenesis Block
+		# save Nemesis Block
 		if db_height == 0:
-			await sync_nemesis_block(nem_client, databases, nem_facade)
+			await facade.sync_nemesis_block()
 			db_height = 1
 
 		# sync network blocks in database
-		await sync_blocks(nem_client, databases, db_height, chain_height, nem_facade)
+		await facade.sync_blocks(db_height, chain_height)
 
 		log.info('Database is up to date')
 
