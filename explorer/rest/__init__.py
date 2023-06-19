34dac319d3a1dcd51514568d45bb296390230826
AnthonyLaw <anthony@symbol.dev>
2023-06-20 04:08:45 +0800

    [explorer/rest]: setup flask and route

--- explorer/rest/__init__.py
+++ explorer/rest/__init__.py
@@ -1,16 +1,47 @@
+import configparser
+
 from pathlib import Path
 
-from flask import Flask, jsonify
+from flask import abort, Flask, jsonify, request
 from zenlog import log
 
+from rest.facade.NemRestFacade import NemRestFacade
+
 def create_app():
-    app = Flask(__name__)
+	app = Flask(__name__)
 
-    app.config.from_envvar('EXPLORER_REST_SETTINGS')
+	setup_app_config(app)
+	setup_error_handlers(app)
 
-    db_path = Path(app.config.get('DATABASE_PATH'))
+	nem_api_facade = setup_nem_facade(app)
+	setup_nem_routes(app, nem_api_facade)
 
-    log.info(f'loading database config from {db_path}')
+	return app
 
+def setup_app_config(app):
+	app.config.from_envvar('EXPLORER_REST_SETTINGS')
+	db_path = Path(app.config.get('DATABASE_PATH'))
+	log.info(f'loading database config from {db_path}')
 
-    return app
+def setup_nem_facade(app):
+	config = configparser.ConfigParser()
+	db_path = Path(app.config.get('DATABASE_PATH'))
+	config.read(db_path)
+	return NemRestFacade(config['nem_db'])
+
+def setup_nem_routes(app, nem_api_facade):
+	@app.route('/api/nem/block/<height>')
+	def api_get_nem_block_by_height(height):
+		result = nem_api_facade.get_block(height)
+		if not result:
+			abort(404)
+		return jsonify(result)
+
+def setup_error_handlers(app):
+	@app.errorhandler(404)
+	def not_found(_):
+		response = {
+			'status': 404,
+			'message': 'Resource not found'
+		}
+		return jsonify(response), 404
