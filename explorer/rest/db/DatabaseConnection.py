d00599a912df5831d839ca0d59973e4b87faab39
AnthonyLaw <anthony@symbol.dev>
2023-06-20 03:23:55 +0800

    [explorer/rest]: added db connection pool, improve performance

--- explorer/rest/db/DatabaseConnection.py
+++ explorer/rest/db/DatabaseConnection.py
@@ -1,15 +1,20 @@
-import psycopg2
+from psycopg2 import pool
 
 
 class DatabaseConnection:
-	def __init__(self, db_config):
+	def __init__(self, db_config, min_connections=1, max_connections=10):
 		self.db_config = db_config
+		self.min_connections = min_connections
+		self.max_connections = max_connections
+		self.pool = None
 		self.connection = None
 
-	def __enter__(self):
-		"""Connects to databases."""
+	def connect(self):
+		"""Create connection pool."""
 
-		self.connection = psycopg2.connect(
+		self.pool = pool.SimpleConnectionPool(
+			self.min_connections,
+			self.max_connections,
 			database=self.db_config['database'],
 			user=self.db_config['user'],
 			password=self.db_config['password'],
@@ -17,9 +22,16 @@ class DatabaseConnection:
 			port=self.db_config['port']
 		)
 
+	def __enter__(self):
+		"""Connects to databases."""
+
+		self.connect()
+		self.connection = self.pool.getconn()
+
 		return self
 
 	def __exit__(self, exc_type, exc_value, traceback):
 		"""Disconnects from databases."""
 
-		self.connection.close()
+		self.pool.putconn(self.connection)
+		self.connection = None
