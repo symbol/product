e0f7423ce6608eddc10ded6996113401d224b2c4
AnthonyLaw <anthony@symbol.dev>
2023-06-27 05:52:37 +0800

    [explorer/puller] task: add nem client

---
+++ explorer/puller/tests/test_data.py
@@ -0,0 +1,41 @@
+CHAIN_BLOCK_1 = {
+	'difficulty': 100000000000000,
+	'txes': [],
+	'block': {
+		'timeStamp': 73976,
+		'signature': (
+			'fdf6a9830e9320af79123f467fcb03d6beab735575ff50eab363d812c5581436'
+			'2ad7be0503db2ee70e60ac3408d83cdbcbd941067a6df703e0c21c7bf389f105'
+		),
+		'prevBlockHash': {
+			'data': '438cf6375dab5a0d32f9b7bf151d4539e00a590f7c022d5572c7d41815a24be4'
+		},
+		'type': 1,
+		'transactions': [],
+		'version': 1744830465,
+		'signer': 'f9bd190dd0c364261f5c8a74870cc7f7374e631352293c62ecc437657e5de2cd',
+		'height': 2
+	},
+	'hash': '1dd9d4d7b6af603d29c082f9aa4e123f07d18154ddbcd7ddc6702491b854c5e4'
+}
+
+CHAIN_BLOCK_2 = {
+	'difficulty': 90250000000000,
+	'txes': [],
+	'block': {
+		'timeStamp': 73976,
+		'signature': (
+			'919ae66a34119b49812b335827b357f86884ab08b628029fd6e8db3572faeb4f'
+			'323a7bf9488c76ef8faa5b513036bbcce2d949ba3e41086d95a54c0007403c0b'
+		),
+		'prevBlockHash': {
+			'data': '1dd9d4d7b6af603d29c082f9aa4e123f07d18154ddbcd7ddc6702491b854c5e4'
+		},
+		'type': 1,
+		'transactions': [],
+		'version': 1744830465,
+		'signer': '45c1553fb1be7f25b6f79278b9ede1129bb9163f3b85883ea90f1c66f497e68b',
+		'height': 3
+	},
+	'hash': '9708256e8a8dfb76eed41dcfa2e47f4af520b7b3286afb7f60dca02851f8a53e'
+}
