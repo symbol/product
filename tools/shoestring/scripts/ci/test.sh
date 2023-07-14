684429471d8e6ef85ff939a39f639efc08961ed5
gimre <gimre@symbol.dev>
2023-07-14 17:25:59 +0200

    [tools/shoestring]: bump dependency to proper 0.0.4

--- tools/shoestring/scripts/ci/test.sh
+++ tools/shoestring/scripts/ci/test.sh
@@ -2,10 +2,5 @@
 
 set -ex
 
-pushd .
-cd ../../lightapi/python
-./scripts/ci/setup_lint.sh
-popd
-
 TEST_RUNNER=$([ "$1" = "code-coverage" ] && echo "coverage run --append" || echo "python3")
-PYTHONPATH=.:../../lightapi/python ${TEST_RUNNER} -m pytest --asyncio-mode=auto -v
+PYTHONPATH=. ${TEST_RUNNER} -m pytest --asyncio-mode=auto -v
