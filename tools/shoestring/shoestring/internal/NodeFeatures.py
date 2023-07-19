74796631161db0c5027efa9588c2ca5581b9b22e
wayonb <wayon@symbol.dev>
2023-07-19 01:05:17 -0400

    [tools/shoestring]: Python 3.11 IntFlag update problem: In python 3.11, IntFlag __str__ change to return enum value instead of the enum name solution: to support python 3.11, override the default __str__ to always return enum names for all python versions

--- tools/shoestring/shoestring/internal/NodeFeatures.py
+++ tools/shoestring/shoestring/internal/NodeFeatures.py
@@ -1,4 +1,4 @@
-from enum import IntFlag
+from enum import Enum, IntFlag
 from functools import reduce
 
 
@@ -23,3 +23,31 @@ class NodeFeatures(IntFlag):
 
 		values = [NodeFeatures[name.strip()] for name in node_features_str.split(',') if name]
 		return reduce(lambda rhs, lhs: rhs | lhs, values, NodeFeatures.PEER)
+
+	@staticmethod
+	def _iter_bits_lsb(num):
+		# num must be a positive integer
+		original = num
+		if isinstance(num, Enum):
+			num = num.value
+
+		if num < 0:
+			raise ValueError(f'{original} is not a positive integer')
+
+		while num:
+			value = num & (~num + 1)
+			yield value
+			num ^= value
+
+	def get_flag_values(self, value):
+		return list(self._iter_bits_lsb(value))
+
+	def get_flags(self, value):
+		flag_values = self.get_flag_values(value)
+		return [NodeFeatures(flag_value) for flag_value in flag_values]
+
+	def __str__(self):
+		if self.name is None:
+			return f'{self.__class__.__name__}.{"|".join([str(m.name) for m in self.get_flags(self.value)])}'
+
+		return f'{self.__class__.__name__}.{self.name}'
