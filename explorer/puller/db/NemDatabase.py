9606fbefe3e07ef54eb5e1963c64b6570f813ab6
AnthonyLaw <anthony@symbol.dev>
2023-07-24 04:28:35 +0800

    [explorer/puller] task: add new DB table transaction and transfer transactions , model for insert db

--- explorer/puller/db/NemDatabase.py
+++ explorer/puller/db/NemDatabase.py
@@ -1,5 +1,7 @@
 from binascii import unhexlify
 
+from symbolchain.nem.Network import Address
+
 from db.DatabaseConnection import DatabaseConnection
 
 
@@ -25,6 +27,40 @@ class NemDatabase(DatabaseConnection):
 			'''
 		)
 
+		# Create transactions table
+		cursor.execute(
+			'''
+			CREATE TABLE IF NOT EXISTS transactions (
+				id serial PRIMARY KEY,
+				transaction_hash bytea NOT NULL,
+				height bigint NOT NULL,
+				sender bytea NOT NULL,
+				fee bigint DEFAULT 0,
+				timestamp timestamp NOT NULL,
+				deadline timestamp NOT NULL,
+				signature bytea NOT NULL,
+				transaction_type int NOT NULL
+			)
+			'''
+		)
+
+		# Create transfer transactions table
+		cursor.execute(
+			'''
+			CREATE TABLE IF NOT EXISTS transfer_transactions (
+				id serial PRIMARY KEY,
+				transaction_id serial NOT NULL,
+				amount bigint NOT NULL,
+				mosaics json,
+				recipient bytea NOT NULL,
+				message json,
+				is_apostille boolean DEFAULT false,
+				FOREIGN KEY (transaction_id) REFERENCES transactions(id)
+				ON DELETE CASCADE
+			)
+			'''
+		)
+
 		self.connection.commit()
 
 	def insert_block(self, cursor, block):  # pylint: disable=no-self-use
@@ -43,6 +79,47 @@ class NemDatabase(DatabaseConnection):
 				unhexlify(block.block_hash),
 				unhexlify(block.signer),
 				unhexlify(block.signature)
+			)
+		)
+
+	def insert_transaction(self, cursor, transaction):
+		"""Adds transactions into transactions table"""
+
+		cursor.execute(
+			'''
+			INSERT INTO transactions (transaction_hash, height, sender, fee, timestamp, deadline, signature, transaction_type)
+			VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+			RETURNING id
+			''',
+			(
+				unhexlify(transaction.transaction_hash),
+				transaction.height,
+				unhexlify(transaction.sender),
+				transaction.fee,
+				transaction.timestamp,
+				transaction.deadline,
+				unhexlify(transaction.signature),
+				transaction.transaction_type
+			)
+		)
+
+		return cursor.fetchone()[0]
+
+	def insert_transfer_transactions(self, cursor, transaction_id, transfer_transactions):
+		"""Adds transfer into transfer_transactions table"""
+
+		cursor.execute(
+			'''
+			INSERT INTO transfer_transactions (transaction_id, amount, mosaics, recipient, message, is_apostille)
+			VALUES (%s, %s, %s, %s, %s, %s)
+			''',
+			(
+				transaction_id,
+				transfer_transactions.amount,
+				transfer_transactions.mosaics,
+				Address(transfer_transactions.recipient).bytes,
+				transfer_transactions.message,
+				transfer_transactions.is_apostille
 			)
 		)
 
---
+++ explorer/puller/model/Transaction.py
@@ -0,0 +1,48 @@
+class Transaction:
+	def __init__(self, transaction_hash, height, sender, fee, timestamp, deadline, signature, transaction_type):
+		"""Create Block model."""
+
+		# pylint: disable=too-many-arguments
+
+		self.transaction_hash = transaction_hash
+		self.height = height
+		self.sender = sender
+		self.fee = fee
+		self.timestamp = timestamp
+		self.deadline = deadline
+		self.signature = signature
+		self.transaction_type = transaction_type
+
+	def __eq__(self, other):
+		return isinstance(other, Transaction) and all([
+			self.transaction_hash == other.transaction_hash,
+			self.height == other.height,
+			self.sender == other.sender,
+			self.fee == other.fee,
+			self.timestamp == other.timestamp,
+			self.deadline == other.deadline,
+			self.signature == other.signature,
+			self.transaction_type == other.transaction_type
+		])
+
+
+class TransferTransaction:
+	def __init__(self, amount, recipient, mosaics, message, is_apostille):
+		"""Create TransferTransaction model."""
+
+		# pylint: disable=too-many-arguments
+
+		self.amount = amount
+		self.recipient = recipient
+		self.mosaics = mosaics
+		self.message = message
+		self.is_apostille = is_apostille
+
+	def __eq__(self, other):
+		return isinstance(other, TransferTransaction) and all([
+			self.amount == other.amount,
+			self.recipient == other.recipient,
+			self.mosaics == other.mosaics,
+			self.message == other.message,
+			self.is_apostille == other.is_apostille
+		])
