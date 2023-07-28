2fd1bfee8512da488e14fe39c8c12727453b5594
Oleg Makarenko <olegomm@gmail.com>
2023-07-28 19:57:10 +0300

    [explorer/frontend] feat: add ButtonCSV and export feture

---
+++ explorer/frontend/components/ButtonCSV.jsx
@@ -0,0 +1,17 @@
+import styles from '@/styles/components/ButtonCSV.module.scss';
+import { useTranslation } from 'next-i18next';
+import { CSVLink } from 'react-csv';
+
+const ButtonCSV = ({ className, data, fileName }) => {
+	const { t } = useTranslation();
+	const text = t('button_csv');
+	const defaultFileName = 'explorer-export';
+
+	return <CSVLink
+		className={`${styles.buttonCSV} ${className}`}
+		filename={`${fileName || defaultFileName}.csv`}
+		data={data}
+	>{text}</CSVLink>;
+};
+
+export default ButtonCSV;
