c0efd6b678a4571a1cd0bc40df919b17e6b251cb
AnthonyLaw <anthony@symbol.dev>
2023-08-12 04:04:39 +0800

    [explorer/rest]: add docs in my database connection class

--- explorer/rest/db/DatabaseConnection.py
+++ explorer/rest/db/DatabaseConnection.py
@@ -5,14 +5,34 @@ class DatabaseConnectionPool:
 	"""Database connection pool class."""
 
 	def __init__(self, db_config, min_connections=1, max_connections=10):
-		"""Initialize database connection parameters."""
+		"""
+		Initialize the database connection pool with given configurations.
+
+		Parameters:
+		----------
+		db_config : dict
+			Dictionary containing connection details.
+		min_connections : int, optional
+			Minimum number of connections in the pool. Default is 1.
+		max_connections : int, optional
+			Maximum number of connections in the pool. Default is 10.
+		"""
+
 		self.db_config = db_config
 		self.min_connections = min_connections
 		self.max_connections = max_connections
 		self._pool = self._create_pool()
 
 	def _create_pool(self):
-		"""Create a connection pool."""
+		"""
+		Create a connection pool.
+
+		Returns:
+		-------
+		SimpleConnectionPool
+			A connection pool instance.
+		"""
+
 		return pool.SimpleConnectionPool(
 			self.min_connections,
 			self.max_connections,
@@ -24,23 +44,61 @@ class DatabaseConnectionPool:
 		)
 
 	def connection(self):
-		"""Provide a managed connection."""
+		"""
+		Provides a managed database connection instance from the pool.
+
+		Returns:
+		-------
+		PooledConnection
+			A managed database connection instance.
+		"""
+
 		return PooledConnection(self._pool)
 
 
 class PooledConnection:
-	"""Managed database connection from the connection pool."""
+	"""
+	Represents a managed database connection from the connection pool.
+	Intended for use within a context manager (`with` statement).
+
+	Attributes:
+	----------
+	_pool : object
+		Reference to the DatabaseConnectionPool from which this connection is derived.
+	connection : object
+		The actual active database connection. Initially set to None.
+	"""
 
 	def __init__(self, _pool):
+		"""
+		Initialize with a reference to a connection pool.
+
+		Parameters:
+		----------
+		_pool : object
+			Reference to the DatabaseConnectionPool instance.
+		"""
+
 		self._pool = _pool
 		self.connection = None
 
 	def __enter__(self):
-		"""Acquire a connection from the pool."""
+		"""
+		Acquire a database connection from the pool upon entering the context of a `with` statement.
+
+		Returns:
+		-------
+		connection : object
+			An active database connection from the pool.
+		"""
+
 		self.connection = self._pool.getconn()
 		return self.connection
 
 	def __exit__(self, exc_type, exc_value, traceback):
-		"""Return the connection to the pool."""
+		"""
+		Ensure the connection is returned to the pool upon exiting the context of a `with` statement.
+		"""
+
 		if self.connection:
 			self._pool.putconn(self.connection)
