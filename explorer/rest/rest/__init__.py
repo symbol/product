758925954ba42fcecc090c4abb8b61e4c1640012
AnthonyLaw <anthony@symbol.dev>
2023-09-18 22:01:35 +0800

    [explorer/rest]: apply CORS

--- explorer/rest/rest/__init__.py
+++ explorer/rest/rest/__init__.py
@@ -3,6 +3,7 @@ from collections import namedtuple
 from pathlib import Path
 
 from flask import Flask, abort, jsonify, request
+from flask_cors import CORS
 from zenlog import log
 
 from rest.facade.NemRestFacade import NemRestFacade
@@ -12,6 +13,8 @@ DatabaseConfig = namedtuple('DatabaseConfig', ['database', 'user', 'password', '
 
 def create_app():
 	app = Flask(__name__)
+
+	CORS(app)
 
 	setup_error_handlers(app)
 
