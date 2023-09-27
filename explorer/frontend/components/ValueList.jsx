94908009e620e8bfbcab864dc3131b2f1d079540
Oleg Makarenko <olegomm@gmail.com>
2023-09-27 13:17:18 +0300

    [explorer/frontend] feat: add ValueList component

---
+++ explorer/frontend/components/ValueList.jsx
@@ -0,0 +1,39 @@
+import Modal from './Modal';
+import { arrayToText, useToggle } from '@/utils';
+import { useTranslation } from 'next-i18next';
+import ButtonClose from './ButtonClose';
+import styles from '@/styles/components/ValueList.module.scss';
+
+const ValueList = ({ className, data, direction, max, title }) => {
+	const { t } = useTranslation();
+	const [isMenuOpen, toggleMenu] = useToggle(false);
+	const isColumn = direction === 'col';
+	const directionStyle = isColumn ? styles.listColumn : styles.listRow;
+	const slicedList = data.slice(0, max);
+	const remainingItemCount = data.length - slicedList.length;
+	const isButtonMoreShown = remainingItemCount > 0;
+
+	const renderColumn = (data) =>
+		data.map((item, index) => <div key={index}>{item}</div>);
+
+	return (
+		<div className={`${directionStyle} ${className}`}>
+			{isColumn && renderColumn(slicedList)}
+			{!isColumn && arrayToText(slicedList)}
+			{isButtonMoreShown && (
+				<div className={styles.buttonMore} onClick={toggleMenu}>
+					{t('button_more', { count: remainingItemCount })}
+				</div>
+			)}
+			<Modal className={styles.modal} isVisible={isMenuOpen} onClose={toggleMenu}>
+				<h4>{title}</h4>
+				<div className={styles.modalContent}>
+					{renderColumn(data)}
+				</div>
+				<ButtonClose className={styles.buttonClose} onClick={toggleMenu} />
+			</Modal>
+		</div>
+	);
+};
+
+export default ValueList;
