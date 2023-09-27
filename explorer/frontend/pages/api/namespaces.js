b2033fbe8d89a14e04be231e8dd6d95b1af80b55
Oleg Makarenko <olegomm@gmail.com>
2023-09-27 13:32:28 +0300

    [explorer/frontend] feat: add Namespace list page

--- explorer/frontend/pages/api/namespaces.js
+++ explorer/frontend/pages/api/namespaces.js
@@ -1,14 +1,50 @@
 import config from '@/config';
-import { createAPICallFunction } from '@/utils';
+import { createAPICallFunction, createAPISearchURL, createMosaicName, createPage, createSearchCriteria } from '@/utils';
+import { formatMosaic } from './mosaics';
 
 export const getNamespaceInfo = createAPICallFunction(async id => {
 	const response = await fetch(`${config.API_BASE_URL}/namespace/${id}`);
 	const namespace = await response.json();
+	console.log(namespace)
+
+	return formatNamespace(namespace);
+});
+
+export const getNamespacePage = async searchCriteria => {
+	const { pageNumber, pageSize } = createSearchCriteria(searchCriteria);
+	const url = createAPISearchURL(`${config.API_BASE_URL}/namespaces`, { pageNumber, pageSize });
+	const response = await fetch(url);
+	const namespaces = await response.json();
+
+	return createPage(namespaces, pageNumber, formatNamespace);
+};
+
+const formatNamespace = data => {
+	const namespaceMosaicsMap = {};
+	data.mosaics.forEach(item => {
+		if (!namespaceMosaicsMap[item.namespaceName]) namespaceMosaicsMap[item.namespaceName] = [];
+		namespaceMosaicsMap[item.namespaceName].push(({
+			id: createMosaicName(item.namespaceName, item.mosaicName),
+			name: createMosaicName(item.namespaceName, item.mosaicName),
+			registrationHeight: item.registeredHeight,
+			registrationTimestamp: item.registeredTimestamp,
+			supply: item.totalSupply
+		}));
+	});
 
 	return {
-		...namespace,
-		registrationHeight: namespace.registeredHeight,
-		id,
-		name: id
-	};
-});
+		name: data.rootNamespace,
+		id: data.rootNamespace,
+		creator: data.owner,
+		subNamespaceCount: data.subNamespaces.length,
+		subNamespaces: data.subNamespaces,
+		registrationTimestamp: data.registeredTimestamp,
+		registrationHeight: data.registeredHeight,
+		expirationHeight: data.expirationHeight,
+		namespaceMosaics: Object.entries(namespaceMosaicsMap).map(([namespaceName, mosaics]) => ({
+			namespaceId: namespaceName,
+			namespaceName,
+			data: mosaics
+		}))
+	}
+};
---
+++ explorer/frontend/pages/namespaces/index.jsx
@@ -0,0 +1,77 @@
+import Section from '@/components/Section';
+import Table from '@/components/Table';
+import ValueAccount from '@/components/ValueAccount';
+import styles from '@/styles/pages/Home.module.scss';
+import { createPageHref, usePagination } from '@/utils';
+import Head from 'next/head';
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { getNamespacePage } from '../api/namespaces';
+import Link from 'next/link';
+import ValueBlockHeight from '@/components/ValueBlockHeight';
+import ItemNamespaceMobile from '@/components/ItemNamespaceMobile';
+
+export const getServerSideProps = async ({ locale }) => {
+	const namespacePage = await getNamespacePage();
+
+	return {
+		props: {
+			namespaces: namespacePage.data,
+			...(await serverSideTranslations(locale, ['common']))
+		}
+	};
+};
+
+const Blocks = ({ namespaces }) => {
+	const { t } = useTranslation();
+	const { requestNextPage, data, isLoading, pageNumber, isLastPage } = usePagination(getNamespacePage, namespaces);
+
+	const tableColumns = [
+		{
+			key: 'name',
+			size: '32rem',
+			renderValue: value => <Link href={createPageHref('namespaces', value)}>{value}</Link>
+		},
+		{
+			key: 'subNamespaceCount',
+			size: '4rem',
+			renderValue: value => value
+		},
+		{
+			key: 'creator',
+			size: '30rem',
+			renderValue: value => <ValueAccount address={value} size="sm" />
+		},
+		{
+			key: 'registrationHeight',
+			size: '10rem',
+			renderValue: value => <ValueBlockHeight value={value} />
+		},
+		{
+			key: 'expirationHeight',
+			size: '10rem',
+			renderValue: value => value
+		},
+	];
+
+	return (
+		<div className={styles.wrapper}>
+			<Head>
+				<title>{t('page_namespaces')}</title>
+			</Head>
+			<Section>
+				<Table
+					data={data}
+					columns={tableColumns}
+					ItemMobile={ItemNamespaceMobile}
+					isLoading={isLoading}
+					isLastPage={isLastPage}
+					isLastColumnAligned={true}
+					onEndReached={() => requestNextPage({ pageNumber: pageNumber + 1 })}
+				/>
+			</Section>
+		</div>
+	);
+};
+
+export default Blocks;
