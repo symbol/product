3b6fe4e37bdc7a3ab8441ff2349bd4796f43aeb8
Oleg Makarenko <olegomm@gmail.com>
2023-09-27 13:35:06 +0300

    [explorer/frontend] task: add namespace and mosaic pages to router cache

--- explorer/frontend/components/ValueTimestamp.jsx
+++ explorer/frontend/components/ValueTimestamp.jsx
@@ -2,12 +2,12 @@ import { formatDate, useStorage } from '../utils';
 import { STORAGE_KEY } from '@/constants';
 import { useTranslation } from 'next-i18next';
 
-const ValueTimestamp = ({ value, hasTime, hasSeconds, hasDays }) => {
+const ValueTimestamp = ({ className, value, hasTime, hasSeconds, hasDays }) => {
 	const { t } = useTranslation('common');
 	const [type] = useStorage(STORAGE_KEY.TIMESTAMP_TYPE);
 	const formattedDate = formatDate(value, t, { type, hasTime, hasSeconds, hasDays });
 
-	return <div>{formattedDate}</div>;
+	return <div className={className}>{formattedDate}</div>;
 };
 
 export default ValueTimestamp;
