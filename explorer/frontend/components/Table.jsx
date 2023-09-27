0b281fa45d72e5b0371eb9af055cf07a7b2bfce4
Oleg Makarenko <olegomm@gmail.com>
2023-09-27 13:29:21 +0300

    [explorer/frontend] feat: add last column alignment to Table compomnent

--- explorer/frontend/components/Table.jsx
+++ explorer/frontend/components/Table.jsx
@@ -3,15 +3,17 @@ import dynamic from 'next/dynamic';
 import { useTranslation } from 'next-i18next';
 const TablePageLoader = dynamic(() => import('./TablePageLoader'), { ssr: false });
 
-const Table = ({ data, sections, columns, ItemMobile, renderSectionHeader, onEndReached, isLoading, isLastPage }) => {
+const Table = ({ data, sections, columns, ItemMobile, renderSectionHeader, onEndReached, isLoading, isLastPage, isLastColumnAligned }) => {
 	const { t } = useTranslation('common');
 
 	const desktopTableStyle = !ItemMobile ? styles.dataMobile : '';
+	const headerCellStyle = `${styles.headerCell} ${isLastColumnAligned && styles.headerCell_aligned}`;
+	const dataCellStyle = isLastColumnAligned ? styles.dataCell_aligned : '';
 
 	const renderRow = (row, index) => (
 		<div className={styles.dataRow} key={'tr' + index}>
 			{columns.map((item, index) => (
-				<div className={styles.dataCell} style={{ width: item.size }} key={'td' + index}>
+				<div className={dataCellStyle} style={{ width: item.size }} key={'td' + index}>
 					{item.renderValue ? item.renderValue(row[item.key], row) : row[item.key]}
 				</div>
 			))}
@@ -27,7 +29,7 @@ const Table = ({ data, sections, columns, ItemMobile, renderSectionHeader, onEnd
 		<div className={styles.table}>
 			<div className={styles.header}>
 				{columns.map((item, index) => (
-					<div className={styles.headerCell} style={{ width: item.size }} key={'th' + index}>
+					<div className={headerCellStyle} style={{ width: item.size }} key={'th' + index}>
 						{item.renderTitle ? item.renderTitle(item.key) : t(`table_field_${item.key}`)}
 					</div>
 				))}
@@ -35,6 +37,7 @@ const Table = ({ data, sections, columns, ItemMobile, renderSectionHeader, onEnd
 			{!!data && <div className={`${styles.data} ${desktopTableStyle}`}>{data.map(renderRow)}</div>}
 			{!!data && !!ItemMobile && <div className={styles.listMobile}>{data.map(renderMobileListItem)}</div>}
 			{!!data && !data.length && <div className={styles.emptyListMessage}>{t('message_emptyTable')}</div>}
+			{!!sections && !sections.length && <div className={styles.emptyListMessage}>{t('message_emptyTable')}</div>}
 
 			{!!sections &&
 				sections.map((section, index) => (
