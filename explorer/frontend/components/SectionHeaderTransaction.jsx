5ab141a50e5bd93acd4231c70fb5d131a5a5c9e1
Oleg Makarenko <olegomm@gmail.com>
2023-09-29 17:23:18 +0300

    [explorer/frontend] feat: add ValueBlockHeight component, update namespace icon

--- explorer/frontend/components/SectionHeaderTransaction.jsx
+++ explorer/frontend/components/SectionHeaderTransaction.jsx
@@ -1,7 +1,19 @@
-import ValueBlockHeight from './ValueBlockHeight';
+import styles from '@/styles/components/SectionHeaderTransaction.module.scss';
+import CustomImage from './CustomImage';
+import ValueTimestamp from './ValueTimestamp';
+import Link from 'next/link';
+import { createPageHref } from 'utils/client';
 
 const SectionHeaderTransaction = ({ height, timestamp }) => {
-	return <ValueBlockHeight value={height} timestamp={timestamp} size="md" />;
+	return (
+		<Link className={styles.valueBlockHeight} href={createPageHref('blocks', height)}>
+			<CustomImage className={styles.icon} src="/images/icon-transaction-header-block.svg" alt="Block" />
+			<div>
+				<div className={styles.title}>{height}</div>
+				<ValueTimestamp value={timestamp} hasTime />
+			</div>
+		</Link>
+	);
 };
 
 export default SectionHeaderTransaction;
