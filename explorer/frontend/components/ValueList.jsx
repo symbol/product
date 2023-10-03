5f6f12cfb74c94339dd17f4ab3547436c877b22c
Oleg Makarenko <olegomm@gmail.com>
2023-10-03 22:04:09 +0300

    [explorer/frontend] feat: add custom item renderer option for ValueList, fix oversize button

--- explorer/frontend/components/ValueList.jsx
+++ explorer/frontend/components/ValueList.jsx
@@ -4,16 +4,16 @@ import styles from '@/styles/components/ValueList.module.scss';
 import { arrayToText, useToggle } from '@/utils';
 import { useTranslation } from 'next-i18next';
 
-const ValueList = ({ className, data, direction, max, title }) => {
+const ValueList = ({ className, data, direction, max, title, renderItem }) => {
 	const { t } = useTranslation();
 	const [isMenuOpen, toggleMenu] = useToggle(false);
-	const isColumn = direction === 'col';
+	const isColumn = direction === 'column';
 	const directionStyle = isColumn ? styles.listColumn : styles.listRow;
 	const slicedList = data.slice(0, max);
 	const remainingItemCount = data.length - slicedList.length;
 	const isButtonMoreShown = remainingItemCount > 0;
 
-	const renderColumn = data => data.map((item, index) => <div key={index}>{item}</div>);
+	const renderColumn = data => data.map((item, index) => <div key={index}>{renderItem ? renderItem(item) : item}</div>);
 
 	return (
 		<div className={`${directionStyle} ${className}`}>
