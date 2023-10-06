8f7f6f480c8099507d795c43a3553f700bde2311
Oleg Makarenko <olegomm@gmail.com>
2023-10-06 20:52:41 +0300

    [explorer/frontend] feat: add AccountMultisigTree component, add multisig info to Account page, add account DTO mapper

--- explorer/frontend/stubs/accounts.js
+++ explorer/frontend/stubs/accounts.js
@@ -1,3 +1,4 @@
+import { getMosaicPage } from '@/pages/api/mosaics';
 import symbolSDK from 'symbol-sdk';
 
 export const getAccountsStub = async searchCriteria => {
@@ -20,44 +21,46 @@ export const getAccountsStub = async searchCriteria => {
 };
 
 export const getAccountInfoStub = async address => {
+	const facade = new symbolSDK.facade.NemFacade('testnet');
 	const key_pair1 = new symbolSDK.facade.NemFacade.KeyPair(symbolSDK.PrivateKey.random());
+	const address1 = facade.network.publicKeyToAddress(key_pair1.publicKey);
+	const key_pair2 = new symbolSDK.facade.NemFacade.KeyPair(symbolSDK.PrivateKey.random());
+	const address2 = facade.network.publicKeyToAddress(key_pair2.publicKey);
+	const key_pair3 = new symbolSDK.facade.NemFacade.KeyPair(symbolSDK.PrivateKey.random());
+	const address3 = facade.network.publicKeyToAddress(key_pair3.publicKey);
+	const mosaics = (await getMosaicPage({ pageSize: 10 })).data;
+
+	let minCosignatories = 0;
+	let cosignatoryOf = [];
+	let cosignatories = [];
+
+	if (address[1] === 'A') {
+		minCosignatories = 2;
+		cosignatories = [address2.toString(), address3.toString(), address2.toString()];
+	}
+	if (address[1] === 'B') {
+		cosignatoryOf = [address2.toString(), address3.toString()];
+	}
 
 	return {
+		remoteAddress: address1.toString(),
 		address,
 		publicKey: key_pair1.publicKey.toString(),
+		remarkLabel: 'Lorem ipsum',
 		balance: 8423.142938,
-		balanceUSD: 10000,
-		height: 458880,
-		importance: 0.037,
 		vestedBalance: 8423.142938,
-		names: ['namespace.account', 'another.name'],
-		isMultisig: false,
-		isHarvestingActive: true,
-		namespaces: [
-			{
-				id: 'namespace.account',
-				name: 'namespace.account',
-				linkedId: address
-			},
-			{
-				id: 'namespace.supercoin',
-				name: 'namespace.supercoin',
-				linkedId: 'supercoin'
-			}
-		],
-		mosaics: [
-			{
-				id: '6BED913FA20223F8',
-				name: 'nem.xem',
-				amount: '8423.142938',
-				isCreatedByAccount: false
-			},
-			{
-				id: '5AED903FB202130B',
-				name: 'namespace.supercoin',
-				amount: 1,
-				isCreatedByAccount: true
-			}
-		]
+		mosaics: mosaics.map(mos => ({
+			...mos,
+			amount: (Math.random() * 10000).toFixed()
+		})),
+		importance: 0,
+		harvestedBlocks: 1200,
+		harvestedFees: 137,
+		harvestStatus: 'locked',
+		harvestRemoteStatus: 'active',
+		createdHeight: 73737,
+		minCosignatories,
+		cosignatoryOf,
+		cosignatories
 	};
 };
