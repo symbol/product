cbd253844be206530aec9fa4c92e8c22849c77c8
Oleg Makarenko <olegomm@gmail.com>
2023-10-11 18:46:11 +0300

    [explorer/frontend] feat: add favicon and manifest file

--- explorer/frontend/pages/_document.jsx
+++ explorer/frontend/pages/_document.jsx
@@ -4,6 +4,13 @@ export default function Document() {
 	return (
 		<Html>
 			<Head>
+				<meta charset="utf-8" />
+				<link rel="icon" href="/favicon.ico" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta name="theme-color" content="#000000" />
+				<meta name="description" content="NEM Block Explorer" />
+				<link rel="apple-touch-icon" href="/logo192.png" />
+				<link rel="manifest" href="/manifest.json" />
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link
 					// eslint-disable-next-line max-len
