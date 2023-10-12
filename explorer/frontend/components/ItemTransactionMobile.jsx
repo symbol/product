85147a451b1c1593a0cf82a06118a9e5899f0ab6
Oleg Makarenko <olegomm@gmail.com>
2023-10-13 00:00:23 +0300

    [explorer/frontend] task: reorder items in ItemTransactionMobile, use sender address instead of signer

--- explorer/frontend/components/ItemTransactionMobile.jsx
+++ explorer/frontend/components/ItemTransactionMobile.jsx
@@ -1,40 +1,57 @@
 import CustomImage from './CustomImage';
 import IconTransactionType from './IconTransactionType';
 import ValueAccount from './ValueAccount';
+import ValueList from './ValueList';
 import ValueMosaic from './ValueMosaic';
 import ValueTimestamp from './ValueTimestamp';
 import ValueTransactionHash from './ValueTransactionHash';
 import styles from '@/styles/components/ItemTransactionMobile.module.scss';
+import { createPageHref } from '@/utils';
+import Link from 'next/link';
 import { useTranslation } from 'next-i18next';
 
-const ItemTransactionMobile = ({ data }) => {
-	const { hash, signer, recipient, amount, type, timestamp } = data;
+const ItemTransactionMobile = ({ data, isTimestampShown }) => {
+	const { hash, sender, recipient, value, type, timestamp, direction } = data;
 	const { t } = useTranslation();
 	const typeText = t(`transactionType_${type}`);
 
 	return (
-		<div className={styles.itemTransactionMobile}>
+		<Link className={styles.itemTransactionMobile} href={createPageHref('transactions', hash)}>
 			<IconTransactionType value={type} />
 			<div className={styles.middle}>
-				<div className={styles.type}>{typeText}</div>
-				<div className={styles.row}>
-					<CustomImage className={styles.iconDirection} src="/images/icon-hash.svg" />
-					<ValueTransactionHash value={hash} />
+				<div className={styles.rowAmount}>
+					<div>
+						<div className={styles.type}>{typeText}</div>
+						<div className={styles.row}>
+							<CustomImage className={styles.iconDirection} src="/images/icon-hash.svg" />
+							<ValueTransactionHash value={hash} />
+						</div>
+					</div>
+					<ValueList
+						data={value}
+						max={2}
+						direction="column"
+						renderItem={item => (
+							<ValueMosaic
+								mosaicId={item.id}
+								mosaicName={item.name}
+								amount={item.amount}
+								isTickerShown
+								direction={direction}
+							/>
+						)}
+					/>
 				</div>
-				<ValueAccount address={signer} size="sm" />
+				<ValueAccount address={sender} size="sm" />
 				{!!recipient && (
 					<div className={styles.row}>
 						<CustomImage className={styles.iconDirection} src="/images/icon-transaction-direction.svg" />
 						<ValueAccount address={recipient} size="sm" />
 					</div>
 				)}
-
-				<div className={styles.end}>
-					<ValueTimestamp value={timestamp} hasTime />
-					<ValueMosaic isNative amount={amount} />
-				</div>
+				{isTimestampShown && <ValueTimestamp value={timestamp} hasTime />}
 			</div>
-		</div>
+		</Link>
 	);
 };
 
