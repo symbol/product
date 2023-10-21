a825e23e8b67aa7fddc7a86db001eca70be68bb5
Oleg Makarenko <olegomm@gmail.com>
2023-10-21 16:48:36 +0300

    [wallet/mobile] feat: add custom 500 error page, add translations on 404 error page

--- explorer/frontend/pages/500.jsx
+++ explorer/frontend/pages/500.jsx
@@ -1,31 +1,33 @@
-import styles from '@/styles/pages/404.module.scss';
+import styles from '@/styles/pages/500.module.scss';
 import { createPageHref } from '@/utils';
 import Head from 'next/head';
 import Link from 'next/link';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-export const getStaticProps = async ({ locale }) => {
+export const getStaticProps = async ({ locale, err }) => {
 	return {
 		props: {
+			error: err || null,
 			...(await serverSideTranslations(locale, ['common']))
 		}
 	};
 };
 
-const Error404 = () => {
+const Error500 = ({ error }) => {
 	const { t } = useTranslation();
+	const errorNameText = error?.name || '';
 
 	return (
 		<div className={styles.wrapper}>
 			<Head>
-				<title>404</title>
+				<title>500</title>
 			</Head>
 			<div className={styles.container}>
-				<div className={styles.code}>404</div>
+				<div className={styles.code}>500</div>
 				<div className={styles.separator} />
 				<div className={styles.text}>
-					<div className={styles.description}>{t('message_404')}</div>
+					<div className={styles.description}>{`${t('message_500')} ${errorNameText}`}</div>
 					<Link className={styles.link} href={createPageHref('home')}>
 						{t('button_backHome')}
 					</Link>
@@ -35,4 +37,4 @@ const Error404 = () => {
 	);
 };
 
-export default Error404;
+export default Error500;
