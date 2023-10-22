5e197b693023762f017302f093df27efb59f7305
Oleg Makarenko <olegomm@gmail.com>
2023-10-22 23:05:34 +0300

    [explorer/frontend] feat: add account API integration

--- explorer/frontend/api/accounts.js
+++ explorer/frontend/api/accounts.js
@@ -1,32 +1,45 @@
-import { getAccountInfoStub, getAccountsStub } from '../stubs/accounts';
-import { createPage, createSearchCriteria } from '@/utils';
+import config from '@/config';
+import { createAPICallFunction, createAPISearchURL, createPage, createSearchCriteria, makeRequest } from '@/utils';
 
 export const fetchAccountPage = async (searchCriteria = {}) => {
-	const { pageNumber, pageSize } = createSearchCriteria(searchCriteria);
-	const accounts = await getAccountsStub({ pageNumber, pageSize });
+	const { pageNumber, pageSize, filter } = createSearchCriteria(searchCriteria);
+	const url = createAPISearchURL(`${config.API_BASE_URL}/accounts`, { pageNumber, pageSize }, filter);
+	const accounts = await makeRequest(url);
 
-	return createPage(searchCriteria.isService ? [] : accounts, pageNumber);
+	return createPage(accounts, pageNumber, formatAccount);
 };
 
-export const fetchAccountInfo = async height => {
-	const accountInfo = await getAccountInfoStub(height);
+export const fetchAccountInfo = createAPICallFunction(async address => {
+	const accountInfo = await makeRequest(`${config.API_BASE_URL}/account?address=${address}`);
 
-	return {
-		remoteAddress: accountInfo.remoteAddress,
-		address: accountInfo.address,
-		publicKey: accountInfo.publicKey,
-		description: accountInfo.remarkLabel,
-		balance: accountInfo.balance,
-		vestedBalance: accountInfo.vestedBalance,
-		mosaics: accountInfo.mosaics,
-		importance: accountInfo.importance,
-		harvestedBlocks: accountInfo.harvestedBlocks,
-		harvestedFees: accountInfo.harvestedFees,
-		height: accountInfo.createdHeight,
-		minCosignatories: accountInfo.minCosignatories,
-		cosignatoryOf: accountInfo.cosignatoryOf,
-		cosignatories: accountInfo.cosignatories,
-		isMultisig: accountInfo.cosignatories.length > 0,
-		isHarvestingActive: accountInfo.harvestRemoteStatus === 'active'
-	};
-};
+	return formatAccount(accountInfo);
+});
+
+export const fetchAccountInfoByPublicKey = createAPICallFunction(async publicKey => {
+	const accountInfo = await makeRequest(`${config.API_BASE_URL}/account?publicKey=${publicKey}`);
+
+	return formatAccount(accountInfo);
+});
+
+const formatAccount = data => ({
+	remoteAddress: data.remoteAddress || null,
+	address: data.address,
+	publicKey: data.publicKey,
+	description: data.remarks,
+	balance: data.balance,
+	vestedBalance: data.vestedBalance || null,
+	mosaics: data.mosaics.map(item => ({
+		name: item.namespace_name,
+		id: item.namespace_name,
+		amount: item.quantity
+	})),
+	importance: +data.importance * 100,
+	harvestedBlocks: data.harvestedBlocks,
+	harvestedFees: data.harvestedFees,
+	height: data.height,
+	minCosignatories: data.minCosignatories || 0,
+	cosignatoryOf: data.cosignatoryOf || [],
+	cosignatories: data.cosignatories || [],
+	isMultisig: data.cosignatories?.length > 0,
+	isHarvestingActive: data.harvestRemoteStatus === 'ACTIVE'
+});
