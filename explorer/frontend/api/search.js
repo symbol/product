93116e6bf6296deb7feb8610f7822d13d76aa3da
Oleg Makarenko <olegomm@gmail.com>
2023-10-22 23:06:03 +0300

    [explorer/frontend] feat: add search by address and publicKey

--- explorer/frontend/api/search.js
+++ explorer/frontend/api/search.js
@@ -1,18 +1,20 @@
-import { fetchAccountInfo } from './accounts';
+import { fetchAccountInfo, fetchAccountInfoByPublicKey } from './accounts';
 import { fetchBlockInfo } from './blocks';
 import { fetchMosaicInfo } from './mosaics';
 import { fetchNamespaceInfo } from './namespaces';
 import { fetchTransactionInfo } from './transactions';
 
 export const search = async text => {
-	const query = `${text}`.trim();
+	const query = `${text}`.trim().toUpperCase();
 	const results = {};
+	let account;
 	let transaction;
 	let namespace;
 
 	const block = await fetchBlockInfo(query);
-	if (query.length === 64) transaction = await fetchTransactionInfo(query.toUpperCase());
-	//const account = await fetchAccountInfo(query.toUpperCase());
+	if (query.length === 64) transaction = await fetchTransactionInfo(query);
+	if (query.length === 40) account = await fetchAccountInfo(query);
+	if (!account && query.length === 64) account = await fetchAccountInfoByPublicKey(query);
 	const mosaic = await fetchMosaicInfo(query.toLowerCase());
 	namespace = await fetchNamespaceInfo(query.toLowerCase());
 	if (!namespace && query.split('.').length > 1) {
@@ -22,7 +24,7 @@ export const search = async text => {
 
 	if (block) results.block = block;
 	if (transaction) results.transaction = transaction;
-	// if (account) results.account = account;
+	if (account) results.account = account;
 	if (mosaic) results.mosaic = mosaic;
 	if (namespace) results.namespace = namespace;
 
