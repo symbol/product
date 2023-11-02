07b64ad08dd097ded4797c979d903153b9858837
Oleg Makarenko <olegomm@gmail.com>
2023-11-02 13:00:28 +0200

    [explorer/frontend] fix: uppercase user currency symbol, add constant scrollbars to body to avoid page jumps

--- explorer/frontend/components/ValueAccountBalance.jsx
+++ explorer/frontend/components/ValueAccountBalance.jsx
@@ -3,13 +3,15 @@ import styles from '@/styles/components/ValueAccountBalance.module.scss';
 import { numberToString, truncateDecimals } from '@/utils';
 
 const ValueAccountBalance = ({ value, valueInUserCurrency, userCurrency }) => {
+	const userCurrencyText = (userCurrency || '').toUpperCase();
+
 	return (
 		<div className={styles.valueAccountBalance}>
 			<div className={styles.value}>{numberToString(value)}</div>
 			<div className={styles.ticker}>{config.NATIVE_MOSAIC_TICKER}</div>
 			<div className={styles.valueUSD}>
 				{' '}
-				~{truncateDecimals(valueInUserCurrency, 2)} {userCurrency}
+				~{truncateDecimals(valueInUserCurrency, 2)} {userCurrencyText}
 			</div>
 		</div>
 	);
