cfeef461636e11d6974e4d618e147eb534cce9c9
AnthonyLaw <anthony@symbol.dev>
2023-11-07 20:44:39 +0800

    [explorer/rest]:  extract common namespace SQL query into a single method

--- explorer/rest/rest/db/NemDatabase.py
+++ explorer/rest/rest/db/NemDatabase.py
@@ -25,6 +25,48 @@ class NemDatabase(DatabaseConnectionPool):
 		super().__init__(db_config)
 		self.network = NetworkLocator.find_by_name(Network.NETWORKS, network_name)
 
+	def _generate_namespace_sql_query(self, where_condition=None):  # pylint: disable=no-self-use
+		"""Base SQL query for namespaces."""
+
+		where_clause = f'WHERE {where_condition}' if where_condition else ''
+
+		return f'''
+			SELECT
+				n.id,
+				n.root_namespace,
+				n.owner,
+				n.registered_height,
+				b1.timestamp AS registered_timestamp,
+				n.expiration_height,
+				n.sub_namespaces,
+				CASE
+					WHEN COUNT(m.namespace_name) = 0 THEN '[]'
+					ELSE json_agg(json_build_object(
+						'namespace_name', namespace_name,
+						'total_supply', m.total_supply,
+						'divisibility', m.divisibility,
+						'registered_height', m.registered_height,
+						'registered_timestamp', b2.timestamp
+					))
+				END AS mosaics
+			FROM namespaces n
+			LEFT JOIN mosaics m
+				ON n.root_namespace = m.root_namespace
+			LEFT JOIN blocks b1
+				ON n.registered_height = b1.height
+			LEFT JOIN blocks b2
+				ON m.registered_height = b2.height
+			{where_clause}
+			GROUP BY
+				n.id,
+				n.root_namespace,
+				n.owner,
+				n.registered_height,
+				b1.timestamp,
+				n.expiration_height,
+				n.sub_namespaces
+		'''
+
 	def _create_block_view(self, result):
 		harvest_public_key = PublicKey(_format_bytes(result[7]))
 		return BlockView(
@@ -40,13 +82,13 @@ class NemDatabase(DatabaseConnectionPool):
 		)
 
 	def _create_namespace_view(self, result):
-		owner_public_key = PublicKey(_format_bytes(result[1]))
+		owner_public_key = PublicKey(_format_bytes(result[2]))
 
 		mosaics = []
 
-		if result[6] != []:
+		if result[7] != []:
 			# Formatting mosaics info
-			for mosaic in result[6]:
+			for mosaic in result[7]:
 				namespace_mosaic_name = mosaic['namespace_name'].split('.')
 				namespace_name = '.'.join(namespace_mosaic_name[:-1])
 				mosaic_name = namespace_mosaic_name[-1]
@@ -61,12 +103,12 @@ class NemDatabase(DatabaseConnectionPool):
 				})
 
 		return NamespaceView(
-			root_namespace=result[0],
+			root_namespace=result[1],
 			owner=self.network.public_key_to_address(owner_public_key),
-			registered_height=result[2],
-			registered_timestamp=str(result[3]),
-			expiration_height=result[4],
-			sub_namespaces=result[5],
+			registered_height=result[3],
+			registered_timestamp=str(result[4]),
+			expiration_height=result[5],
+			sub_namespaces=result[6],
 			mosaics=mosaics
 		)
 
@@ -103,42 +145,14 @@ class NemDatabase(DatabaseConnectionPool):
 	def get_namespace(self, namespace):
 		"""Gets namespace by name in database."""
 
+		sql = self._generate_namespace_sql_query(
+			'n.root_namespace = %s or %s = ANY(n.sub_namespaces)'
+		)
+		params = (namespace, namespace)
+
 		with self.connection() as connection:
 			cursor = connection.cursor()
-			cursor.execute('''
-				SELECT
-					n.root_namespace,
-					n.owner,
-					n.registered_height,
-					b1.timestamp AS registered_timestamp,
-					n.expiration_height,
-					n.sub_namespaces,
-					CASE
-						WHEN COUNT(m.namespace_name) = 0 THEN '[]'
-						ELSE json_agg(json_build_object(
-							'namespace_name', namespace_name,
-							'total_supply', m.total_supply,
-							'divisibility', m.divisibility,
-							'registered_height', m.registered_height,
-							'registered_timestamp', b2.timestamp
-						))
-					END AS mosaics
-				FROM namespaces n
-				LEFT JOIN mosaics m
-					ON n.root_namespace = m.root_namespace
-				LEFT JOIN blocks b1
-					ON n.registered_height = b1.height
-				LEFT JOIN blocks b2
-					ON m.registered_height = b2.height
-				WHERE n.root_namespace = %s or %s = ANY(n.sub_namespaces)
-				GROUP BY
-					n.root_namespace,
-					n.owner,
-					n.registered_height,
-					b1.timestamp,
-					n.expiration_height,
-					n.sub_namespaces
-			''', (namespace, namespace))
+			cursor.execute(sql, params)
 			result = cursor.fetchone()
 
 			return self._create_namespace_view(result) if result else None
@@ -146,43 +160,16 @@ class NemDatabase(DatabaseConnectionPool):
 	def get_namespaces(self, limit, offset, sort):
 		"""Gets namespaces pagination in database."""
 
+		sql = self._generate_namespace_sql_query()
+		sql += f'''
+			ORDER BY n.id {sort}
+			LIMIT %s OFFSET %s
+		'''
+		params = (limit, offset)
+
 		with self.connection() as connection:
 			cursor = connection.cursor()
-			cursor.execute(f'''
-			SELECT
-				n.root_namespace,
-				n.owner,
-				n.registered_height,
-				b1.timestamp AS registered_timestamp,
-				n.expiration_height,
-				n.sub_namespaces,
-				CASE
-					WHEN COUNT(m.namespace_name) = 0 THEN '[]'
-					ELSE json_agg(json_build_object(
-						'namespace_name', namespace_name,
-						'total_supply', m.total_supply,
-						'divisibility', m.divisibility,
-						'registered_height', m.registered_height,
-						'registered_timestamp', b2.timestamp
-					))
-				END AS mosaics
-			FROM namespaces n
-			LEFT JOIN mosaics m
-				ON n.root_namespace = m.root_namespace
-			LEFT JOIN blocks b1
-				ON n.registered_height = b1.height
-			LEFT JOIN blocks b2
-				ON m.registered_height = b2.height
-			GROUP BY
-				n.root_namespace,
-				n.owner,
-				n.registered_height,
-				b1.timestamp,
-				n.expiration_height,
-				n.sub_namespaces
-			ORDER BY n.registered_height {sort}
-			LIMIT %s OFFSET %s
-			''', (limit, offset,))
+			cursor.execute(sql, params)
 			results = cursor.fetchall()
 
 			return [self._create_namespace_view(result) for result in results]
