2633a9a3235db226a8835b6c7dcc79fbc43a5dda
AnthonyLaw <anthony@symbol.dev>
2024-03-12 03:27:44 +0800

    [snap/backend]: added jest unit test config

---
+++ snap/backend/jest.config.js
@@ -0,0 +1,6 @@
+const jestConfig = {
+	preset: '@metamask/snaps-jest',
+	testMatch: ['**/test/**/*_spec.js']
+};
+
+export default jestConfig;
--- snap/backend/test/index_spec.js
+++ snap/backend/test/index_spec.js
@@ -1,50 +1,44 @@
 // import { installSnap } from '@metamask/snaps-jest';
 import snapsJest from '@metamask/snaps-jest';
 import { panel, text } from '@metamask/snaps-sdk';
-import { expect } from 'chai';
+import { expect } from '@jest/globals';
 
 const { installSnap } = snapsJest;
-
-describe('test', () => {
-	it('test', async () => {
-		expect(true).to.be.true;
-	});
-});
 
 describe('onRpcRequest', () => {
 	describe('hello', () => {
 		it('shows a confirmation dialog', async () => {
 			const { request } = await installSnap();
 
-			// const origin = 'Jest';
-			// const response = request({
-			// 	method: 'hello',
-			// 	origin
-			// });
+			const origin = 'Jest';
+			const response = request({
+				method: 'hello',
+				origin
+			});
 
-			// const ui = await response.getInterface();
-			// expect(ui.type).toBe('confirmation');
-			// expect(ui).toRender(panel([text(`Hello, **${origin}**!`)]));
+			const ui = await response.getInterface();
+			expect(ui.type).toBe('confirmation');
+			expect(ui).toRender(panel([text(`Hello, **${origin}**!`)]));
 
-			// await ui.ok();
+			await ui.ok();
 
-			// expect(await response).toRespondWith(true);
+			expect(await response).toRespondWith(true);
 		});
 	});
 
-	// it('throws an error if the requested method does not exist', async () => {
-	// 	const { request, close } = await installSnap();
+	it('throws an error if the requested method does not exist', async () => {
+		const { request, close } = await installSnap();
 
-	// 	const response = await request({
-	// 		method: 'foo'
-	// 	});
+		const response = await request({
+			method: 'foo'
+		});
 
-	// 	expect(response).toRespondWithError({
-	// 		code: -32603,
-	// 		message: 'Method not found.',
-	// 		stack: expect.any(String)
-	// 	});
+		expect(response).toRespondWithError({
+			code: -32603,
+			message: 'Method not found.',
+			stack: expect.any(String)
+		});
 
-	// 	await close();
-	// });
+		await close();
+	});
 });
