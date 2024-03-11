47eeda205ebfc1ad15ca562108762659511cd2ab
AnthonyLaw <anthony@symbol.dev>
2024-03-12 02:03:51 +0800

    [snap/backend]: added unit test config

---
+++ snap/backend/test/index_spec.js
@@ -0,0 +1,50 @@
+// import { installSnap } from '@metamask/snaps-jest';
+import snapsJest from '@metamask/snaps-jest';
+import { panel, text } from '@metamask/snaps-sdk';
+import { expect } from 'chai';
+
+const { installSnap } = snapsJest;
+
+describe('test', () => {
+	it('test', async () => {
+		expect(true).to.be.true;
+	});
+});
+
+describe('onRpcRequest', () => {
+	describe('hello', () => {
+		it('shows a confirmation dialog', async () => {
+			const { request } = await installSnap();
+
+			// const origin = 'Jest';
+			// const response = request({
+			// 	method: 'hello',
+			// 	origin
+			// });
+
+			// const ui = await response.getInterface();
+			// expect(ui.type).toBe('confirmation');
+			// expect(ui).toRender(panel([text(`Hello, **${origin}**!`)]));
+
+			// await ui.ok();
+
+			// expect(await response).toRespondWith(true);
+		});
+	});
+
+	// it('throws an error if the requested method does not exist', async () => {
+	// 	const { request, close } = await installSnap();
+
+	// 	const response = await request({
+	// 		method: 'foo'
+	// 	});
+
+	// 	expect(response).toRespondWithError({
+	// 		code: -32603,
+	// 		message: 'Method not found.',
+	// 		stack: expect.any(String)
+	// 	});
+
+	// 	await close();
+	// });
+});
