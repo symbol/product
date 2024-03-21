33655fc014b45f23b02e53124247a1e25d591f27
AnthonyLaw <anthony@symbol.dev>
2024-03-21 23:25:09 +0800

    [snap/frontend]: added button component and unit test

---
+++ snap/frontend/components/Button/index.jsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+
+const Button = ({ children, className, icon, ...props }) => {
+	const combinedClassName = `bg-[#333333] justify-between p-2 inline-flex items-center rounded-xl ${className || ''}`;
+
+	return (
+		<button className={combinedClassName} {...props}>
+			{icon && <Image width={24} height={24} src={icon} className="mr-2" alt='' />}
+			{children}
+		</button>
+	);
+};
+
+export default Button;
