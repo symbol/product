ea3305e31813471f8936bbdfbca6e18bfc85105d
AnthonyLaw <anthony@symbol.dev>
2024-03-21 23:25:09 +0800

    [snap/frontend]: added button component and unit test

--- snap/frontend/setupTests.js
+++ snap/frontend/setupTests.js
@@ -3,3 +3,9 @@ import '@testing-library/jest-dom';
 import { jest } from '@jest/globals';
 
 global.jest = jest;
+
+/* eslint-disable jsx-a11y/alt-text, @next/next/no-img-element */
+jest.mock('next/image', () => {
+	const mockImage = ({...props}) => <img {...props} />;
+	return mockImage;
+});
