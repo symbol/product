d1fa78b0d72eab136ab20105bbca0cac113e0820
AnthonyLaw <anthony@symbol.dev>
2024-03-24 02:57:39 +0800

    [snap/frontend]: added modal box component and unit test

---
+++ snap/frontend/components/ModalBox/index.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useRef } from 'react';
+
+const ModalBox = ({ children, isOpen, onRequestClose }) => {
+	const modalRef = useRef(null);
+
+	useEffect(() => {
+		const handleClickOutside = event => {
+			if (modalRef.current && !modalRef.current.contains(event.target))
+				onRequestClose();
+		};
+
+		const handleEscPress = event => {
+			if ('Escape' === event.key)
+				onRequestClose();
+
+		};
+
+		document.addEventListener('mousedown', handleClickOutside);
+		document.addEventListener('keydown', handleEscPress);
+
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+			document.removeEventListener('keydown', handleEscPress);
+		};
+	}, [onRequestClose]);
+
+	if (!isOpen)
+		return null;
+
+	return (
+		<>
+			<div role='overlay' className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+				<div role='modal' className="bg-[#232329] p-6 rounded-lg shadow-lg min-w-[300px]" ref={modalRef}>
+					<button
+						className="absolute mt-4 mr-4"
+						onClick={onRequestClose}
+					>
+					Close
+					</button>
+					{children}
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default ModalBox;
