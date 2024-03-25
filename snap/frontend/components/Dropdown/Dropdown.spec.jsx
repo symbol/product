25bb6b57f4e3483e0d9b495862c57e22521ae9ce
AnthonyLaw <anthony@symbol.dev>
2024-03-26 01:34:13 +0800

    [snap/frontend]: added dropdown component unit test

---
+++ snap/frontend/components/Dropdown/Dropdown.spec.jsx
@@ -0,0 +1,57 @@
+import Dropdown from '.';
+import { fireEvent, render, screen } from '@testing-library/react';
+
+describe('components/Dropdown', () => {
+	// Arrange:
+	let label = 'Select an option';
+	const options = [
+		{ label: 'Option 1', value: 'option1' },
+		{ label: 'Option 2', value: 'option2' },
+		{ label: 'Option 3', value: 'option3' }
+	];
+
+	const onSelect = jest.fn();
+
+	describe('toggle', () => {
+		it('expand dropdown list', () => {
+			// Arrange:
+			render(<Dropdown label={label} options={options} onSelect={onSelect} />);
+
+			// Act:
+			fireEvent.click(screen.getByText(label));
+
+			// Assert:
+			expect(screen.getByText(label)).toBeInTheDocument();
+			options.forEach(option => {
+				expect(screen.queryByText(option.label)).toBeInTheDocument();
+			});
+		});
+
+		it('collapse dropdown list when dropdown is expanded', () => {
+			// Arrange:
+			render(<Dropdown label={label} options={options} onSelect={onSelect} />);
+			fireEvent.click(screen.getByText(label));
+
+			// Act:
+			fireEvent.click(screen.getByText(label));
+
+			// Assert:
+			expect(screen.getByText(label)).toBeInTheDocument();
+			options.forEach(option => {
+				expect(screen.queryByText(option.label)).not.toBeInTheDocument();
+			});
+		});
+	});
+
+	it('should fire onSelect event when clicked on dropdown item', () => {
+		// Arrange:
+		render(<Dropdown label={label} options={options} onSelect={onSelect} />);
+		fireEvent.click(screen.getByText(label));
+
+		// Act:
+		fireEvent.click(screen.getByText(options[0].label));
+
+		// Assert:
+		expect(onSelect).toHaveBeenCalledWith(options[0]);
+	});
+});
