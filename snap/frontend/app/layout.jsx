5760495734db562dfd6ee0b7c92a07066c2c6cda
AnthonyLaw <anthony@symbol.dev>
2024-03-27 22:23:28 +0800

    [snap/frontend]: apply eslint-plugin-jsdoc rules

--- snap/frontend/app/layout.jsx
+++ snap/frontend/app/layout.jsx
@@ -1,10 +1,16 @@
 import './globals.css';
+import React from 'react';
 
 export const metadata = {
 	title: 'Symbol Snap',
 	description: 'Symbol wallet in metamask'
 };
 
+/**
+ * Root layout component.
+ * @param {children} children - The children component to render.
+ * @returns {React.JSX} The root layout component.
+ */
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en" className='dark'>
