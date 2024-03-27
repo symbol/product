64143d09e32323eff90e8e39c19c47e754247617
AnthonyLaw <anthony@symbol.dev>
2024-03-27 22:23:28 +0800

    [snap/frontend]: apply eslint-plugin-jsdoc rules

--- snap/frontend/app/page.jsx
+++ snap/frontend/app/page.jsx
@@ -1,7 +1,12 @@
 'use client';
 
 import HomeComponent from '../components/Home';
+import React from 'react';
 
+/**
+ * Renders the home page.
+ * @returns {React.JSX} The home page component.
+ */
 export default function Home() {
 	return (
 		<HomeComponent />
