f01cdeb68131a53b6c5559d76246d1dd1001a107
AnthonyLaw <anthony@symbol.dev>
2024-04-05 03:31:31 +0800

    [snap/frontend]: added screen modal in home page

--- snap/frontend/components/Home/Home.spec.jsx
+++ snap/frontend/components/Home/Home.spec.jsx
@@ -1,13 +1,35 @@
 import Home from '.';
+import WalletContextProvider from '../../context/store';
 import { render, screen } from '@testing-library/react';
 
 describe('components/Home', () => {
-	it('renders home page', async () => {
+	const assertModalScreen = async (walletState, expectedModal) => {
+		// Arrange:
+		const context = {
+			walletState,
+			dispatch: jest.fn()
+		};
+
 		// Act:
-		render(<Home />);
-		const textElement = await screen.findByText('Symbol Snap');
+		render(<WalletContextProvider value={context}>
+			<Home />
+		</WalletContextProvider>);
+
+		const textElement = await screen.findByText(expectedModal);
 
 		// Assert:
 		expect(textElement).toBeInTheDocument();
+	};
+
+	it('renders DetectMetamask modal when metamask is not install', async () => {
+		await assertModalScreen({ isMetamaskInstalled: false, isSnapInstalled: false }, 'Download MetaMask');
+	});
+
+	it('renders ConnectMetamask modal when metamask is installed but snap is not installed', async () => {
+		await assertModalScreen({ isMetamaskInstalled: true, isSnapInstalled: false }, 'Connect MetaMask');
+	});
+
+	it('renders connected when metamask and snap are installed', async () => {
+		await assertModalScreen({ isMetamaskInstalled: true, isSnapInstalled: true }, 'connected');
 	});
 });
--- snap/frontend/components/Home/index.jsx
+++ snap/frontend/components/Home/index.jsx
@@ -1,7 +1,20 @@
+import useWalletInstallation from '../../hooks/useWalletInstallation';
+import ConnectMetamask from '../ConnectMetamask';
+import DetectMetamask from '../DetectMetamask';
+
 const Home = () => {
+	const { isMetamaskInstalled, isSnapInstalled } = useWalletInstallation();
+
 	return (
 		<div className='m-auto max-w-screen-xl max-h-min'>
 			Symbol Snap
+			{
+				!isMetamaskInstalled ?
+					<DetectMetamask isOpen={!isMetamaskInstalled} onRequestClose={() => false} /> :
+					!isSnapInstalled ?
+						<ConnectMetamask isOpen={!isSnapInstalled} onRequestClose={() => false} /> :
+						<div>connected</div>
+			}
 		</div>
 	);
 };
