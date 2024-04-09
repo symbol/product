35fe9276f6358cf3a7282725c2c848cfe46db92f
AnthonyLaw <anthony@symbol.dev>
2024-04-10 05:04:46 +0800

    [snap/frontend]: added transaction table component

---
+++ snap/frontend/components/TransactionTable/index.jsx
@@ -0,0 +1,98 @@
+import { useWalletContext } from '../../context';
+import Image from 'next/image';
+
+const TransactionTable =({transactions}) => {
+
+	const { walletState } = useWalletContext();
+	const { account, finalizedHeight } = walletState;
+	const { address } = account;
+
+	return (
+		<div className='overflow-y-auto'>
+			<table className='text-[#BEBEBE] w-full'>
+				<tbody>
+					{
+						transactions.map((transaction, index) => (
+							<tr key={`tx_row_${index}`} className='border-t-[1px] border-[#BEBEBE] h-16'>
+								<td className='px-2'>
+									{
+										transaction.sender === address ?
+											<Image
+												src='/send-icon.svg'
+												width={24}
+												height={24}
+												alt='Send logo'
+											/> :
+											<Image
+												src='/receive-icon.svg'
+												width={24}
+												height={24}
+												alt='Receive logo'
+											/>
+									}
+								</td>
+
+								<td className='px-2'>
+									{
+										'Transfer' === transaction.transactionType ?
+											transaction.sender === address ? 'Sent' : 'Received'
+											: transaction.transactionType
+									}
+
+									{
+										null === transaction.height ?
+											<div className='font-bold'>Pending</div> :
+											<div className='text-green-500'>Confirmed</div>
+									}
+								</td>
+
+								<td className='flex pt-2'>
+									<div className='flex flex-col items-end pr-2'>
+										{
+											null !== transaction.amount ?
+												<>
+													<div>{transaction.amount} XYM</div>
+													<div>{transaction.currency}</div>
+												</>
+												: null
+										}
+									</div>
+									{
+										null !== transaction.message ?
+											<Image
+												src='/message-icon.svg'
+												width={24}
+												height={24}
+												alt='Message logo'
+											/> :
+											null
+									}
+								</td>
+
+								<td>
+									<div className='flex items-center'>
+										{transaction.height}
+										{
+											finalizedHeight >= transaction.height && transaction.height ?
+												<Image
+													className='pl-2'
+													src='/finalized-icon.svg'
+													width={24}
+													height={24}
+													alt='Finalized logo'
+												/> : null
+										}
+									</div>
+
+									{transaction.date}
+								</td>
+							</tr>
+						))
+					}
+				</tbody>
+			</table>
+		</div>
+	);
+};
+
+export default TransactionTable;
