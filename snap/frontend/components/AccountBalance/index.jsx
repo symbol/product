e28f732370852e4339c7b29271046b33b175154d
AnthonyLaw <anthony@symbol.dev>
2024-04-10 04:34:31 +0800

    [snap/frontend]: added account balance component

---
+++ snap/frontend/components/AccountBalance/index.jsx
@@ -0,0 +1,19 @@
+import { useWalletContext } from '../../context';
+
+const AccountBalance = () => {
+	const { walletState } = useWalletContext();
+	const { mosaics, currency, currencyPerXYM } = walletState;
+
+	const xymBalance = mosaics.find(m => '10BA3BAA50DEB76C' === m.id)?.amount || 0;
+	const convertToCurrency = (xymBalance * currencyPerXYM).toFixed(2);
+
+	return (
+		<div className='flex flex-col items-center'>
+			<div className='text-2xl font-bold'> {xymBalance} XYM</div>
+			<div className='text-2xl text-[#BEBEBE]'>{convertToCurrency} {currency}</div>
+		</div>
+	);
+
+};
+
+export default AccountBalance;
