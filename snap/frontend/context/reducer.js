d8fd3d37c7e893c06b97a0d3b96e04a854d5c499
AnthonyLaw <anthony@symbol.dev>
2024-04-13 02:22:36 +0800

    [snap/frontend]: add mock data in home page

--- snap/frontend/context/reducer.js
+++ snap/frontend/context/reducer.js
@@ -1,6 +1,41 @@
 export const initialState = {
 	isMetamaskInstalled: false,
-	isSnapInstalled: false
+	isSnapInstalled: false,
+	finalizedHeight: 1984644,
+	currency: 'JPY',
+	currencyPerXYM: 4.03,
+	account: {
+		label: 'Account Wallet 1',
+		address: 'NDUGJWR3NASYOM4FMRCLD2UCLXEYQOVHSH4GVEI',
+		publicKey: '3DF89F171990ADEFEC112080558075DC1771E9B3470770F1103EA5C0D635D47D'
+	},
+	mosaics: [
+		{
+			id: '10BA3BAA50DEB76C',
+			name: 'symbol.xym',
+			amount: '10.000000'
+		},
+		{
+			id: '6B6511925501765B',
+			name: null,
+			amount: '10.000000'
+		},
+		{
+			id: '6AE25FA5E8CA0646',
+			name: null,
+			amount: '10'
+		},
+		{
+			id: '027C6AD49DE2C9F9',
+			name: 'comsa.cms',
+			amount: '10.00'
+		},
+		{
+			id: 'B27C6AD49AE2C9F9',
+			name: 'rootNamespace',
+			amount: '10.00'
+		}
+	]
 };
 
 export const actionTypes = {
