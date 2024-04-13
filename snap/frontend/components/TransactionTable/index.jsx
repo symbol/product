f38040a91166acd6d0f22560b4b217aeb25c372f
AnthonyLaw <anthony@symbol.dev>
2024-04-14 03:42:36 +0800

    [snap/frontend]: refactor transactions table component

--- snap/frontend/components/TransactionTable/index.jsx
+++ snap/frontend/components/TransactionTable/index.jsx
@@ -1,93 +1,99 @@
 import { useWalletContext } from '../../context';
 import Image from 'next/image';
 
-const TransactionTable =({transactions}) => {
+const TransactionTable =() => {
 
 	const { walletState } = useWalletContext();
-	const { account, finalizedHeight } = walletState;
-	const { address } = account;
+	const { selectedAccount, finalizedHeight, transactions } = walletState;
+	const { address } = selectedAccount;
 
 	return (
 		<div className='overflow-y-auto'>
 			<table className='w-full'>
 				<tbody>
 					{
-						transactions.map((transaction, index) => (
-							<tr key={`tx_row_${index}`} className='border-t-[1px] border-text h-16'>
-								<td className='px-2'>
-									{
-										transaction.sender === address ?
-											<Image
-												src='/send-icon.svg'
-												width={24}
-												height={24}
-												alt='Send logo'
-											/> :
-											<Image
-												src='/receive-icon.svg'
-												width={24}
-												height={24}
-												alt='Receive logo'
-											/>
-									}
-								</td>
+						(0 === transactions.length) ?
+							<tr className='flex justify-center'>
+								<td> No transactions found </td>
+							</tr>
+							:
 
-								<td className='px-2'>
-									{
-										'Transfer' === transaction.transactionType ?
-											transaction.sender === address ? 'Sent' : 'Received'
-											: transaction.transactionType
-									}
-
-									{
-										null === transaction.height ?
-											<div className='font-bold'>Pending</div> :
-											<div className='font-bold text-green'>Confirmed</div>
-									}
-								</td>
-
-								<td className='flex pt-2 text-sub-title'>
-									<div className='flex flex-col items-end pr-2'>
+							transactions.map((transaction, index) => (
+								<tr key={`tx_row_${index}`} className='border-t-[1px] border-text h-16'>
+									<td className='px-2'>
 										{
-											null !== transaction.amount ?
-												<>
-													<div>{transaction.amount} XYM</div>
-													<div>{transaction.currency}</div>
-												</>
-												: null
-										}
-									</div>
-									{
-										null !== transaction.message ?
-											<Image
-												src='/message-icon.svg'
-												width={24}
-												height={24}
-												alt='Message logo'
-											/> :
-											null
-									}
-								</td>
-
-								<td className='text-sub-title'>
-									<div className='flex items-center'>
-										{transaction.height}
-										{
-											finalizedHeight >= transaction.height && transaction.height ?
+											transaction.sender === address ?
 												<Image
-													className='pl-2'
-													src='/finalized-icon.svg'
+													src='/send-icon.svg'
 													width={24}
 													height={24}
-													alt='Finalized logo'
-												/> : null
+													alt='Send logo'
+												/> :
+												<Image
+													src='/receive-icon.svg'
+													width={24}
+													height={24}
+													alt='Receive logo'
+												/>
 										}
-									</div>
+									</td>
 
-									{transaction.date}
-								</td>
-							</tr>
-						))
+									<td className='px-2'>
+										{
+											'Transfer' === transaction.transactionType ?
+												transaction.sender === address ? 'Sent' : 'Received'
+												: transaction.transactionType
+										}
+
+										{
+											null === transaction.height ?
+												<div className='font-bold'>Pending</div> :
+												<div className='font-bold text-green'>Confirmed</div>
+										}
+									</td>
+
+									<td className='flex pt-2 text-sub-title'>
+										<div className='flex flex-col items-end pr-2'>
+											{
+												null !== transaction.amount ?
+													<>
+														<div>{transaction.amount} XYM</div>
+														<div>{transaction.currency}</div>
+													</>
+													: null
+											}
+										</div>
+										{
+											null !== transaction.message ?
+												<Image
+													src='/message-icon.svg'
+													width={24}
+													height={24}
+													alt='Message logo'
+												/> :
+												null
+										}
+									</td>
+
+									<td className='text-sub-title'>
+										<div className='flex items-center'>
+											{transaction.height}
+											{
+												finalizedHeight >= transaction.height && transaction.height ?
+													<Image
+														className='pl-2'
+														src='/finalized-icon.svg'
+														width={24}
+														height={24}
+														alt='Finalized logo'
+													/> : null
+											}
+										</div>
+
+										{transaction.date}
+									</td>
+								</tr>
+							))
 					}
 				</tbody>
 			</table>
