9c8b60b83128c391baa23558fd730a5449442862
AnthonyLaw <anthony@symbol.dev>
2024-04-14 03:37:13 +0800

    [snap/frontend]: added global loading screen

--- snap/frontend/context/reducer.js
+++ snap/frontend/context/reducer.js
@@ -1,46 +1,15 @@
 export const initialState = {
 	isMetamaskInstalled: false,
 	isSnapInstalled: false,
-	finalizedHeight: 1984644,
-	currency: 'JPY',
-	currencyPerXYM: 4.03,
-	account: {
-		label: 'Account Wallet 1',
-		address: 'NDUGJWR3NASYOM4FMRCLD2UCLXEYQOVHSH4GVEI',
-		publicKey: '3DF89F171990ADEFEC112080558075DC1771E9B3470770F1103EA5C0D635D47D'
-	},
-	mosaics: [
-		{
-			id: '10BA3BAA50DEB76C',
-			name: 'symbol.xym',
-			amount: '10.000000'
-		},
-		{
-			id: '6B6511925501765B',
-			name: null,
-			amount: '10.000000'
-		},
-		{
-			id: '6AE25FA5E8CA0646',
-			name: null,
-			amount: '10'
-		},
-		{
-			id: '027C6AD49DE2C9F9',
-			name: 'comsa.cms',
-			amount: '10.00'
-		},
-		{
-			id: 'B27C6AD49AE2C9F9',
-			name: 'rootNamespace',
-			amount: '10.00'
-		}
-	]
+	isLoading: false,
+	loadingMessage: '',
 };
 
 export const actionTypes = {
 	SET_METAMASK_INSTALLED: 'setMetamaskInstalled',
-	SET_SNAP_INSTALLED: 'setSnapInstalled'
+	SET_SNAP_INSTALLED: 'setSnapInstalled',
+	SET_IS_LOADING: 'setIsLoading',
+	SET_LOADING_MESSAGE: 'setLoadingMessage'
 };
 
 export const reducer = (state, action) => {
@@ -49,6 +18,10 @@ export const reducer = (state, action) => {
 		return { ...state, isMetamaskInstalled: action.payload };
 	case actionTypes.SET_SNAP_INSTALLED:
 		return { ...state, isSnapInstalled: action.payload };
+	case actionTypes.SET_IS_LOADING:
+		return { ...state, isLoading: action.payload };
+	case actionTypes.SET_LOADING_MESSAGE:
+		return { ...state, loadingMessage: action.payload };
 	default:
 		return state;
 	}
