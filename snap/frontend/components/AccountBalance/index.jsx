842cf9e906ed4c69dcab0b13ee9e41feebc65608
AnthonyLaw <anthony@symbol.dev>
2024-04-14 03:39:57 +0800

    [snap/frontend]: refactor account balance component

--- snap/frontend/components/AccountBalance/index.jsx
+++ snap/frontend/components/AccountBalance/index.jsx
@@ -2,7 +2,8 @@ import { useWalletContext } from '../../context';
 
 const AccountBalance = () => {
 	const { walletState } = useWalletContext();
-	const { mosaics, currency, currencyPerXYM } = walletState;
+	const { mosaics, currency } = walletState;
+	const { symbol, currencyPerXYM } = currency;
 
 	const xymBalance = mosaics.find(m => '10BA3BAA50DEB76C' === m.id)?.amount || 0;
 	const convertToCurrency = (xymBalance * currencyPerXYM).toFixed(2);
@@ -10,7 +11,7 @@ const AccountBalance = () => {
 	return (
 		<div className='flex flex-col items-center'>
 			<div className='text-2xl font-bold'> {xymBalance} XYM</div>
-			<div className='text-2xl text-sub-title'>{convertToCurrency} {currency}</div>
+			<div className='text-2xl text-sub-title'>{convertToCurrency} {symbol}</div>
 		</div>
 	);
 
