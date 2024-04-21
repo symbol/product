051633c6b45ba54e81af964c627964c2088134cd
AnthonyLaw <anthony@symbol.dev>
2024-04-22 04:17:27 +0800

    [snap/backend]: install and config symbol-sdk

--- snap/backend/snap.config.js
+++ snap/backend/snap.config.js
@@ -1,10 +1,25 @@
+import webpack from 'webpack';
+
 const config = {
 	bundler: 'webpack',
 	input: 'src/index.js',
 	server: {
 		port: 8080
 	},
-	polyfills: true
+	polyfills: true,
+	customizeWebpackConfig(webpackConfig) {
+		// Disable the `symbol-crypto-wasm-node` package, which is not compatible
+		webpackConfig.plugins.push(
+			new webpack.NormalModuleReplacementPlugin(
+				/symbol-crypto-wasm-node/,
+				'empty-module'
+			),
+			new webpack.ProvidePlugin({
+				process: 'process/browser'
+			})
+		);
+		return webpackConfig;
+	}
 };
 
 export default config;
