1f380c69653018fc0394d483240bf3835f9d7347
AnthonyLaw <anthony@symbol.dev>
2024-04-29 23:44:09 +0800

    [snap/frontend]: implement account list modal box component in account info

--- snap/frontend/components/AccountInfo/index.jsx
+++ snap/frontend/components/AccountInfo/index.jsx
@@ -1,16 +1,28 @@
 import { useWalletContext } from '../../context';
+import AccountListModalBox from '../AccountListModalBox';
 import Address from '../Address';
+import { useState } from 'react';
 
 const AccountInfo = () => {
 	const { walletState } = useWalletContext();
-	const { selectedAccount } = walletState;
+	const { accounts, selectedAccount } = walletState;
+
+	const [ isOpenAccountListModalBox, setOpenAccountListModalBox ] = useState(false);
+
+	const handleAccountListModalBox = () => {
+		setOpenAccountListModalBox(!isOpenAccountListModalBox);
+	};
 
 	return (
-		selectedAccount && <div className='flex flex-col items-center justify-center p-2'>
-			<div role='profile-image' className="rounded-full w-16 h-16 bg-gray-300" />
-			{/* Text Address and wallet label */}
-			<Address account={selectedAccount} />
-		</div>
+		<>
+			<AccountListModalBox accounts={accounts} isOpen={isOpenAccountListModalBox} onRequestClose={setOpenAccountListModalBox} />
+
+			<div className='flex flex-col items-center justify-center p-2'>
+				<div role='profile-image' className="rounded-full w-16 h-16 bg-gray-300" onClick={handleAccountListModalBox}/>
+				{/* Text Address and wallet label */}
+				<Address account={selectedAccount} />
+			</div>
+		</>
 	);
 };
 
