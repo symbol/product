50382948b9713eb8f727d39bad30ee6a2682e1c9
AnthonyLaw <anthony@symbol.dev>
2024-04-29 23:42:37 +0800

    [snap/frontend]: added disabled in input box components

--- snap/frontend/components/Input/index.jsx
+++ snap/frontend/components/Input/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Input = ({ label, placeholder, value, onChange, type = 'text', className, ...props }) => {
+const Input = ({ label, placeholder, value, onChange, type = 'text', className, disabled = false, ...props }) => {
 	const combinedClassName = `flex flex-col ${className || ''}`;
 
 	return (
@@ -11,7 +11,8 @@ const Input = ({ label, placeholder, value, onChange, type = 'text', className, 
 				value={value}
 				placeholder={placeholder}
 				onChange={e => onChange(e.target.value)}
-				className="px-4 py-2 text-black bg-[#D9D9D9] rounded-xl"
+				className="w-full px-4 py-2 text-black bg-[#D9D9D9] rounded-xl"
+				disabled={disabled}
 			/>
 		</div>
 	);
