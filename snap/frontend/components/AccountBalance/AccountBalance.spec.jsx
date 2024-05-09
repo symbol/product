882ea3bfc991eaee2359035d1bd750ce46f7a09a
AnthonyLaw <anthony@symbol.dev>
2024-05-10 00:42:34 +0800

    [snap/frontend]: Added unit test for account balance component

---
+++ snap/frontend/components/AccountBalance/AccountBalance.spec.jsx
@@ -0,0 +1,64 @@
+import AccountBalance from '.';
+import testHelper from '../testHelper';
+import { screen } from '@testing-library/react';
+
+const context = {
+	dispatch: jest.fn(),
+	walletState: {
+		currency: {
+			symbol: 'usd',
+			currencyPerXYM: 0.25
+		}
+	}
+};
+
+describe('components/AccountBalance', () => {
+	const assertAccountBalance = (context, xymBalance, convertedCurrency) => {
+		// Arrange:
+		testHelper.customRender(<AccountBalance />, context);
+
+		// Act:
+		const xymBalanceElement = screen.getByText(xymBalance);
+		const convertedCurrencyElement = screen.getByText(convertedCurrency);
+
+		// Assert:
+		expect(xymBalanceElement).toBeInTheDocument();
+		expect(convertedCurrencyElement).toBeInTheDocument();
+	};
+
+	it('renders xym balance and converted currency when found symbol.xym mosaic', () => {
+		// Arrange:
+		context.walletState.mosaics = [
+			{
+				id: 'E74B99BA41F4AFEE',
+				name: 'symbol.xym',
+				amount: 100
+			},
+			{
+				id: '3C596F764B5A1160',
+				name: null,
+				amount: 2
+			}
+		];
+
+		assertAccountBalance(context, '100 XYM', '25.00 usd');
+	});
+
+	it('renders xym balance and converted currency with 0 balance when symbol.xym mosaic does not found', () => {
+		// Arrange:
+		context.walletState.mosaics = [
+			{
+				id: '3C596F764B5A1160',
+				name: null,
+				amount: 2
+			}
+		];
+
+		assertAccountBalance(context, '0 XYM', '0.00 usd');
+	});
+
+	it('renders xym balance and converted currency with 0 balance when mosaics is empty', () => {
+		assertAccountBalance(context, '0 XYM', '0.00 usd');
+	});
+
+});
