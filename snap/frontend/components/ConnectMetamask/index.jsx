4ed7b1b6808ad025bbc9202f88616e3864658aed
AnthonyLaw <anthony@symbol.dev>
2024-05-16 23:23:54 +0800

    [snap/frontend]: Implement symbolSnapFactory and update unit test

--- snap/frontend/components/ConnectMetamask/index.jsx
+++ snap/frontend/components/ConnectMetamask/index.jsx
@@ -1,15 +1,14 @@
 import { actionTypes, useWalletContext } from '../../context';
-import symbolSnap from '../../utils/snap';
 import WarningModalBox from '../WarningModalBox';
 
 const ConnectMetamask = ({ isOpen, onRequestClose }) => {
-	const { dispatch } = useWalletContext();
+	const { dispatch, symbolSnap } = useWalletContext();
 
 	const title = 'Connect to MetaMask Symbol Snap';
 	const description = 'If you do not have the Symbol snap installed you will be prompted to install it.';
 
 	const handleConnectClick = async () => {
-		const isConnected = await symbolSnap().connectSnap();
+		const isConnected = await symbolSnap.connectSnap();
 
 		dispatch({ type: actionTypes.SET_SNAP_INSTALLED, payload: isConnected });
 	};
--- snap/frontend/hooks/useWalletInstallation.jsx
+++ snap/frontend/hooks/useWalletInstallation.jsx
@@ -1,25 +1,24 @@
 import { actionTypes, useWalletContext } from '../context';
-import symbolSnap from '../utils/snap';
 import { useEffect } from 'react';
 
 const useWalletInstallation = () => {
-	const { walletState, dispatch } = useWalletContext();
+	const { walletState, dispatch, symbolSnap } = useWalletContext();
 	const { isMetamaskInstalled, isSnapInstalled } = walletState;
 
 	useEffect(() => {
 		const checkInstallationStatus = async () => {
-			if (window.ethereum && window.ethereum.isMetaMask) {
+			if (window.ethereum && window.ethereum.isMetaMask)
 				dispatch({ type: actionTypes.SET_METAMASK_INSTALLED, payload: true });
 
-				const installedSnap = await symbolSnap().getSnap();
+			const installedSnap = await symbolSnap.getSnap();
 
-				if (installedSnap && installedSnap.enabled)
-					dispatch({ type: actionTypes.SET_SNAP_INSTALLED, payload: true });
-			}
+			if (installedSnap && installedSnap.enabled)
+				dispatch({ type: actionTypes.SET_SNAP_INSTALLED, payload: true });
+
 		};
 
 		checkInstallationStatus();
-	}, [dispatch]);
+	}, [dispatch, symbolSnap]);
 
 	return { isMetamaskInstalled, isSnapInstalled };
 };
