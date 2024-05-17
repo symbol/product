a15bf5b107e80b602015e3a08f0ff8eee753e365
AnthonyLaw <anthony@symbol.dev>
2024-05-17 16:05:14 +0800

    [snap/frontend]: Removed isMetamaskInstalled boolean status, and refactor effect component

--- snap/frontend/hooks/useWalletInstallation.jsx
+++ snap/frontend/hooks/useWalletInstallation.jsx
@@ -3,24 +3,20 @@ import { useEffect } from 'react';
 
 const useWalletInstallation = () => {
 	const { walletState, dispatch, symbolSnap } = useWalletContext();
-	const { isMetamaskInstalled, isSnapInstalled } = walletState;
+	const { isSnapInstalled } = walletState;
 
 	useEffect(() => {
 		const checkInstallationStatus = async () => {
-			if (window.ethereum && window.ethereum.isMetaMask)
-				dispatch({ type: actionTypes.SET_METAMASK_INSTALLED, payload: true });
-
 			const installedSnap = await symbolSnap.getSnap();
 
 			if (installedSnap && installedSnap.enabled)
 				dispatch({ type: actionTypes.SET_SNAP_INSTALLED, payload: true });
-
 		};
 
 		checkInstallationStatus();
 	}, [dispatch, symbolSnap]);
 
-	return { isMetamaskInstalled, isSnapInstalled };
+	return { isSnapInstalled };
 };
 
 export default useWalletInstallation;
