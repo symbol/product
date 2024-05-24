3dd5b8112fb1e030dbf36774fe66d08a4eb8c9c9
AnthonyLaw <anthony@symbol.dev>
2024-05-25 00:44:18 +0800

    [snap/backend]: refactor, and added unit test

--- snap/backend/src/stateManager.js
+++ snap/backend/src/stateManager.js
@@ -1,27 +1,22 @@
-const manageStateSnapRequest = async (operation, newState) => {
-	try {
-		return await snap.request({
+const stateManager = {
+	async manageStateSnapRequest(operation, newState) {
+		return snap.request({
 			method: 'snap_manageState',
 			params: {
 				operation,
 				newState
 			}
 		});
-	} catch (error) {
-		throw new Error('Error in manageStateSnapRequest:', error);
-	}
-};
-
-const stateManager = {
+	},
 	async update(state) {
-		await manageStateSnapRequest('update', state);
+		await this.manageStateSnapRequest('update', state);
 	},
 	async getState() {
-		const state = await manageStateSnapRequest('get');
+		const state = await this.manageStateSnapRequest('get');
 		return state;
 	},
 	async clear() {
-		await manageStateSnapRequest('clear');
+		await this.manageStateSnapRequest('clear');
 	}
 };
 
