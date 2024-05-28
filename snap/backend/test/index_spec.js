0a6046b0caedb858201e551ea2655f38f86aa59c
AnthonyLaw <anthony@symbol.dev>
2024-05-28 22:31:09 +0800

    [snap/backend]: refactor onRpcRequest and added unit test

--- snap/backend/test/index_spec.js
+++ snap/backend/test/index_spec.js
@@ -1,43 +1,114 @@
-import { expect } from '@jest/globals';
-import snapsJest from '@metamask/snaps-jest';
-import { panel, text } from '@metamask/snaps-sdk';
+import { onRpcRequest } from '../src/index.js';
+import statisticsClient from '../src/services/statisticsClient.js';
+import stateManager from '../src/stateManager.js';
+import {
+	describe, expect, it, jest
+} from '@jest/globals';
 
-const { installSnap } = snapsJest;
+jest.spyOn(stateManager, 'getState').mockResolvedValue();
+jest.spyOn(stateManager, 'update').mockResolvedValue();
+jest.spyOn(statisticsClient, 'getNodeInfo').mockResolvedValue();
+global.snap = {
+	request: jest.fn()
+};
 
 describe('onRpcRequest', () => {
-	describe('hello', () => {
-		it('shows a confirmation dialog', async () => {
-			const { request } = await installSnap();
+	const mockNodeInfo = {
+		identifier: 104,
+		networkName: 'mainnet',
+		url: 'http://localhost:3000'
+	};
 
-			const origin = 'Jest';
-			const response = request({
-				method: 'hello',
-				origin
+	beforeEach(() => {
+		jest.clearAllMocks();
+
+		stateManager.getState.mockResolvedValue({
+			network: mockNodeInfo
+		});
+	});
+
+	describe('initialize state', () => {
+		it('set default network (mainnet) if state is empty', async () => {
+			// Arrange:
+			stateManager.getState.mockResolvedValue(null);
+			statisticsClient.getNodeInfo.mockResolvedValue(mockNodeInfo);
+
+			// Act:
+			await onRpcRequest({
+				request: {
+					method: 'getNetwork'
+				}
 			});
 
-			const ui = await response.getInterface();
-			expect(ui.type).toBe('confirmation');
-			expect(ui).toRender(panel([text(`Hello, **${origin}**!`)]));
+			// Assert:
+			expect(statisticsClient.getNodeInfo).toHaveBeenCalledWith('mainnet');
+			expect(stateManager.update).toHaveBeenCalledWith({
+				network: {
+					...mockNodeInfo
+				}
+			});
+		});
 
-			await ui.ok();
+		it('does not set default network if state is not empty', async () => {
+			// Act:
+			await onRpcRequest({
+				request: {
+					method: 'getNetwork'
+				}
+			});
 
-			expect(await response).toRespondWith(true);
+			// Assert:
+			expect(statisticsClient.getNodeInfo).not.toHaveBeenCalled();
+			expect(stateManager.update).not.toHaveBeenCalled();
 		});
 	});
 
 	it('throws an error if the requested method does not exist', async () => {
-		const { request, close } = await installSnap();
+		// Act + Assert:
+		await expect(onRpcRequest({
+			request: {
+				method: 'unknownMethod'
+			}
+		})).rejects.toThrow('Method not found.');
+	});
 
-		const response = await request({
-			method: 'foo'
+	describe('getNetwork', () => {
+		it('returns the current network', async () => {
+			// Act:
+			const response = await onRpcRequest({
+				request: {
+					method: 'getNetwork'
+				}
+			});
+
+			// Assert:
+			expect(response).toStrictEqual(mockNodeInfo);
 		});
+	});
 
-		expect(response).toRespondWithError({
-			code: -32603,
-			message: 'Method not found.',
-			stack: expect.any(String)
+	describe('switchNetwork', () => {
+		it('switches the network', async () => {
+			// Arrange:
+			const mockExpectedNodeInfo = {
+				identifier: 152,
+				networkName: 'testnet',
+				url: 'http://localhost:3000'
+			};
+
+			statisticsClient.getNodeInfo.mockResolvedValue(mockExpectedNodeInfo);
+
+			// Act:
+			const response = await onRpcRequest({
+				request: {
+					method: 'switchNetwork',
+					params: {
+						networkName: 'testnet'
+					}
+				}
+			});
+
+			// Assert:
+			expect(response).toStrictEqual(mockExpectedNodeInfo);
 		});
-
-		await close();
 	});
 });
