c9104d81f3f2a3938b9f61d243a2f5f638bcefdb
AnthonyLaw <anthony@symbol.dev>
2024-05-28 22:31:09 +0800

    [snap/backend]: refactor onRpcRequest and added unit test

--- snap/backend/src/index.js
+++ snap/backend/src/index.js
@@ -1,18 +1,42 @@
-import { panel, text } from '@metamask/snaps-sdk';
+import statisticsClient from './services/statisticsClient.js';
+import stateManager from './stateManager.js';
+import networkUtils from './utils/networkUtils.js';
+import { SymbolFacade } from 'symbol-sdk/symbol';
 
 // eslint-disable-next-line import/prefer-default-export
-export const onRpcRequest = async ({ origin, request }) => {
-	switch (request.method) {
-	case 'hello':
-		return snap.request({
-			method: 'snap_dialog',
-			params: {
-				type: 'confirmation',
-				content: panel([
-					text(`Hello, **${origin}**!`)
-				])
+export const onRpcRequest = async ({ request }) => {
+	const requestParams = request?.params || {};
+
+	let state = await stateManager.getState();
+
+	if (!state) {
+		// initialize state if empty and set default data
+		const nodeInfo = await statisticsClient.getNodeInfo('mainnet');
+
+		state = {
+			network: {
+				...nodeInfo
 			}
-		});
+		};
+
+		await stateManager.update(state);
+	}
+
+	const facade = new SymbolFacade(state.network.networkName);
+
+	const apiParams = {
+		state,
+		requestParams,
+		snap,
+		facade
+	};
+
+	// handle request
+	switch (request.method) {
+	case 'getNetwork':
+		return state.network;
+	case 'switchNetwork':
+		return networkUtils.switchNetwork(apiParams);
 	default:
 		throw new Error('Method not found.');
 	}
