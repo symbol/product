ae2e4fd0079dc5f41b0f4ac0d85a8b838961890a
AnthonyLaw <anthony@symbol.dev>
2024-05-30 10:36:24 +0800

    [snap/frontend]: Implement dispatch utils in other components

--- snap/frontend/hooks/useWalletInstallation.spec.jsx
+++ snap/frontend/hooks/useWalletInstallation.spec.jsx
@@ -5,7 +5,9 @@ import { act, renderHook } from '@testing-library/react';
 describe('hooks/useWalletInstallation', () => {
 	const assertSetSnapInstalledDispatch = async (isSymbolSnapInstalled, expectedDispatchCall) => {
 		// Arrange:
-		const dispatch = jest.fn();
+		const dispatch = {
+			setIsSnapInstalled: jest.fn()
+		};
 		const walletState = { isSnapInstalled: false };
 		const symbolSnap = { getSnap: jest.fn().mockResolvedValue({ enabled: isSymbolSnapInstalled })};
 		const context = { dispatch, walletState, symbolSnap };
@@ -20,15 +22,15 @@ describe('hooks/useWalletInstallation', () => {
 		await act(async () => renderHook(() => useWalletInstallation(), { wrapper }));
 
 		// Assert:
-		if (expectedDispatchCall) 
-			expect(dispatch).toHaveBeenCalledWith(expectedDispatchCall);
-		 else 
-			expect(dispatch).not.toHaveBeenCalled();
-		
+		if (expectedDispatchCall)
+			expect(dispatch.setIsSnapInstalled).toHaveBeenCalledWith(expectedDispatchCall);
+		else
+			expect(dispatch.setIsSnapInstalled).not.toHaveBeenCalled();
+
 	};
 
 	it('dispatch setSnapInstalled action when snap is installed and enabled', async () => {
-		await assertSetSnapInstalledDispatch(true, { type: 'setSnapInstalled', payload: true });
+		await assertSetSnapInstalledDispatch(true, true);
 	});
 
 	it('does not dispatch setSnapInstalled action when snap is not installed', async () => {
