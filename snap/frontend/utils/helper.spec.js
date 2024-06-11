2715e0de45a98abe18e5582b727ecbf55ecf1ee5
AnthonyLaw <anthony@symbol.dev>
2024-06-11 23:27:34 +0800

    [snap/frontend]: Extract initializeSnap logic in helper, which can share in different components

---
+++ snap/frontend/utils/helper.spec.js
@@ -0,0 +1,94 @@
+import helper from './helper';
+
+describe('helper', () => {
+	describe('setupSnap', () => {
+		beforeEach(() => {
+			jest.clearAllMocks();
+		});
+
+		const dispatch = {
+			setLoadingStatus: jest.fn(),
+			setNetwork: jest.fn(),
+			setSelectedAccount: jest.fn()
+		};
+
+		const symbolSnap = {
+			initialSnap: jest.fn(),
+			createAccount: jest.fn()
+		};
+
+		const mockSnapState = {
+			network: {
+				identifier: 152,
+				networkName: 'testnet',
+				url: 'http://localhost:3000'
+			},
+			accounts: {}
+		};
+
+		const assertSetupSnap = (mockSnapState, networkName) => {
+			// Assert:
+			expect(dispatch.setLoadingStatus).toHaveBeenNthCalledWith(1, {
+				isLoading: true,
+				message: 'Initializing Snap...'
+			});
+			expect(dispatch.setLoadingStatus).toHaveBeenNthCalledWith(2, {
+				isLoading: false,
+				message: ''
+			});
+			expect(symbolSnap.initialSnap).toHaveBeenCalledWith(networkName);
+			expect(dispatch.setNetwork).toHaveBeenCalledWith(mockSnapState.network);
+		};
+
+		it('initializes snap and sets network and selected account if accounts exist', async () => {
+			// Arrange:
+			const networkName = 'testnet';
+
+			mockSnapState.accounts = {
+				'0x1': {
+					id: '0x1',
+					address: 'address',
+					label: 'Primary Account',
+					networkName: 'testnet'
+				},
+				'0x2': {
+					id: '0x2',
+					address: 'address',
+					label: 'Primary Account',
+					networkName: 'testnet'
+				}
+			};
+
+			symbolSnap.initialSnap.mockResolvedValue(mockSnapState);
+
+			// Act:
+			await helper.setupSnap(dispatch, symbolSnap, networkName);
+
+			// Assert:
+			assertSetupSnap(mockSnapState, networkName);
+			expect(symbolSnap.createAccount).not.toHaveBeenCalled();
+			expect(dispatch.setSelectedAccount).toHaveBeenCalledWith(Object.values(mockSnapState.accounts)[0]);
+		});
+
+		it('initializes snap and creates account when no accounts are found', async () => {
+			// Arrange:
+			const networkName = 'testnet';
+			mockSnapState.accounts = {};
+			const mockAccount = {
+				id: '0x1',
+				name: 'Wallet 1'
+			};
+
+			symbolSnap.initialSnap.mockResolvedValue(mockSnapState);
+			symbolSnap.createAccount.mockResolvedValue(mockAccount);
+
+			// Act:
+			await helper.setupSnap(dispatch, symbolSnap, networkName);
+
+			// Assert:
+			assertSetupSnap(mockSnapState, networkName);
+			expect(symbolSnap.createAccount).toHaveBeenCalledWith('Wallet 1');
+			expect(dispatch.setSelectedAccount).toHaveBeenCalledWith(mockAccount);
+		});
+	});
+});
