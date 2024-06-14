4365a3c3a3856c083e3176f15a70efea1309ba63
Oleg Makarenko <olegomm@gmail.com>
2024-06-14 21:33:17 +0200

    [explorer/frontend] feat: add tests for TransactionList page

---
+++ explorer/frontend/__tests__/pages/TransactionList.test.js
@@ -0,0 +1,81 @@
+import '@testing-library/jest-dom';
+import 'react-intersection-observer/test-utils';
+import { setDevice } from '../test-utils/device';
+import { transactionStatsResult } from '../test-utils/stats';
+import { transactionPageResult } from '../test-utils/transactions';
+import * as StatsService from '@/api/stats';
+import * as TransactionService from '@/api/transactions';
+import TransactionList, { getServerSideProps } from '@/pages/transactions/index.jsx';
+import * as utils from '@/utils';
+import { render, screen } from '@testing-library/react';
+
+jest.mock('@/api/transactions', () => {
+	return {
+		__esModule: true,
+		...jest.requireActual('@/api/transactions')
+	};
+});
+
+jest.mock('@/api/stats', () => {
+	return {
+		__esModule: true,
+		...jest.requireActual('@/api/stats')
+	};
+});
+
+describe('TransactionList', () => {
+	describe('getServerSideProps', () => {
+		it('fetches transaction list and statistics', async () => {
+			// Arrange:
+			const locale = 'en';
+			const fetchTransactionPage = jest.spyOn(TransactionService, 'fetchTransactionPage');
+			fetchTransactionPage.mockResolvedValue(transactionPageResult);
+			const fetchTransactionStats = jest.spyOn(StatsService, 'fetchTransactionStats');
+			fetchTransactionStats.mockResolvedValue(transactionStatsResult);
+			const expectedResult = {
+				props: {
+					preloadedData: transactionPageResult.data,
+					stats: transactionStatsResult
+				}
+			};
+
+			// Act:
+			const result = await getServerSideProps({ locale });
+
+			// Assert:
+			expect(fetchTransactionPage).toHaveBeenCalledWith();
+			expect(fetchTransactionStats).toHaveBeenCalledWith();
+			expect(result).toEqual(expectedResult);
+		});
+	});
+
+	describe('page', () => {
+		const runTest = () => {
+			// Arrange:
+			const pageSectionText = 'section_transactions';
+			const transactionHashes = transactionPageResult.data.map(transaction => utils.truncateString(transaction.hash, 'hash'));
+
+			// Act:
+			render(<TransactionList preloadedData={transactionPageResult.data} stats={transactionStatsResult} />);
+
+			// Assert:
+			expect(screen.getByText(pageSectionText)).toBeInTheDocument();
+			transactionHashes.forEach(hash => {
+				expect(screen.getByText(hash)).toBeInTheDocument();
+			});
+		};
+
+		it('renders page with the list of transactions on desktop', () => {
+			// Act & Assert:
+			runTest();
+		});
+
+		it('renders page with the list of transactions on mobile', () => {
+			// Arrange:
+			setDevice('mobile');
+
+			// Act & Assert:
+			runTest();
+		});
+	});
+});
