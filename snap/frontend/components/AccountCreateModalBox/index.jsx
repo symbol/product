65bdf610f8507074100f3c17fc405aef19b02761
AnthonyLaw <anthony@symbol.dev>
2024-06-22 03:54:02 +0800

    [snap/frontend]: Rename 'wallet' to 'account'

--- snap/frontend/components/AccountCreateModalBox/index.jsx
+++ snap/frontend/components/AccountCreateModalBox/index.jsx
@@ -9,43 +9,43 @@ const AccountCreateModalBox = ({ isOpen, onRequestClose }) => {
 	const { walletState, dispatch, symbolSnap } = useWalletContext();
 	const { accounts } = walletState;
 
-	const [walletName, setWalletName] = useState('');
+	const [accountName, setAccountName] = useState('');
 	const [error, setError] = useState('');
 
-	const isWalletNameExist = newWalletName => {
-		return Object.values(accounts).some(account => account.label.toUpperCase() === newWalletName.toUpperCase());
+	const isAccountNameExist = newAccountName => {
+		return Object.values(accounts).some(account => account.label.toUpperCase() === newAccountName.toUpperCase());
 	};
 
-	const validateWalletName = walletName => {
-		if ('' === walletName.trim())
-			return 'Wallet name is required';
-		if (isWalletNameExist(walletName))
-			return 'Wallet name already exists';
+	const validateAccountName = accountName => {
+		if ('' === accountName.trim())
+			return 'Account name is required';
+		if (isAccountNameExist(accountName))
+			return 'Account name already exists';
 	};
 
 	const handleCreateNewAccount = async () => {
-		if (error || '' === walletName)
+		if (error || '' === accountName)
 			return;
 
-		await helper.createNewAccount(dispatch, symbolSnap, accounts, walletName);
+		await helper.createNewAccount(dispatch, symbolSnap, accounts, accountName);
 
-		setWalletName('');
+		setAccountName('');
 		onRequestClose(false);
 	};
 
-	const handleOnChangeWalletName = newWalletName => {
-		setWalletName(newWalletName);
-		setError(validateWalletName(newWalletName));
+	const handleOnChangeAccountName = newAccountName => {
+		setAccountName(newAccountName);
+		setError(validateAccountName(newAccountName));
 	};
 
 	return (
 		<ModalBox isOpen={isOpen} onRequestClose={onRequestClose}>
 			<div className='flex flex-col px-5 text-center'>
 				<div className='text-2xl font-bold mb-6'>
-                        Create Wallet
+                        Create Account
 				</div>
 
-				<Input label='Wallet Name:' placeholder='Wallet Name' value={walletName} onChange={handleOnChangeWalletName} />
+				<Input label='Account Name:' placeholder='Account Name' value={accountName} onChange={handleOnChangeAccountName} />
 
 				{
 					error && <p className='text-red-500 text-xs'>{error}</p>
