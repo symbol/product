c260d71f21c6b0a2c2a9445ee23d77f2b1bbc927
AnthonyLaw <anthony@symbol.dev>
2024-06-21 17:20:44 +0800

    [snap/frontend]: Refactor and added more unit test

--- snap/frontend/components/AccountListModalBox/index.jsx
+++ snap/frontend/components/AccountListModalBox/index.jsx
@@ -1,10 +1,28 @@
+import { useWalletContext } from '../../context';
+import AccountCreateModalBox from '../AccountCreateModalBox';
 import Button from '../Button';
 import ModalBox from '../ModalBox';
+import { useState } from 'react';
 
-const AccountListModalBox = ({ accounts, isOpen, onRequestClose }) => {
+const AccountListModalBox = ({ isOpen, onRequestClose }) => {
+	const { walletState, dispatch } = useWalletContext();
+	const { accounts } = walletState;
+	const [ accountCreateModalBoxVisible, setAccountCreateModalBoxVisible ] = useState(false);
+
+	const handleOpenAccountCreateModalBox = () => {
+		setAccountCreateModalBoxVisible(!accountCreateModalBoxVisible);
+		onRequestClose(false);
+	};
+
+	const handleSelectAccount = account => {
+		dispatch.setSelectedAccount(account);
+		onRequestClose(false);
+	};
+
 	const renderAccount = account => {
 		return (
-			<div key={`wallet_${account.id}`} className='flex items-center'>
+			<div key={`wallet_${account.id}`} className='flex items-center cursor-pointer'
+				onClick={() => handleSelectAccount(account)}>
 				<div className="rounded-full w-10 h-10 bg-gray-300"/>
 				<div className='flex flex-col items-start justify-center p-2'>
 					<div className='font-bold'>{account.label}</div>
@@ -15,28 +33,32 @@ const AccountListModalBox = ({ accounts, isOpen, onRequestClose }) => {
 
 	};
 	return (
-		<ModalBox isOpen={isOpen} onRequestClose={onRequestClose}>
-			<div className='flex flex-col px-5 text-center'>
-				<div className='text-2xl font-bold mb-6'>
+		<>
+			<ModalBox isOpen={isOpen} onRequestClose={onRequestClose}>
+				<div className='flex flex-col px-5 text-center'>
+					<div className='text-2xl font-bold mb-6'>
                     Wallet
-				</div>
+					</div>
 
-				<div className='flex items-center justify-start mb-6 text-xs'>
-					{
-						Object.keys(accounts).map(key => renderAccount(accounts[key]))
-					}
-				</div>
+					<div className='flex flex-col overflow-y-auto h-72 items-center justify-start mb-6 text-xs'>
+						{
+							Object.keys(accounts).map(key => renderAccount(accounts[key]))
+						}
+					</div>
 
-				<div className='flex justify-center font-bold pt-2'>
-					<Button className='uppercase w-40 h-10 bg-secondary m-2'>
+					<div className='flex justify-center font-bold pt-2'>
+						<Button className='uppercase w-40 h-10 bg-secondary m-2' onClick={handleOpenAccountCreateModalBox}>
                         Create
-					</Button>
-					<Button className='uppercase w-40 h-10 bg-secondary m-2'>
+						</Button>
+						<Button className='uppercase w-40 h-10 bg-secondary m-2'>
                         Import
-					</Button>
+						</Button>
+					</div>
 				</div>
-			</div>
-		</ModalBox>
+			</ModalBox>
+
+			<AccountCreateModalBox isOpen={accountCreateModalBoxVisible} onRequestClose={setAccountCreateModalBoxVisible} />
+		</>
 	);
 };
 
