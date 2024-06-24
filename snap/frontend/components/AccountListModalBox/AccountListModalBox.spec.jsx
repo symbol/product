441d4587aa6cfd6e527c0665fcfc9470169374a2
AnthonyLaw <anthony@symbol.dev>
2024-06-25 00:57:35 +0800

    [snap/frontend]: Implement account creation form in create account form

--- snap/frontend/components/AccountListModalBox/AccountListModalBox.spec.jsx
+++ snap/frontend/components/AccountListModalBox/AccountListModalBox.spec.jsx
@@ -1,4 +1,5 @@
 import AccountListModalBox from '.';
+import helper from '../../utils/helper';
 import testHelper from '../testHelper';
 import { fireEvent, screen } from '@testing-library/react';
 
@@ -6,6 +7,7 @@ const context = {
 	dispatch: {
 		setSelectedAccount: jest.fn()
 	},
+	symbolSnap: {},
 	walletState: {
 		selectedAccount: {
 			address: 'address 1',
@@ -61,5 +63,103 @@ describe('components/AccountListModalBox', () => {
 		// Assert:
 		const createAccountScreen = screen.getByText('Create Account');
 		expect(createAccountScreen).toBeInTheDocument();
+	});
+
+	describe('create account modal box', () => {
+		beforeEach(() => {
+			jest.clearAllMocks();
+		});
+
+		it('renders account create form', () => {
+			// Arrange:
+			testHelper.customRender(<AccountListModalBox isOpen={true} onRequestClose={jest.fn()} />, context);
+
+			const createButton = screen.getByText('Create');
+			fireEvent.click(createButton);
+
+			// Act:
+			const accountNameInput = screen.getByPlaceholderText('Account Name');
+
+			// Assert:
+			expect(accountNameInput).toBeInTheDocument();
+		});
+
+		describe('validation', () => {
+			const assertValidation = (accountName, expectedErrorMessage) => {
+				// Arrange:
+				testHelper.customRender(<AccountListModalBox isOpen={true} onRequestClose={jest.fn()} />, context);
+
+				const createButton = screen.getByText('Create');
+				fireEvent.click(createButton);
+
+				const accountNameInput = screen.getByPlaceholderText('Account Name');
+				fireEvent.change(accountNameInput, { target: { value: accountName } });
+
+				const submitButton = screen.getByText('Submit');
+
+				// Act:
+				fireEvent.click(submitButton);
+
+				// Assert:
+				const accountNameError = screen.getByText(expectedErrorMessage);
+				expect(accountNameError).toBeInTheDocument();
+			};
+
+			it('can validate account name is required', () => {
+				assertValidation(' ', 'Account name is required');
+			});
+
+			it('can validate account name is exist', () => {
+				// Arrange:
+				context.walletState.accounts = {
+					'0x1234': {
+						address: 'address 1',
+						label: 'wallet 1'
+					}
+				};
+
+				assertValidation('wallet 1', 'Account name already exists');
+			});
+		});
+
+		describe('submit', () => {
+			jest.spyOn(helper, 'createNewAccount').mockImplementation();
+
+			const assertSubmit = (accountName, expectedResult) => {
+				// Arrange:
+				testHelper.customRender(<AccountListModalBox isOpen={true} onRequestClose={jest.fn()} />, context);
+
+				const createButton = screen.getByText('Create');
+				fireEvent.click(createButton);
+
+				const accountNameInput = screen.getByPlaceholderText('Account Name');
+				fireEvent.change(accountNameInput, { target: { value: accountName } });
+
+				const submitButton = screen.getByText('Submit');
+
+				// Act:
+				fireEvent.click(submitButton);
+
+				// Assert:
+				if (expectedResult) {
+					expect(helper.createNewAccount).toHaveBeenCalledWith(
+						context.dispatch,
+						context.symbolSnap,
+						context.walletState.accounts,
+						accountName
+					);
+				} else {
+					expect(helper.createNewAccount).not.toHaveBeenCalled();
+				}
+			};
+
+			it('can create account with valid data', () => {
+				assertSubmit('wallet 2', true);
+			});
+
+			it('does not create account with invalid data', () => {
+				assertSubmit(' ', false);
+			});
+		});
 	});
 });
