c974f64faf90117bb722a04950939ee03dfa168e
AnthonyLaw <anthony@symbol.dev>
2024-06-25 03:02:26 +0800

    [snap/frontend]: Installed symbol-sdk dependencies

--- snap/frontend/next.config.js
+++ snap/frontend/next.config.js
@@ -1,3 +1,16 @@
-const nextConfig = {};
+import webpack from 'webpack';
+
+const nextConfig = {
+	webpack: config => {
+		config.plugins.push(new webpack.NormalModuleReplacementPlugin(
+			/symbol-crypto-wasm-node/,
+			'empty-module'
+		));
+
+		config.experiments = { asyncWebAssembly: true, topLevelAwait: true, layers: true };
+
+		return config;
+	}
+};
 
 export default nextConfig;
