9a1f31d33522b8efb40122f8cf3f9bd4f28f71e5
AnthonyLaw <anthony@symbol.dev>
2024-06-26 23:56:13 +0800

    [snap/frontend]: Implement receive modal box in receive button

--- snap/frontend/components/ReceiveModalBox/index.jsx
+++ snap/frontend/components/ReceiveModalBox/index.jsx
@@ -32,7 +32,7 @@ const ReceiveModalBox = ({ isOpen, onRequestClose }) => {
 
 	return (
 		<ModalBox isOpen={isOpen} onRequestClose={onRequestClose}>
-			<div className='flex flex-col px-5 text-center'>
+			<div role='receive-qr' className='flex flex-col px-5 text-center'>
 				<div className='text-2xl font-bold mb-6'>
                     Receive
 				</div>
