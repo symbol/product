4e6f04ae7fe2ac283ddc6d898bbf4a090af3c0d3
AnthonyLaw <anthony@symbol.dev>
2024-06-26 23:21:16 +0800

    [snap/frontend]: Refactor Receive modal box and unit test

--- snap/frontend/components/ReceiveModalBox/index.jsx
+++ snap/frontend/components/ReceiveModalBox/index.jsx
@@ -1,16 +1,26 @@
+import { useWalletContext } from '../../context';
 import helper from '../../utils/helper';
 import Address from '../Address';
 import ModalBox from '../ModalBox';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
 
-const ReceiveModalBox = ({ account, isOpen, onRequestClose }) => {
+const ReceiveModalBox = ({ isOpen, onRequestClose }) => {
+	const { walletState } = useWalletContext();
+	const { selectedAccount, network } = walletState;
+
 	const [base64QR, setBase64QR] = useState('');
 
 	useEffect(() => {
 		const generateQR = async () => {
 			try {
-				const qr = await helper.generateAccountQRBase64(account);
+				const qr = await helper.generateAccountQRBase64(
+					selectedAccount.label,
+					selectedAccount.publicKey,
+					network.networkIdentifier,
+					network.networkGenerationHashSeed
+				);
+
 				setBase64QR(qr);
 			} catch (error) {
 				throw new Error(`Fail to generate QR: ${error}`);
@@ -18,7 +28,7 @@ const ReceiveModalBox = ({ account, isOpen, onRequestClose }) => {
 		};
 
 		generateQR();
-	}, [account]);
+	}, [selectedAccount, network]);
 
 	return (
 		<ModalBox isOpen={isOpen} onRequestClose={onRequestClose}>
@@ -28,18 +38,15 @@ const ReceiveModalBox = ({ account, isOpen, onRequestClose }) => {
 				</div>
 
 				<div className='flex items-center justify-center mb-6'>
-					{
-						base64QR && <Image
-							src={base64QR}
-							width={200}
-							height={200}
-							alt='Account QR'
-						/>
-					}
-
+					<Image
+						src={base64QR}
+						width={200}
+						height={200}
+						alt='Account QR'
+					/>
 				</div>
 
-				<Address account={account} />
+				<Address account={selectedAccount} />
 			</div>
 		</ModalBox>
 	);
