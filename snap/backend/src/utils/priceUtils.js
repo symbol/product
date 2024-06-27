1e0e5de4745fa2d480434ea3c3535ad8f1d88c11
AnthonyLaw <anthony@symbol.dev>
2024-06-28 03:34:58 +0800

    [snap/backend]: Added price utils

---
+++ snap/backend/src/utils/priceUtils.js
@@ -0,0 +1,26 @@
+import config from '../config.js';
+import cryptoCompareClient from '../services/cryptocompareClient.js';
+import stateManager from '../stateManager.js';
+
+const priceUtils = {
+	async getPrice({ state }) {
+		const price = await cryptoCompareClient.fetchPrice();
+
+		state.currencies = price;
+
+		await stateManager.update(state);
+	},
+	async getCurrencyPrice({ state, requestParams }) {
+		const { currency } = requestParams;
+
+		if (-1 === config.supportCurrency.indexOf(currency.toUpperCase()))
+			throw new Error('Currency not supported.');
+
+		return {
+			symbol: currency,
+			price: state.currencies[currency]
+		};
+	}
+};
+
+export default priceUtils;
