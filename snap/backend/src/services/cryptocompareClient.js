3133abea273ac69e16726d2448c231b5873291c1
AnthonyLaw <anthony@symbol.dev>
2024-06-28 03:24:50 +0800

    [snap/backend]: Added crypto compare client to query xym price in usd and jpy

---
+++ snap/backend/src/services/cryptocompareClient.js
@@ -0,0 +1,32 @@
+import config from '../config.js';
+import fetchUtils from '../utils/fetchUtils.js';
+
+const cryptoCompareClient = {
+	/**
+     * Fetch the price of the supported currencies.
+     * @returns {Promise<PriceInfo>} The price of the supported currencies.
+     */
+	fetchPrice: async () => {
+		try {
+			const currencies = config.supportCurrency.join(',');
+			const prices = await fetchUtils.fetchData(`${config.cryptoCompareURL}${currencies}`, 'GET');
+
+			return prices;
+		} catch (error) {
+			throw new Error(`Failed to fetch price from cryptoCompare service: ${error.message}`);
+		}
+	}
+};
+
+export default cryptoCompareClient;
+
+// region type declarations
+
+/**
+ * Result of a price request.
+ * @typedef {object} PriceInfo
+ * @property {number} USD - The conversion rate to US Dollars.
+ * @property {number} JPY - The conversion rate to Japanese Yen.
+ */
+
+// endregion
---
+++ snap/backend/test/services/cryptocompareClient_spec.js
@@ -0,0 +1,39 @@
+import cryptoCompareClient from '../../src/services/cryptocompareClient.js';
+import fetchUtils from '../../src/utils/fetchUtils.js';
+import { describe, expect, jest } from '@jest/globals';
+
+jest.spyOn(fetchUtils, 'fetchData').mockImplementation();
+
+describe('cryptocompareClient', () => {
+	beforeEach(() => {
+		jest.clearAllMocks();
+	});
+
+	describe('fetchPrice', () => {
+		it('can fetch price successfully', async () => {
+			// Arrange:
+			const prices = {
+				USD: 0.25,
+				JPY: 30
+			};
+
+			fetchUtils.fetchData.mockResolvedValue(prices);
+
+			// Act:
+			const result = await cryptoCompareClient.fetchPrice();
+
+			// Assert:
+			expect(result).toStrictEqual(prices);
+			expect(fetchUtils.fetchData).toHaveBeenCalledWith('https://min-api.cryptocompare.com/data/price?fsym=xym&tsyms=USD,JPY', 'GET');
+		});
+
+		it('should throw an error when fetch fails', async () => {
+			// Arrange:
+			fetchUtils.fetchData.mockRejectedValue(new Error('Failed to fetch'));
+
+			// Assert:
+			const errorMessage = 'Failed to fetch price from cryptoCompare service: Failed to fetch';
+			await expect(cryptoCompareClient.fetchPrice()).rejects.toThrow(errorMessage);
+		});
+	});
+});
