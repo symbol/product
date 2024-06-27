e205f4b1991c7964c77b5df024f79e20e819e391
AnthonyLaw <anthony@symbol.dev>
2024-06-28 03:15:47 +0800

    [snap/backend]: Added config variable and snap cronjob permission

--- snap/backend/src/config.js
+++ snap/backend/src/config.js
@@ -2,7 +2,9 @@ const config = {
 	statisticsServiceURL: {
 		mainnet: 'https://symbol.services',
 		testnet: 'https://testnet.symbol.services'
-	}
+	},
+	cryptoCompareURL: 'https://min-api.cryptocompare.com/data/price?fsym=xym&tsyms=',
+	supportCurrency: ['USD', 'JPY']
 };
 
 export default config;
