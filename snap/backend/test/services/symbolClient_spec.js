134a119083132c0f4282aab01c902a2b7209bd3e
AnthonyLaw <anthony@symbol.dev>
2024-07-02 00:06:28 +0800

    [snap/backend]: Refactor symbol client unit test

--- snap/backend/test/services/symbolClient_spec.js
+++ snap/backend/test/services/symbolClient_spec.js
@@ -4,15 +4,17 @@ import { describe, expect, jest } from '@jest/globals';
 
 describe('symbolClient', () => {
 	jest.spyOn(fetchUtils, 'fetchData').mockImplementation();
+	const nodeUrl = 'http://localhost:3000';
+	let client;
 
 	beforeEach(() => {
 		jest.clearAllMocks();
+		client = symbolClient(nodeUrl);
 	});
 
 	describe('fetchNetworkCurrencyMosaicId', () => {
 		it('can fetch network currency mosaic id successfully', async () => {
 			// Arrange:
-			const nodeUrl = 'http://localhost:3000';
 			const chain = {
 				currencyMosaicId: '0x72C0\'212E\'67A0\'8BCE'
 			};
@@ -20,7 +22,7 @@ describe('symbolClient', () => {
 			fetchUtils.fetchData.mockResolvedValue({ chain });
 
 			// Act:
-			const result = await symbolClient(nodeUrl).fetchNetworkCurrencyMosaicId();
+			const result = await client.fetchNetworkCurrencyMosaicId();
 
 			// Assert:
 			expect(result).toBe('72C0212E67A08BCE');
@@ -29,12 +31,11 @@ describe('symbolClient', () => {
 
 		it('should throw an error when fetch fails', async () => {
 			// Arrange:
-			const nodeUrl = 'http://localhost:3000';
 			fetchUtils.fetchData.mockRejectedValue(new Error('Failed to fetch'));
 
 			// Assert:
 			const errorMessage = 'Failed to fetch network properties info: Failed to fetch';
-			await expect(symbolClient(nodeUrl).fetchNetworkCurrencyMosaicId()).rejects.toThrow(errorMessage);
+			await expect(client.fetchNetworkCurrencyMosaicId()).rejects.toThrow(errorMessage);
 		});
 	});
 });
