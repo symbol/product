78367a1358c9308dbb41e4d41b77065d34298b99
AnthonyLaw <anthony@symbol.dev>
2024-07-04 01:48:06 +0800

    [snap/backend]: Added query currency mosaic id when switch network

--- snap/backend/test/utils/networkUtils_spec.js
+++ snap/backend/test/utils/networkUtils_spec.js
@@ -1,4 +1,5 @@
 import statisticsClient from '../../src/services/statisticsClient.js';
+import symbolClient from '../../src/services/symbolClient.js';
 import stateManager from '../../src/stateManager.js';
 import networkUtils from '../../src/utils/networkUtils.js';
 import { describe, jest } from '@jest/globals';
@@ -12,7 +13,7 @@ describe('networkUtils', () => {
 	});
 
 	describe('switchNetwork', () => {
-		it('should switch network when giving network name', async () => {
+		it('should switch network and fetch mosaic id when giving network name', async () => {
 			// Arrange:
 			const state = {
 				network: {}
@@ -27,16 +28,27 @@ describe('networkUtils', () => {
 				networkName: requestParams.networkName,
 				url: 'http://localhost:3000'
 			};
+			const mockCurrencyMosaicId = 'mosaicId';
 
 			statisticsClient.getNodeInfo.mockResolvedValue(mockNodeInfo);
+			jest.spyOn(symbolClient, 'create').mockReturnValue({
+				fetchNetworkCurrencyMosaicId: jest.fn().mockResolvedValue(mockCurrencyMosaicId)
+			});
 
 			// Act:
 			const result = await networkUtils.switchNetwork({ state, requestParams });
 
 			// Assert:
+			const expectedResult = {
+				...mockNodeInfo,
+				currencyMosaicId: mockCurrencyMosaicId
+			};
+
 			expect(statisticsClient.getNodeInfo).toHaveBeenCalledWith(requestParams.networkName);
-			expect(stateManager.update).toHaveBeenCalledWith({ network: mockNodeInfo });
-			expect(result).toStrictEqual(mockNodeInfo);
+			expect(stateManager.update).toHaveBeenCalledWith({
+				network: expectedResult
+			});
+			expect(result).toStrictEqual(expectedResult);
 		});
 	});
 });
