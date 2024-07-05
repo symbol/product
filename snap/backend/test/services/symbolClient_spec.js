d22ddb5aa203c7d64fb0c9ec5bb816bccd549f1e
AnthonyLaw <anthony@symbol.dev>
2024-07-05 18:06:38 +0800

    [snap/backend]: Added query for get mosaic and account info

--- snap/backend/test/services/symbolClient_spec.js
+++ snap/backend/test/services/symbolClient_spec.js
@@ -1,6 +1,8 @@
 import symbolClient from '../../src/services/symbolClient.js';
 import fetchUtils from '../../src/utils/fetchUtils.js';
-import { describe, expect, jest } from '@jest/globals';
+import {
+	describe, expect, it, jest
+} from '@jest/globals';
 
 describe('symbolClient', () => {
 	jest.spyOn(fetchUtils, 'fetchData').mockImplementation();
@@ -36,6 +38,146 @@ describe('symbolClient', () => {
 			// Assert:
 			const errorMessage = 'Failed to fetch network properties info: Failed to fetch';
 			await expect(client.fetchNetworkCurrencyMosaicId()).rejects.toThrow(errorMessage);
+		});
+	});
+
+	describe('fetchMosaicsInfo', () => {
+		it('can fetch mosaics info successfully', async () => {
+			// Arrange:
+			const mosaicIds = ['393AFB0B19902759', '0005EC25E3F9072D'];
+
+			const mockResponse = [
+				{
+					mosaic: {
+						id: '393AFB0B19902759',
+						divisibility: 6
+					}
+				},
+				{
+					mosaic: {
+						id: '0005EC25E3F9072D',
+						divisibility: 2
+					}
+				}
+			];
+
+			fetchUtils.fetchData.mockResolvedValue(mockResponse);
+
+			// Act:
+			const result = await client.fetchMosaicsInfo(mosaicIds);
+
+			// Assert:
+			expect(result).toStrictEqual({
+				'0005EC25E3F9072D': {
+					divisibility: 2
+				},
+				'393AFB0B19902759': {
+					divisibility: 6
+				}
+			});
+			expect(fetchUtils.fetchData).toHaveBeenCalledWith(`${nodeUrl}/mosaics`, 'POST', { mosaicIds });
+		});
+
+		it('should return empty object when mosaicIds is empty', async () => {
+			// Act:
+			const result = await client.fetchMosaicsInfo([]);
+
+			// Assert:
+			expect(result).toStrictEqual({});
+			expect(fetchUtils.fetchData).not.toHaveBeenCalled();
+		});
+
+		it('should throw an error when fetch fails', async () => {
+			// Arrange:
+			const mosaicIds = ['393AFB0B19902759', '0005EC25E3F9072D'];
+
+			fetchUtils.fetchData.mockRejectedValue(new Error('Failed to fetch'));
+
+			// Assert:
+			const errorMessage = 'Failed to fetch mosaics info: Failed to fetch';
+			await expect(client.fetchMosaicsInfo(mosaicIds)).rejects.toThrow(errorMessage);
+		});
+	});
+
+	describe('fetchAccountsMosaics', () => {
+		it('can fetch accounts mosaics successfully', async () => {
+			// Arrange:
+			const addresses = ['TARDV42KTAIZEF64EQT4NXT7K55DHWBEFIXVJQY', 'TAUYVCHWXD7KPNLJIPRLKBBFKCNIHLJMQCE6BFQ'];
+
+			const mockResponse = [
+				{
+					account: {
+						address: '98223AF34A98119217DC2427C6DE7F577A33D8242A2F54C3',
+						mosaics: [
+							{
+								id: '72C0212E67A08BCE',
+								amount: '100'
+							},
+							{
+								id: '62AAA97AD859F66A',
+								amount: '20'
+							}
+						]
+					}
+				},
+				{
+					account: {
+						address: '98298A88F6B8FEA7B56943E2B50425509A83AD2C8089E096',
+						mosaics: [
+							{
+								id: '72C0212E67A08BCE',
+								amount: '30'
+							}
+						]
+					}
+				}
+			];
+
+			fetchUtils.fetchData.mockResolvedValue(mockResponse);
+
+			// Act:
+			const result = await client.fetchAccountsMosaics(addresses);
+
+			// Assert:
+			expect(result).toStrictEqual({
+				TARDV42KTAIZEF64EQT4NXT7K55DHWBEFIXVJQY: [
+					{
+						id: '72C0212E67A08BCE',
+						amount: '100'
+					},
+					{
+						id: '62AAA97AD859F66A',
+						amount: '20'
+					}
+				],
+				TAUYVCHWXD7KPNLJIPRLKBBFKCNIHLJMQCE6BFQ: [
+					{
+						id: '72C0212E67A08BCE',
+						amount: '30'
+					}
+				]
+			});
+			expect(fetchUtils.fetchData).toHaveBeenCalledWith(`${nodeUrl}/accounts`, 'POST', { addresses });
+		});
+
+		it('should return empty object when addresses is empty', async () => {
+			// Act:
+			const result = await client.fetchAccountsMosaics([]);
+
+			// Assert:
+			expect(result).toStrictEqual({});
+			expect(fetchUtils.fetchData).not.toHaveBeenCalled();
+		});
+
+		it('should throw an error when fetch fails', async () => {
+			// Arrange:
+			const addresses = ['TARDV42KTAIZEF64EQT4NXT7K55DHWBEFIXVJQY'];
+
+			fetchUtils.fetchData.mockRejectedValue(new Error('Failed to fetch'));
+
+			// Assert:
+			const errorMessage = 'Failed to fetch account mosaics: Failed to fetch';
+			await expect(client.fetchAccountsMosaics(addresses)).rejects.toThrow(errorMessage);
 		});
 	});
 });
