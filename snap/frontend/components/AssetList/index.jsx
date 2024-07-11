a13327ad5f9757b86300862e35f8f353fe7c5d33
AnthonyLaw <anthony@symbol.dev>
2024-07-11 18:49:46 +0800

    [snap/frontend]: Refactor asset list

--- snap/frontend/components/AssetList/index.jsx
+++ snap/frontend/components/AssetList/index.jsx
@@ -1,8 +1,11 @@
 import { useWalletContext } from '../../context';
+import { useEffect, useState } from 'react';
 
 const AssetList = () => {
 	const { walletState } = useWalletContext();
-	const { mosaics } = walletState;
+	const { selectedAccount, mosaicInfo } = walletState;
+
+	const [isLoading, setIsLoading] = useState(true);
 
 	const renderSubNamespace = name => {
 		const split = name.split('.');
@@ -13,20 +16,58 @@ const AssetList = () => {
 		return split.slice(1).join('.');
 	};
 
+	const renderMosaicNameOrId = mosaic => {
+		// Check if mosaic information exists and render based on condition
+		const mosaicDetail = mosaicInfo[mosaic.id];
+
+		if (mosaicDetail) {
+			return 0 === mosaicDetail.name.length ? (
+				<div>{mosaic.id}</div>
+			) : (
+				<div>{mosaicDetail.name[0].split('.')[0]}</div>
+			);
+		}
+		return null;
+	};
+
+	const renderMosaicDetails = mosaic => {
+		const mosaicDetail = mosaicInfo[mosaic.id];
+
+		if (!mosaicDetail)
+			return null;
+
+		const amount = mosaic.amount / (10 ** mosaicDetail.divisibility);
+		const subNamespace = 0 === mosaicDetail.name.length ? null : renderSubNamespace(mosaicDetail.name[0]);
+
+		return (
+			<div className='text-xs text-gray-400'>
+				{amount} {subNamespace}
+			</div>
+		);
+	};
+
+	useEffect(() => {
+		// Check if mosaicInfo and selectedAccount are loaded
+		if (mosaicInfo && selectedAccount && Array.isArray(selectedAccount.mosaics))
+			setIsLoading(false);
+		else
+			setIsLoading(true);
+
+	}, [mosaicInfo, selectedAccount]);
+
+	if (isLoading)
+		return <div>Loading...</div>;
+
+
 	return (
 		<div className='flex flex-col overflow-y-auto'>
 			{
-				mosaics.map((mosaic, index) => (
+				selectedAccount.mosaics.map((mosaic, index) => (
 					<div key={`asset_${index}`} className='flex items-center justify-start p-2'>
 						<div role={`mosaic-image_${index}`} className="rounded-full w-5 h-5 bg-gray-300 mr-2" />
 						<div className='text-xs'>
-							{
-								null === mosaic.name ?
-									<div>{mosaic.id}</div> :
-									<div>{mosaic.name.split('.')[0]}</div>
-							}
-
-							<div>{mosaic.amount} {null !== mosaic.name ? renderSubNamespace(mosaic.name) : null}</div>
+							{ renderMosaicNameOrId(mosaic) }
+							{ renderMosaicDetails(mosaic) }
 						</div>
 					</div>
 				))
