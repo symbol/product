5ccc6a166cb2912384110e9bcbdd7f7320092186
AnthonyLaw <anthony@symbol.dev>
2024-07-20 23:27:01 +0800

    [snap/frontend]: Added explorer url

--- snap/frontend/config/index.js
+++ snap/frontend/config/index.js
@@ -1,3 +1,8 @@
 export const defaultSnapOrigin =
 // eslint-disable-next-line no-restricted-globals
     process.env.SNAP_ORIGIN ?? 'local:http://localhost:8080';
+
+export const explorerUrl = {
+	mainnet: 'https://symbol.fyi',
+	testnet: 'https://testnet.symbol.fyi'
+};
