98f0df821c91c05ac349abd3e5e496c04a094c70
AnthonyLaw <anthony@symbol.dev>
2024-08-05 11:29:26 +0800

    [snap/frontend]: Added more unit test

---
+++ snap/frontend/components/FeeMultiplier/FeeMultiplier.spec.jsx
@@ -0,0 +1,49 @@
+import FeeMultiplier from '.';
+import testHelper from '../testHelper';
+import { act, fireEvent, screen, waitFor } from '@testing-library/react';
+
+const context = {
+	symbolSnap: {
+		getFeeMultiplier: jest.fn()
+	}
+};
+
+describe('components/FeeMultiplier', () => {
+	it('renders FeeMultiplier', async () => {
+		// Arrange:
+		context.symbolSnap.getFeeMultiplier.mockResolvedValue({
+			slow: 10,
+			average: 100,
+			fast: 1000
+		});
+
+		// Act:
+		await act(() => testHelper.customRender(<FeeMultiplier />, context));
+
+		// Assert:
+		await waitFor(() => {
+			expect(screen.getByText('SLOW')).toBeInTheDocument();
+			expect(screen.getByText('AVERAGE')).toBeInTheDocument();
+			expect(screen.getByText('FAST')).toBeInTheDocument();
+		});
+	});
+
+	it('update selected fee multiplier', async () => {
+		// Arrange:
+		context.symbolSnap.getFeeMultiplier.mockResolvedValue({
+			slow: 10,
+			average: 100,
+			fast: 1000
+		});
+
+		const mockSetSelectedFeeMultiplier = jest.fn();
+
+		await act(() => testHelper.customRender(<FeeMultiplier setSelectedFeeMultiplier={mockSetSelectedFeeMultiplier} />, context));
+
+		// Act:
+		await waitFor(() => fireEvent.click(screen.getByText('SLOW')));
+
+		// Assert:
+		expect(mockSetSelectedFeeMultiplier).toHaveBeenCalledWith(10);
+	});
+});
