5635255dca6cc608e9867c50fbabf775bb235f71
AnthonyLaw <anthony@symbol.dev>
2024-08-14 15:28:52 +0800

    [snap/backend]: Fix config naming in env

--- snap/frontend/config/index.js
+++ snap/frontend/config/index.js
@@ -1,6 +1,6 @@
 export const defaultSnapOrigin =
 // eslint-disable-next-line no-restricted-globals
-    process.env.SNAP_ORIGIN ?? 'local:http://localhost:8080';
+    process.env.NEXT_PUBLIC_SNAP_ORIGIN ?? 'local:http://localhost:8080';
 
 export const explorerUrl = {
 	mainnet: 'https://symbol.fyi',
