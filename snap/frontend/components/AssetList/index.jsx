307dadaf6ac7009241ba5c61ecc1842720113dd0
AnthonyLaw <anthony@symbol.dev>
2024-08-23 16:47:46 +0800

    [snap/frontend]: refactor asset list component

--- snap/frontend/components/AssetList/index.jsx
+++ snap/frontend/components/AssetList/index.jsx
@@ -16,21 +16,8 @@ const AssetList = () => {
 		return split.slice(1).join('.');
 	};
 
-	const renderMosaicNameOrId = mosaic => {
+	const renderMosaicDetailsWithName = mosaic => {
 		// Check if mosaic information exists and render based on condition
-		const mosaicDetail = mosaicInfo[mosaic.id];
-
-		if (!mosaicDetail)
-			throw new Error('Mosaic information not found');
-
-		return 0 === mosaicDetail.name.length ? (
-			<div>{mosaic.id}</div>
-		) : (
-			<div>{mosaicDetail.name[0].split('.')[0]}</div>
-		);
-	};
-
-	const renderMosaicDetails = mosaic => {
 		const mosaicDetail = mosaicInfo[mosaic.id];
 
 		if (!mosaicDetail)
@@ -40,8 +27,15 @@ const AssetList = () => {
 		const subNamespace = 0 === mosaicDetail.name.length ? null : renderSubNamespace(mosaicDetail.name[0]);
 
 		return (
-			<div className='text-xs text-gray-400'>
-				{amount} {subNamespace}
+			<div>
+				{0 === mosaicDetail.name.length ? (
+					<div>{mosaic.id}</div>
+				) : (
+					<div>{mosaicDetail.name[0].split('.')[0]}</div>
+				)}
+				<div className='text-xs text-gray-400'>
+					{amount} {subNamespace}
+				</div>
 			</div>
 		);
 	};
@@ -66,8 +60,7 @@ const AssetList = () => {
 					<div key={`asset_${index}`} className='flex items-center justify-start p-2'>
 						<div role={`mosaic-image_${index}`} className="rounded-full w-5 h-5 bg-gray-300 mr-2" />
 						<div className='text-xs'>
-							{ renderMosaicNameOrId(mosaic) }
-							{ renderMosaicDetails(mosaic) }
+							{ renderMosaicDetailsWithName(mosaic) }
 						</div>
 					</div>
 				))
