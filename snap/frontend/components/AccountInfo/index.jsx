743a545c9545b4a77f83f2932e25051222e0d8cd
AnthonyLaw <anthony@symbol.dev>
2024-08-27 20:46:24 +0800

    [snap/frontend]: added profile icon in account info

--- snap/frontend/components/AccountInfo/index.jsx
+++ snap/frontend/components/AccountInfo/index.jsx
@@ -1,11 +1,14 @@
 import { useWalletContext } from '../../context';
 import AccountListModalBox from '../AccountListModalBox';
 import Address from '../Address';
+import makeBlockie from 'ethereum-blockies-base64';
+import Image from 'next/image';
 import { useState } from 'react';
 
 const AccountInfo = () => {
 	const { walletState } = useWalletContext();
 	const { selectedAccount } = walletState;
+	const { address } = selectedAccount;
 
 	const [ accountListModalBoxVisible, setAccountListModalBoxVisible ] = useState(false);
 
@@ -18,7 +21,17 @@ const AccountInfo = () => {
 			<AccountListModalBox isOpen={accountListModalBoxVisible} onRequestClose={setAccountListModalBoxVisible} />
 
 			<div className='flex flex-col items-center justify-center p-2'>
-				<div role='profile-image' className="rounded-full w-16 h-16 bg-gray-300" onClick={handleAccountListModalBox}/>
+
+				{/* Account Profile */}
+				{ address &&
+					<Image src={makeBlockie(address)}
+						alt='account-profile'
+						width={24}
+						height={24}
+						className='rounded-full w-16 h-16'
+						onClick={handleAccountListModalBox} />
+				}
+
 				{/* Text Address and wallet label */}
 				<Address account={selectedAccount} />
 			</div>
