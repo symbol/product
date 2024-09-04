e35472ba15e1be2797918eb92d72c017864bbcfe
AnthonyLaw <anthony@symbol.dev>
2024-09-04 15:33:57 +0800

    [snap/frontend]: implement rename component in account list

--- snap/frontend/components/AccountListModalBox/index.jsx
+++ snap/frontend/components/AccountListModalBox/index.jsx
@@ -1,6 +1,7 @@
 import { useWalletContext } from '../../context';
 import helper from '../../utils/helper';
 import AccountCreationFormModalBox from '../AccountCreationFormModalBox';
+import AccountRenameModalBox from '../AccountRenameModalBox';
 import Button from '../Button';
 import ModalBox from '../ModalBox';
 import makeBlockie from 'ethereum-blockies-base64';
@@ -13,6 +14,7 @@ const AccountListModalBox = ({ isOpen, onRequestClose }) => {
 	const { accounts } = walletState;
 	const [ accountCreateFormVisible, setAccountCreateFormVisible ] = useState(false);
 	const [ accountImportFormVisible, setAccountImportFormVisible ] = useState(false);
+	const [ renameAccountId, setRenameAccountId ] = useState(null);
 
 	const handleOpenAccountCreateForm = () => {
 		setAccountCreateFormVisible(!accountCreateFormVisible);
@@ -21,6 +23,11 @@ const AccountListModalBox = ({ isOpen, onRequestClose }) => {
 
 	const handleOpenAccountImportForm = () => {
 		setAccountImportFormVisible(!accountImportFormVisible);
+		onRequestClose(false);
+	};
+
+	const handleOpenRenameForm = accountId => {
+		setRenameAccountId(accountId);
 		onRequestClose(false);
 	};
 
@@ -66,20 +73,35 @@ const AccountListModalBox = ({ isOpen, onRequestClose }) => {
 
 	const renderAccount = account => {
 		return (
-			<div key={`wallet_${account.id}`} className='flex items-center cursor-pointer'
-				onClick={() => handleSelectAccount(account)}>
-				<Image
-					src={makeBlockie(account.address)}
-					alt='account-profile'
-					width={24}
-					height={24}
-					className='rounded-full w-10 h-10' />
-				<div className='flex flex-col items-start justify-center p-2'>
-					<div className='font-bold'>{account.label} {renderImportType(account.type)}</div>
-					<div className='truncate w-[180px]'>{account.address}</div>
+			<div key={`wallet_${account.id}`} className='flex items-center justify-between'>
+				<div className='flex items-center cursor-pointer'
+					onClick={() => handleSelectAccount(account)}>
+					<Image
+						src={makeBlockie(account.address)}
+						alt='account-profile'
+						width={24}
+						height={24}
+						className='rounded-full w-10 h-10' />
+					<div className='flex flex-col items-start justify-center p-2'>
+						<div className='font-bold'>{account.label} {renderImportType(account.type)}</div>
+						<div className='truncate w-[180px]'>{account.address}</div>
+					</div>
 				</div>
+				<Button
+					className='uppercase w-20 h-8 bg-secondary text-xs'
+					onClick={() => handleOpenRenameForm(account.id)}
+				>
+					Rename
+				</Button>
 			</div>
 		);
+	};
+
+	const handleRenameAccount = newLabel => {
+		if (renameAccountId) {
+			helper.renameAccountLabel(dispatch, symbolSnap, renameAccountId, newLabel);
+			setRenameAccountId(null);
+		}
 	};
 
 	return (
@@ -87,7 +109,7 @@ const AccountListModalBox = ({ isOpen, onRequestClose }) => {
 			<ModalBox isOpen={isOpen} onRequestClose={onRequestClose}>
 				<div className='flex flex-col px-5 text-center'>
 					<div className='text-2xl font-bold mb-6'>
-                    Wallet
+							Wallet
 					</div>
 
 					<div className='flex flex-col overflow-y-auto h-72 items-center justify-start mb-6 text-xs'>
@@ -98,10 +120,10 @@ const AccountListModalBox = ({ isOpen, onRequestClose }) => {
 
 					<div className='flex justify-center font-bold pt-2'>
 						<Button className='uppercase w-40 h-10 bg-secondary m-2' onClick={handleOpenAccountCreateForm}>
-                        Create
+								Create
 						</Button>
 						<Button className='uppercase w-40 h-10 bg-secondary m-2' onClick={handleOpenAccountImportForm}>
-                        Import
+								Import
 						</Button>
 					</div>
 				</div>
@@ -144,6 +166,14 @@ const AccountListModalBox = ({ isOpen, onRequestClose }) => {
 						validate: value => validatePrivateKey(value)
 					}
 				]}
+			/>
+
+			<AccountRenameModalBox
+				isOpen={!!renameAccountId}
+				onRequestClose={() => setRenameAccountId(null)}
+				onRename={handleRenameAccount}
+				currentName={renameAccountId ? accounts[renameAccountId].label : ''}
+				validateAccountName={validateAccountName}
 			/>
 		</>
 	);
