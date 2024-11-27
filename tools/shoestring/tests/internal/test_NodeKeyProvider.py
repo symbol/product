74bd488cbab80ae95629896d3f33a86d6d6a6210
wayonb <wayon@symbol.dev>
2024-11-27 13:01:38 -0500

    [tools/shoestring]: address feedback

--- tools/shoestring/tests/internal/test_NodeKeyProvider.py
+++ tools/shoestring/tests/internal/test_NodeKeyProvider.py
@@ -35,7 +35,7 @@ class NodeKeyProviderTest(unittest.TestCase):
 			self.assertEqual(False, node_key_provider.is_imported)
 			self.assertEqual(True, node_key_filepath.exists())
 
-	def test_can_reuse_node_key(self):
+	def test_can_reuse_existing_node_key(self):
 		# Arrange:
 		with tempfile.TemporaryDirectory() as package_directory:
 			with CertificateFactory(self._create_executor(), Path(package_directory) / 'xyz.key.pem') as factory:
@@ -66,12 +66,12 @@ class NodeKeyProviderTest(unittest.TestCase):
 				renew_node_key_data = _load_binary_file_data(renew_node_key_filepath)
 				assert node_key_data == renew_node_key_data
 
-	def test_cannot_generates_node_key(self):
+	def test_cannot_generates_node_key_with_no_certificate_factory(self):
 		with self.assertRaises(RuntimeError):
 			node_key_provider = NodeKeyProvider(None)
 			node_key_provider.create_key_file(None)
 
-	def test_cannot_reuse_node_key(self):
+	def test_cannot_reuse_node_key_with_invalid_path(self):
 		with self.assertRaises(FileNotFoundError):
 			node_key_provider = NodeKeyProvider('/path/to/node/key/pem')
 			node_key_provider.create_key_file(None)
