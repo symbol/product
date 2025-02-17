b24120a8d9dbd137e850fe4dcbcbb982d7159934
OlegMakarenko <olegomm@gmail.com>
2025-02-17 16:35:37 +0100

    [wallet/mobile] feat: add MobileWalletController

---
+++ wallet/mobile/src/lib/controller/MobileWalletController.js
@@ -0,0 +1,20 @@
+import { AddressBookModule } from 'src/lib/controller/modules/AddressBookModule';
+import { HarvestingModule } from 'src/lib/controller/modules/HarvestingModule';
+import { MarketModule } from 'src/lib/controller/modules/MarketModule';
+import { TransferModule } from 'src/lib/controller/modules/TransferModule';
+import { WalletController } from 'src/lib/controller/WalletController';
+import { PersistentStorage, SecureStorage } from 'src/lib/storage';
+
+const MobileWalletController = new WalletController({
+    persistentStorage: PersistentStorage,
+    secureStorage: SecureStorage,
+    isObservable: true,
+    modules: [
+        AddressBookModule,
+        HarvestingModule,
+        MarketModule,
+        TransferModule,
+    ],
+});
+
+export default MobileWalletController;
