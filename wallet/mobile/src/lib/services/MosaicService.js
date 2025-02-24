28cd958c731e8fa9504868f754f55942403900c4
OlegMakarenko <olegomm@gmail.com>
2025-02-24 18:29:27 +0100

    [wallet/mobile] feat: add comments for market, mosaic and network services

--- wallet/mobile/src/lib/services/MosaicService.js
+++ wallet/mobile/src/lib/services/MosaicService.js
@@ -2,21 +2,29 @@ import _ from 'lodash';
 import { NamespaceService } from './';
 import { addressFromRaw } from '@/app/utils/account';
 import { makeRequest } from '@/app/utils/network';
-import {
-    getMosaicRelativeAmountString,
-    isRestrictableFlag,
-    isRevokableFlag,
-    isSupplyMutableFlag,
-    isTransferableFlag,
-} from '@/app/utils/mosaic';
+import { absoluteToRelativeAmount, isRestrictableFlag, isRevokableFlag, isSupplyMutableFlag, isTransferableFlag } from '@/app/utils/mosaic';
+import * as MosaicTypes from '@/app/types/Mosaic';
+import * as NetworkTypes from '@/app/types/Network';
 
 export class MosaicService {
+    /**
+     * Fetches mosaic info from the node.
+     * @param {NetworkTypes.NetworkProperties} networkProperties - Network properties.
+     * @param {string} mosaicId - Requested mosaic id.
+     * @returns {Promise<MosaicTypes.Mosaic>} - The mosaic info.
+     */
     static async fetchMosaicInfo(networkProperties, mosaicId) {
         const mosaicInfos = await MosaicService.fetchMosaicInfos(networkProperties, [mosaicId]);
 
         return mosaicInfos[mosaicId];
     }
 
+    /**
+     * Fetches mosaic infos for the list of ids from the node.
+     * @param {NetworkTypes.NetworkProperties} networkProperties - Network properties.
+     * @param {string[]} mosaicIds - Requested mosaic ids.
+     * @returns {Promise<{ [key: string]: MosaicTypes.Mosaic }>} - The mosaic infos map.
+     */
     static async fetchMosaicInfos(networkProperties, mosaicIds) {
         // Fetch mosaic infos from API
         const endpoint = `${networkProperties.nodeUrl}/mosaics`;
@@ -38,7 +46,7 @@ export class MosaicService {
             const endHeight = startHeight + duration;
             const isUnlimitedDuration = duration === 0;
             const creator = addressFromRaw(mosaicInfos.mosaic.ownerAddress);
-            const supply = getMosaicRelativeAmountString(mosaicInfos.mosaic.supply, parseInt(mosaicInfos.mosaic.divisibility));
+            const supply = absoluteToRelativeAmount(mosaicInfos.mosaic.supply, parseInt(mosaicInfos.mosaic.divisibility));
             const { flags } = mosaicInfos.mosaic;
 
             return [
@@ -66,7 +74,7 @@ export class MosaicService {
 
         // Fetch namespace infos to extract mosaic ids from there
         const namespaceInfos = await NamespaceService.fetchNamespaceInfos(networkProperties, namespaceIds);
-        const remainedMosaicIds = Object.values(namespaceInfos).map((namespaceInfo) => namespaceInfo.alias.mosaicId);
+        const remainedMosaicIds = Object.values(namespaceInfos).map((namespaceInfo) => namespaceInfo.linkedMosaicId);
         const shouldFetchRemainedMosaicInfos = remainedMosaicIds.length > 0;
 
         // Fetch remained mosaic infos for extracted mosaics from namespace infos
@@ -84,7 +92,7 @@ export class MosaicService {
 
         for (const namespaceId of namespaceIds) {
             if (namespaceInfos[namespaceId]) {
-                const mosaicId = namespaceInfos[namespaceId].alias.mosaicId;
+                const mosaicId = namespaceInfos[namespaceId].linkedMosaicId;
                 mosaicInfos[namespaceId] = remainedMosaicInfos[mosaicId];
             }
         }
