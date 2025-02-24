7d1deda738107caaff5b647065f6f94b4bbb5f93
OlegMakarenko <olegomm@gmail.com>
2025-02-24 18:31:09 +0100

    [wallet/mobile] feat: add comments for MobileWalletController

--- wallet/mobile/src/lib/controller/MobileWalletController.js
+++ wallet/mobile/src/lib/controller/MobileWalletController.js
@@ -5,11 +5,26 @@ import { TransferModule } from '@/app/lib/controller/modules/TransferModule';
 import { WalletController } from '@/app/lib/controller/WalletController';
 import { PersistentStorage, SecureStorage } from '@/app/lib/storage';
 
-const MobileWalletController = new WalletController({
-    persistentStorage: PersistentStorage,
-    secureStorage: SecureStorage,
-    isObservable: true,
-    modules: [AddressBookModule, HarvestingModule, MarketModule, TransferModule],
-});
+/**
+ * @typedef {Object} MobileWalletControllerModules
+ * @property {AddressBookModule} addressBook - The address book module.
+ * @property {HarvestingModule} harvesting - The module that handles harvesting information and operations.
+ * @property {MarketModule} market - The module that handles market information.
+ * @property {TransferModule} transfer - The module that handles transfer transaction operations.
+ */
+const modules = [AddressBookModule, HarvestingModule, MarketModule, TransferModule];
+
+/**
+ * Mobile wallet controller.
+ * @type {WalletController & { modules: MobileWalletControllerModules }}
+ */
+const MobileWalletController = /** @type {any} */ (
+    new WalletController({
+        persistentStorage: PersistentStorage,
+        secureStorage: SecureStorage,
+        isObservable: true,
+        modules,
+    })
+);
 
 export default MobileWalletController;
