38bb4c380c721382b62f9e8e74f6e7cfe7eb6241
OlegMakarenko <olegomm@gmail.com>
2025-02-26 18:11:12 +0100

    [wallet/mobile] feat: add tests for wallet utils, move seed account creation to wallet controller

---
+++ wallet/mobile/__tests__/utils/wallet.test.js
@@ -0,0 +1,75 @@
+import { createOptInPrivateKeyFromMnemonic, createPrivateKeysFromMnemonic, generateMnemonic } from '@/app/utils';
+import { mnemonic, networkIdentifiers, walletStorageAccounts } from '__fixtures__/local/wallet';
+
+const OPT_IN_PRIVATE_KEY = '8965375F47E64529E6FBBB968764ED8DE64A0973701ABDEEF91A56CA626604B7';
+const OPT_IN_PUBLIC_KEY = '98D11F7EF395131E5CDF5EC0F9105145A56FB337DC6699F7B652AB393E90003B';
+
+jest.mock('@/app/config', () => ({
+    ...jest.requireActual('@/app/config'),
+    optInWhiteList: [OPT_IN_PUBLIC_KEY],
+}));
+
+const onlyUnique = (value, index, array) => array.indexOf(value) === index;
+
+describe('utils/account', () => {
+    describe('generateMnemonic', () => {
+        it('generates a random mnemonic phrase string of 24 words', () => {
+            // Arrange:
+            const expectedWordsCount = 24;
+
+            // Act:
+            const result = generateMnemonic();
+            const mnemonicWords = result.split(' ');
+            const uniqueMnemonicWords = mnemonicWords.filter(onlyUnique);
+
+            // Assert:
+            expect(typeof result).toBe('string');
+            expect(mnemonicWords.length).toBe(expectedWordsCount);
+            expect(uniqueMnemonicWords.length).toBe(expectedWordsCount);
+        });
+    });
+
+    describe('createPrivateKeysFromMnemonic', () => {
+        it('derives private keys from a given mnemonic phrase', () => {
+            networkIdentifiers.forEach((networkIdentifier) => {
+                // Arrange:
+                const indexes = [0, 1, 2, 3, 4];
+                const expectedPrivateKeys = walletStorageAccounts[networkIdentifier].map((account) => account.privateKey);
+
+                // Act:
+                const result = createPrivateKeysFromMnemonic(mnemonic, indexes, networkIdentifier);
+
+                // Assert:
+                expect(result).toEqual(expectedPrivateKeys);
+            });
+        });
+    });
+
+    describe('createOptInPrivateKeyFromMnemonic', () => {
+        const runTest = (mnemonic, expectedResult) => {
+            // Act:
+            const result = createOptInPrivateKeyFromMnemonic(mnemonic);
+
+            // Assert:
+            expect(result).toBe(expectedResult);
+        };
+
+        it('derives private key from a given mnemonic phrase using the opt-in curve', () => {
+            // Arrange:
+            const expectedResult = OPT_IN_PRIVATE_KEY;
+
+            // Act & Assert:
+            runTest(mnemonic, expectedResult);
+        });
+
+        it('returns null if the is no public key match in the opt-in white list', () => {
+            // Arrange:
+            const mnemonic =
+                'pizza road phone chase web cancel release hand mail engine custom pole damp envelope fame topple patch asset attract purse venture clay elevator program';
+            const expectedResult = null;
+
+            // Act & Assert:
+            runTest(mnemonic, expectedResult);
+        });
+    });
+});
--- wallet/mobile/src/utils/wallet.js
+++ wallet/mobile/src/utils/wallet.js
@@ -2,12 +2,10 @@ import { SymbolPaperWallet } from 'symbol-wallets-lib';
 import { Buffer } from 'buffer';
 import { networkIdentifierToNetworkType } from './network';
 import { optInWhiteList } from '@/app/config';
-import { createWalletStorageAccount, publicAccountFromPrivateKey } from './account';
+import { publicAccountFromPrivateKey } from './account';
 import { Bip32 } from 'symbol-sdk';
 import { SymbolFacade } from 'symbol-sdk/symbol';
-import { DEFAULT_ACCOUNT_NAME, MAX_SEED_ACCOUNTS_PER_NETWORK, NetworkIdentifier, WalletAccountType } from '@/app/constants';
-import { createNetworkMap } from '@/app/utils/network';
-import * as AccountTypes from '@/app/types/Account'; // eslint-disable-line no-unused-vars
+import { NetworkIdentifier } from '@/app/constants';
 
 /**
  * Generates a random mnemonic phrase.
@@ -22,17 +20,20 @@ export const generateMnemonic = () => {
 
 /**
  * Creates an array of private keys from a given mnemonic phrase.
+ * ```js
+ * const mnemonic = '...';
+ * const indexes = [0, 1, 2, 3, 4];
+ * const networkIdentifier = 'mainnet';
+ * const privateKeys = createPrivateKeysFromMnemonic(mnemonic, indexes, networkIdentifier);
+ * ```
  * @param {string} mnemonic - The mnemonic phrase used to generate the private keys.
  * @param {number[]} indexes - An array of indexes to derive the private keys from.
  * @param {string} networkIdentifier - The network identifier for the Symbol blockchain.
- * @param {boolean} isOptInCurve - A flag indicating whether to use the opt-in curve (secp256k1) or the Symbol curve.
+ * @param {boolean} [curve] - The curve used to derive the private keys. Default is "ed25519".
  * @returns {string[]} An array of private keys derived from the mnemonic and indexes.
  */
-export const createPrivateKeysFromMnemonic = (mnemonic, indexes, networkIdentifier, isOptInCurve) => {
+export const createPrivateKeysFromMnemonic = (mnemonic, indexes, networkIdentifier, curve = SymbolFacade.BIP32_CURVE_NAME) => {
     const facade = new SymbolFacade(networkIdentifier);
-    const symbolCurve = facade.static.BIP32_CURVE_NAME;
-    const optInCurve = 'secp256k1';
-    const curve = isOptInCurve ? optInCurve : symbolCurve;
     const bip = new Bip32(curve);
     const rootNode = bip.fromMnemonic(mnemonic, '');
 
@@ -45,6 +46,20 @@ export const createPrivateKeysFromMnemonic = (mnemonic, indexes, networkIdentifi
     });
 
     return privateKeys;
+};
+
+/**
+ * Creates an opt-in private key from a mnemonic phrase.
+ * @param {string} mnemonic - The mnemonic phrase used to generate the private key.
+ * @returns {string|null} The opt-in private key if it is whitelisted, otherwise null.
+ */
+export const createOptInPrivateKeyFromMnemonic = (mnemonic) => {
+    const optInCurve = 'secp256k1';
+    const [optInPrivateKey] = createPrivateKeysFromMnemonic(mnemonic.trim(), [0], NetworkIdentifier.MAIN_NET, optInCurve);
+    const optInAccount = publicAccountFromPrivateKey(optInPrivateKey, NetworkIdentifier.MAIN_NET);
+    const isKeyWhitelisted = optInWhiteList.some((publicKey) => publicKey === optInAccount.publicKey);
+
+    return isKeyWhitelisted ? optInPrivateKey : null;
 };
 
 /**
@@ -69,32 +84,4 @@ export const createPaperWallet = async (mnemonic, rootAccount, networkIdentifier
     const paperWalletBase64 = Buffer.from(paperWalletPdf).toString('base64');
 
     return paperWalletBase64;
-};
-
-/**
- * Creates an opt-in private key from a mnemonic phrase.
- * @param {string} mnemonic - The mnemonic phrase used to generate the private key.
- * @returns {string|null} The opt-in private key if it is whitelisted, otherwise null.
- */
-export const createOptInPrivateKeyFromMnemonic = (mnemonic) => {
-    const [optInPrivateKey] = createPrivateKeysFromMnemonic(mnemonic.trim(), [0], NetworkIdentifier.MAIN_NET, 'optin');
-    const optInAccount = publicAccountFromPrivateKey(optInPrivateKey, NetworkIdentifier.MAIN_NET);
-    const isKeyWhitelisted = optInWhiteList.some((publicKey) => publicKey === optInAccount.publicKey);
-
-    return isKeyWhitelisted ? optInPrivateKey : null;
-};
-
-/**
- * Generates seed accounts based on the provided mnemonic.
- * @param {string} mnemonic - The mnemonic phrase used to generate the seed accounts.
- * @returns {Promise<Object.<string, AccountTypes.WalletStorageAccount>} A promise that resolves to a network map containing the generated seed accounts.
- */
-export const generateSeedAccounts = async (mnemonic) => {
-    const seedIndexes = [...Array(MAX_SEED_ACCOUNTS_PER_NETWORK).keys()];
-
-    return createNetworkMap((networkIdentifier) =>
-        createPrivateKeysFromMnemonic(mnemonic, seedIndexes, networkIdentifier).map((privateKey, index) =>
-            createWalletStorageAccount(privateKey, networkIdentifier, `${DEFAULT_ACCOUNT_NAME} ${index + 1}`, WalletAccountType.SEED, index)
-        )
-    );
 };
