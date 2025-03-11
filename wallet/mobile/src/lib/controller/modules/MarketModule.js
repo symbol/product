42fa0c33b4367c5b925951b14fb9efeac61aabdc
OlegMakarenko <olegomm@gmail.com>
2025-03-11 17:09:21 +0100

    [wallet/mobile] feat: add comments to market controller module

--- wallet/mobile/src/lib/controller/modules/MarketModule.js
+++ wallet/mobile/src/lib/controller/modules/MarketModule.js
@@ -30,6 +30,10 @@ export class MarketModule {
         };
     }
 
+    /**
+     * Initializes the module. Loads the user currency from the persistent storage.
+     * @returns {Promise<void>} A promise that resolves when the module is initialized.
+     */
     loadCache = async () => {
         const userCurrency = await this._root._persistentStorage.getUserCurrency();
 
@@ -40,10 +44,17 @@ export class MarketModule {
         });
     };
 
+    /**
+     * Clears the module state.
+     */
     clearState = () => {
         this._state = cloneDeep(defaultState);
     };
 
+    /**
+     * Selects the user currency.
+     * @param {string} userCurrency - The user currency.
+     */
     selectUserCurrency = async (userCurrency) => {
         await this._root._persistentStorage.setUserCurrency(userCurrency);
 
@@ -52,6 +63,10 @@ export class MarketModule {
         });
     };
 
+    /**
+     * Fetches an actual market data.
+     * @returns {Promise<void>} - A promise that resolves when the market data is fetched.
+     */
     fetchData = async () => {
         const { marketData } = this._state;
         const currentTimestamp = Date.now();
