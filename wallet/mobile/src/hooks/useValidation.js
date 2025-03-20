47e2ab463f199eb42a7df45cc04dedc8abffe3f4
OlegMakarenko <olegomm@gmail.com>
2025-03-20 22:59:46 +0100

    [wallet/mobile] feat: add jsdoc for useValidation hook

--- wallet/mobile/src/hooks/useValidation.js
+++ wallet/mobile/src/hooks/useValidation.js
@@ -1,3 +1,12 @@
+/**
+ * A custom hook that validates a value using an array of validator functions.
+ *
+ * @param {*} value - The value to be validated.
+ * @param {Array<(value: any) => string | null>} validators - An array of validator functions.
+ *        Each validator should return an error message if validation fails, or `null` if it passes.
+ * @param {(error: string) => any} [formatResult] - Optional function to format the validation result.
+ * @returns {string | any | undefined} - The validation error message (formatted if `formatResult` is provided), or `undefined` if no errors.
+ */
 export const useValidation = (value, validators, formatResult) => {
     for (const validator of validators) {
         const validationResult = validator(value);
