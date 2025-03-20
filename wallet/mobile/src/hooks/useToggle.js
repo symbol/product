9f0a25ff5fd23a4a8aede8d47c537c35100e04a1
OlegMakarenko <olegomm@gmail.com>
2025-03-20 22:59:12 +0100

    [wallet/mobile] feat: add jsdoc for useToggle hook

--- wallet/mobile/src/hooks/useToggle.js
+++ wallet/mobile/src/hooks/useToggle.js
@@ -1,5 +1,11 @@
 import { useState } from 'react';
 
+/**
+ * A custom hook that manages a boolean state with a toggle function.
+ *
+ * @param {boolean} initialValue - The initial boolean state value.
+ * @returns {[boolean, () => void]} - A tuple containing the current boolean state and a function to toggle it.
+ */
 export const useToggle = (initialValue) => {
     const [value, setValue] = useState(initialValue);
 
