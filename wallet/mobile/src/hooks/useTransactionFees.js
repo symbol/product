2bbceda06183f2603a58ce37aa64e3c41ef2d010
OlegMakarenko <olegomm@gmail.com>
2025-03-20 22:59:29 +0100

    [wallet/mobile] feat: add jsdoc for useTransactionFees hook

--- wallet/mobile/src/hooks/useTransactionFees.js
+++ wallet/mobile/src/hooks/useTransactionFees.js
@@ -1,6 +1,14 @@
 import { useMemo } from 'react';
 import { calculateTransactionFees } from '@/app/utils';
+import * as NetworkTypes from '@/app/types/Network';
 
+/**
+ * A custom hook that calculates transaction fees based on network properties.
+ *
+ * @param {object} transaction - The transaction object for which fees are calculated.
+ * @param {NetworkTypes.NetworkProperties} networkProperties - The network properties containing fee multipliers and identifiers.
+ * @returns {{ fast: number, medium: number, slow: number }} - An object containing fee estimates for different transaction speeds.
+ */
 export const useTransactionFees = (transaction, networkProperties) => {
     const defaultTransactionFees = {
         fast: 0,
