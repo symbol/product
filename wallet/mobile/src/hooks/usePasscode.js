a3e47336dcea40e4e985c8ca9e48b2084697776d
OlegMakarenko <olegomm@gmail.com>
2025-03-20 22:58:45 +0100

    [wallet/mobile] feat: add jsdoc for usePasscode hook

--- wallet/mobile/src/hooks/usePasscode.js
+++ wallet/mobile/src/hooks/usePasscode.js
@@ -1,6 +1,15 @@
 import { DeviceEventEmitter } from 'react-native';
 import { Router } from '@/app/Router';
 
+/**
+ * A custom hook that returns a function to trigger the passcode screen.
+ * It sets up event listeners for success and cancel events and navigates to the passcode screen.
+ *
+ * @param {"choose" | "enter"} type - The type of passcode action (e.g., "choose" or "enter").
+ * @param {() => void} [onSuccess] - Optional callback triggered when the passcode action succeeds.
+ * @param {() => void} [onCancel] - Optional callback triggered when the passcode action is canceled.
+ * @returns {() => void} - A function that, when called, navigates to the passcode screen and sets up event listeners.
+ */
 export const usePasscode = (type, onSuccess, onCancel) => {
     const successState = '.s';
     const cancelState = '.c';
