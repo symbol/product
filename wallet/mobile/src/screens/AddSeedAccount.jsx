21159c470cfb8037fa4199c71255450cde882d60
OlegMakarenko <olegomm@gmail.com>
2025-03-20 16:26:41 +0100

    [wallet/mobile] feat: fetch account balances in account selector, add balance change badge to account cards

--- wallet/mobile/src/screens/AddSeedAccount.jsx
+++ wallet/mobile/src/screens/AddSeedAccount.jsx
@@ -5,16 +5,17 @@ import { $t } from '@/app/localization';
 import { Router } from '@/app/Router';
 import { colors } from '@/app/styles';
 import { handleError, validateAccountName } from '@/app/utils';
-import { useDataManager, usePromises, useValidation } from '@/app/hooks';
+import { useDataManager, usePromiseMap, useValidation } from '@/app/hooks';
 import WalletController from '@/app/lib/controller/MobileWalletController';
 import { observer } from 'mobx-react-lite';
 import { WalletAccountType } from '@/app/constants';
+import { AccountService } from '@/app/lib/services';
 
 export const AddSeedAccount = observer(function AddSeedAccount() {
-    const { accounts, accountInfos, seedAddresses, networkIdentifier, ticker } = WalletController;
+    const { accounts, seedAddresses, networkIdentifier, networkProperties, ticker } = WalletController;
     const [accountName, setAccountName] = useState('');
     const nameErrorMessage = useValidation(accountName, [validateAccountName()], $t);
-    const [accountBalanceStateMap, setAccountBalanceStateMap] = usePromises({});
+    const [accountBalanceStateMap, fetchAccountBalances] = usePromiseMap();
     const networkAccounts = accounts[networkIdentifier];
     const networkSeedAccounts = seedAddresses[networkIdentifier];
     const remainedSeedAccounts = networkSeedAccounts.filter(
@@ -37,11 +38,11 @@ export const AddSeedAccount = observer(function AddSeedAccount() {
         handleError
     );
     const fetchBalances = async () => {
-        const updatedAccountBalanceStateMap = {};
-        for (const account of networkSeedAccounts) {
-            updatedAccountBalanceStateMap[account.publicKey] = () => WalletController.fetchAccountInfo(account.publicKey);
-        }
-        setAccountBalanceStateMap(updatedAccountBalanceStateMap);
+        const accountBalanceFetchMap = {};
+        networkSeedAccounts.forEach((seedAccount) => {
+            accountBalanceFetchMap[seedAccount.publicKey] = AccountService.fetchAccountBalance(networkProperties, seedAccount.address);
+        });
+        fetchAccountBalances(accountBalanceFetchMap);
     };
 
     useEffect(() => {
@@ -80,9 +81,9 @@ export const AddSeedAccount = observer(function AddSeedAccount() {
                                 <AccountCard
                                     name={getDefaultAccountName(item.index)}
                                     address={item.address}
-                                    balance={accountInfos[networkIdentifier][item.publicKey]?.balance || 0}
+                                    balance={accountBalanceStateMap[item.publicKey]?.value || 0}
                                     ticker={ticker}
-                                    isLoading={accountBalanceStateMap[item.publicKey]}
+                                    isLoading={!accountBalanceStateMap[item.publicKey]}
                                     isActive={false}
                                     isSimplified
                                 />
