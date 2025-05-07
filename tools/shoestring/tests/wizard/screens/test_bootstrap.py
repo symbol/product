5a31d8c27f6256352956b90589157f9d2fba04c3
wayonb <wayon@symbol.dev>
2025-05-07 14:55:58 -0400

    [tools/shoestring] fix: add more tests to improve CC

--- tools/shoestring/tests/wizard/screens/test_bootstrap.py
+++ tools/shoestring/tests/wizard/screens/test_bootstrap.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from shoestring.wizard.ScreenContainer import ScreenContainer
 from shoestring.wizard.screens.bootstrap import create
 from shoestring.wizard.screens.harvesting import create as create_harvesting
+from shoestring.wizard.screens.node_settings import create as create_node_settings
 from shoestring.wizard.screens.voting import create as create_voting
 
 # pylint: disable=invalid-name
@@ -70,7 +71,6 @@ async def test_can_disable_include_node_key():
 	screen.accessor._include_node_key_flag.current_values = []  # pylint: disable=protected-access
 
 	# Assert: check entered values
-	# Assert: check entered values
 	assert screen.accessor.active
 	assert not screen.accessor.include_node_key
 	assert '' == screen.accessor.bootstrap_path
@@ -116,3 +116,93 @@ async def test_can_disable_harvesting_and_voting_screen():
 	assert screen.accessor.active
 	assert not screens.get_screen('harvesting').should_show()
 	assert not screens.get_screen('voting').should_show()
+
+
+async def test_can_update_node_settings_screen():
+	# Arrange:
+	with tempfile.TemporaryDirectory() as bootstrap_path:
+		bootstrap_resources_path = Path(bootstrap_path) / 'nodes/node/server-config/resources'
+		bootstrap_resources_path.mkdir(parents=True)
+
+		with open(bootstrap_resources_path / 'config-node.properties', 'wt', encoding='utf8') as outfile:
+			outfile.write('\n'.join([
+				'[localnode]',
+				'host = test.xyz',
+				'friendlyName = test'
+			]))
+
+		screens = ScreenContainer(None)
+		screens.add('bootstrap', create(screens))
+		screens.add('node-settings', create_node_settings(screens))
+
+		# Act:
+		screen = screens.get_screen('bootstrap')
+		screen.accessor._flag.current_values = [()]  # pylint: disable=protected-access
+		screen.accessor._bootstrap_path.input.text = bootstrap_path  # pylint: disable=protected-access
+		screen.accessor._bootstrap_path.input.buffer.validate()  # pylint: disable=protected-access
+
+		# Assert: check entered values
+		node_settings = screens.get('node-settings')
+		assert 'test.xyz' == node_settings.domain_name
+		assert 'test' == node_settings.friendly_name
+
+
+async def test_can_generate_diagnostic_accessor_representation_bootstrap_disabled():
+	# Arrange:
+	screen = create(None)
+	screen.accessor._flag.current_values = []  # pylint: disable=protected-access
+	screen.accessor._include_node_key_flag.current_values = [()]  # pylint: disable=protected-access
+	screen.accessor._bootstrap_path.input.text = ''  # pylint: disable=protected-access
+	screen.accessor._bootstrap_path.input.buffer.validate()  # pylint: disable=protected-access
+
+	# Assert: check entered values
+	assert (
+		'(active=False, include_node_key=True, bootstrap_path=\'\')'
+	) == repr(screen.accessor)
+	assert [
+		('bootstrap import', 'disabled')
+	] == screen.accessor.tokens
+
+
+async def test_can_generate_diagnostic_accessor_representation_bootstrap_enabled_include_node_key_enabled():
+	# Arrange:
+	with tempfile.TemporaryDirectory() as bootstrap_path:
+		(Path(bootstrap_path) / 'nodes/node').mkdir(parents=True)
+
+		screen = create(None)
+		screen.accessor._flag.current_values = [()]  # pylint: disable=protected-access
+		screen.accessor._include_node_key_flag.current_values = [()]  # pylint: disable=protected-access
+		screen.accessor._bootstrap_path.input.text = bootstrap_path  # pylint: disable=protected-access
+		screen.accessor._bootstrap_path.input.buffer.validate()  # pylint: disable=protected-access
+
+		# Assert: check entered values
+		assert (
+			f'(active=True, include_node_key=True, bootstrap_path=\'{bootstrap_path}\')'
+		) == repr(screen.accessor)
+		assert [
+			('bootstrap import', 'enabled'),
+			('include node key', 'enabled'),
+			('target directory', bootstrap_path)
+		] == screen.accessor.tokens
+
+
+async def test_can_generate_diagnostic_accessor_representation_bootstrap_enabled_include_node_key_disabled():
+	# Arrange:
+	with tempfile.TemporaryDirectory() as bootstrap_path:
+		(Path(bootstrap_path) / 'nodes/node').mkdir(parents=True)
+
+		screen = create(None)
+		screen.accessor._flag.current_values = [()]  # pylint: disable=protected-access
+		screen.accessor._include_node_key_flag.current_values = []  # pylint: disable=protected-access
+		screen.accessor._bootstrap_path.input.text = bootstrap_path  # pylint: disable=protected-access
+		screen.accessor._bootstrap_path.input.buffer.validate()  # pylint: disable=protected-access
+
+		# Assert: check entered values
+		assert (
+			f'(active=True, include_node_key=False, bootstrap_path=\'{bootstrap_path}\')'
+		) == repr(screen.accessor)
+		assert [
+			('bootstrap import', 'enabled'),
+			('include node key', 'disabled'),
+			('target directory', bootstrap_path)
+		] == screen.accessor.tokens
