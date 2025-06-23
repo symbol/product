8c8c2c811ebe6b3540eb6edefef1d9fba1979b5b
ccHarvestasya <harvestasya84@gmail.com>
2025-06-23 23:02:24 +0900

    [tools/shoestring] fix: entry_point test

--- tools/shoestring/tests/wizard/test_main.py
+++ tools/shoestring/tests/wizard/test_main.py
@@ -4,7 +4,7 @@ import shoestring.wizard.__main__
 
 
 def test_entry_point_runs(monkeypatch):
-	async def dummy_run_async(self):
+	async def dummy_run_async():
 		return None
 	monkeypatch.setattr(
 		'shoestring.wizard.__main__.Application.run_async',
