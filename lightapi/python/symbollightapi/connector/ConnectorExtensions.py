ce656031fb9484ad5e51cc20bfff0e1139f1a64f
Jaguar0625 <jaguar0625@127.0.0.1>
2025-06-24 19:28:42 -0400

    [lightapi/python]: update get_incoming_transactions_from to filter by start and end height
     problem: get_incoming_transactions_from only has a (required) start height filter
    solution: add an end height filter and make both height filters optional

--- lightapi/python/symbollightapi/connector/ConnectorExtensions.py
+++ lightapi/python/symbollightapi/connector/ConnectorExtensions.py
@@ -1,16 +1,20 @@
-async def get_incoming_transactions_from(connector, address, start_height):
-	"""Uses the specified connector to retrieve all transactions sent an account at or after a specified block height."""
+async def get_incoming_transactions_from(connector, address, start_height=None, end_height=None):
+	"""Uses the specified connector to retrieve all transactions sent to an account in the range [start_height, end_height)."""
 
 	start_id = None
 	while True:
-		transactions = await connector.incoming_transactions(address, start_id)
-		if not transactions:
+		transactions_json = await connector.incoming_transactions(address, start_id)
+		if not transactions_json:
 			return
 
-		for transaction in transactions:
-			if int(transaction['meta']['height']) < start_height:
+		for transaction_json in transactions_json:
+			transaction_height = int(transaction_json['meta']['height'])
+			if start_height and transaction_height < start_height:
 				return
 
-			yield transaction
+			if end_height and transaction_height >= end_height:
+				continue
 
-		start_id = connector.extract_transaction_id(transactions[-1])
+			yield transaction_json
+
+		start_id = connector.extract_transaction_id(transactions_json[-1])
