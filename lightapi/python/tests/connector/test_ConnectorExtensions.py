18b378a6de5d1d8b17db9c4edb29c67c862a6f63
Jaguar0625 <jaguar0625@127.0.0.1>
2025-06-24 19:28:42 -0400

    [lightapi/python]: update get_incoming_transactions_from to filter by start and end height
     problem: get_incoming_transactions_from only has a (required) start height filter
    solution: add an end height filter and make both height filters optional

--- lightapi/python/tests/connector/test_ConnectorExtensions.py
+++ lightapi/python/tests/connector/test_ConnectorExtensions.py
@@ -21,6 +21,14 @@ class MockConnector:
 
 # region get_incoming_transactions_from
 
+def _make_transaction_template_from_height(height):
+	transaction_json = {'meta': {'height': height}}
+	if 0 == height % 25:
+		transaction_json['meta']['id'] = (height // 25) * (height // 25)
+
+	return transaction_json
+
+
 async def test_get_incoming_transactions_from_can_return_none():
 	# Arrange:
 	connector = MockConnector({
@@ -28,52 +36,93 @@ async def test_get_incoming_transactions_from_can_return_none():
 	})
 
 	# Act:
-	transactions = [transaction async for transaction in get_incoming_transactions_from(connector, 'foo_address', 100)]
+	transactions = [transaction async for transaction in get_incoming_transactions_from(connector, 'foo_address')]
 
 	# Assert:
 	assert [] == transactions
 
 
-async def test_get_incoming_transactions_from_can_complete_in_single_remote_call():
+async def _assert_get_incoming_transactions_from_can_complete_in_single_remote_call(expected_returned_heights, **kwargs):
 	# Arrange:
 	connector = MockConnector({
-		('foo_address', None): [{'meta': {'height': height}} for height in [175, 125, 101, 100, 99, 75]]
+		('foo_address', None): [_make_transaction_template_from_height(height) for height in [176, 130, 125, 101, 100, 99, 75]],
+		('foo_address', 9): []
 	})
 
 	# Act:
-	transactions = [transaction async for transaction in get_incoming_transactions_from(connector, 'foo_address', 100)]
+	transactions = [transaction async for transaction in get_incoming_transactions_from(connector, 'foo_address', **kwargs)]
 
 	# Assert:
-	assert [{'meta': {'height': height}} for height in [175, 125, 101, 100]] == transactions
+	assert [_make_transaction_template_from_height(height) for height in expected_returned_heights] == transactions
+
+
+async def test_get_incoming_transactions_from_can_complete_in_single_remote_call():
+	await _assert_get_incoming_transactions_from_can_complete_in_single_remote_call([176, 130, 125, 101, 100, 99, 75])
+
+
+async def test_get_incoming_transactions_from_can_complete_in_single_remote_call_with_start_height_filter():
+	await _assert_get_incoming_transactions_from_can_complete_in_single_remote_call([176, 130, 125, 101, 100], start_height=100)
+
+
+async def test_get_incoming_transactions_from_can_complete_in_single_remote_call_with_end_height_filter():
+	await _assert_get_incoming_transactions_from_can_complete_in_single_remote_call([100, 99, 75], end_height=101)
+
+
+async def test_get_incoming_transactions_from_can_complete_in_single_remote_call_with_start_and_end_height_filter():
+	await _assert_get_incoming_transactions_from_can_complete_in_single_remote_call([125, 101, 100], start_height=100, end_height=130)
+
+
+async def _assert_get_incoming_transactions_from_can_complete_in_multiple_remote_calls(expected_returned_heights, **kwargs):
+	# Arrange:
+	connector = MockConnector({
+		('foo_address', None): [_make_transaction_template_from_height(height) for height in [176, 130, 125]],
+		('foo_address', 25): [_make_transaction_template_from_height(height) for height in [101, 100]],
+		('foo_address', 16): [_make_transaction_template_from_height(height) for height in [99, 75]],
+		('foo_address', 9): []
+	})
+
+	# Act:
+	transactions = [transaction async for transaction in get_incoming_transactions_from(connector, 'foo_address', **kwargs)]
+
+	# Assert:
+	assert [_make_transaction_template_from_height(height) for height in expected_returned_heights] == transactions
 
 
 async def test_get_incoming_transactions_from_can_complete_in_multiple_remote_calls():
+	await _assert_get_incoming_transactions_from_can_complete_in_multiple_remote_calls([176, 130, 125, 101, 100, 99, 75])
+
+
+async def test_get_incoming_transactions_from_can_complete_in_multiple_remote_calls_with_start_height_filter():
+	await _assert_get_incoming_transactions_from_can_complete_in_multiple_remote_calls([176, 130, 125, 101, 100], start_height=100)
+
+
+async def test_get_incoming_transactions_from_can_complete_in_multiple_remote_calls_with_end_height_filter():
+	await _assert_get_incoming_transactions_from_can_complete_in_multiple_remote_calls([100, 99, 75], end_height=101)
+
+
+async def test_get_incoming_transactions_from_can_complete_in_multiple_remote_calls_with_start_and_end_height_filter():
+	await _assert_get_incoming_transactions_from_can_complete_in_multiple_remote_calls([125, 101, 100], start_height=100, end_height=130)
+
+
+async def test_get_incoming_transactions_from_supports_string_heights_and_ids():
 	# Arrange:
+	def _make_transaction_template_from_height_str(height):
+		transaction_json = _make_transaction_template_from_height(height)
+		transaction_json['meta']['height'] = str(transaction_json['meta']['height'])
+		if 'id' in transaction_json['meta']:
+			transaction_json['meta']['id'] = str(transaction_json['meta']['id'])
+
+		return transaction_json
+
 	connector = MockConnector({
-		('foo_address', None): [{'meta': {'height': 175}}, {'meta': {'height': 125, 'id': 4}}],
-		('foo_address', 4): [{'meta': {'height': 101}}, {'meta': {'height': 100, 'id': 9}}],
-		('foo_address', 9): [{'meta': {'height': 99}}, {'meta': {'height': 75}}]
+		('foo_address', None): [_make_transaction_template_from_height_str(height) for height in [176, 130, 125, 101, 100, 99, 75]],
+		('foo_address', '9'): []
 	})
 
 	# Act:
-	transactions = [transaction async for transaction in get_incoming_transactions_from(connector, 'foo_address', 100)]
+	transactions = [transaction async for transaction in get_incoming_transactions_from(connector, 'foo_address')]
 
 	# Assert:
-	assert [
-		{'meta': {'height': 175}}, {'meta': {'height': 125, 'id': 4}}, {'meta': {'height': 101}}, {'meta': {'height': 100, 'id': 9}}
-	] == transactions
-
-
-async def test_get_incoming_transactions_from_supports_string_heights():
-	# Arrange:
-	connector = MockConnector({
-		('foo_address', None): [{'meta': {'height': str(height)}} for height in [175, 125, 101, 100, 99, 75]]
-	})
-
-	# Act:
-	transactions = [transaction async for transaction in get_incoming_transactions_from(connector, 'foo_address', 100)]
-
-	# Assert:
-	assert [{'meta': {'height': str(height)}} for height in [175, 125, 101, 100]] == transactions
+	assert [_make_transaction_template_from_height_str(height) for height in [176, 130, 125, 101, 100, 99, 75]] == transactions
 
 # endregion
