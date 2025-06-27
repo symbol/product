fca5c488e9229583862386c9cba2d099255f79de
Jaguar0625 <jaguar0625@127.0.0.1>
2025-06-27 13:31:46 -0400

    [bridge]: add Symbol support for downloading wrap requests
     problem: download_wrap_request_transactions needs to work with Symbol too
    solution: add Symbol adapters to `download_wrap_request_transactions`
              generalize database to allow multiple wrap requests and/or errors per transaction

--- bridge/tests/test/BridgeTestUtils.py
+++ bridge/tests/test/BridgeTestUtils.py
@@ -7,12 +7,10 @@ HEIGHTS = [
 	12345678905, 12345678901, 12345678903, 12345678902
 ]
 
-
 HASHES = [  # sorted
 	'FA650B75CC01187E004FCF547796930CC95D9CF55E6E6188FC7D413526A840FA', 'C16CF93C5A1B6620F3D4C7A7EAAE1990D9C2678775FBC18EAE577A78C8D52B25',
 	'7D7EB08675D6F78FAB8E7D703994390DD95C6A9E8ADD18A9CF13CE4C632F8F01', '22DB80B584620906DF2BF5361B5A31D3CE0E78672CC4D31532C1043EAA1D5624'
 ]
-
 
 PUBLIC_KEYS = [
 	'5C80309DD937ACB0869F1BEEDE14B552E02AEC4B3569E2E2E76477F84B7E3121', '5AA84DF43CD5173A79D91310CE2042434F8B4A7DA157CE3C9DE5C993AEC18293',
@@ -20,12 +18,10 @@ PUBLIC_KEYS = [
 	'AF39DF4A01DA4DDB3545CB1694F71C5D0EB062048DA13ABA871D34BDB04BA715'
 ]
 
-
 NEM_ADDRESSES = [
 	'TAHTNAEQNDJOBDHHRON7SKU7PO6GAWXAJZ4CB2QG', 'TA4FO6TJBIG3VODLUJTPYTJPXDOSTOFZKDGMXPZF', 'TA7TKNUYFE5BVW6HELM7GAUECYPQ6ATK5MR4DNQ5',
 	'TB7GF436SYPM4UQF2YYI563QIETUO5NZR6EREKPI', 'TCYIHED7HZQ3IPBY5WRDPDLV5CCMMOOVSOMSPD6B'
 ]
-
 
 SYMBOL_ADDRESSES = [
 	'NCU36L7K7B4I5JP5HHROFVFZYSCKKXWQI6PDT6I', 'NCLAZCJ36LUDVHNYZPWN67NI4V5E6VZJNZ666XY', 'NBLVHBI6VOMCI4QV53ZCKV5IRM7ZKCAYZYBECXQ',
@@ -37,20 +33,64 @@ def make_request_error(index, message, **kwargs):
 	address = Address(NEM_ADDRESSES[kwargs.get('address_index', index)])
 	height = kwargs.get('height', None) or HEIGHTS[index]
 	transaction_hash = Hash256(HASHES[kwargs.get('hash_index', index)])
-	return WrapError(height, transaction_hash, address, message)
+	transaction_subindex = kwargs.get('transaction_subindex', 0)
+	return WrapError(height, transaction_hash, transaction_subindex, address, message)
 
 
 def make_request(index, **kwargs):
 	address = Address(NEM_ADDRESSES[kwargs.get('address_index', index)])
 	height = kwargs.get('height', None) or HEIGHTS[index]
 	transaction_hash = Hash256(HASHES[kwargs.get('hash_index', index)])
+	transaction_subindex = kwargs.get('transaction_subindex', 0)
 	destination_address = PublicKey(PUBLIC_KEYS[kwargs.get('destination_address_index', index)])
-	return WrapRequest(height, transaction_hash, address, height % 1000, f'0x{destination_address}')
+	return WrapRequest(height, transaction_hash, transaction_subindex, address, height % 1000, f'0x{destination_address}')
+
+
+def make_wrap_error_from_request(request, message):
+	return WrapError(
+		request.transaction_height,
+		request.transaction_hash,
+		request.transaction_subindex,
+		request.sender_address,
+		message)
+
+
+def change_request_amount(request, amount):
+	return WrapRequest(
+		request.transaction_height,
+		request.transaction_hash,
+		request.transaction_subindex,
+		request.sender_address,
+		amount,
+		request.destination_address)
+
+
+def change_request_destination_address(request, destination_address):
+	return WrapRequest(
+		request.transaction_height,
+		request.transaction_hash,
+		request.transaction_subindex,
+		request.sender_address,
+		request.amount,
+		destination_address)
 
 
 def assert_equal_request(asserter, expected, actual):
 	asserter.assertEqual(expected.transaction_height, actual.transaction_height)
 	asserter.assertEqual(expected.transaction_hash, actual.transaction_hash)
+	asserter.assertEqual(expected.transaction_subindex, actual.transaction_subindex)
 	asserter.assertEqual(expected.sender_address, actual.sender_address)
 	asserter.assertEqual(expected.amount, actual.amount)
 	asserter.assertEqual(expected.destination_address, actual.destination_address)
+
+
+def assert_wrap_request_success(asserter, result, expected_request):
+	asserter.assertEqual(False, result.is_error)
+	asserter.assertEqual(expected_request, result.request)
+	asserter.assertEqual(None, result.error)
+
+
+def assert_wrap_request_failure(asserter, result, expected_error):
+	asserter.assertEqual(True, result.is_error)
+	asserter.assertEqual(None, result.request)
+	asserter.assertEqual(expected_error, result.error)
