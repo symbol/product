4bc6d75d4058915ed6a8bcf4c9daf189a3d8da2e
Jaguar0625 <jaguar0625@127.0.0.1>
2025-07-01 11:52:40 -0400

    [bridge]: add new calculate_conversion workflow
     problem: need to calculate wrapped token : native token conversion rate
    solution: add (temporary) workflow to perform calculation and add missing functions

--- bridge/tests/nem/test_NemNetworkFacade.py
+++ bridge/tests/nem/test_NemNetworkFacade.py
@@ -1,6 +1,6 @@
-import unittest
 from binascii import hexlify
 
+import pytest
 from symbolchain.CryptoTypes import Hash256
 from symbolchain.nc import MessageType, NetworkType, TransferTransactionV1
 from symbolchain.nem.Network import Address
@@ -10,70 +10,95 @@ from bridge.models.WrapRequest import WrapRequest
 from bridge.nem.NemNetworkFacade import NemNetworkFacade
 
 from ..test.BridgeTestUtils import assert_wrap_request_success
+from ..test.PytestUtils import PytestAsserter, create_simple_nem_client
 
 
-class NemNetworkFacadeTest(unittest.TestCase):
-	def test_can_create_facade(self):
-		# Act:
-		facade = NemNetworkFacade('testnet')
+@pytest.fixture
+async def server(aiohttp_client):
+	return await create_simple_nem_client(aiohttp_client, {
+		'TCQKTUUNUOPQGDQIDQT2CCJGQZ4QNAAGBZRV5YJJ': 9988776655
+	})
 
-		# Assert:
-		self.assertEqual('testnet', facade.network.name)
 
-	def test_can_create_connector(self):
-		# Arrange:
-		facade = NemNetworkFacade('testnet')
+# pylint: disable=invalid-name
 
-		# Act:
-		connector = facade.create_connector('http://foo.bar:1234')
 
-		# Assert:
-		self.assertIsInstance(connector, NemConnector)
+def test_can_create_facade():
+	# Act:
+	facade = NemNetworkFacade('testnet')
 
-	def test_can_make_address(self):
-		# Arrange:
-		facade = NemNetworkFacade('testnet')
+	# Assert:
+	assert 'testnet' == facade.network.name
 
-		# Act:
-		address_from_string = facade.make_address('TAHTNAEQNDJOBDHHRON7SKU7PO6GAWXAJZ4CB2QG')
-		address_from_bytes = facade.make_address(b'\x98\x0f6\x80\x90h\xd2\xe0\x8c\xe7\x8b\x9b\xf9*\x9f{\xbc`Z\xe0Nx \xea\x06')
 
-		# Assert:
-		self.assertEqual(Address('TAHTNAEQNDJOBDHHRON7SKU7PO6GAWXAJZ4CB2QG'), address_from_string)
-		self.assertEqual(Address('TAHTNAEQNDJOBDHHRON7SKU7PO6GAWXAJZ4CB2QG'), address_from_bytes)
+def test_can_create_connector():
+	# Arrange:
+	facade = NemNetworkFacade('testnet')
 
-	def test_can_extract_wrap_request_from_transaction(self):
-		# Arrange:
-		facade = NemNetworkFacade('testnet')
+	# Act:
+	connector = facade.create_connector('http://foo.bar:1234')
 
-		# Act:
-		results = facade.extract_wrap_request_from_transaction({
-			'meta': {
-				'height': 1234,
-				'hash': {
-					'data': 'FA650B75CC01187E004FCF547796930CC95D9CF55E6E6188FC7D413526A840FA'
-				},
+	# Assert:
+	assert isinstance(connector, NemConnector)
+
+
+def test_can_make_address():
+	# Arrange:
+	facade = NemNetworkFacade('testnet')
+
+	# Act:
+	address_from_string = facade.make_address('TAHTNAEQNDJOBDHHRON7SKU7PO6GAWXAJZ4CB2QG')
+	address_from_bytes = facade.make_address(b'\x98\x0f6\x80\x90h\xd2\xe0\x8c\xe7\x8b\x9b\xf9*\x9f{\xbc`Z\xe0Nx \xea\x06')
+
+	# Assert:
+	assert Address('TAHTNAEQNDJOBDHHRON7SKU7PO6GAWXAJZ4CB2QG') == address_from_string
+	assert Address('TAHTNAEQNDJOBDHHRON7SKU7PO6GAWXAJZ4CB2QG') == address_from_bytes
+
+
+async def test_can_extract_wrap_request_from_transaction():
+	# Arrange:
+	facade = NemNetworkFacade('testnet')
+
+	# Act:
+	results = facade.extract_wrap_request_from_transaction({
+		'meta': {
+			'height': 1234,
+			'hash': {
+				'data': 'FA650B75CC01187E004FCF547796930CC95D9CF55E6E6188FC7D413526A840FA'
 			},
-			'transaction': {
-				'type': TransferTransactionV1.TRANSACTION_TYPE.value,
-				'version': (NetworkType.TESTNET.value << 24) | TransferTransactionV1.TRANSACTION_VERSION,
-				'amount': 8888,
-				'signer': '3917578FF27A88B20E137D9D2E54E775163F9C493A193A64F96748EBF8B21F3C',
-				'message': {
-					'type': MessageType.PLAIN.value,
-					'payload': hexlify('0x4838b106fce9647bdf1e7877bf73ce8b0bad5f97'.encode('utf8')).decode('utf8')
-				}
+		},
+		'transaction': {
+			'type': TransferTransactionV1.TRANSACTION_TYPE.value,
+			'version': (NetworkType.TESTNET.value << 24) | TransferTransactionV1.TRANSACTION_VERSION,
+			'amount': 8888,
+			'signer': '3917578FF27A88B20E137D9D2E54E775163F9C493A193A64F96748EBF8B21F3C',
+			'message': {
+				'type': MessageType.PLAIN.value,
+				'payload': hexlify('0x4838b106fce9647bdf1e7877bf73ce8b0bad5f97'.encode('utf8')).decode('utf8')
 			}
-		})
+		}
+	})
 
-		# Assert:
-		self.assertEqual(1, len(results))
+	# Assert:
+	assert 1 == len(results)
 
-		expected_request = WrapRequest(
-			1234,
-			Hash256('FA650B75CC01187E004FCF547796930CC95D9CF55E6E6188FC7D413526A840FA'),
-			-1,
-			Address('TCQKTUUNUOPQGDQIDQT2CCJGQZ4QNAAGBZRV5YJJ'),
-			8888,
-			'0x4838b106fce9647bdf1e7877bf73ce8b0bad5f97')
-		assert_wrap_request_success(self, results[0], expected_request)
+	expected_request = WrapRequest(
+		1234,
+		Hash256('FA650B75CC01187E004FCF547796930CC95D9CF55E6E6188FC7D413526A840FA'),
+		-1,
+		Address('TCQKTUUNUOPQGDQIDQT2CCJGQZ4QNAAGBZRV5YJJ'),
+		8888,
+		'0x4838b106fce9647bdf1e7877bf73ce8b0bad5f97')
+	assert_wrap_request_success(PytestAsserter(), results[0], expected_request)
+
+
+async def test_can_lookup_account_balance(server):  # pylint: disable=redefined-outer-name
+	# Arrange:
+	facade = NemNetworkFacade('testnet')
+	connector = facade.create_connector(server.make_url(''))
+
+	# Act:
+	balance = await facade.lookup_account_balance(connector, Address('TCQKTUUNUOPQGDQIDQT2CCJGQZ4QNAAGBZRV5YJJ'))
+
+	# Assert:
+	assert 9988776655 == balance
--- bridge/tests/test/PytestUtils.py
+++ bridge/tests/test/PytestUtils.py
@@ -2,6 +2,8 @@ import json
 
 from aiohttp import web
 
+# region PytestAsserter
+
 
 class PytestAsserter:
 	"""Adapter for mapping unittest-style asserts to pytest asserts."""
@@ -10,15 +12,34 @@ class PytestAsserter:
 	def assertEqual(expected, actual):  # pylint: disable=invalid-name
 		assert expected == actual
 
+# endregion
 
-async def create_symbol_client_with_network_properties(aiohttp_client, currency_mosaic_id):  # pylint: disable=invalid-name
-	"""Creates a symbol client with a single /network/properties endpoint."""
+
+# region create_simple_symbol_client
+
+async def create_simple_symbol_client(aiohttp_client, currency_mosaic_id, address_to_balance_map=None):  # pylint: disable=invalid-name
+	"""Creates a symbol client with support for looking up a network currency and account balances ."""
 
 	class MockSymbolServer:
 		@staticmethod
 		async def network_properties(request):
 			return await MockSymbolServer._process(request, {
 				'chain': {'currencyMosaicId': currency_mosaic_id}
+			})
+
+		@staticmethod
+		async def accounts_by_id(request):
+			balance = 0
+			if address_to_balance_map:
+				account_id = request.match_info['account_id']
+				balance = address_to_balance_map.get(account_id, 0)
+
+			return await MockSymbolServer._process(request, {
+				'account': {
+					'mosaics': [
+						{'id': currency_mosaic_id[2:].replace('\'', ''), 'amount': str(balance)},
+					]
+				}
 			})
 
 		@staticmethod
@@ -31,7 +52,48 @@ async def create_symbol_client_with_network_properties(aiohttp_client, currency_
 	# create an app using the server
 	app = web.Application()
 	app.router.add_get('/network/properties', mock_server.network_properties)
+	app.router.add_get(r'/accounts/{account_id}', mock_server.accounts_by_id)
 	server = await aiohttp_client(app)  # pylint: disable=redefined-outer-name
 
 	server.mock = mock_server
 	return server
+
+# endregion
+
+
+# region create_simple_nem_client
+
+async def create_simple_nem_client(aiohttp_client, address_to_balance_map=None):  # pylint: disable=invalid-name
+	"""Creates a nem client with support for looking up account balances ."""
+
+	class MockNemServer:
+		@staticmethod
+		async def account_info(request):
+			balance = 0
+			if address_to_balance_map:
+				address = request.url.query['address']
+				balance = address_to_balance_map.get(address, 0)
+
+			return await MockNemServer._process(request, {
+				'meta': {},
+				'account': {
+					'balance': balance
+				}
+			})
+
+		@staticmethod
+		async def _process(_request, response_body, status_code=200):
+			return web.Response(body=json.dumps(response_body), headers={'Content-Type': 'application/json'}, status=status_code)
+
+	# create a mock server
+	mock_server = MockNemServer()
+
+	# create an app using the server
+	app = web.Application()
+	app.router.add_get('/account/get', mock_server.account_info)
+	server = await aiohttp_client(app)  # pylint: disable=redefined-outer-name
+
+	server.mock = mock_server
+	return server
+
+# endregion
