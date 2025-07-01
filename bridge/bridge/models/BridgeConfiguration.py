44dd547d185429e0ac621c30c009d25905a50172
Jaguar0625 <jaguar0625@127.0.0.1>
2025-07-01 19:02:50 -0400

    [bridge]: add support for creating (redemption) transactions
     problem: need to be able to create transfer transactions for unwrap/redemption process
    solution: add create_transfer_transaction to nem and symbol NetworkFacade

--- bridge/bridge/models/BridgeConfiguration.py
+++ bridge/bridge/models/BridgeConfiguration.py
@@ -2,32 +2,47 @@ import configparser
 from collections import namedtuple
 
 MachineConfiguration = namedtuple('MachineConfiguration', ['database_directory', 'log_filename'])
-NetworkConfiguration = namedtuple('NetworkConfiguration', ['blockchain', 'network', 'endpoint', 'bridge_address'])
+NetworkConfiguration = namedtuple('NetworkConfiguration', ['blockchain', 'network', 'endpoint', 'bridge_address', 'extensions'])
 BridgeConfiguration = namedtuple('BridgeConfiguration', ['machine', 'native_network', 'wrapped_network'])
+
+
+def _camel_case_to_snake_case(value):
+	result = ''
+	for char in value:
+		if char.isupper():
+			result += f'_{char.lower()}'
+		else:
+			result += char
+
+	return result
 
 
 def parse_machine_configuration(config):
 	"""Parses machine configuration."""
 
-	database_directory = config['databaseDirectory']
-	log_filename = config['logFilename']
-	return MachineConfiguration(database_directory, log_filename)
+	return MachineConfiguration(config['databaseDirectory'], config['logFilename'])
 
 
 def parse_network_configuration(config):
 	"""Parses network configuration."""
 
-	blockchain = config['blockchain']
-	network = config['network']
-	endpoint = config['endpoint']
-	bridge_address = config['bridgeAddress']
-	return NetworkConfiguration(blockchain, network, endpoint, bridge_address)
+	required_config_properties = ['blockchain', 'network', 'endpoint', 'bridgeAddress']
+	extensions = {}
+
+	for (key, value) in config.items():
+		if key in required_config_properties:
+			continue
+
+		extensions[_camel_case_to_snake_case(key)] = value
+
+	return NetworkConfiguration(*(config[property_name] for property_name in required_config_properties), extensions)
 
 
 def parse_bridge_configuration(filename):
 	"""Parses a bridge configuration file."""
 
 	parser = configparser.ConfigParser()
+	parser.optionxform = str
 	parser.read(filename)
 
 	return BridgeConfiguration(
