3a982ce306075e0b56e31f90262730eb5734f4be
Jaguar0625 <jaguar0625@127.0.0.1>
2025-07-01 19:02:50 -0400

    [bridge]: add support for creating (redemption) transactions
     problem: need to be able to create transfer transactions for unwrap/redemption process
    solution: add create_transfer_transaction to nem and symbol NetworkFacade

--- bridge/bridge/NetworkFacadeLoader.py
+++ bridge/bridge/NetworkFacadeLoader.py
@@ -6,11 +6,11 @@ async def load_network_facade(config):
 	"""Loads a network facade for the specified blockchain."""
 
 	if 'nem' == config.blockchain:
-		return NemNetworkFacade(config.network)
+		return NemNetworkFacade(config)
 
 	if 'symbol' == config.blockchain:
-		facade = SymbolNetworkFacade(config.network)
-		await facade.load_currency_mosaic_ids(config.endpoint)
+		facade = SymbolNetworkFacade(config)
+		await facade.load_currency_mosaic_ids()
 		return facade
 
 	raise ValueError(f'blockchain "{config.blockchain}" is unsupported')
--- bridge/bridge/nem/NemNetworkFacade.py
+++ bridge/bridge/nem/NemNetworkFacade.py
@@ -1,23 +1,24 @@
+from symbolchain.facade.NemFacade import NemFacade
 from symbolchain.nem.Network import Address, Network
 from symbolchain.Network import NetworkLocator
 from symbollightapi.connector.NemConnector import NemConnector
 
-from .NemUtils import extract_wrap_request_from_transaction
+from .NemUtils import calculate_transfer_transaction_fee, extract_wrap_request_from_transaction
 
 
 class NemNetworkFacade:
 	"""NEM network facade."""
 
-	def __init__(self, network_name):
+	def __init__(self, config):
 		"""Creates a NEM network facade."""
 
-		self.network = NetworkLocator.find_by_name(Network.NETWORKS, network_name)
+		self.config = config
+		self.network = NetworkLocator.find_by_name(Network.NETWORKS, config.network)
 
-	@staticmethod
-	def create_connector(endpoint):
+	def create_connector(self):
 		"""Creates a connector to the network."""
 
-		return NemConnector(endpoint)
+		return NemConnector(self.config.endpoint)
 
 	@staticmethod
 	def make_address(raw_address):
@@ -30,8 +31,50 @@ class NemNetworkFacade:
 
 		return [extract_wrap_request_from_transaction(self.network, transaction_with_meta_json)]
 
-	@staticmethod
-	async def lookup_account_balance(connector, address):
+	async def lookup_account_balance(self, address):
 		"""Gets account balance for network currency."""
 
+		connector = self.create_connector()
 		return await connector.balance(address)
+
+	def create_transfer_transaction(self, timestamp, balance_transfer, use_version_one=True):
+		"""Creates a transfer transaction."""
+
+		fee = calculate_transfer_transaction_fee(balance_transfer.amount // 1_000000, balance_transfer.message)
+		transfer_json = {
+			'signer_public_key': balance_transfer.signer_public_key,
+			'recipient_address': balance_transfer.recipient_address,
+			'timestamp': timestamp.timestamp,
+			'deadline': timestamp.add_hours(1).timestamp,
+			'fee': fee
+		}
+
+		if balance_transfer.message:
+			transfer_json['message'] = {
+				'message_type': 'plain',
+				'message': balance_transfer.message
+			}
+
+		if use_version_one:
+			transfer_json = {
+				**transfer_json,
+				'type': 'transfer_transaction_v1',
+				'amount': balance_transfer.amount
+			}
+		else:
+			transfer_json = {
+				**transfer_json,
+				'type': 'transfer_transaction_v2',
+				'amount': 1_000000,
+				'mosaics': [
+					{
+						'mosaic': {
+							'mosaic_id': {'namespace_id': {'name': 'nem'.encode('utf8')}, 'name': 'xem'.encode('utf8')},
+							'amount': balance_transfer.amount
+						}
+					}
+				]
+			}
+
+		facade = NemFacade(self.network)
+		return facade.transaction_factory.create(transfer_json)
