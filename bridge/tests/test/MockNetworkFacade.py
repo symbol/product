93183b02eb3a8741979d2298b3d0797eb265489b
Jaguar0625 <jaguar0625@127.0.0.1>
2025-07-04 11:25:24 -0400

    [bridge]: add payout_transaction table to WrapRequestDatabase
     problem: need to track payout/issuance transactions
    solution: add payout_transaction table

--- bridge/tests/test/MockNetworkFacade.py
+++ bridge/tests/test/MockNetworkFacade.py
@@ -1,8 +1,13 @@
-from symbolchain.nem.Network import Address
+from symbolchain.nem.Network import Address, Network
 
 
 class MockNetworkFacade:
 	"""Mock network facade."""
+
+	def __init__(self):
+		"""Creates a mock network facade."""
+
+		self.network = Network.TESTNET
 
 	@staticmethod
 	def make_address(raw_address):
