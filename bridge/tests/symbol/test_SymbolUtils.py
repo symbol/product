28bbd4ace4868c9752a6d6816371dc63ff95306a
Jaguar0625 <jaguar0625@127.0.0.1>
2025-07-08 15:11:24 -0400

    [bridge]: fix some naming issues in tests
     problem: inconsistent namings should be fixed
    solution: rename can_create_wrap_request_*
              * can(not)_create_wrap_request_* => can(not)_extract_wrap_request_*
              * transaction_meta_json => transaction_with_meta_json

--- bridge/tests/symbol/test_SymbolUtils.py
+++ bridge/tests/symbol/test_SymbolUtils.py
@@ -49,7 +49,7 @@ class SymbolUtilsTest(unittest.TestCase):
 			Address('TA6MYQRFJI24C2Y2WPX7QKAPMUDIS5FWZOBIBEA'): '4B7E7A084005D2149B44F6A782D9E597C0FABE56F4FEEC1738FE5152C69D55C3'
 		}
 
-		transaction_meta_json = {
+		transaction_with_meta_json = {
 			'meta': {
 				'height': str(request.transaction_height),
 				'hash': str(request.transaction_hash),
@@ -62,42 +62,42 @@ class SymbolUtilsTest(unittest.TestCase):
 			}
 		}
 
-		return transaction_meta_json
+		return transaction_with_meta_json
 
-	def _assert_can_create_wrap_request_from_simple_transfer(self, mosaics, **kwargs):
+	def _assert_can_extract_wrap_request_from_simple_transfer(self, mosaics, **kwargs):
 		# Arrange:
 		request = self._create_simple_wrap_request()
-		transaction_meta_json = self._create_transfer_json(request, mosaics, **kwargs)
+		transaction_with_meta_json = self._create_transfer_json(request, mosaics, **kwargs)
 
 		# Act:
-		results = self._extract_wrap_request_from_transaction(transaction_meta_json)
+		results = self._extract_wrap_request_from_transaction(transaction_with_meta_json)
 
 		# Assert:
 		self.assertEqual(1, len(results))
 		assert_wrap_request_success(self, results[0], request)
 
-	def test_can_create_wrap_request_from_transfer_with_single_mosaic(self):
-		self._assert_can_create_wrap_request_from_simple_transfer([
+	def test_can_extract_wrap_request_from_transfer_with_single_mosaic(self):
+		self._assert_can_extract_wrap_request_from_simple_transfer([
 			{'id': 'FAF0EBED913FA202', 'amount': '7777000000'}
 		])
 
-	def test_can_create_wrap_request_from_transfer_with_multiple_mosaics(self):
-		self._assert_can_create_wrap_request_from_simple_transfer([
+	def test_can_extract_wrap_request_from_transfer_with_multiple_mosaics(self):
+		self._assert_can_extract_wrap_request_from_simple_transfer([
 			{'id': 'F9F0EBED913FA202', 'amount': '5555000000'},
 			{'id': 'FAF0EBED913FA202', 'amount': '7777000000'},
 			{'id': 'FBF0EBED913FA202', 'amount': '8888000000'},
 		])
 
-	def test_can_create_wrap_request_from_transfer_with_multiple_mosaics_without_currency_mosaic(self):
+	def test_can_extract_wrap_request_from_transfer_with_multiple_mosaics_without_currency_mosaic(self):
 		# Arrange:
 		request = self._create_simple_wrap_request()
-		transaction_meta_json = self._create_transfer_json(request, [
+		transaction_with_meta_json = self._create_transfer_json(request, [
 			{'id': 'F9F0EBED913FA202', 'amount': '5555000000'},
 			{'id': 'FBF0EBED913FA202', 'amount': '8888000000'},
 		])
 
 		# Act:
-		results = self._extract_wrap_request_from_transaction(transaction_meta_json)
+		results = self._extract_wrap_request_from_transaction(transaction_with_meta_json)
 
 		# Assert:
 		self.assertEqual(1, len(results))
@@ -107,43 +107,43 @@ class SymbolUtilsTest(unittest.TestCase):
 
 	# region extract_wrap_request_from_transaction - transfer (failure)
 
-	def _assert_cannot_create_wrap_request_from_simple_transfer(self, expected_error_message, **kwargs):
+	def _assert_cannot_extract_wrap_request_from_simple_transfer(self, expected_error_message, **kwargs):
 		# Arrange:
 		request = self._create_simple_wrap_request()
-		transaction_meta_json = self._create_transfer_json(request, [
+		transaction_with_meta_json = self._create_transfer_json(request, [
 			{'id': 'FAF0EBED913FA202', 'amount': '7777000000'}
 		], **kwargs)
 
 		if kwargs.get('clear_message', False):
-			del transaction_meta_json['transaction']['message']
+			del transaction_with_meta_json['transaction']['message']
 
 		# Act:
-		results = self._extract_wrap_request_from_transaction(transaction_meta_json)
+		results = self._extract_wrap_request_from_transaction(transaction_with_meta_json)
 
 		# Assert:
 		self.assertEqual(1, len(results))
 		assert_wrap_request_failure(self, results[0], make_wrap_error_from_request(request, expected_error_message))
 
-	def test_cannot_create_wrap_request_from_simple_transfer_without_message(self):
-		self._assert_cannot_create_wrap_request_from_simple_transfer(
+	def test_cannot_extract_wrap_request_from_simple_transfer_without_message(self):
+		self._assert_cannot_extract_wrap_request_from_simple_transfer(
 			'required message is missing',
 			clear_message=True)
 
-	def test_cannot_create_wrap_request_from_other_transaction(self):
-		self._assert_cannot_create_wrap_request_from_simple_transfer(
+	def test_cannot_extract_wrap_request_from_other_transaction(self):
+		self._assert_cannot_extract_wrap_request_from_simple_transfer(
 			'transaction type 16718 is not supported',
 			transaction_type=TransactionType.NAMESPACE_REGISTRATION.value)
 
-	def test_cannot_create_wrap_request_from_transfer_with_invalid_message(self):
+	def test_cannot_extract_wrap_request_from_transfer_with_invalid_message(self):
 		# Arrange:
 		request = self._create_simple_wrap_request()
 		request = change_request_destination_address(request, '0x4838b106fce9647bdf1e7877bf73ce8b0bad5f')  # too short
-		transaction_meta_json = self._create_transfer_json(request, [
+		transaction_with_meta_json = self._create_transfer_json(request, [
 			{'id': 'FAF0EBED913FA202', 'amount': '7777000000'}
 		])
 
 		# Act:
-		results = self._extract_wrap_request_from_transaction(transaction_meta_json)
+		results = self._extract_wrap_request_from_transaction(transaction_with_meta_json)
 
 		# Assert:
 		self.assertEqual(1, len(results))
@@ -156,13 +156,13 @@ class SymbolUtilsTest(unittest.TestCase):
 	# region extract_wrap_request_from_transaction - aggregate (success)
 
 	@staticmethod
-	def _create_aggregate_transaction_meta_json(aggregate_transaction_type, embedded_transaction_meta_jsons):
+	def _create_aggregate_transaction_with_meta_json(aggregate_transaction_type, embedded_transaction_with_meta_jsons):
 		return {
 			'meta': {'height': '23456', 'hash': 'C0B52BE17C2F41539E50857855A226A24AFE8B23B51E42F3186FBC725EA63550'},
 			'transaction': {
 				'type': aggregate_transaction_type.value,
 				'signerPublicKey': '167F2FFDAFE27B6A8DCEFB4A12F0D8A2DB8908FC93D69B09A2E9237E55D7043E',
-				'transactions': embedded_transaction_meta_jsons
+				'transactions': embedded_transaction_with_meta_jsons
 			}
 		}
 
@@ -190,14 +190,14 @@ class SymbolUtilsTest(unittest.TestCase):
 			}
 		}
 
-	def _assert_can_create_wrap_request_from_aggregate_with_single_transfer(self, aggregate_transaction_type):
+	def _assert_can_extract_wrap_request_from_aggregate_with_single_transfer(self, aggregate_transaction_type):
 		# Arrange:
-		transaction_meta_json = self._create_aggregate_transaction_meta_json(aggregate_transaction_type, [
+		transaction_with_meta_json = self._create_aggregate_transaction_with_meta_json(aggregate_transaction_type, [
 			self._create_embedded_transaction_transfer_json(2, PUBLIC_KEYS[0], 1111, '0x4838b106fce9647bdf1e7877bf73ce8b0bad5f90')
 		])
 
 		# Act:
-		results = self._extract_wrap_request_from_transaction(transaction_meta_json)
+		results = self._extract_wrap_request_from_transaction(transaction_with_meta_json)
 
 		# Assert:
 		self.assertEqual(1, len(results))
@@ -211,23 +211,23 @@ class SymbolUtilsTest(unittest.TestCase):
 			'0x4838b106fce9647bdf1e7877bf73ce8b0bad5f90')
 		assert_wrap_request_success(self, results[0], expected_request)
 
-	def test_can_create_wrap_request_from_aggregate_complete_with_single_transfer(self):
-		self._assert_can_create_wrap_request_from_aggregate_with_single_transfer(TransactionType.AGGREGATE_COMPLETE)
+	def test_can_extract_wrap_request_from_aggregate_complete_with_single_transfer(self):
+		self._assert_can_extract_wrap_request_from_aggregate_with_single_transfer(TransactionType.AGGREGATE_COMPLETE)
 
-	def test_can_create_wrap_request_from_aggregate_bonded_with_single_transfer(self):
-		self._assert_can_create_wrap_request_from_aggregate_with_single_transfer(TransactionType.AGGREGATE_BONDED)
+	def test_can_extract_wrap_request_from_aggregate_bonded_with_single_transfer(self):
+		self._assert_can_extract_wrap_request_from_aggregate_with_single_transfer(TransactionType.AGGREGATE_BONDED)
 
-	def _assert_can_create_wrap_request_from_aggregate_with_multiple_transfers(self, aggregate_transaction_type):
+	def _assert_can_extract_wrap_request_from_aggregate_with_multiple_transfers(self, aggregate_transaction_type):
 		# Arrange:
 		destination_addresses = [f'0x4838b106fce9647bdf1e7877bf73ce8b0bad5f{digit}0' for digit in ['9', 'a', 'b']]
-		transaction_meta_json = self._create_aggregate_transaction_meta_json(aggregate_transaction_type, [
+		transaction_with_meta_json = self._create_aggregate_transaction_with_meta_json(aggregate_transaction_type, [
 			self._create_embedded_transaction_transfer_json(2, PUBLIC_KEYS[0], 1111, destination_addresses[0]),
 			self._create_embedded_transaction_transfer_json(4, PUBLIC_KEYS[1], 3333, destination_addresses[1]),
 			self._create_embedded_transaction_transfer_json(5, PUBLIC_KEYS[2], 2222, destination_addresses[2])
 		])
 
 		# Act:
-		results = self._extract_wrap_request_from_transaction(transaction_meta_json)
+		results = self._extract_wrap_request_from_transaction(transaction_with_meta_json)
 
 		# Assert:
 		self.assertEqual(3, len(results))
@@ -242,19 +242,19 @@ class SymbolUtilsTest(unittest.TestCase):
 		assert_wrap_request_success(self, results[1], expected_requests[1])
 		assert_wrap_request_success(self, results[2], expected_requests[2])
 
-	def test_can_create_wrap_request_from_aggregate_complete_with_multiple_transfers(self):
-		self._assert_can_create_wrap_request_from_aggregate_with_multiple_transfers(TransactionType.AGGREGATE_COMPLETE)
+	def test_can_extract_wrap_request_from_aggregate_complete_with_multiple_transfers(self):
+		self._assert_can_extract_wrap_request_from_aggregate_with_multiple_transfers(TransactionType.AGGREGATE_COMPLETE)
 
-	def test_can_create_wrap_request_from_aggregate_bonded_with_multiple_transfers(self):
-		self._assert_can_create_wrap_request_from_aggregate_with_multiple_transfers(TransactionType.AGGREGATE_BONDED)
+	def test_can_extract_wrap_request_from_aggregate_bonded_with_multiple_transfers(self):
+		self._assert_can_extract_wrap_request_from_aggregate_with_multiple_transfers(TransactionType.AGGREGATE_BONDED)
 
-	def _assert_can_create_wrap_request_from_aggregate_with_mix_of_transfers_and_non_transfers(self, aggregate_transaction_type):
+	def _assert_can_extract_wrap_request_from_aggregate_with_mix_of_transfers_and_non_transfers(self, aggregate_transaction_type):
 		# Arrange:
 		def make_error_message(value):
 			return f'transaction type {value} is not supported'
 
 		destination_addresses = [f'0x4838b106fce9647bdf1e7877bf73ce8b0bad5f{digit}0' for digit in ['9', 'b']]
-		transaction_meta_json = self._create_aggregate_transaction_meta_json(aggregate_transaction_type, [
+		transaction_with_meta_json = self._create_aggregate_transaction_with_meta_json(aggregate_transaction_type, [
 			self._create_embedded_transaction_transfer_json(2, PUBLIC_KEYS[0], 1111, destination_addresses[0]),
 			self._create_embedded_transaction_other_json(4, PUBLIC_KEYS[1], TransactionType.NAMESPACE_REGISTRATION),
 			self._create_embedded_transaction_transfer_json(5, PUBLIC_KEYS[2], 2222, destination_addresses[1]),
@@ -262,7 +262,7 @@ class SymbolUtilsTest(unittest.TestCase):
 		])
 
 		# Act:
-		results = self._extract_wrap_request_from_transaction(transaction_meta_json)
+		results = self._extract_wrap_request_from_transaction(transaction_with_meta_json)
 
 		# Assert:
 		self.assertEqual(4, len(results))
@@ -283,24 +283,24 @@ class SymbolUtilsTest(unittest.TestCase):
 		assert_wrap_request_success(self, results[2], expected_requests[1])
 		assert_wrap_request_failure(self, results[3], expected_errors[1])
 
-	def test_can_create_wrap_request_from_aggregate_complete_with_mix_of_transfers_and_non_transfers(self):
-		self._assert_can_create_wrap_request_from_aggregate_with_mix_of_transfers_and_non_transfers(TransactionType.AGGREGATE_COMPLETE)
+	def test_can_extract_wrap_request_from_aggregate_complete_with_mix_of_transfers_and_non_transfers(self):
+		self._assert_can_extract_wrap_request_from_aggregate_with_mix_of_transfers_and_non_transfers(TransactionType.AGGREGATE_COMPLETE)
 
-	def test_can_create_wrap_request_from_aggregate_bonded_with_mix_of_transfers_and_non_transfers(self):
-		self._assert_can_create_wrap_request_from_aggregate_with_mix_of_transfers_and_non_transfers(TransactionType.AGGREGATE_BONDED)
+	def test_can_extract_wrap_request_from_aggregate_bonded_with_mix_of_transfers_and_non_transfers(self):
+		self._assert_can_extract_wrap_request_from_aggregate_with_mix_of_transfers_and_non_transfers(TransactionType.AGGREGATE_BONDED)
 
-	def _assert_can_create_wrap_request_from_aggregate_with_no_transfers(self, aggregate_transaction_type):
+	def _assert_can_extract_wrap_request_from_aggregate_with_no_transfers(self, aggregate_transaction_type):
 		# Arrange:
 		def make_error_message(value):
 			return f'transaction type {value} is not supported'
 
-		transaction_meta_json = self._create_aggregate_transaction_meta_json(aggregate_transaction_type, [
+		transaction_with_meta_json = self._create_aggregate_transaction_with_meta_json(aggregate_transaction_type, [
 			self._create_embedded_transaction_other_json(4, PUBLIC_KEYS[1], TransactionType.NAMESPACE_REGISTRATION),
 			self._create_embedded_transaction_other_json(6, PUBLIC_KEYS[3], TransactionType.ADDRESS_ALIAS)
 		])
 
 		# Act:
-		results = self._extract_wrap_request_from_transaction(transaction_meta_json)
+		results = self._extract_wrap_request_from_transaction(transaction_with_meta_json)
 
 		# Assert:
 		self.assertEqual(2, len(results))
@@ -314,10 +314,10 @@ class SymbolUtilsTest(unittest.TestCase):
 		assert_wrap_request_failure(self, results[0], expected_errors[0])
 		assert_wrap_request_failure(self, results[1], expected_errors[1])
 
-	def test_can_create_wrap_request_from_aggregate_complete_with_no_transfers(self):
-		self._assert_can_create_wrap_request_from_aggregate_with_no_transfers(TransactionType.AGGREGATE_COMPLETE)
+	def test_can_extract_wrap_request_from_aggregate_complete_with_no_transfers(self):
+		self._assert_can_extract_wrap_request_from_aggregate_with_no_transfers(TransactionType.AGGREGATE_COMPLETE)
 
-	def test_can_create_wrap_request_from_aggregate_bonded_with_no_transfers(self):
-		self._assert_can_create_wrap_request_from_aggregate_with_no_transfers(TransactionType.AGGREGATE_BONDED)
+	def test_can_extract_wrap_request_from_aggregate_bonded_with_no_transfers(self):
+		self._assert_can_extract_wrap_request_from_aggregate_with_no_transfers(TransactionType.AGGREGATE_BONDED)
 
 	# endregion
