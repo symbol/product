0d02f59ae1edeb47720d7330efcd74652cb2f50b
Jaguar0625 <jaguar0625@127.0.0.1>
2025-07-08 15:11:24 -0400

    [bridge]: fix some naming issues in tests
     problem: inconsistent namings should be fixed
    solution: rename can_create_wrap_request_*
              * can(not)_create_wrap_request_* => can(not)_extract_wrap_request_*
              * transaction_meta_json => transaction_with_meta_json

--- bridge/tests/nem/test_NemUtils.py
+++ bridge/tests/nem/test_NemUtils.py
@@ -67,7 +67,7 @@ class NemUtilsTest(unittest.TestCase):
 		}
 
 		transaction_version = kwargs.get('transaction_version', TransferTransactionV1.TRANSACTION_VERSION)
-		transaction_meta_json = {
+		transaction_with_meta_json = {
 			'meta': {
 				'height': request.transaction_height,
 				'hash': {
@@ -87,70 +87,70 @@ class NemUtilsTest(unittest.TestCase):
 		}
 
 		if 1 < transaction_version:
-			transaction_meta_json['transaction']['mosaics'] = []
+			transaction_with_meta_json['transaction']['mosaics'] = []
 
-		return transaction_meta_json
+		return transaction_with_meta_json
 
-	def _assert_can_create_wrap_request_from_simple_transfer(self, **kwargs):
+	def _assert_can_extract_wrap_request_from_simple_transfer(self, **kwargs):
 		# Arrange:
 		request = self._create_simple_wrap_request()
-		transaction_meta_json = self._create_transfer_json(request, **kwargs)
+		transaction_with_meta_json = self._create_transfer_json(request, **kwargs)
 
 		# Act:
-		result = self._extract_wrap_request_from_transaction(transaction_meta_json)
+		result = self._extract_wrap_request_from_transaction(transaction_with_meta_json)
 
 		# Assert:
 		assert_wrap_request_success(self, result, request)
 
-	def test_can_create_wrap_request_from_simple_transfer_v1(self):
-		self._assert_can_create_wrap_request_from_simple_transfer(transaction_version=TransferTransactionV1.TRANSACTION_VERSION)
+	def test_can_extract_wrap_request_from_simple_transfer_v1(self):
+		self._assert_can_extract_wrap_request_from_simple_transfer(transaction_version=TransferTransactionV1.TRANSACTION_VERSION)
 
-	def test_can_create_wrap_request_from_simple_transfer_v2(self):
-		self._assert_can_create_wrap_request_from_simple_transfer(transaction_version=TransferTransactionV2.TRANSACTION_VERSION)
+	def test_can_extract_wrap_request_from_simple_transfer_v2(self):
+		self._assert_can_extract_wrap_request_from_simple_transfer(transaction_version=TransferTransactionV2.TRANSACTION_VERSION)
 
 	@staticmethod
 	def _create_transfer_json_with_single_bag(request, amount, mosaic_amount):
-		transaction_meta_json = NemUtilsTest._create_transfer_json(request, transaction_version=TransferTransactionV2.TRANSACTION_VERSION)
-		transaction_meta_json['transaction']['amount'] = amount  # override amount from request
-		transaction_meta_json['transaction']['mosaics'] = [
+		transaction_with_meta_json = NemUtilsTest._create_transfer_json(request, transaction_version=TransferTransactionV2.TRANSACTION_VERSION)
+		transaction_with_meta_json['transaction']['amount'] = amount  # override amount from request
+		transaction_with_meta_json['transaction']['mosaics'] = [
 			{
 				'mosaicId': {'namespaceId': 'nem', 'name': 'xem'},
 				'quantity': mosaic_amount
 			}
 		]
 
-		return transaction_meta_json
+		return transaction_with_meta_json
 
-	def _assert_can_create_wrap_request_from_single_xem_mosaic_in_bag(self, amount, mosaic_amount, expected_amount):
+	def _assert_can_extract_wrap_request_from_single_xem_mosaic_in_bag(self, amount, mosaic_amount, expected_amount):
 		# Arrange:
 		request = self._create_simple_wrap_request()
-		transaction_meta_json = self._create_transfer_json_with_single_bag(request, amount, mosaic_amount)
+		transaction_with_meta_json = self._create_transfer_json_with_single_bag(request, amount, mosaic_amount)
 
 		# Act:
-		result = self._extract_wrap_request_from_transaction(transaction_meta_json)
+		result = self._extract_wrap_request_from_transaction(transaction_with_meta_json)
 
 		# Assert:
 		assert_wrap_request_success(self, result, change_request_amount(request, expected_amount))
 
-	def test_can_create_wrap_request_from_single_bag_transfer_v2(self):
-		self._assert_can_create_wrap_request_from_single_xem_mosaic_in_bag(2_000000, 12345_000000, 24690_000000)
+	def test_can_extract_wrap_request_from_single_bag_transfer_v2(self):
+		self._assert_can_extract_wrap_request_from_single_xem_mosaic_in_bag(2_000000, 12345_000000, 24690_000000)
 
-	def test_can_create_wrap_request_from_single_bag_transfer_v2_large_amount(self):
+	def test_can_extract_wrap_request_from_single_bag_transfer_v2_large_amount(self):
 		# notice that amount * mosaicAmount > Number.MAX_SAFE_INTEGER
-		self._assert_can_create_wrap_request_from_single_xem_mosaic_in_bag(1_000000, 14570747_490000, 14570747_490000)
+		self._assert_can_extract_wrap_request_from_single_xem_mosaic_in_bag(1_000000, 14570747_490000, 14570747_490000)
 
-	def test_can_create_wrap_request_from_single_bag_transfer_v2_fractional_1(self):
-		self._assert_can_create_wrap_request_from_single_xem_mosaic_in_bag(50000, 34_152375, 1_707618)
+	def test_can_extract_wrap_request_from_single_bag_transfer_v2_fractional_1(self):
+		self._assert_can_extract_wrap_request_from_single_xem_mosaic_in_bag(50000, 34_152375, 1_707618)
 
-	def test_can_create_wrap_request_from_single_bag_transfer_v2_fractional_2(self):
-		self._assert_can_create_wrap_request_from_single_xem_mosaic_in_bag(32_495622, 1_000000, 32_495622)
+	def test_can_extract_wrap_request_from_single_bag_transfer_v2_fractional_2(self):
+		self._assert_can_extract_wrap_request_from_single_xem_mosaic_in_bag(32_495622, 1_000000, 32_495622)
 
-	def test_can_create_wrap_request_from_multi_bag_transfer_v2_with_xem(self):
+	def test_can_extract_wrap_request_from_multi_bag_transfer_v2_with_xem(self):
 		# Arrange:
 		request = self._create_simple_wrap_request()
-		transaction_meta_json = self._create_transfer_json(request, transaction_version=TransferTransactionV2.TRANSACTION_VERSION)
-		transaction_meta_json['transaction']['amount'] = 2_000000
-		transaction_meta_json['transaction']['mosaics'] = [
+		transaction_with_meta_json = self._create_transfer_json(request, transaction_version=TransferTransactionV2.TRANSACTION_VERSION)
+		transaction_with_meta_json['transaction']['amount'] = 2_000000
+		transaction_with_meta_json['transaction']['mosaics'] = [
 			{
 				'mosaicId': {'namespaceId': 'baz', 'name': 'baz'},
 				'quantity': 7733_000000
@@ -166,17 +166,17 @@ class NemUtilsTest(unittest.TestCase):
 		]
 
 		# Act:
-		result = self._extract_wrap_request_from_transaction(transaction_meta_json)
+		result = self._extract_wrap_request_from_transaction(transaction_with_meta_json)
 
 		# Assert:
 		assert_wrap_request_success(self, result, change_request_amount(request, 2222_000000))
 
-	def test_can_create_wrap_request_from_multi_bag_transfer_v2_without_xem(self):
+	def test_can_extract_wrap_request_from_multi_bag_transfer_v2_without_xem(self):
 		# Arrange:
 		request = self._create_simple_wrap_request()
-		transaction_meta_json = self._create_transfer_json(request, transaction_version=TransferTransactionV2.TRANSACTION_VERSION)
-		transaction_meta_json['transaction']['amount'] = 2_000000
-		transaction_meta_json['transaction']['mosaics'] = [
+		transaction_with_meta_json = self._create_transfer_json(request, transaction_version=TransferTransactionV2.TRANSACTION_VERSION)
+		transaction_with_meta_json['transaction']['amount'] = 2_000000
+		transaction_with_meta_json['transaction']['mosaics'] = [
 			{
 				'mosaicId': {'namespaceId': 'baz', 'name': 'baz'},
 				'quantity': 7733_000000
@@ -188,7 +188,7 @@ class NemUtilsTest(unittest.TestCase):
 		]
 
 		# Act:
-		result = self._extract_wrap_request_from_transaction(transaction_meta_json)
+		result = self._extract_wrap_request_from_transaction(transaction_with_meta_json)
 
 		# Assert:
 		assert_wrap_request_success(self, result, change_request_amount(request, 0))
@@ -197,43 +197,43 @@ class NemUtilsTest(unittest.TestCase):
 
 	# region extract_wrap_request_from_transaction - transfer (failure)
 
-	def _assert_cannot_create_wrap_request_from_simple_transfer(self, expected_error_message, **kwargs):
+	def _assert_cannot_extract_wrap_request_from_simple_transfer(self, expected_error_message, **kwargs):
 		# Arrange:
 		request = self._create_simple_wrap_request()
-		transaction_meta_json = self._create_transfer_json(request, **kwargs)
+		transaction_with_meta_json = self._create_transfer_json(request, **kwargs)
 
 		if kwargs.get('clear_message', False):
-			transaction_meta_json['transaction']['message'] = {}
+			transaction_with_meta_json['transaction']['message'] = {}
 
 		# Act:
-		result = self._extract_wrap_request_from_transaction(transaction_meta_json)
+		result = self._extract_wrap_request_from_transaction(transaction_with_meta_json)
 
 		# Assert:
 		assert_wrap_request_failure(self, result, make_wrap_error_from_request(request, expected_error_message))
 
-	def test_cannot_create_wrap_request_from_simple_transfer_with_invalid_message_type(self):
-		self._assert_cannot_create_wrap_request_from_simple_transfer(
+	def test_cannot_extract_wrap_request_from_simple_transfer_with_invalid_message_type(self):
+		self._assert_cannot_extract_wrap_request_from_simple_transfer(
 			'message type 2 is not supported',
 			message_type=MessageType.ENCRYPTED.value)
 
-	def test_cannot_create_wrap_request_from_simple_transfer_without_message(self):
-		self._assert_cannot_create_wrap_request_from_simple_transfer(
+	def test_cannot_extract_wrap_request_from_simple_transfer_without_message(self):
+		self._assert_cannot_extract_wrap_request_from_simple_transfer(
 			'required message is missing',
 			clear_message=True)
 
-	def test_cannot_create_wrap_request_from_other_transaction(self):
-		self._assert_cannot_create_wrap_request_from_simple_transfer(
+	def test_cannot_extract_wrap_request_from_other_transaction(self):
+		self._assert_cannot_extract_wrap_request_from_simple_transfer(
 			'transaction type 8193 is not supported',
 			transaction_type=TransactionType.NAMESPACE_REGISTRATION.value)
 
-	def test_cannot_create_wrap_request_from_transfer_with_invalid_message(self):
+	def test_cannot_extract_wrap_request_from_transfer_with_invalid_message(self):
 		# Arrange:
 		request = self._create_simple_wrap_request()
 		request = change_request_destination_address(request, '0x4838b106fce9647bdf1e7877bf73ce8b0bad5f')  # too short
-		transaction_meta_json = self._create_transfer_json(request)
+		transaction_with_meta_json = self._create_transfer_json(request)
 
 		# Act:
-		result = self._extract_wrap_request_from_transaction(transaction_meta_json)
+		result = self._extract_wrap_request_from_transaction(transaction_with_meta_json)
 
 		# Assert:
 		expected_error_message = 'destination address 0x4838b106fce9647bdf1e7877bf73ce8b0bad5f is invalid'
@@ -243,11 +243,11 @@ class NemUtilsTest(unittest.TestCase):
 
 	# region extract_wrap_request_from_transaction - aggregate (success)
 
-	def test_can_create_wrap_request_from_aggregate_with_transfer(self):
+	def test_can_extract_wrap_request_from_aggregate_with_transfer(self):
 		# Arrange:
 		request = self._create_simple_wrap_request(transaction_subindex=0)
-		transaction_meta_json = self._create_transfer_json(request)
-		transaction_meta_json = {
+		transaction_with_meta_json = self._create_transfer_json(request)
+		transaction_with_meta_json = {
 			'meta': self._create_transfer_json(request)['meta'],
 			'transaction': {
 				'type': TransactionType.MULTISIG.value,
@@ -259,7 +259,7 @@ class NemUtilsTest(unittest.TestCase):
 		}
 
 		# Act:
-		result = self._extract_wrap_request_from_transaction(transaction_meta_json)
+		result = self._extract_wrap_request_from_transaction(transaction_with_meta_json)
 
 		# Assert:
 		assert_wrap_request_success(self, result, request)
@@ -268,11 +268,11 @@ class NemUtilsTest(unittest.TestCase):
 
 	# region extract_wrap_request_from_transaction - aggregate (failure)
 
-	def test_can_create_wrap_request_from_aggregate_with_other_transaction(self):
+	def test_can_extract_wrap_request_from_aggregate_with_other_transaction(self):
 		# Arrange:
 		request = self._create_simple_wrap_request(transaction_subindex=0)
-		transaction_meta_json = self._create_transfer_json(request)
-		transaction_meta_json = {
+		transaction_with_meta_json = self._create_transfer_json(request)
+		transaction_with_meta_json = {
 			'meta': self._create_transfer_json(request)['meta'],
 			'transaction': {
 				'type': TransactionType.MULTISIG.value,
@@ -286,7 +286,7 @@ class NemUtilsTest(unittest.TestCase):
 		}
 
 		# Act:
-		result = self._extract_wrap_request_from_transaction(transaction_meta_json)
+		result = self._extract_wrap_request_from_transaction(transaction_with_meta_json)
 
 		# Assert:
 		expected_error_message = 'inner transaction type 8193 is not supported'
