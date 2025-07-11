04952bda2c8b11185d867cba98d100e999d0275b
Jaguar0625 <jaguar0625@127.0.0.1>
2025-07-11 12:04:01 -0400

    [bridge]: refactor and cleanup workflows to support unwrap mode
     problem: most workflows need to support wrap and unwrap modes
    solution: restructure the workflows to support dual modes
              extract common helpers into WorkflowUtils

--- bridge/workflows/main_impl.py
+++ bridge/workflows/main_impl.py
@@ -1,25 +1,22 @@
 import argparse
 import logging
-from collections import namedtuple
 
 from bridge.db.Databases import Databases
 from bridge.models.BridgeConfiguration import parse_bridge_configuration
 from bridge.NetworkFacadeLoader import load_network_facade
 
-Network = namedtuple('Network', ['facade', 'connector', 'bridge_address'])
-
 
 def parse_args(description):
 	parser = argparse.ArgumentParser(description=description)
 	parser.add_argument('--config', help='wrap configuration file', required=True)
+	parser.add_argument('--unwrap', help='run command in unwrap mode', action='store_true')
 	return parser.parse_args()
 
 
-async def load_network(config):
-	network_facade = await load_network_facade(config)
-	connector = network_facade.create_connector()
-	bridge_address = network_facade.make_address(config.bridge_address)
-	return Network(network_facade, connector, bridge_address)
+def print_banner(lines):
+	print('\n*** *** ***')
+	print('\n'.join(lines))
+	print('*** *** ***\n')
 
 
 async def main_bootstrapper(program_description, main_impl):
@@ -27,11 +24,11 @@ async def main_bootstrapper(program_description, main_impl):
 	config = parse_bridge_configuration(args.config)
 	logging.basicConfig(filename=config.machine.log_filename, level=logging.DEBUG)
 
-	native_network = await load_network(config.native_network)
-	wrapped_network = await load_network(config.wrapped_network)
+	native_facade = await load_network_facade(config.native_network)
+	wrapped_facade = await load_network_facade(config.wrapped_network)
 
-	with Databases(config.machine.database_directory, native_network.facade, wrapped_network.facade) as databases:
-		databases.balance_change.create_tables()
-		databases.wrap_request.create_tables()
+	with Databases(config.machine.database_directory, native_facade, wrapped_facade) as databases:
+		databases.create_tables()
 
-		await main_impl(databases, native_network, wrapped_network)
+		print(f'running in unwrap mode? {args.unwrap}')
+		await main_impl(args.unwrap, databases, native_facade, wrapped_facade)
---
+++ bridge/workflows/send_payouts.py
@@ -0,0 +1,81 @@
+import asyncio
+
+from bridge.db.WrapRequestDatabase import WrapRequestStatus
+from bridge.NetworkUtils import TransactionSender
+from bridge.WorkflowUtils import ConversionRateCalculator, extract_mosaic_id
+
+from .main_impl import main_bootstrapper, print_banner
+
+
+class ConversionRateManager:
+	def __init__(self, databases, network, is_unwrap_mode):
+		self.databases = databases
+		self.network = network
+		self.is_unwrap_mode = is_unwrap_mode
+		self.mosaic_id = extract_mosaic_id(network.config, network.is_currency_mosaic_id).formatted
+
+	async def create_calculator(self, height):
+
+		historical_native_height = height
+		if self.is_unwrap_mode:
+			timestamp = self.databases.unwrap_request.lookup_block_timestamp(height)
+			historical_native_height = self.databases.wrap_request.lookup_block_height(timestamp)
+		else:
+			timestamp = self.databases.wrap_request.lookup_block_timestamp(height)
+
+		native_balance = self.databases.balance_change.balance_at(historical_native_height, self.mosaic_id)
+		wrapped_balance = self.databases.wrap_request.cumulative_wrapped_amount_at(timestamp)
+		unwrapped_balance = self.databases.unwrap_request.cumulative_wrapped_amount_at(timestamp)
+
+		calculator = ConversionRateCalculator(native_balance, wrapped_balance, unwrapped_balance)
+		return calculator.to_native_amount if self.is_unwrap_mode else calculator.to_wrapped_amount
+
+
+async def _send_payout(network, request, conversion_rate_calculator):
+	mosaic_id = extract_mosaic_id(network.config, network.is_currency_mosaic_id)
+	sender = TransactionSender(network, mosaic_id.args)
+	transfer_amount = conversion_rate_calculator(request.amount)
+
+	print(
+		f'> issuing {transfer_amount} [{mosaic_id.formatted}] tokens (gross) to {request.destination_address}'
+		f' for deposit of {request.amount} tokens')
+	print(f'  redeeming 1:{transfer_amount/request.amount:.6f}')
+
+	await sender.init()
+	return await sender.try_send_transfer(request.destination_address, transfer_amount)
+
+
+async def send_payouts(conversion_rate_manager, database, network):
+	requests_to_send = database.requests_by_status(WrapRequestStatus.UNPROCESSED)
+	print(f'{len(requests_to_send)} requests need to be sent')
+
+	count = 0
+	error_count = 0
+	for request in requests_to_send:
+		conversion_rate_calculator = await conversion_rate_manager.create_calculator(request.transaction_height)
+		(is_success, error_message_or_transaction_hash) = await _send_payout(network, request, conversion_rate_calculator)
+		if is_success:
+			database.mark_payout_sent(request, error_message_or_transaction_hash)
+			print(f'  sent transaction with hash: {error_message_or_transaction_hash}')
+			count += 1
+		else:
+			database.mark_payout_failed(request, error_message_or_transaction_hash)
+			print(f'  payout failed with error: {error_message_or_transaction_hash}')
+			error_count += 1
+
+	print_banner([
+		f'==>      total payouts processed: {count}',
+		f'==>        total payouts errored: {error_count}'
+	])
+
+
+async def main_impl(is_unwrap_mode, databases, native_facade, wrapped_facade):
+	conversion_rate_manager = ConversionRateManager(databases, native_facade, is_unwrap_mode)
+	if is_unwrap_mode:
+		await send_payouts(conversion_rate_manager, databases.unwrap_request, native_facade)
+	else:
+		await send_payouts(conversion_rate_manager, databases.wrap_request, wrapped_facade)
+
+
+if '__main__' == __name__:
+	asyncio.run(main_bootstrapper('sends wrap or unwrap token payouts', main_impl))
