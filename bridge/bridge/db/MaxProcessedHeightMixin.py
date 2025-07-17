7502034b4c81dc4f3996d73dd9103bf3d606acdf
Jaguar0625 <jaguar0625@127.0.0.1>
2025-07-17 09:10:22 -0400

    [bridge]: explicitly set max_processed_height
     problem: max_processed_height is calculated from data but it can be set explicitly
    solution: add database mixin for managing max_processed_height
    
     problem: on fault tables might be left in a bad state
    solution: add reset to database classes to drop all rows beyond max_processed_height

---
+++ bridge/bridge/db/MaxProcessedHeightMixin.py
@@ -0,0 +1,38 @@
+class MaxProcessedHeightMixin:
+	"""Mixin that adds a max processed height table."""
+
+	def __init__(self, connection):
+		"""Creates a max processed height mixin."""
+
+		self.connection = connection
+
+	def create_tables(self):
+		"""Creates max processed height database tables."""
+
+		cursor = self.connection.cursor()
+		cursor.execute('''CREATE TABLE IF NOT EXISTS max_processed_height (
+			height integer,
+			marker integer UNIQUE
+		)''')
+
+	def set_max_processed_height(self, height):
+		"""Sets max processed height."""
+
+		cursor = self.connection.cursor()
+		cursor.execute(
+			'''
+				INSERT INTO max_processed_height VALUES (?, ?)
+				ON CONFLICT(marker)
+				DO UPDATE SET height=?
+			''',
+			(height, 1, height))
+		self.connection.commit()
+
+	def max_processed_height(self):
+		"""Gets max processed height."""
+
+		cursor = self.connection.cursor()
+		cursor.execute('''SELECT height FROM max_processed_height LIMIT 1''')
+
+		row = cursor.fetchone()
+		return row[0] if row else 0
