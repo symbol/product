ec8ccfa2c174f81ccccffb2918fc9904ff377630
dusanjp-and-pasomi <dusanjp@gmail.com>
2025-07-20 05:10:48 +0900

    upload ShoestringOperation.py
    wizardでの setup実行時に、nodeﾃﾞｨﾚｸﾄﾘを指定して、nodeﾃﾞｨﾚｸﾄﾘに node本体を格納する様に変更。
    又、node証明書の更新時に、nodekeyを保持する様に変更。
    When running setup with the wizard, you now specify a node directory and store the node itself in the node directory.
    Also, when updating the node certificate, the nodekey is now retained.

---
+++ tools/shoestring/shoestring/wizard/ShoestringOperation.py
@@ -0,0 +1,71 @@
+from enum import Enum
+from pathlib import Path
+
+
+class ShoestringOperation(Enum):
+	"""Supported shoestring operations."""
+
+	SETUP = 1
+	UPGRADE = 2
+	RESET_DATA = 3
+	RENEW_CERTIFICATES = 4
+	RENEW_VOTING_KEYS = 5
+	IMPORT_BOOTSTRAP = 6
+
+
+def requires_ca_key_path(operation):
+	"""Determines if CA key is required for completing operation."""
+
+	return operation in (ShoestringOperation.SETUP, ShoestringOperation.RENEW_CERTIFICATES)
+
+
+def build_shoestring_command(
+	operation,
+	destination_directory,
+	shoestring_directory,
+	ca_pem_path,
+	package,
+	has_custom_rest_overrides=False
+):  # pylint: disable=too-many-arguments,too-many-positional-arguments
+	"""Builds shoestring command arguments."""
+
+	command_name = None
+	if ShoestringOperation.SETUP == operation:
+		command_name = 'setup'
+	elif ShoestringOperation.UPGRADE == operation:
+		command_name = 'upgrade'
+	elif ShoestringOperation.RESET_DATA == operation:
+		command_name = 'reset-data'
+	elif ShoestringOperation.RENEW_CERTIFICATES == operation:
+		command_name = 'renew-certificates'
+	elif ShoestringOperation.RENEW_VOTING_KEYS == operation:
+		command_name = 'renew-voting-keys'
+
+	shoestring_args = [
+		command_name,
+		'--config', str(Path(shoestring_directory) / 'shoestring.ini'),
+		'--directory', str(Path(destination_directory) / 'node')
+	]
+
+	if requires_ca_key_path(operation):
+		shoestring_args.extend(['--ca-key-path', str(ca_pem_path)])
+
+	if operation in (ShoestringOperation.SETUP, ShoestringOperation.UPGRADE):
+		shoestring_args.extend([
+			'--overrides', str(Path(shoestring_directory) / 'overrides.ini'),
+			'--package', package
+		])
+
+		if (Path(shoestring_directory) / 'rest_overrides.json').exists():
+			shoestring_args.extend([
+				'--rest-overrides', str(Path(shoestring_directory) / 'rest_overrides.json'),
+			])
+
+	if ShoestringOperation.SETUP == operation:
+		shoestring_args.extend(['--security', 'insecure'])
+
+	if ShoestringOperation.RENEW_CERTIFICATES == operation:
+		shoestring_args.extend(['--retain-node-key'])
+
+	print('wizardで発行される命令：', shoestring_args)
+	return shoestring_args
