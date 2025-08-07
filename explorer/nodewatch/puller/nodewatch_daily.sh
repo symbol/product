7395bb4e9b1b7e21a27ed08d976cd60c4196e309
AnthonyLaw <anthony@thebigidea.company>
2025-08-07 23:42:08 +0800

    [explorer/nodewatch]: Added nem geo location script

--- explorer/nodewatch/puller/nodewatch_daily.sh
+++ explorer/nodewatch/puller/nodewatch_daily.sh
@@ -4,9 +4,15 @@ echo "$0: generating files in \"$1\""
 
 python3 -m network.geolocation \
 	--input "$1/symbol_nodes.json" \
-	--output "$1/geo_location.json"
+	--output "$1/symbol_geo_location.json"
 
 echo "[$(date)] downloading geo location for Symbol nodes"
+
+python3 -m network.geolocation \
+	--input "$1/nem_nodes.json" \
+	--output "$1/nem_geo_location.json"
+
+echo "[$(date)] downloading geo location for Nem nodes"
 
 python3 -m network.nodeTracker \
 	--input "$1/symbol_nodes.json" \
