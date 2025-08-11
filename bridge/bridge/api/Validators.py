8fccc7cc46bdec9386df041f3628a223e06b7428
Jaguar0625 <jaguar0625@127.0.0.1>
2025-08-11 18:13:45 -0400

    [bridge]: validate parameters sent to filter routes
     problem: filter parameters are not validated
    solution: validate parameters and reject invalid parameters with 400

---
+++ bridge/bridge/api/Validators.py
@@ -0,0 +1,24 @@
+from symbolchain.CryptoTypes import Hash256
+
+
+def is_valid_address_string(network_facade, address_string):
+	"""Determines if the specified string is a valid address string."""
+
+	(is_valid, _) = network_facade.is_valid_address(address_string)
+	return is_valid
+
+
+def _contains_only_alphabet(alphabet, test_string):
+	return all(ch in alphabet for ch in test_string)
+
+
+def is_valid_hash_string(hash_string):
+	"""Determines if the specified string is a valid hex string."""
+
+	return 2 * Hash256.SIZE == len(hash_string) and _contains_only_alphabet('0123456789ABCDEF', hash_string)
+
+
+def is_valid_decimal_string(decimal_string):
+	"""Determines if the specified string is a valid decimal string."""
+
+	return _contains_only_alphabet('0123456789', decimal_string)
