493cb9436d522203c6200712b6c68122ac8d942c
OlegMakarenko <olegomm@gmail.com>
2025-08-19 17:19:57 +0200

    [wallet/common-symbol] feat: add api test helper

--- wallet/common/symbol/tests/test-utils.js
+++ wallet/common/symbol/tests/test-utils.js
@@ -50,3 +50,46 @@ export const generateBitCombinations = (baseValue, bits) => {
 
 	return Array.from(results);
 };
+
+/**
+ * Runs a test for a function that makes API calls.
+ * @param {jest.Mock} mockFetch - The mocked fetch function.
+ * @param {function} functionToTest - The function to test.
+ * @param {Array<{url: string, options: object, response: any}>}expectedCalls - The expected API calls.
+ * @param {object} [expectedResult] - Optional checks if the result matches the expected calls; if an object, compares the result with it.
+ */
+export const runApiTest = async (mockFetch, functionToTest, expectedCalls, expectedResult) => {
+	// Arrange:
+	mockFetch.mockImplementation(async (url, options) => {
+		const matchingCall = expectedCalls.find(call => {
+			if (call.url !== url) 
+				return false;
+
+			// If options are not specified in the expected call, ensure no options were passed
+			if (!call.options)
+				return !options;
+
+			// Otherwise, compare options
+			return JSON.stringify(call.options) === JSON.stringify(options);
+		});
+		
+		if (matchingCall)
+			return Promise.resolve(matchingCall.response);
+		
+		return Promise.reject(new Error(`[Test error] Unexpected API call. URL: ${url}, Options: ${JSON.stringify(options)}`));
+	});
+
+	// Act:
+	const result = await functionToTest();
+
+	// Assert:
+	expectedCalls.forEach(call => {
+		if (call.options)
+			expect(mockFetch).toHaveBeenCalledWith(call.url, call.options);
+		else
+			expect(mockFetch).toHaveBeenCalledWith(call.url);
+	});
+
+	if (expectedResult) 
+		expect(result).toStrictEqual(expectedResult);
+};
