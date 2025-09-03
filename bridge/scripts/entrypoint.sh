7984a326627936ddd3eb5811733396a2a5cd2947
Wayon <wayon@symbol.dev>
2025-09-03 14:52:56 -0400

    [bridge] fix: linting

--- bridge/scripts/entrypoint.sh
+++ bridge/scripts/entrypoint.sh
@@ -3,10 +3,10 @@
 set -ex
 
 if [ "$1" = "api" ]; then
-	gunicorn --workers ${NUMBER_OF_WORKERS} --bind 0.0.0.0:5000 "bridge.api:create_app()"
+	gunicorn --workers "${NUMBER_OF_WORKERS}" --bind 0.0.0.0:5000 "bridge.api:create_app()"
 elif [ -f "$1" ]; then
 	echo "executing script file '$1' with interval ${INTERVAL_TIME_IN_SECONDS} seconds"
-	while true; do ${1}; sleep ${INTERVAL_TIME_IN_SECONDS}; done
+	while true; do ${1}; sleep "${INTERVAL_TIME_IN_SECONDS}"; done
 else
 	echo "[$0] unknown argument '$1'"
 fi
