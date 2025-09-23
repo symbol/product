47d3b50d838410ec152f24f5387ae72e30ac6bfe
AnthonyLaw <anthony@thebigidea.company>
2025-09-24 03:33:09 +0800

    [explorer/puller] feat: init database connection and block table

---
+++ explorer/puller/puller/db/DatabaseConnection.py
@@ -0,0 +1,32 @@
+import psycopg2
+
+from zenlog import log
+
+class DatabaseConnection:
+	def __init__(self, db_config, timeout_seconds=10):
+		self.db_config = db_config
+		self.connection = None
+		self.timeout_seconds = timeout_seconds
+
+	def __enter__(self):
+		"""Connects to databases."""
+
+		try:
+			self.connection = psycopg2.connect(
+				database=self.db_config['database'],
+				user=self.db_config['user'],
+				password=self.db_config['password'],
+				host=self.db_config['host'],
+				port=self.db_config['port']
+			)
+
+			return self
+		except Exception as e:
+			log.error(f"Failed to connect to database: {e}")
+			raise e
+
+	def __exit__(self, exc_type, exc_value, traceback):
+		"""Disconnects from databases."""
+
+		if self.connection is not None:
+			self.connection.close()
