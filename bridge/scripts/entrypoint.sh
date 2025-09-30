a6892a74a426041089b874d5b1d7fb29dcc66ad3
Wayon <wayon@symbol.dev>
2025-09-29 23:22:35 -0400

    [bridge]: add workflow scripts for wrapped and native

--- bridge/scripts/entrypoint.sh
+++ bridge/scripts/entrypoint.sh
@@ -2,11 +2,35 @@
 
 set -ex
 
-if [ "$1" = "api" ]; then
-	gunicorn --workers "${NUMBER_OF_WORKERS}" --bind 0.0.0.0:5000 "bridge.api:create_app()"
-elif [ -f "$1" ]; then
-	echo "executing script file '$1' with interval ${INTERVAL_TIME_IN_SECONDS} seconds"
-	while true; do ${1}; sleep "${INTERVAL_TIME_IN_SECONDS}"; done
-else
-	echo "[$0] unknown argument '$1'"
-fi
+# Function to run the workflow
+run_workflow() {
+  local workflow_to_run="$*"
+  while true; do
+    echo "Running command: ${workflow_to_run} at $(date)"
+    eval "${workflow_to_run}"
+    sleep "${INTERVAL_TIME_IN_SECONDS}"
+  done
+}
+
+case "$1" in
+
+  "nativeflow")
+    echo -n "Running native flow"
+    run_workflow /app/bridge/scripts/nativeflow.sh "${CONFIG_PATH}"
+    ;;
+
+   "wrappedflow")
+    echo -n "Running wrapped flow"
+    run_workflow /app/bridge/scripts/wrappedflow.sh "${CONFIG_PATH}"
+    ;;
+
+  "api")
+    echo -n "Running API"
+    gunicorn --workers "${NUMBER_OF_WORKERS}" --bind 0.0.0.0:5000 "bridge.api:create_app()"
+    ;;
+
+  *)
+    echo "[$0] unknown argument '$1'"
+    exit 1
+    ;;
+esac
