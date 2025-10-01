3618d13a34258b413468ea384b176a9b9eca735b
Wayonb <wayon@symbol.dev>
2025-09-30 22:19:20 -0400

    [bridge]: add Dockerfile for running the Bridge
    problem: no Dockerfile to build a Docker image
    solution: add a Dockerfile to build an image for the API and workflow

---
+++ bridge/scripts/entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -ex
+
+# Function to run the workflow
+run_workflow() {
+	local workflow_to_run="$*"
+	while true; do
+		echo "Running command: ${workflow_to_run} at $(date)"
+		eval "${workflow_to_run}"
+		sleep "${INTERVAL_TIME_IN_SECONDS}"
+	done
+}
+
+case "$1" in
+	"nativeflow")
+		echo -n "Running native flow"
+		run_workflow /app/bridge/scripts/nativeflow.sh "${BRIDGE_CONFIG_PATH}"
+		;;
+
+	 "wrappedflow")
+		echo -n "Running wrapped flow"
+		run_workflow /app/bridge/scripts/wrappedflow.sh "${BRIDGE_CONFIG_PATH}"
+		;;
+
+	"api")
+		echo -n "Running API"
+		export BRIDGE_API_SETTINGS="/tmp/api_configuration.ini"
+		echo "CONFIG_PATH=\"${BRIDGE_CONFIG_PATH}\"" > "${BRIDGE_API_SETTINGS}"
+		gunicorn --workers "${NUMBER_OF_WORKERS}" --bind 0.0.0.0:5000 "bridge.api:create_app()"
+		;;
+
+	*)
+		echo "[$0] unknown argument '$1'"
+		exit 1
+		;;
+esac
