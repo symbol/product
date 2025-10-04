c9bcbf257b73c53b96243af21f088840b45325ef
Anthony Law <anthony@symbol.dev>
2025-10-04 19:39:25 +0800

    [explorer/puller] feat: init database connection and blocks table
    Problem: working on nem puller, missing database connection.
    Solution: Added db connection and nem database (blocks tables)

---
+++ explorer/puller/puller/db/NemDatabase.py
@@ -0,0 +1,75 @@
+from binascii import unhexlify
+
+from .DatabaseConnection import DatabaseConnection
+
+
+class NemDatabase(DatabaseConnection):
+	"""Database containing Nem blockchain data."""
+
+	@staticmethod
+	def _create_table_indexes(cursor):
+		"""Creates table indexes."""
+
+		cursor.execute(
+			'''
+			CREATE INDEX IF NOT EXISTS idx_blocks_height_desc ON blocks(height DESC);
+			'''
+		)
+
+	def create_tables(self):
+		"""Creates blocks database tables."""
+
+		cursor = self.connection.cursor()
+		cursor.execute(
+			'''
+			CREATE TABLE IF NOT EXISTS blocks (
+				id serial PRIMARY KEY,
+				height bigint NOT NULL UNIQUE,
+				timestamp timestamptz NOT NULL,
+				total_fees bigint DEFAULT 0,
+				total_transactions int DEFAULT 0,
+				difficulty bigint NOT NULL,
+				hash bytea NOT NULL,
+				signer bytea NOT NULL,
+				signature bytea NOT NULL,
+				size bigint DEFAULT 0
+			)
+			'''
+		)
+
+		self._create_table_indexes(cursor)
+
+		self.connection.commit()
+
+	def insert_block(self, cursor, block):  # pylint: disable=no-self-use
+		"""Adds block height into table."""
+
+		cursor.execute(
+			'''
+			INSERT INTO blocks (height, timestamp, total_fees, total_transactions, difficulty, hash, signer, signature, size)
+			VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+			''', (
+				block.height,
+				block.timestamp,
+				block.total_fees,
+				block.total_transactions,
+				block.difficulty,
+				unhexlify(block.block_hash),
+				unhexlify(block.signer),
+				unhexlify(block.signature),
+				block.size
+			)
+		)
+
+	def get_current_height(self):
+		"""Gets current height from database"""
+
+		cursor = self.connection.cursor()
+		cursor.execute(
+			'''
+			SELECT MAX(height)
+			FROM blocks
+			'''
+		)
+		results = cursor.fetchone()
+		return 0 if results[0] is None else results[0]
