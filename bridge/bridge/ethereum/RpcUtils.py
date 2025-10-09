b79bb541b8820188a9c31d4f0be9235c756e2c41
Jaguar0625 <jaguar0625@127.0.0.1>
2025-10-09 09:08:31 -0400

    [bridge]: update RpcUtils to not assume leading 0x hex prefix
     problem: RpcUtils assumes hex prefix
    solution: handle hex values both with and without hex prefix

--- bridge/bridge/ethereum/RpcUtils.py
+++ bridge/bridge/ethereum/RpcUtils.py
@@ -12,10 +12,14 @@ def make_rpc_request_json(method, params, identifier=1):
 	}
 
 
+def _strip_hex_prefix(hex_value):
+	return hex_value[2:] if any(hex_value.startswith(prefix) for prefix in ('0x', '0X')) else hex_value
+
+
 def parse_rpc_response_hex_value(hex_value):
 	"""Parses a hex value representing an integer in an Ethereum RPC response."""
 
-	hex_value = hex_value[2:]
+	hex_value = _strip_hex_prefix(hex_value)
 	if 1 == len(hex_value) % 2:
 		hex_value = f'0{hex_value}'
 
@@ -26,4 +30,4 @@ def parse_rpc_response_hex_value(hex_value):
 def parse_rpc_response_hex_bytes(hex_value):
 	"""Parses a hex value representing a byte array in an Ethereum RPC response."""
 
-	return unhexlify(hex_value[2:])
+	return unhexlify(_strip_hex_prefix(hex_value))
