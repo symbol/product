622b732a1146e4c529d49d828c94b6d1d00ee1b6
Jaguar0625 <jaguar0625@127.0.0.1>
2025-10-17 16:06:00 -0400

    [bridge]: add file lock to prevent multiple concurrent workflow execution
     problem: single workflow is not expected to run concurrently
    solution: use lockfile to throttle workflow execution

--- bridge/workflows/main_impl.py
+++ bridge/workflows/main_impl.py
@@ -2,7 +2,10 @@ import argparse
 import logging
 import sys
 from logging.handlers import RotatingFileHandler
+from pathlib import Path
 from traceback import format_exception
+
+from filelock import FileLock, Timeout
 
 from bridge.db.Databases import Databases
 from bridge.models.BridgeConfiguration import parse_bridge_configuration
@@ -30,10 +33,7 @@ def unhandled_exception_handler(exc_type, exc_value, exc_traceback):
 	sys.exit(1)
 
 
-async def main_bootstrapper(program_description, main_impl):
-	args = parse_args(program_description)
-	config = parse_bridge_configuration(args.config)
-
+def configure_logging(config):
 	logging.basicConfig(
 		level=logging.DEBUG,
 		format='%(asctime)s [%(levelname)s] %(module)s: %(message)s',
@@ -44,6 +44,17 @@ async def main_bootstrapper(program_description, main_impl):
 				maxBytes=config.machine.max_log_size),
 			logging.StreamHandler(sys.stdout)
 		])
+
+	# only log warnings or higher from filelock
+	logger = logging.getLogger('filelock')
+	logger.setLevel(logging.WARN)
+
+
+async def main_bootstrapper(program_description, main_impl):
+	args = parse_args(program_description)
+	config = parse_bridge_configuration(args.config)
+
+	configure_logging(config)
 	logger = logging.getLogger(__name__)
 
 	sys.excepthook = unhandled_exception_handler
@@ -55,10 +66,15 @@ async def main_bootstrapper(program_description, main_impl):
 
 	price_oracle = load_price_oracle(config.price_oracle)
 
-	with Databases(config.machine.database_directory, native_facade, wrapped_facade) as databases:
-		databases.create_tables()
+	lock_filepath = Path(config.machine.database_directory) / 'bridge.db.lock'
+	try:
+		with FileLock(lock_filepath, blocking=False):
+			with Databases(config.machine.database_directory, native_facade, wrapped_facade) as databases:
+				databases.create_tables()
 
-		execution_context = ExecutionContext(args.unwrap, config.global_.mode)
-		logger.info('         strategy mode: %s', execution_context.strategy_mode)
-		logger.info('running in unwrap mode? %s', execution_context.is_unwrap_mode)
-		await main_impl(execution_context, databases, native_facade, wrapped_facade, price_oracle)
+				execution_context = ExecutionContext(args.unwrap, config.global_.mode)
+				logger.info('         strategy mode: %s', execution_context.strategy_mode)
+				logger.info('running in unwrap mode? %s', execution_context.is_unwrap_mode)
+				await main_impl(execution_context, databases, native_facade, wrapped_facade, price_oracle)
+	except Timeout:
+		logger.fatal('could not acquire exclusive lock to %s', lock_filepath)
