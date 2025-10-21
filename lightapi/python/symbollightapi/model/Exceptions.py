5271c9d8dd77950acf3641119eb82ca9d6b6d7a2
Jaguar0625 <jaguar0625@127.0.0.1>
2025-10-21 15:17:58 -0400

    [lightapi/python]: add HttpException subclass with http_status_code
     problem: upstream code can't differentiate transient and permanent HTTP errors
    solution: attach the HTTP status code to a new NodeException subclass

--- lightapi/python/symbollightapi/model/Exceptions.py
+++ lightapi/python/symbollightapi/model/Exceptions.py
@@ -2,6 +2,16 @@ class NodeException(Exception):
 	"""Exception raised when there is an error communicating with a node."""
 
 
+class HttpException(NodeException):
+	"""Exception raised when there is an HTTP error communicating with a node."""
+
+	def __init__(self, message, http_status_code):
+		"""Creates an HTTP exception."""
+
+		super().__init__(message)
+		self.http_status_code = http_status_code
+
+
 class CorruptDataException(NodeException):
 	"""Exception raised when corrupt data is received from a node."""
 
