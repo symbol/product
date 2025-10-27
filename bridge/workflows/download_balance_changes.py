794efa5d3fe049ce868daf240e9cf3202661bdae
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2025-10-27 17:47:42 +0100

    Early exit and log on non coherent ranges

--- bridge/workflows/download_balance_changes.py
+++ bridge/workflows/download_balance_changes.py
@@ -46,25 +46,28 @@ class BalanceChangesDownloader:
 
 
 async def download_balance_changes(database, network):
-	logger = logging.getLogger(__name__)
+    logger = logging.getLogger(__name__)
 
-	connector = network.create_connector()
-	config_extensions = network.config.extensions
-	(start_height, end_height) = await calculate_search_range(connector, database, config_extensions, 'balance_change_scan_start_height')
+    connector = network.create_connector()
+    config_extensions = network.config.extensions
+    (start_height, end_height) = await calculate_search_range(connector, database, config_extensions, 'balance_change_scan_start_height')
 
-	logger.info('searching blockchain for balance changes to %s in range [%s, %s)...', network.bridge_address, start_height, end_height)
+    logger.info('searching for balance changes to %s in blocks range [%s, %s)...', network.bridge_address, start_height, end_height)
+    database.reset()
 
-	database.reset()
+    if start_height >= end_height:
+        logger.info("no new blocks to process, maybe node resyncing?. Exiting.")
+        return
 
-	downloader = BalanceChangesDownloader(database, network)
+    downloader = BalanceChangesDownloader(database, network)
 
-	tasks = [downloader.download(height) for height in range(start_height, end_height)]
-	counts = await asyncio.gather(*tasks)
+    tasks = [downloader.download(height) for height in range(start_height, end_height)]
+    counts = await asyncio.gather(*tasks)
 
-	# add marker in database to indicate last height processed
-	database.set_max_processed_height(end_height - 1)
+    # add marker in database to indicate last height processed
+    database.set_max_processed_height(end_height - 1)
 
-	print_banner([
+    print_banner([
 		'\n',
 		f'==> last processed transaction height: {database.max_processed_height()}',
 		f'==>   total balance changes processed: {sum(counts)}'
