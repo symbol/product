5d0a079d43ccd643ce8f59aa2349f621962eaa8f
AnthonyLaw <anthony@thebigidea.company>
2025-12-27 03:55:18 +0800

    [explorer/puller] feat: added accounts table

--- explorer/puller/puller/db/NemDatabase.py
+++ explorer/puller/puller/db/NemDatabase.py
@@ -1,3 +1,4 @@
+import json
 from binascii import unhexlify
 
 from .DatabaseConnection import DatabaseConnection
@@ -10,6 +11,43 @@ class NemDatabase(DatabaseConnection):
 	def _create_table_indexes(cursor):
 		"""Creates table indexes."""
 
+		# Create indexes for accounts table
+		cursor.execute(
+			'''
+			CREATE INDEX IF NOT EXISTS idx_accounts_address
+				ON accounts (address)
+			'''
+		)
+
+		cursor.execute(
+			'''
+			CREATE INDEX IF NOT EXISTS idx_accounts_balance
+				ON accounts(balance DESC);
+			'''
+		)
+
+		cursor.execute(
+			'''
+			CREATE INDEX IF NOT EXISTS idx_accounts_importance
+				ON accounts(importance DESC);
+			'''
+		)
+
+		cursor.execute(
+			'''
+			CREATE INDEX IF NOT EXISTS idx_accounts_public_key
+				ON accounts (public_key)
+			'''
+		)
+
+		cursor.execute(
+			'''
+			CREATE INDEX IF NOT EXISTS idx_accounts_mosaics_gin
+				ON accounts USING GIN (mosaics)
+			'''
+		)
+
+		# Create indexes for blocks table
 		cursor.execute(
 			'''
 			CREATE INDEX IF NOT EXISTS idx_blocks_height_desc ON blocks(height DESC);
@@ -17,9 +55,36 @@ class NemDatabase(DatabaseConnection):
 		)
 
 	def create_tables(self):
-		"""Creates blocks database tables."""
+		"""Creates database tables."""
 
 		cursor = self.connection.cursor()
+
+		# Create accounts table
+		cursor.execute(
+			'''
+			CREATE TABLE IF NOT EXISTS accounts (
+				id serial PRIMARY KEY,
+				address bytea NOT NULL UNIQUE,
+				public_key bytea,
+				remote_address bytea,
+				importance decimal(20, 10) DEFAULT 0,
+				balance bigint DEFAULT 0,
+				vested_balance bigint DEFAULT 0,
+				mosaics jsonb DEFAULT '[]'::jsonb,
+				harvested_fees bigint DEFAULT 0,
+				harvested_blocks bigint DEFAULT 0,
+				status varchar(8) DEFAULT NULL,
+				remote_status varchar(12) DEFAULT NULL,
+				last_harvested_height bigint DEFAULT 0,
+				min_cosignatories int DEFAULT NULL,
+				cosignatory_of bytea[] DEFAULT NULL,
+				cosignatories bytea[] DEFAULT NULL,
+				updated_at timestamp DEFAULT CURRENT_TIMESTAMP
+			)
+			'''
+		)
+
+		# Create blocks table
 		cursor.execute(
 			'''
 			CREATE TABLE IF NOT EXISTS blocks (
@@ -75,3 +140,55 @@ class NemDatabase(DatabaseConnection):
 		)
 		results = cursor.fetchone()
 		return 0 if results[0] is None else results[0]
+
+	def upsert_account(self, cursor, account_info):  # pylint: disable=no-self-use
+		"""Insert or update account information."""
+
+		cursor.execute(
+			'''
+			INSERT INTO accounts (
+				address,
+				public_key,
+				remote_address,
+				importance,
+				balance,
+				vested_balance,
+				mosaics,
+				harvested_blocks,
+				status,
+				remote_status,
+				min_cosignatories,
+				cosignatory_of,
+				cosignatories
+			)
+			VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+			ON CONFLICT (address)
+			DO UPDATE SET
+				remote_address = EXCLUDED.remote_address,
+				importance = EXCLUDED.importance,
+				balance = EXCLUDED.balance,
+				vested_balance = EXCLUDED.vested_balance,
+				mosaics = EXCLUDED.mosaics,
+				harvested_blocks = EXCLUDED.harvested_blocks,
+				status = EXCLUDED.status,
+				remote_status = EXCLUDED.remote_status,
+				min_cosignatories = EXCLUDED.min_cosignatories,
+				cosignatory_of = EXCLUDED.cosignatory_of,
+				cosignatories = EXCLUDED.cosignatories,
+				updated_at = CURRENT_TIMESTAMP
+			''', (
+				account_info.address.bytes,
+				account_info.public_key.bytes if account_info.public_key else None,
+				account_info.remote_address.bytes if account_info.remote_address else None,
+				account_info.importance,
+				account_info.balance,
+				account_info.vested_balance,
+				json.dumps(account_info.mosaics),
+				account_info.harvested_blocks,
+				account_info.status,
+				account_info.remote_status,
+				account_info.min_cosignatories,
+				[address.bytes for address in account_info.cosignatory_of] if len(account_info.cosignatory_of) > 0 else None,
+				[address.bytes for address in account_info.cosignatories] if len(account_info.cosignatories) > 0 else None,
+			)
+		)
