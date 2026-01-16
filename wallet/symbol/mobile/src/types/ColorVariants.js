a7ea3df41aa54130b5853d838a454550850ef5a5
OlegMakarenko <olegomm@gmail.com>
2026-01-16 19:47:44 +0100

    [wallet/symbol/mobile] feat: add color variants types

---
+++ wallet/symbol/mobile/src/types/ColorVariants.js
@@ -0,0 +1,11 @@
+/** @typedef {'danger'|'warning'|'success'|'neutral'|'info'} SemanticRoleColorVariants */
+
+/** @typedef {'primary'} Generic1ColorVariants */
+
+/** @typedef {'primary'|'secondary'} Generic2ColorVariants */
+
+/** @typedef {'primary'|'secondary'|'tertiary'} Generic3ColorVariants */
+
+/** @typedef {'default'|'inverse'} InverseColorVariants */
+
+export {};
