b32a1201970badadf6ef97a1bd511be652d9043c
OlegMakarenko <olegomm@gmail.com>
2026-02-09 18:26:20 +0100

    [wallet/symbol/mobile] feat: add test mocks

--- wallet/symbol/mobile/__mocks__/react-native-safe-area-context.js
+++ wallet/symbol/mobile/__mocks__/react-native-safe-area-context.js
@@ -1,2 +1,26 @@
-export const SafeAreaProvider = ({ children }) => children;
+import React from 'react';
+
+const insets = { top: 0, right: 0, bottom: 0, left: 0 };
+const frame = { x: 0, y: 0, width: 320, height: 640 };
+
+export const SafeAreaInsetsContext = React.createContext(insets);
+export const SafeAreaFrameContext = React.createContext(frame);
+
+export const SafeAreaProvider = ({ children }) => (
+	<SafeAreaFrameContext.Provider value={frame}>
+		<SafeAreaInsetsContext.Provider value={insets}>
+			{children}
+		</SafeAreaInsetsContext.Provider>
+	</SafeAreaFrameContext.Provider>
+);
+
 export const SafeAreaView = ({ children }) => children;
+
+export const useSafeAreaInsets = () => insets;
+
+export const useSafeAreaFrame = () => frame;
+
+export const initialWindowMetrics = {
+	frame,
+	insets
+};
