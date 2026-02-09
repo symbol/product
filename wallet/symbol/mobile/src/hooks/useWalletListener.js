aa827c6df9246e6d1f962211a693b3738bc0d4d8
OlegMakarenko <olegomm@gmail.com>
2026-02-09 18:09:38 +0100

    [wallet/symbol/mobile] task: refactor Router

---
+++ wallet/symbol/mobile/src/hooks/useWalletListener.js
@@ -0,0 +1,57 @@
+import { useEffect } from 'react';
+import { constants } from 'wallet-common-core';
+const { ControllerEventName } = constants;
+
+/** @typedef {import('wallet-common-core').WalletController} WalletController */
+
+/**
+ * Hook that monitors wallet lifecycle and account change events across multiple wallet controllers.
+ * This enables components to respond to wallet creation, clearing, and account switches by
+ * providing callback functions that are triggered when these events occur.
+ *
+ * @param {Object} options - Hook options.
+ * @param {WalletController[]} options.walletControllers - Array of wallet controller instances to listen to.
+ * @param {Function} options.[onWalletCreate] - Callback invoked when a wallet is created.
+ *   Receives the newly created wallet object as its first parameter.
+ * @param {Function} options.[onWalletClear] - Callback invoked when a wallet is cleared.
+ *   Receives the cleared wallet identifier as its first parameter.
+ * @param {Function} options.[onAccountChange] - Callback invoked when the active account changes.
+ *   Receives the new account details as its first parameter.
+ * @param {Array} [options.deps=[]] - Dependency array for the useEffect hook to control when listeners are re-registered.
+ * @returns {void}
+ */
+export const useWalletListener = ({ 
+	walletControllers, 
+	onWalletCreate,
+	onWalletClear, 
+	onAccountChange,
+	deps = []
+}) => {
+	useEffect(() => {
+		if (onWalletCreate)
+		{walletControllers.forEach(walletController =>
+			walletController.on(ControllerEventName.WALLET_CREATE, onWalletCreate));}
+
+		if (onWalletClear)
+		{walletControllers.forEach(walletController =>
+			walletController.on(ControllerEventName.WALLET_CLEAR, onWalletClear));}
+
+		if (onAccountChange)
+		{walletControllers.forEach(walletController =>
+			walletController.on(ControllerEventName.ACCOUNT_CHANGE, onAccountChange));}
+
+		return () => {
+			if (onWalletCreate)
+			{walletControllers.forEach(walletController =>
+				walletController.removeListener(ControllerEventName.WALLET_CREATE, onWalletCreate));}
+			
+			if (onWalletClear)
+			{walletControllers.forEach(walletController =>
+				walletController.removeListener(ControllerEventName.WALLET_CLEAR, onWalletClear));}
+
+			if (onAccountChange)
+			{walletControllers.forEach(walletController =>
+				walletController.removeListener(ControllerEventName.ACCOUNT_CHANGE, onAccountChange));}
+		};
+	}, deps);
+};
