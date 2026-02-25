8a4579703955f16b01e24b6166447064c1701f3f
Anthony Law <anthony@symbol.dev>
2026-02-25 19:09:44 +0800

    [explorer/rest] feat: Added beneficiary properties in block query
    Problem: missing beneficiary field in query.
    Solution: added beneficiary and renamed total_fee

--- explorer/rest/tests/test/DatabaseTestUtils.py
+++ explorer/rest/tests/test/DatabaseTestUtils.py
@@ -16,10 +16,11 @@ Block = namedtuple(
 	[
 		'height',
 		'timestamp',
-		'total_fees',
+		'total_fee',
 		'total_transactions',
 		'difficulty',
 		'block_hash',
+		'beneficiary',
 		'signer',
 		'signature',
 		'size'
@@ -52,7 +53,8 @@ BLOCKS = [
 		5,
 		100000000000000,
 		'438CF6375DAB5A0D32F9B7BF151D4539E00A590F7C022D5572C7D41815A24BE4',
-		'8D07F90FB4BBE7715FA327C926770166A11BE2E494A970605F2E12557F66C9B9',
+		Address('NANEMOABLAGR72AZ2RV3V4ZHDCXW25XQ73O7OBT5'),
+		PublicKey('8D07F90FB4BBE7715FA327C926770166A11BE2E494A970605F2E12557F66C9B9'),
 		'2ABDD19AD3EFAB0413B42772A586FAA19DEDB16D35F665F90D598046A2132C4A'
 		'D1E71001545CEAA44E63C04345591E7AADBFD330AF82A0D8A1DA5643E791FF0F',
 		936),
@@ -63,7 +65,8 @@ BLOCKS = [
 		3,
 		80000000000000,
 		'1DD9D4D7B6AF603D29C082F9AA4E123F07D18154DDBCD7DDC6702491B854C5E4',
-		'F9BD190DD0C364261F5C8A74870CC7F7374E631352293C62ECC437657E5DE2CD',
+		Address('NALICEPFLZQRZGPRIJTMJOCPWDNECXTNNG7QLSG3'),
+		PublicKey('F9BD190DD0C364261F5C8A74870CC7F7374E631352293C62ECC437657E5DE2CD'),
 		'1B81379847241E45DA86B27911E5C9A9192EC04F644D98019657D32838B49C14'
 		'3EAA4815A3028B80F9AFFDBF0B94CD620F7A925E02783DDA67B8627B69DDF70E',
 		752),
@@ -101,8 +104,16 @@ ACCOUNTS = [
 ]
 
 BLOCK_VIEWS = [
-	BlockView(*BLOCKS[0]._replace(total_fees=102.0, signer=Address('NANEMOABLAGR72AZ2RV3V4ZHDCXW25XQ73O7OBT5'))),
-	BlockView(*BLOCKS[1]._replace(total_fees=201.0, signer=Address('NALICEPFLZQRZGPRIJTMJOCPWDNECXTNNG7QLSG3')))
+	BlockView(*BLOCKS[0]._replace(
+		total_fee=102.0,
+		beneficiary='NANEMOABLAGR72AZ2RV3V4ZHDCXW25XQ73O7OBT5',
+		signer='NANEMOABLAGR72AZ2RV3V4ZHDCXW25XQ73O7OBT5')
+	),
+	BlockView(*BLOCKS[1]._replace(
+		total_fee=201.0,
+		beneficiary='NALICEPFLZQRZGPRIJTMJOCPWDNECXTNNG7QLSG3',
+		signer='NALICEPFLZQRZGPRIJTMJOCPWDNECXTNNG7QLSG3')
+	)
 ]
 
 ACCOUNT_VIEWS = [
@@ -187,10 +198,11 @@ def initialize_database(db_config, network_name):
 				id serial NOT NULL PRIMARY KEY,
 				height bigint NOT NULL,
 				timestamp timestamp NOT NULL,
-				total_fees bigint DEFAULT 0,
+				total_fee bigint DEFAULT 0,
 				total_transactions int DEFAULT 0,
 				difficulty bigInt NOT NULL,
 				hash bytea NOT NULL,
+				beneficiary bytea NOT NULL,
 				signer bytea NOT NULL,
 				signature bytea NOT NULL,
 				size bigint DEFAULT 0
@@ -201,16 +213,17 @@ def initialize_database(db_config, network_name):
 		for block in BLOCKS:
 			cursor.execute(
 				'''
-				INSERT INTO blocks (height, timestamp, total_fees, total_transactions, difficulty, hash, signer, signature, size)
-				VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+				INSERT INTO blocks (height, timestamp, total_fee, total_transactions, difficulty, hash, beneficiary, signer, signature, size)
+				VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
 				''', (
 					block.height,
 					block.timestamp,
-					block.total_fees,
+					block.total_fee,
 					block.total_transactions,
 					block.difficulty,
 					unhexlify(block.block_hash),
-					unhexlify(block.signer),
+					block.beneficiary.bytes,
+					block.signer.bytes,
 					unhexlify(block.signature),
 					block.size
 				)
