8c560e0a0d159a5051d32d73031195ad67c355e0
OlegMakarenko <olegomm@gmail.com>
2026-03-30 14:25:54 +0200

    [wallet/symbol/mobile] fix: failing tests

--- wallet/symbol/mobile/__tests__/screens/home/Home.test.js
+++ wallet/symbol/mobile/__tests__/screens/home/Home.test.js
@@ -1,10 +1,27 @@
 import { Home } from '@/app/screens/home/Home';
 import { runRenderComponentTest } from '__tests__/component-tests';
-import { mockLocalization } from '__tests__/mock-helpers';
+import { mockLocalization, mockWalletController } from '__tests__/mock-helpers';
 
 describe('screens/onboarding/Home', () => {
 	beforeEach(() => {
 		mockLocalization();
+		mockWalletController({
+			modules: {
+				multisig: {
+					multisigAccounts: [],
+					fetchData: jest.fn().mockResolvedValue([])
+				},
+				addressBook: {
+					whiteList: [],
+					contacts: [],
+					blackList: [],
+					getContactByAddress: jest.fn().mockReturnValue(null)
+				},
+				market: {
+					price: null
+				}
+			}
+		});
 	});
 
 	runRenderComponentTest(Home);
