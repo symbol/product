36e08d5ae0b3cb2e13ea388e35ea8a84910878e5
OlegMakarenko <olegomm@gmail.com>
2026-04-10 14:01:17 +0200

    [wallet/symbol/mobile] feat: add inverse color property to copy button

--- wallet/symbol/mobile/src/components/display/RowWithButton/CopyButtonContainer.jsx
+++ wallet/symbol/mobile/src/components/display/RowWithButton/CopyButtonContainer.jsx
@@ -8,13 +8,14 @@ import React from 'react';
  * @param {React.ReactNode} props.children - Child component to display
  * @param {string} props.value - Value to be copied
  * @param {boolean} [props.isStretched=false] - If true, the component will stretch to fill the available width
+ * @param {boolean} [props.inverse=false] - If true, uses inverse color scheme for the copy button
  * 
  * @returns {React.ReactNode} Copy view component
  */
-export const CopyButtonContainer = ({ children, value, isStretched }) => (
+export const CopyButtonContainer = ({ children, value, isStretched, inverse = false }) => (
 	<ActionRow 
 		isStretched={isStretched} 
-		button={<CopyButton content={value} size="s" />}
+		button={<CopyButton content={value} size="s" inverse={inverse} />}
 	>
 		{children}
 	</ActionRow>
