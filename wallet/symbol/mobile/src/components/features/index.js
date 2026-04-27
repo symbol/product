c42639334fa1346849206d4e81002e0a922bab20
OlegMakarenko <olegomm@gmail.com>
2026-04-27 19:38:12 +0200

    [wallet/symbol/mobile] feat: add SendReceiveButton component

--- wallet/symbol/mobile/src/components/features/index.js
+++ wallet/symbol/mobile/src/components/features/index.js
@@ -2,4 +2,5 @@ export * from './CopyButton';
 export * from './Filter';
 export * from './QrCodeView';
 export * from './Passcode';
+export * from './SendReceiveButtons';
 export * from './TableView';
