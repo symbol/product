6c5b1b79e05ab1f633f23b3483a92749baf59899
OlegMakarenko <olegomm@gmail.com>
2026-06-18 14:48:22 +0200

    [explorer/frontend] feat: add Symbol DocumentHead, page config and variant re-export

---
+++ explorer/frontend/variants/symbol/config/index.js
@@ -0,0 +1,4 @@
+// Symbol does not add variant-specific runtime config yet.
+const config = {};
+
+export default config;
---
+++ explorer/frontend/variants/symbol/index.js
@@ -0,0 +1,6 @@
+export * as api from './api';
+export { DocumentHead } from './DocumentHead';
+export { default as styleVariables } from './styles/variables.json';
+export { default as pageConfig } from './config/pages';
+export { default as components } from './components';
+export { default as config } from './config';
