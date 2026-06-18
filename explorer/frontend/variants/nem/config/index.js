298ba20c3a702acc33d57566bea46c60ba7af6e5
OlegMakarenko <olegomm@gmail.com>
2026-06-18 14:38:00 +0200

    [explorer/frontend] feat: add NEM variant runtime config

---
+++ explorer/frontend/variants/nem/config/index.js
@@ -0,0 +1,8 @@
+const config = {
+	SUPERNODE_API_URL: process.env.SUPERNODE_API_URL,
+	NODELIST_URL: process.env.NODELIST_URL,
+	MARKET_DATA_URL: process.env.MARKET_DATA_URL,
+	HISTORICAL_PRICE_URL: process.env.HISTORICAL_PRICE_URL
+};
+
+export default config;
---
+++ explorer/frontend/variants/nem/config/pages/index.js
@@ -0,0 +1,7 @@
+import home from './home.json';
+
+const pages = {
+	home
+};
+
+export default pages;
