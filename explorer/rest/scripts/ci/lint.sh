ebf3009bd623b82eb16b711a46733bffbbab3f3b
daoka <daoka@symbolsyndicate.us>
2026-06-24 19:58:42 +0900

    [explorer/backend] ci: make REST lint resolve common Symbol imports

--- explorer/rest/scripts/ci/lint.sh
+++ explorer/rest/scripts/ci/lint.sh
@@ -1,1 +1,19 @@
-../../../../linters/python/lint.sh
+#!/bin/bash
+
+set -ex
+
+if [ $# -gt 0 ]; then
+	PYLINT_DISABLE_COMMANDS="$1"
+fi
+
+find . ../common -type f -name "*.sh" -print0 | xargs -0 shellcheck
+find . ../common -type f -name "*.py" -print0 | PYTHONPATH=.:.. xargs -0 python3 -m isort \
+	--line-length 140 \
+	--indent "	" \
+	--multi-line 3 \
+	--check-only
+find . ../common -type f -name "*.py" -print0 | PYTHONPATH=.:.. xargs -0 python3 -m pycodestyle \
+	--config="$(git rev-parse --show-toplevel)/linters/python/.pycodestyle"
+find . ../common -type f -name "*.py" -print0 | PYTHONPATH=.:.. xargs -0 python3 -m pylint \
+	--rcfile "$(git rev-parse --show-toplevel)/linters/python/.pylintrc" \
+	--disable "${PYLINT_DISABLE_COMMANDS}"
