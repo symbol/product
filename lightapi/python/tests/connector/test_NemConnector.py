e5621454a38fe237e174695ddfa4600dff409667
Anthony Law <anthony@symbolsyndicate.us>
2026-08-22 17:11:34 +0800

    [lightapi/python] feat: Added prevBlockHash in block data
    Problem: missing previous block hash in block information. It need in the NEM Explorer rollback.
    Solution: Added the `previous_block_hash` field.

--- lightapi/python/tests/connector/test_NemConnector.py
+++ lightapi/python/tests/connector/test_NemConnector.py
@@ -1393,7 +1393,8 @@ EXPECTED_BLOCK_2 = Block(
 		'fdf6a9830e9320af79123f467fcb03d6beab735575ff50eab363d812c5581436'
 		'2ad7be0503db2ee70e60ac3408d83cdbcbd941067a6df703e0c21c7bf389f105'
 	),
-	2052
+	2052,
+	'438cf6375dab5a0d32f9b7bf151d4539e00a590f7c022d5572c7d41815a24be4'
 )
 
 
@@ -1423,7 +1424,8 @@ async def test_can_query_blocks_after(server):  # pylint: disable=redefined-oute
 			'919ae66a34119b49812b335827b357f86884ab08b628029fd6e8db3572faeb4f'
 			'323a7bf9488c76ef8faa5b513036bbcce2d949ba3e41086d95a54c0007403c0b'
 		),
-		168
+		168,
+		'1dd9d4d7b6af603d29c082f9aa4e123f07d18154ddbcd7ddc6702491b854c5e4'
 	) == blocks[1]
 
 # endregion
