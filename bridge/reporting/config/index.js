88e7578f990775731c30ad9a207819db247fb8f3
AnthonyLaw <anthony@symbolsyndicate.us>
2026-09-25 18:56:37 +0800

    [bridge/reporting] feat: added docker file

--- bridge/reporting/config/index.js
+++ bridge/reporting/config/index.js
@@ -1,7 +1,9 @@
-const config = {
-	PUBLIC_BRIDGE_WRAPPED_URL: process.env.PUBLIC_BRIDGE_WRAPPED_URL,
-	PUBLIC_BRIDGE_NATIVE_URL: process.env.PUBLIC_BRIDGE_NATIVE_URL,
-	PUBLIC_REQUEST_TIMEOUT: Number(process.env.PUBLIC_REQUEST_TIMEOUT)
+const serverAppConfig = {
+	PUBLIC_BRIDGE_WRAPPED_URL: process.env.PUBLIC_BRIDGE_WRAPPED_URL || '',
+	PUBLIC_BRIDGE_NATIVE_URL: process.env.PUBLIC_BRIDGE_NATIVE_URL || '',
+	PUBLIC_REQUEST_TIMEOUT: Number(process.env.PUBLIC_REQUEST_TIMEOUT || 15000)
 };
 
-export default config;
+const isClientSide = 'undefined' !== typeof window;
+
+export default isClientSide ? window.appConfig : serverAppConfig;
--- bridge/reporting/pages/_document.jsx
+++ bridge/reporting/pages/_document.jsx
@@ -1,9 +1,11 @@
+import config from '@/config';
 import { Head, Html, Main, NextScript } from 'next/document';
 
 const Document = () => (
 	<Html lang="en">
 		<Head>
 			<meta content="#160820" name="theme-color" />
+			<script dangerouslySetInnerHTML={{ __html: `window.appConfig = ${JSON.stringify(config).replace(/</g, '\\u003c')};` }} />
 		</Head>
 		<body>
 			<Main />
